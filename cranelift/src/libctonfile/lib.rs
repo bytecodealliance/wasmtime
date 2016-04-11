1228abca29a0e7c280cec9295ea1e17c491c5c0e
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-04-11 16:41:00 -0700

    Begin lexer implementation.
    Add a lexer module which implements the lexical analysis of .cton files.

--- cranelift/src/libctonfile/lib.rs
+++ cranelift/src/libctonfile/lib.rs
@@ -1,12 +1,16 @@
 
 // ====------------------------------------------------------------------------------------==== //
 //
-// Cretonne file read/write library
+// Cretonne file read/write library.
 //
 // ====------------------------------------------------------------------------------------==== //
 //
 // The libctonfile library supports reading and writing .cton files. This functionality is needed
 // for testing Cretonne, but is not essential for a JIT compiler.
+//
+// ====------------------------------------------------------------------------------------==== //
 
 extern crate cretonne;
+
+pub mod lexer;
 pub mod parser;
