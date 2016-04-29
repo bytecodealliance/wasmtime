aec94912d7be4def7566b006f642e96f78af353e
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-04-29 15:45:48 -0700

    Exit test script on errors.

--- cranelift/src/test-all.sh
+++ cranelift/src/test-all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Exit immediately on errors.
+set -e
+
+# Run from the src/tools directory which includes all our crates.
 cd $(dirname "$0")/tools
+
 PKGS="-p cretonne -p cretonne-reader -p cretonne-tools"
 cargo build $PKGS
 cargo doc  $PKGS
