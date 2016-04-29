01ed9fc6c8bef152da5b85d16f6d9d4c75a0aacf
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-04-29 14:32:10 -0700

    Rename libraries
    libctonfile -> libreader.
    
    This library will only provide .cton file reading/parsing services which are
    not needed after deployment.
    
    Code for writing .cton files lives in the main cretonne library because it is
    fairly small, and because it is useful for extracting test cases from a
    deployed library.

--- cranelift/src/test-all.sh
+++ cranelift/src/test-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")/tools
-cargo test -p cretonne -p ctonfile -p cretonne-tools
-cargo doc -p cretonne -p ctonfile -p cretonne-tools
+cargo test -p cretonne -p cretonne-reader -p cretonne-tools
+cargo doc -p cretonne -p cretonne-reader -p cretonne-tools
