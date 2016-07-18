28c1eda4f691c5b0c7d23004b32cfd746c817540
Morgan Phillips <winter2718@gmail.com>
2016-07-18 14:28:00 -0700

    Move test utility functions to their own module

---
+++ src/libcretonne/test_utils/make_inst.rs
@@ -0,0 +1,35 @@
+///! Helper functions for generating dummy instructions.
+
+use repr::Function;
+use entities::{Ebb, Inst, NO_VALUE};
+use instructions::{
+    InstructionData,
+    Opcode,
+    VariableArgs,
+    JumpData,
+    BranchData,
+};
+use types;
+
+pub fn jump(func: &mut Function, dest: Ebb) -> Inst {
+    func.make_inst(InstructionData::Jump {
+        opcode: Opcode::Jump,
+        ty: types::VOID,
+        data: Box::new(JumpData {
+            destination: dest,
+            arguments: VariableArgs::new(),
+        }),
+    })
+}
+
+pub fn branch(func: &mut Function, dest: Ebb) -> Inst {
+    func.make_inst(InstructionData::Branch {
+        opcode: Opcode::Brz,
+        ty: types::VOID,
+        data: Box::new(BranchData {
+            arg: NO_VALUE,
+            destination: dest,
+            arguments: VariableArgs::new(),
+        }),
+    })
+}
