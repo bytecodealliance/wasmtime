2f322e3d0e7b3f7109b1280b4d06aee898b030a7
Jakob Stoklund Olesen <stoklund@2pi.dk>
2016-08-23 16:54:40 -0700

    Add script for Python 3 compat checks.

---
+++ meta/check-py3k.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Check Python sources for Python 3 compatibility using pylint.
+#
+# Install pylint with 'pip install pylint'.
+cd $(dirname "$0")
+pylint --py3k --reports=no -- *.py cretonne isa
