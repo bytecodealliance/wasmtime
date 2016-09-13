d0fb647357c5500a32fd60bd907b20cecc61a649
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-09-13 10:51:15 -0700

    Add a data structure representing a parsed test command.
    It's not used for anything yet.

--- src/libreader/lib.rs
+++ src/libreader/lib.rs
@@ -1,16 +1,12 @@
-
-// ====------------------------------------------------------------------------------------==== //
-//
-// Cretonne file reader library.
-//
-// ====------------------------------------------------------------------------------------==== //
-//
-// The cton_reader library supports reading and writing .cton files. This functionality is needed
-// for testing Cretonne, but is not essential for a JIT compiler.
-//
-// ====------------------------------------------------------------------------------------==== //
+//! Cretonne file reader library.
+//!
+//! The cton_reader library supports reading .cton files. This functionality is needed for testing
+//! Cretonne, but is not essential for a JIT compiler.
 
 extern crate cretonne;
 
+pub use testcommand::{TestCommand, TestOption};
+
 pub mod lexer;
 pub mod parser;
+mod testcommand;
