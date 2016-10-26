5dc9ea1de498f884bd94e87f741a3eafd36b0f59
Jakob Stoklund Olesen <jolesen@mozilla.com>
2016-10-26 12:26:23 -0700

    Run mypy if it's available.
    The Python tools pyliint, flake8, and mypy are only run if they exist in
    $PATH.

--- lib/cretonne/meta/check.sh
+++ lib/cretonne/meta/check.sh
@@ -2,11 +2,21 @@
 set -e
 cd $(dirname "$0")
 
-# Run unit tests.
+echo "=== Python unit tests ==="
 python -m unittest discover
+
+runif() {
+    if command -v "$1" > /dev/null; then
+        echo "=== $1 ==="
+        "$@"
+    else
+        echo "$1 not found"
+    fi
+}
 
 # Check Python sources for Python 3 compatibility using pylint.
 #
 # Install pylint with 'pip install pylint'.
-pylint --py3k --reports=no -- *.py cretonne isa
-flake8 .
+runif pylint --py3k --reports=no -- *.py cretonne isa
+runif flake8 .
+runif mypy --py2 build.py
