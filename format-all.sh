0d3990c3945cd1ef0ceeeeea7b84044de2d96236
Jakob Stoklund Olesen <jolesen@mozilla.com>
2017-01-25 15:57:43 -0800

    Make sure we can find rustfmt.

--- format-all.sh
+++ format-all.sh
@@ -8,6 +8,9 @@ set -e
 cd $(dirname "$0")
 src=$(pwd)
 
+# Make sure we can find rustfmt.
+export PATH="$PATH:$HOME/.cargo/bin"
+
 for crate in $(find "$src" -name Cargo.toml); do
     cd $(dirname "$crate")
     cargo fmt -- "$@"
