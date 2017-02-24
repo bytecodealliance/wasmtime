b51cf57e3982d9c204e5fe2b59a3eeb28205ea9a
Jakob Stoklund Olesen <jolesen@mozilla.com>
2017-02-24 11:04:31 -0800

    Add a section about implementation limits.
    Fix a few other minor issues with the documentation.

--- lib/cretonne/meta/isa/arm32/__init__.py
+++ lib/cretonne/meta/isa/arm32/__init__.py
@@ -1,6 +1,6 @@
 """
 ARM 32-bit Architecture
-----------------------
+-----------------------
 
 This target ISA generates code for ARMv7 and ARMv8 CPUs in 32-bit mode
 (AArch32). We support both ARM and Thumb2 instruction encodings.
