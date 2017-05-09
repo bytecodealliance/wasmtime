cdb3a71dd11eed2f533d52408046ff5af5c625f7
Jakob Stoklund Olesen <jolesen@mozilla.com>
2017-05-08 20:56:08 -0700

    Add encodings for Intel dynamic shift instructions.
    These instructions have a fixed register constraint; the shift amount is
    passed in CL.
    
    Add meta language syntax so a fixed register can be specified as
    "GPR.rcx".

--- lib/cretonne/meta/isa/intel/encodings.py
+++ lib/cretonne/meta/isa/intel/encodings.py
@@ -4,7 +4,14 @@ Intel Encodings.
 from __future__ import absolute_import
 from base import instructions as base
 from .defs import I32
-from .recipes import Op1rr
+from .recipes import Op1rr, Op1rc
 from .recipes import OP
 
 I32.enc(base.iadd.i32, Op1rr, OP(0x01))
+
+# 32-bit shifts and rotates.
+# Note that the dynamic shift amount is only masked by 5 or 6 bits; the 8-bit
+# and 16-bit shifts would need explicit masking.
+I32.enc(base.ishl.i32.i32, Op1rc, OP(0xd3, rrr=4))
+I32.enc(base.ushr.i32.i32, Op1rc, OP(0xd3, rrr=5))
+I32.enc(base.sshr.i32.i32, Op1rc, OP(0xd3, rrr=7))
--- lib/cretonne/src/isa/intel/binemit.rs
+++ lib/cretonne/src/isa/intel/binemit.rs
@@ -13,8 +13,19 @@ fn put_op1<CS: CodeSink + ?Sized>(bits: u16, sink: &mut CS) {
     sink.put1(bits as u8);
 }
 
+/// Emit a ModR/M byte for reg-reg operands.
 fn modrm_rr<CS: CodeSink + ?Sized>(rm: RegUnit, reg: RegUnit, sink: &mut CS) {
     let reg = reg as u8 & 7;
+    let rm = rm as u8 & 7;
+    let mut b = 0b11000000;
+    b |= reg << 3;
+    b |= rm;
+    sink.put1(b);
+}
+
+/// Emit a ModR/M byte where the reg bits are part of the opcode.
+fn modrm_r_bits<CS: CodeSink + ?Sized>(rm: RegUnit, bits: u16, sink: &mut CS) {
+    let reg = (bits >> 12) as u8 & 7;
     let rm = rm as u8 & 7;
     let mut b = 0b11000000;
     b |= reg << 3;
@@ -28,6 +39,16 @@ fn recipe_op1rr<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut C
         modrm_rr(func.locations[args[0]].unwrap_reg(),
                  func.locations[args[1]].unwrap_reg(),
                  sink);
+    } else {
+        panic!("Expected Binary format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1rc<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Binary { args, .. } = func.dfg[inst] {
+        let bits = func.encodings[inst].bits();
+        put_op1(bits, sink);
+        modrm_r_bits(func.locations[args[0]].unwrap_reg(), bits, sink);
     } else {
         panic!("Expected Binary format: {:?}", func.dfg[inst]);
     }
