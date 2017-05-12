9629867d0cb45c41ceb52b53badd51631c9eb48f
Jakob Stoklund Olesen <jolesen@mozilla.com>
2017-05-12 14:26:44 -0700

    Encodings for load/store instructions.
    We don't support the full set of Intel addressing modes yet. So far we
    have:
    
    - Register indirect, no displacement.
    - Register indirect, 8-bit signed displacement.
    - Register indirect, 32-bit signed displacement.
    
    The SIB addressing modes will need new Cretonne instruction formats to
    represent.

--- lib/cretonne/meta/isa/intel/encodings.py
+++ lib/cretonne/meta/isa/intel/encodings.py
@@ -4,15 +4,15 @@ Intel Encodings.
 from __future__ import absolute_import
 from base import instructions as base
 from .defs import I32
-from .recipes import Op1rr, Op1rc, Op1rib, Op1rid
-from .recipes import OP
+from . import recipes as rcp
+from .recipes import OP, OP0F, MP66
 
-I32.enc(base.iadd.i32, Op1rr, OP(0x01))
-I32.enc(base.isub.i32, Op1rr, OP(0x29))
+I32.enc(base.iadd.i32, rcp.Op1rr, OP(0x01))
+I32.enc(base.isub.i32, rcp.Op1rr, OP(0x29))
 
-I32.enc(base.band.i32, Op1rr, OP(0x21))
-I32.enc(base.bor.i32, Op1rr, OP(0x09))
-I32.enc(base.bxor.i32, Op1rr, OP(0x31))
+I32.enc(base.band.i32, rcp.Op1rr, OP(0x21))
+I32.enc(base.bor.i32,  rcp.Op1rr, OP(0x09))
+I32.enc(base.bxor.i32, rcp.Op1rr, OP(0x31))
 
 # Immediate instructions with sign-extended 8-bit and 32-bit immediate.
 for inst,                   r in [
@@ -20,12 +20,45 @@ for inst,                   r in [
         (base.band_imm.i32, 4),
         (base.bor_imm.i32,  1),
         (base.bxor_imm.i32, 6)]:
-    I32.enc(inst, Op1rib, OP(0x83, rrr=r))
-    I32.enc(inst, Op1rid, OP(0x81, rrr=r))
+    I32.enc(inst, rcp.Op1rib, OP(0x83, rrr=r))
+    I32.enc(inst, rcp.Op1rid, OP(0x81, rrr=r))
 
 # 32-bit shifts and rotates.
 # Note that the dynamic shift amount is only masked by 5 or 6 bits; the 8-bit
 # and 16-bit shifts would need explicit masking.
-I32.enc(base.ishl.i32.i32, Op1rc, OP(0xd3, rrr=4))
-I32.enc(base.ushr.i32.i32, Op1rc, OP(0xd3, rrr=5))
-I32.enc(base.sshr.i32.i32, Op1rc, OP(0xd3, rrr=7))
+I32.enc(base.ishl.i32.i32, rcp.Op1rc, OP(0xd3, rrr=4))
+I32.enc(base.ushr.i32.i32, rcp.Op1rc, OP(0xd3, rrr=5))
+I32.enc(base.sshr.i32.i32, rcp.Op1rc, OP(0xd3, rrr=7))
+
+# Loads and stores.
+I32.enc(base.store.i32.i32, rcp.Op1st,       OP(0x89))
+I32.enc(base.store.i32.i32, rcp.Op1stDisp8,  OP(0x89))
+I32.enc(base.store.i32.i32, rcp.Op1stDisp32, OP(0x89))
+
+I32.enc(base.istore16.i32.i32, rcp.Mp1st,       MP66(0x89))
+I32.enc(base.istore16.i32.i32, rcp.Mp1stDisp8,  MP66(0x89))
+I32.enc(base.istore16.i32.i32, rcp.Mp1stDisp32, MP66(0x89))
+
+I32.enc(base.istore8.i32.i32, rcp.Op1st_abcd,       OP(0x88))
+I32.enc(base.istore8.i32.i32, rcp.Op1stDisp8_abcd,  OP(0x88))
+I32.enc(base.istore8.i32.i32, rcp.Op1stDisp32_abcd, OP(0x88))
+
+I32.enc(base.load.i32.i32, rcp.Op1ld,       OP(0x8b))
+I32.enc(base.load.i32.i32, rcp.Op1ldDisp8,  OP(0x8b))
+I32.enc(base.load.i32.i32, rcp.Op1ldDisp32, OP(0x8b))
+
+I32.enc(base.uload16.i32.i32, rcp.Op2ld,       OP0F(0xb7))
+I32.enc(base.uload16.i32.i32, rcp.Op2ldDisp8,  OP0F(0xb7))
+I32.enc(base.uload16.i32.i32, rcp.Op2ldDisp32, OP0F(0xb7))
+
+I32.enc(base.sload16.i32.i32, rcp.Op2ld,       OP0F(0xbf))
+I32.enc(base.sload16.i32.i32, rcp.Op2ldDisp8,  OP0F(0xbf))
+I32.enc(base.sload16.i32.i32, rcp.Op2ldDisp32, OP0F(0xbf))
+
+I32.enc(base.uload8.i32.i32, rcp.Op2ld,       OP0F(0xb6))
+I32.enc(base.uload8.i32.i32, rcp.Op2ldDisp8,  OP0F(0xb6))
+I32.enc(base.uload8.i32.i32, rcp.Op2ldDisp32, OP0F(0xb6))
+
+I32.enc(base.sload8.i32.i32, rcp.Op2ld,       OP0F(0xbe))
+I32.enc(base.sload8.i32.i32, rcp.Op2ldDisp8,  OP0F(0xbe))
+I32.enc(base.sload8.i32.i32, rcp.Op2ldDisp32, OP0F(0xbe))
--- lib/cretonne/src/isa/intel/binemit.rs
+++ lib/cretonne/src/isa/intel/binemit.rs
@@ -8,8 +8,27 @@ include!(concat!(env!("OUT_DIR"), "/binemit-intel.rs"));
 
 pub static RELOC_NAMES: [&'static str; 1] = ["Call"];
 
+// Emit single-byte opcode.
 fn put_op1<CS: CodeSink + ?Sized>(bits: u16, sink: &mut CS) {
     debug_assert!(bits & 0x0f00 == 0, "Invalid encoding bits for Op1*");
+    sink.put1(bits as u8);
+}
+
+// Emit two-byte opcode: 0F XX
+fn put_op2<CS: CodeSink + ?Sized>(bits: u16, sink: &mut CS) {
+    debug_assert!(bits & 0x0f00 == 0x0400, "Invalid encoding bits for Op2*");
+    sink.put1(0x0f);
+    sink.put1(bits as u8);
+}
+
+// Mandatory prefix bytes for Mp* opcodes.
+const PREFIX: [u8; 3] = [0x66, 0xf3, 0xf2];
+
+// Emit single-byte opcode with mandatory prefix.
+fn put_mp1<CS: CodeSink + ?Sized>(bits: u16, sink: &mut CS) {
+    debug_assert!(bits & 0x0c00 == 0, "Invalid encoding bits for Mp1*");
+    let pp = (bits >> 8) & 3;
+    sink.put1(PREFIX[(pp - 1) as usize]);
     sink.put1(bits as u8);
 }
 
@@ -28,6 +47,42 @@ fn modrm_r_bits<CS: CodeSink + ?Sized>(rm: RegUnit, bits: u16, sink: &mut CS) {
     let reg = (bits >> 12) as u8 & 7;
     let rm = rm as u8 & 7;
     let mut b = 0b11000000;
+    b |= reg << 3;
+    b |= rm;
+    sink.put1(b);
+}
+
+/// Emit a mode 00 ModR/M byte. This is a register-indirect addressing mode with no offset.
+/// Registers %rsp and %rbp are invalid for `rm`, %rsp indicates a SIB byte, and %rbp indicates an
+/// absolute immediate 32-bit address.
+fn modrm_rm<CS: CodeSink + ?Sized>(rm: RegUnit, reg: RegUnit, sink: &mut CS) {
+    let reg = reg as u8 & 7;
+    let rm = rm as u8 & 7;
+    let mut b = 0b00000000;
+    b |= reg << 3;
+    b |= rm;
+    sink.put1(b);
+}
+
+/// Emit a mode 01 ModR/M byte. This is a register-indirect addressing mode with 8-bit
+/// displacement.
+/// Register %rsp is invalid for `rm`. It indicates the presence of a SIB byte.
+fn modrm_disp8<CS: CodeSink + ?Sized>(rm: RegUnit, reg: RegUnit, sink: &mut CS) {
+    let reg = reg as u8 & 7;
+    let rm = rm as u8 & 7;
+    let mut b = 0b01000000;
+    b |= reg << 3;
+    b |= rm;
+    sink.put1(b);
+}
+
+/// Emit a mode 10 ModR/M byte. This is a register-indirect addressing mode with 32-bit
+/// displacement.
+/// Register %rsp is invalid for `rm`. It indicates the presence of a SIB byte.
+fn modrm_disp32<CS: CodeSink + ?Sized>(rm: RegUnit, reg: RegUnit, sink: &mut CS) {
+    let reg = reg as u8 & 7;
+    let rm = rm as u8 & 7;
+    let mut b = 0b10000000;
     b |= reg << 3;
     b |= rm;
     sink.put1(b);
@@ -75,5 +130,170 @@ fn recipe_op1rid<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut 
         sink.put4(imm as u32);
     } else {
         panic!("Expected BinaryImm format: {:?}", func.dfg[inst]);
+    }
+}
+
+// Store recipes.
+
+fn recipe_op1st<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Store { args, .. } = func.dfg[inst] {
+        put_op1(func.encodings[inst].bits(), sink);
+        modrm_rm(func.locations[args[1]].unwrap_reg(),
+                 func.locations[args[0]].unwrap_reg(),
+                 sink);
+    } else {
+        panic!("Expected Store format: {:?}", func.dfg[inst]);
+    }
+}
+
+// This is just a tighter register class constraint.
+fn recipe_op1st_abcd<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    recipe_op1st(func, inst, sink)
+}
+
+fn recipe_mp1st<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Store { args, .. } = func.dfg[inst] {
+        put_mp1(func.encodings[inst].bits(), sink);
+        modrm_rm(func.locations[args[1]].unwrap_reg(),
+                 func.locations[args[0]].unwrap_reg(),
+                 sink);
+    } else {
+        panic!("Expected Store format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1stdisp8<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Store { args, offset, .. } = func.dfg[inst] {
+        put_op1(func.encodings[inst].bits(), sink);
+        modrm_disp8(func.locations[args[1]].unwrap_reg(),
+                    func.locations[args[0]].unwrap_reg(),
+                    sink);
+        let offset: i32 = offset.into();
+        sink.put1(offset as u8);
+    } else {
+        panic!("Expected Store format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1stdisp8_abcd<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    recipe_op1stdisp8(func, inst, sink)
+}
+
+fn recipe_mp1stdisp8<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Store { args, offset, .. } = func.dfg[inst] {
+        put_mp1(func.encodings[inst].bits(), sink);
+        modrm_disp8(func.locations[args[1]].unwrap_reg(),
+                    func.locations[args[0]].unwrap_reg(),
+                    sink);
+        let offset: i32 = offset.into();
+        sink.put1(offset as u8);
+    } else {
+        panic!("Expected Store format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1stdisp32<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Store { args, offset, .. } = func.dfg[inst] {
+        put_op1(func.encodings[inst].bits(), sink);
+        modrm_disp32(func.locations[args[1]].unwrap_reg(),
+                     func.locations[args[0]].unwrap_reg(),
+                     sink);
+        let offset: i32 = offset.into();
+        sink.put4(offset as u32);
+    } else {
+        panic!("Expected Store format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1stdisp32_abcd<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    recipe_op1stdisp32(func, inst, sink)
+}
+
+fn recipe_mp1stdisp32<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Store { args, offset, .. } = func.dfg[inst] {
+        put_mp1(func.encodings[inst].bits(), sink);
+        modrm_disp32(func.locations[args[1]].unwrap_reg(),
+                     func.locations[args[0]].unwrap_reg(),
+                     sink);
+        let offset: i32 = offset.into();
+        sink.put4(offset as u32);
+    } else {
+        panic!("Expected Store format: {:?}", func.dfg[inst]);
+    }
+}
+
+// Load recipes
+
+fn recipe_op1ld<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Load { arg, .. } = func.dfg[inst] {
+        put_op1(func.encodings[inst].bits(), sink);
+        modrm_rm(func.locations[arg].unwrap_reg(),
+                 func.locations[func.dfg.first_result(inst)].unwrap_reg(),
+                 sink);
+    } else {
+        panic!("Expected Load format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1lddisp8<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Load { arg, offset, .. } = func.dfg[inst] {
+        put_op1(func.encodings[inst].bits(), sink);
+        modrm_disp8(func.locations[arg].unwrap_reg(),
+                    func.locations[func.dfg.first_result(inst)].unwrap_reg(),
+                    sink);
+        let offset: i32 = offset.into();
+        sink.put1(offset as u8);
+    } else {
+        panic!("Expected Load format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op1lddisp32<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Load { arg, offset, .. } = func.dfg[inst] {
+        put_op1(func.encodings[inst].bits(), sink);
+        modrm_disp32(func.locations[arg].unwrap_reg(),
+                     func.locations[func.dfg.first_result(inst)].unwrap_reg(),
+                     sink);
+        let offset: i32 = offset.into();
+        sink.put4(offset as u32);
+    } else {
+        panic!("Expected Load format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op2ld<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Load { arg, .. } = func.dfg[inst] {
+        put_op2(func.encodings[inst].bits(), sink);
+        modrm_rm(func.locations[arg].unwrap_reg(),
+                 func.locations[func.dfg.first_result(inst)].unwrap_reg(),
+                 sink);
+    } else {
+        panic!("Expected Load format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op2lddisp8<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Load { arg, offset, .. } = func.dfg[inst] {
+        put_op2(func.encodings[inst].bits(), sink);
+        modrm_disp8(func.locations[arg].unwrap_reg(),
+                    func.locations[func.dfg.first_result(inst)].unwrap_reg(),
+                    sink);
+        let offset: i32 = offset.into();
+        sink.put1(offset as u8);
+    } else {
+        panic!("Expected Load format: {:?}", func.dfg[inst]);
+    }
+}
+
+fn recipe_op2lddisp32<CS: CodeSink + ?Sized>(func: &Function, inst: Inst, sink: &mut CS) {
+    if let InstructionData::Load { arg, offset, .. } = func.dfg[inst] {
+        put_op2(func.encodings[inst].bits(), sink);
+        modrm_disp32(func.locations[arg].unwrap_reg(),
+                     func.locations[func.dfg.first_result(inst)].unwrap_reg(),
+                     sink);
+        let offset: i32 = offset.into();
+        sink.put4(offset as u32);
+    } else {
+        panic!("Expected Load format: {:?}", func.dfg[inst]);
     }
 }
