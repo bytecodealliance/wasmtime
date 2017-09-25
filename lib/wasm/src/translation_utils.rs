21199d378d7a7fba2c6fd9535746653ff0991b9f
Dan Gohman <sunfish@mozilla.com>
2017-09-25 12:11:56 -0700

    Simplify translate_type's return type.

--- lib/wasm/src/translation_utils.rs
+++ lib/wasm/src/translation_utils.rs
@@ -119,13 +119,13 @@ pub fn f64_translation(x: wasmparser::Ieee64) -> cretonne::ir::immediates::Ieee6
 }
 
 /// Translate a `wasmparser` type into its `Cretonne` equivalent, when possible
-pub fn translate_type(ty: wasmparser::Type) -> Result<Vec<cretonne::ir::Type>, ()> {
+pub fn translate_type(ty: wasmparser::Type) -> Vec<cretonne::ir::Type> {
     match ty {
-        wasmparser::Type::EmptyBlockType => Ok(Vec::new()),
-        wasmparser::Type::I32 => Ok(vec![cretonne::ir::types::I32]),
-        wasmparser::Type::F32 => Ok(vec![cretonne::ir::types::F32]),
-        wasmparser::Type::I64 => Ok(vec![cretonne::ir::types::I64]),
-        wasmparser::Type::F64 => Ok(vec![cretonne::ir::types::F64]),
+        wasmparser::Type::EmptyBlockType => Vec::new(),
+        wasmparser::Type::I32 => vec![cretonne::ir::types::I32],
+        wasmparser::Type::F32 => vec![cretonne::ir::types::F32],
+        wasmparser::Type::I64 => vec![cretonne::ir::types::I64],
+        wasmparser::Type::F64 => vec![cretonne::ir::types::F64],
         _ => panic!("unsupported return value type"),
     }
 }
