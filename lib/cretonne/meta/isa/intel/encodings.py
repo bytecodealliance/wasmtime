2dab70bd4b1b3e30dfdae3aefaa11b2571024a53
Tyler McMullen <tyler@fastly.com>
2017-11-29 21:06:55 -0800

    Simplify x86_(push|pop) encodings.

--- lib/cretonne/meta/isa/intel/encodings.py
+++ lib/cretonne/meta/isa/intel/encodings.py
@@ -228,10 +228,8 @@ enc_both(base.fill.b1, r.fiSib32, 0x8b)
 enc_both(base.regfill.b1, r.rfi32, 0x8b)
 
 # Push and Pop
-I64.enc(x86.push.i64, *r.pushq.rex(0x50))
-I64.enc(x86.push.i64, *r.pushq(0x50))
-I64.enc(x86.pop.i64, *r.popq.rex(0x58))
-I64.enc(x86.pop.i64, *r.popq(0x58))
+enc_i64(x86.push.i64, r.pushq, 0x50)
+enc_i64(x86.pop.i64, r.popq, 0x58)
 
 # Copy Special
 I64.enc(base.copy_special, *r.copysp.rex(0x89, w=1))
