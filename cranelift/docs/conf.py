0ea0146e58a7b54793435e347f8a26de9f8b8b26
Dan Gohman <sunfish@mozilla.com>
2018-03-02 22:53:07 -0800

    Update to sphinx-autogen 1.5.6.

--- cranelift/docs/conf.py
+++ cranelift/docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # cretonne documentation build configuration file, created by
-# sphinx-quickstart on Fri Jan  8 10:11:19 2016.
+# sphinx-quickstart on Fri Mar  2 12:49:24 2018.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -12,14 +12,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-from __future__ import absolute_import
-import sys
-import os
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+
+from __future__ import absolute_import
+import os
+import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 # Also add the meta directory to sys.path so autodoc can find the Cretonne meta
@@ -27,6 +26,10 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../lib/cretonne/meta'))
 
 # -- General configuration ------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+#
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -47,6 +50,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
+#
 # source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
@@ -55,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cretonne'
-copyright = u'2016, Cretonne Developers'
+copyright = u'2018, Cretonne Developers'
 author = u'Cretonne Developers'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -76,7 +80,8 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -89,22 +94,54 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#
+# html_static_path = ['_static']
+
+
+# -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'cretonnedoc'
 
+
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'cretonne.tex', u'cretonne Documentation',
-   author, 'manual'),
+    (master_doc, 'cretonne.tex', u'cretonne Documentation',
+     author, 'manual'),
 ]
 
 
@@ -124,9 +161,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'cretonne', u'cretonne Documentation',
-   author, 'cretonne', 'One line description of project.',
-   'Miscellaneous'),
+    (master_doc, 'cretonne', u'cretonne Documentation',
+     author, 'cretonne', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 
