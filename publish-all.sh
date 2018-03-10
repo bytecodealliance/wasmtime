dfd0e6402e9a96e90badd724c2d571da6f3459d8
Dan Gohman <sunfish@mozilla.com>
2018-03-09 16:32:13 -0800

    Bump version to 0.3.4

--- publish-all.sh
+++ publish-all.sh
@@ -5,7 +5,7 @@ topdir="$(pwd)"
 
 # All the cretonne-* crates have the same version number
 # The filecheck crate version is managed independently.
-version="0.3.3"
+version="0.3.4"
 
 # Update all of the Cargo.toml files.
 #
