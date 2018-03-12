b8a106adf010743b86cce8ef24fae03b7883de05
Dan Gohman <sunfish@mozilla.com>
2018-03-12 10:28:19 -0700

    Remove the "has_sse2" flag.
    Cretonne currently requires SSE2 support pervasively, so it's not meaningful
    to have a setting for it.

--- lib/native/src/lib.rs
+++ lib/native/src/lib.rs
@@ -19,7 +19,7 @@ use raw_cpuid::CpuId;
 /// Return `settings` and `isa` builders configured for the current host
 /// machine, or `Err(())` if the host machine is not supported
 /// in the current configuration.
-pub fn builders() -> Result<(settings::Builder, isa::Builder), ()> {
+pub fn builders() -> Result<(settings::Builder, isa::Builder), &'static str> {
     let mut flag_builder = settings::builder();
 
     // TODO: Add RISC-V support once Rust supports it.
@@ -35,28 +35,28 @@ pub fn builders() -> Result<(settings::Builder, isa::Builder), ()> {
     } else if cfg!(target_arch = "aarch64") {
         "arm64"
     } else {
-        return Err(());
+        return Err("unrecognized architecture");
     };
 
     let mut isa_builder = isa::lookup(name).map_err(|err| match err {
         isa::LookupError::Unknown => panic!(),
-        isa::LookupError::Unsupported => (),
+        isa::LookupError::Unsupported => "unsupported architecture",
     })?;
 
     if cfg!(any(target_arch = "x86", target_arch = "x86_64")) {
-        parse_x86_cpuid(&mut isa_builder);
+        parse_x86_cpuid(&mut isa_builder)?;
     }
 
     Ok((flag_builder, isa_builder))
 }
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-fn parse_x86_cpuid(isa_builder: &mut isa::Builder) {
+fn parse_x86_cpuid(isa_builder: &mut isa::Builder) -> Result<(), &'static str> {
     let cpuid = CpuId::new();
 
     if let Some(info) = cpuid.get_feature_info() {
-        if info.has_sse2() {
-            isa_builder.enable("has_sse2").unwrap();
+        if !info.has_sse2() {
+            return Err("x86 support requires SSE2");
         }
         if info.has_sse3() {
             isa_builder.enable("has_sse3").unwrap();
@@ -87,4 +87,5 @@ fn parse_x86_cpuid(isa_builder: &mut isa::Builder) {
             isa_builder.enable("has_lzcnt").unwrap();
         }
     }
+    Ok(())
 }
