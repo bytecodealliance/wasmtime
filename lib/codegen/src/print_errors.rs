df562aedc29ae10b0b6e0a80f2f48e9df87042f4
Caroline Cullen <cullen.caroline@gmail.com>
2018-06-28 10:17:27 -0700

    Adds decoration to the verifier errors. (#375)
    * Adds decoration to the verifier errors.
    
    example:
    
    function %bad(i32) fast {
    ebb0(v0: i32):
        brnz.i32 v0, ebb1
        return
        ^~~~~~
    
    verifier inst1: Internal return not allowed with return_at_end=1
    
    ebb1:
        trapz.i32 v0, user6
        return
    }
    
    Fixes #68

--- lib/codegen/src/print_errors.rs
+++ lib/codegen/src/print_errors.rs
@@ -1,11 +1,15 @@
 //! Utility routines for pretty-printing error messages.
 
 use ir;
+use ir::entities::Inst;
+use ir::function::Function;
 use isa::TargetIsa;
 use result::CodegenError;
+use std::fmt;
 use std::fmt::Write;
 use std::string::{String, ToString};
 use verifier::VerifierError;
+use write::decorate_function;
 
 /// Pretty-print a verifier error.
 pub fn pretty_verifier_error(
@@ -13,15 +17,52 @@ pub fn pretty_verifier_error(
     isa: Option<&TargetIsa>,
     err: &VerifierError,
 ) -> String {
-    let mut msg = err.to_string();
+    let mut w = String::new();
+    decorate_function(
+        &mut |w, func, isa, inst, indent| pretty_function_error(w, func, isa, inst, indent, err),
+        &mut w,
+        func,
+        isa,
+    ).unwrap();
+    w
+}
+
+/// Pretty-print a function verifier error.
+fn pretty_function_error(
+    w: &mut Write,
+    func: &Function,
+    isa: Option<&TargetIsa>,
+    cur_inst: Inst,
+    indent: usize,
+    err: &VerifierError,
+) -> fmt::Result {
     match err.location {
         ir::entities::AnyEntity::Inst(inst) => {
-            write!(msg, "\n{}: {}\n\n", inst, func.dfg.display_inst(inst, isa)).unwrap()
+            if inst == cur_inst {
+                write!(
+                    w,
+                    "{1:0$}{2}\n",
+                    indent,
+                    "",
+                    func.dfg.display_inst(cur_inst, isa)
+                )?;
+                write!(w, "{1:0$}{2}", indent, "", "^")?;
+                for _c in cur_inst.to_string().chars() {
+                    write!(w, "~")?;
+                }
+                write!(w, "\n\nverifier {}\n\n", err.to_string())
+            } else {
+                write!(
+                    w,
+                    "{1:0$}{2}\n",
+                    indent,
+                    "",
+                    func.dfg.display_inst(cur_inst, isa)
+                )
+            }
         }
-        _ => msg.push('\n'),
+        _ => write!(w, "{}", "\n"),
     }
-    write!(msg, "{}", func.display(isa)).unwrap();
-    msg
 }
 
 /// Pretty-print a Cretonne error.
