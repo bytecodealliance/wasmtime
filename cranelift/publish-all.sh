d6d1e7253dbe4a35e1ac3c89b39eeef103fefbbd
Dan Gohman <sunfish@mozilla.com>
2018-07-23 16:56:25 -0700

    Bump version to 0.17.0

--- cranelift/publish-all.sh
+++ cranelift/publish-all.sh
@@ -9,7 +9,7 @@ topdir=$(dirname "$0")
 cd "$topdir"
 
 # All the cranelift-* crates have the same version number
-version="0.17.0-alpha"
+version="0.17.0"
 
 # Update all of the Cargo.toml files.
 #
