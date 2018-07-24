299898d494b786f65c49968223b542a991611566
Corey Farwell <coreyf@rwell.org>
2018-07-24 09:59:21 -0400

    Add fuzz target for cranelift_reader::parse_test.

---
+++ cranelift/fuzz/fuzz_reader_parse_test.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate cranelift_reader;
+use std::str;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = str::from_utf8(data) {
+        let _ = cranelift_reader::parse_test(s);
+    }
+});
