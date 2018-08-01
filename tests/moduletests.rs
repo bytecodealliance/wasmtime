f1de0359d728172f28f8794305eeff8f475af212
Dan Gohman <sunfish@mozilla.com>
2018-08-01 15:14:49 -0700

    Fix rustfmt errors.

--- tests/moduletests.rs
+++ tests/moduletests.rs
@@ -2,8 +2,8 @@ extern crate cranelift_codegen;
 extern crate cranelift_module;
 extern crate cranelift_simplejit;
 
-use cranelift_codegen::settings::*;
 use cranelift_codegen::ir::*;
+use cranelift_codegen::settings::*;
 use cranelift_module::*;
 use cranelift_simplejit::*;
 
@@ -16,7 +16,12 @@ fn error_on_incompatible_sig_in_declare_function() {
         call_conv: CallConv::SystemV,
         argument_bytes: None,
     };
-    module.declare_function("abc", Linkage::Local, &sig).unwrap();
+    module
+        .declare_function("abc", Linkage::Local, &sig)
+        .unwrap();
     sig.params[0] = AbiParam::new(types::I32);
-    module.declare_function("abc", Linkage::Local, &sig).err().unwrap(); // Make sure this is an error
+    module
+        .declare_function("abc", Linkage::Local, &sig)
+        .err()
+        .unwrap(); // Make sure this is an error
 }
