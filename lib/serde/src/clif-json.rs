2871c6117ec5267c30f941205c1a51e1686e2d93
Dan Gohman <sunfish@mozilla.com>
2018-08-03 15:07:23 -0700

    Synchronize cranelift-serde's lint settings with the other crates.

--- lib/serde/src/clif-json.rs
+++ lib/serde/src/clif-json.rs
@@ -1,7 +1,9 @@
 //! Utility for `cranelift_serde`.
 
-#![deny(trivial_numeric_casts, unused_extern_crates, unstable_features)]
+#![deny(missing_docs, trivial_numeric_casts, unused_extern_crates, unstable_features)]
 #![warn(unused_import_braces)]
+#![cfg_attr(feature = "clippy", plugin(clippy(conf_file = "../../clippy.toml")))]
+#![cfg_attr(feature = "cargo-clippy", allow(new_without_default, new_without_default_derive))]
 #![cfg_attr(
     feature = "cargo-clippy",
     warn(
