bb2589c63f8a68ce47eb55dcd8b6827a9265fbff
Dan Gohman <sunfish@mozilla.com>
2018-08-03 16:00:41 -0700

    Remove python code artifacts.

--- test-all.sh
+++ test-all.sh
@@ -12,10 +12,6 @@ set -euo pipefail
 #
 # All tests run by this script should be passing at all times.
 
-# Disable generation of .pyc files because they cause trouble for vendoring
-# scripts, and this is a build step that isn't run very often anyway.
-export PYTHONDONTWRITEBYTECODE=1
-
 # Repository top-level directory.
 topdir=$(dirname "$0")
 cd "$topdir"
