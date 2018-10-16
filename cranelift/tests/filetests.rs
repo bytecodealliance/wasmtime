e10a3434b8d84874bc7369bb8f73e52e1682e856
Dan Gohman <sunfish@mozilla.com>
2018-10-16 10:03:48 -0700

    Update test files for the cranelift_filetests::run API change.

--- cranelift/tests/filetests.rs
+++ cranelift/tests/filetests.rs
@@ -3,5 +3,6 @@ extern crate cranelift_filetests;
 #[test]
 fn filetests() {
     // Run all the filetests in the following directories.
-    cranelift_filetests::run(false, &["filetests".into(), "docs".into()]).expect("test harness");
+    cranelift_filetests::run(false, false, &["filetests".into(), "docs".into()])
+        .expect("test harness");
 }
