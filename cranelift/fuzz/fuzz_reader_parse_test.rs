c7bc1b7c56657a72a13b98a26516791d2dd4d14a
Dan Robertson <dan@dlrobertson.com>
2018-11-15 00:56:59 +0000

    Fix fuzz_reader_parse_test
    Fix compilation issues with fuzz_reader_parse_test.

--- cranelift/fuzz/fuzz_reader_parse_test.rs
+++ cranelift/fuzz/fuzz_reader_parse_test.rs
@@ -4,6 +4,8 @@ extern crate libfuzzer_sys;
 extern crate cranelift_reader;
 use std::str;
 
-fuzz_target!(|data: &[u8]| if let Ok(s) = str::from_utf8(data) {
-    let _ = cranelift_reader::parse_test(s);
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = str::from_utf8(data) {
+        let _ = cranelift_reader::parse_test(s, None, None);
+    }
 });
