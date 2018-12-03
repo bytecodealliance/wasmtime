a2b4fe6f97a0394d50ed0ccd5d1eea517601a8df
Dan Gohman <sunfish@mozilla.com>
2018-12-03 04:19:20 -0800

    Convert the wast test harness into a crate.
    This uses a build.rs file to collect all the wast tests and create
    individual `#[test]` lines for them, so that `cargo test` can run them
    in parallel.

---
+++ lib/wast/build.rs
@@ -0,0 +1,46 @@
+use std::env;
+use std::fs::{read_dir, File};
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir =
+        PathBuf::from(env::var("OUT_DIR").expect("The OUT_DIR environment variable must be set"));
+    let mut out =
+        File::create(out_dir.join("run_wast_files.rs")).expect("error creating run_wast_files.rs");
+
+    let mut paths: Vec<_> = read_dir("spec_testsuite")
+        .unwrap()
+        .map(|r| r.unwrap())
+        .filter(|p| {
+            // Ignore files starting with `.`, which could be editor temporary files
+            if let Some(stem) = p.path().file_stem() {
+                if let Some(stemstr) = stem.to_str() {
+                    return !stemstr.starts_with('.');
+                }
+            }
+            false
+        }).collect();
+
+    paths.sort_by_key(|dir| dir.path());
+    for path in paths {
+        let path = path.path();
+        writeln!(out, "#[test]");
+        writeln!(
+            out,
+            "fn {}() {{",
+            path.file_stem()
+                .expect("file_stem")
+                .to_str()
+                .expect("to_str")
+        );
+        writeln!(
+            out,
+            "    wast_file(Path::new(\"{}\"), &*native_isa()).expect(\"error loading wast file {}\");",
+            path.display(),
+            path.display()
+        );
+        writeln!(out, "}}");
+        writeln!(out);
+    }
+}
---
+++ lib/wast/src/lib.rs
@@ -0,0 +1,34 @@
+//! JIT-style runtime for WebAssembly using Cranelift.
+
+#![deny(missing_docs, trivial_numeric_casts, unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![deny(unstable_features)]
+#![cfg_attr(
+    feature = "clippy",
+    plugin(clippy(conf_file = "../../clippy.toml"))
+)]
+#![cfg_attr(
+    feature = "cargo-clippy",
+    allow(new_without_default, new_without_default_derive)
+)]
+#![cfg_attr(
+    feature = "cargo-clippy",
+    warn(
+        float_arithmetic,
+        mut_mut,
+        nonminimal_bool,
+        option_map_unwrap_or,
+        option_map_unwrap_or_else,
+        print_stdout,
+        unicode_not_nfc,
+        use_self
+    )
+)]
+
+extern crate cranelift_codegen;
+extern crate wabt;
+extern crate wasmtime_execute;
+
+mod wast;
+
+pub use wast::{wast_buffer, wast_file};
