747dbb23e7ea91281d0c883239e488348cf4fd3b
Dan Gohman <sunfish@mozilla.com>
2019-01-03 11:48:05 -0800

    Add fuzz targets for module instantiation.

--- fuzz/fuzz_targets/instantiate.rs
+++ fuzz/fuzz_targets/instantiate.rs
@@ -10,7 +10,7 @@ extern crate wasmtime_jit;
 
 use cranelift_codegen::settings;
 use wasmparser::validate;
-use wasmtime_environ::{Module, ModuleEnvironment};
+use wasmtime_jit::{instantiate, Compiler, NullResolver};
 
 fuzz_target!(|data: &[u8]| {
     if !validate(data, None) {
@@ -21,15 +21,7 @@ fuzz_target!(|data: &[u8]| {
         panic!("host machine is not a supported target");
     });
     let isa = isa_builder.finish(settings::Flags::new(flag_builder));
-    let mut module = Module::new();
-    let environment = ModuleEnvironment::new(&*isa, &mut module);
-    let translation = match environment.translate(data) {
-        Ok(translation) => translation,
-        Err(_) => return,
-    };
-    let imports_resolver = |_env: &str, _function: &str| None;
-    let _exec = match wasmtime_jit::compile_and_link_module(&*isa, &translation, imports_resolver) {
-        Ok(x) => x,
-        Err(_) => return,
-    };
+    let mut compiler = Compiler::new(isa);
+    let mut imports_resolver = NullResolver {};
+    let _instance = instantiate(&mut compiler, data, &mut imports_resolver).unwrap();
 });
---
+++ fuzz/fuzz_targets/instantiate_translated.rs
@@ -0,0 +1,25 @@
+#![no_main]
+
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate cranelift_codegen;
+extern crate cranelift_native;
+extern crate wasmparser;
+extern crate wasmtime_environ;
+extern crate wasmtime_jit;
+
+use cranelift_codegen::settings;
+use wasmtime_jit::{instantiate, Compiler, NullResolver};
+
+fuzz_target!(|data: &[u8]| {
+    let binaryen_module = binaryen::tools::translate_to_fuzz_mvp(data);
+    let wasm = binaryen_module.write();
+    let flag_builder = settings::builder();
+    let isa_builder = cranelift_native::builder().unwrap_or_else(|_| {
+        panic!("host machine is not a supported target");
+    });
+    let isa = isa_builder.finish(settings::Flags::new(flag_builder));
+    let mut compiler = Compiler::new(isa);
+    let mut imports_resolver = NullResolver {};
+    let _instance = instantiate(&mut compiler, &wasm, &mut imports_resolver).unwrap();
+});
