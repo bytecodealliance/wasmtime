6335a39772f83af3b551bc016b50dc95e50f2e05
Dan Gohman <sunfish@mozilla.com>
2019-01-22 23:44:10 -0800

    Fix legalization of bxor_imm.

--- lib/codegen/meta-python/base/legalize.py
+++ lib/codegen/meta-python/base/legalize.py
@@ -500,7 +500,7 @@ for inst_imm,      inst in [
         (urem_imm, urem),
         (band_imm, band),
         (bor_imm, bor),
-        (bxor_imm, bor),
+        (bxor_imm, bxor),
         (ifcmp_imm, ifcmp)]:
     expand.legalize(
             a << inst_imm(x, y),
