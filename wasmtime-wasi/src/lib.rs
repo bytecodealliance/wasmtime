14e030c0677bd9db2af0fcb5540518df5c9d8e57
Jakub Konka <kubkon@jakubkonka.com>
2019-05-04 14:23:47 +0200

    Migrate to wasi-common crate

--- wasmtime-wasi/src/lib.rs
+++ wasmtime-wasi/src/lib.rs
@@ -1,4 +1,3 @@
-extern crate cast;
 extern crate cranelift_codegen;
 extern crate cranelift_entity;
 extern crate cranelift_wasm;
@@ -8,13 +7,9 @@ extern crate wasmtime_jit;
 extern crate wasmtime_runtime;
 #[macro_use]
 extern crate log;
-extern crate errno;
+extern crate wasi_common;
 
-mod host;
-mod host_impls;
 mod instantiate;
 mod syscalls;
-mod translate;
-mod wasm32;
 
 pub use instantiate::instantiate_wasi;
