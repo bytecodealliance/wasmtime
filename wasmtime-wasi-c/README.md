d4dac13b07bddf189850ff4722fec1073f601719
Dan Gohman <sunfish@mozilla.com>
2019-05-19 20:17:38 -0700

    Provide the C WASI implementation as an option.
    This adds the C WASI implementation as a new crate, wasmtime-wasi-c,
    and adds a command-line flag to the wasmtime command-line driver to
    select which WASI implementation to use.

--- wasmtime-wasi-c/README.md
+++ wasmtime-wasi-c/README.md
@@ -1,5 +1,5 @@
-This is the `wasmtime-wasi` crate, which implements the
-WebAssembly System Interface (WASI) API in Rust.
+This is the `wasmtime-wasi-c` crate, which implements the
+WebAssembly System Interface (WASI) API in C.
 
 WASI is greatly inspired by and directly derived from [CloudABI].
 It differs in that it has aspirations to expand to a greater
---
+++ wasmtime-wasi-c/src/lib.rs
@@ -0,0 +1,18 @@
+extern crate cast;
+extern crate cranelift_codegen;
+extern crate cranelift_entity;
+extern crate cranelift_wasm;
+extern crate target_lexicon;
+extern crate wasmtime_environ;
+extern crate wasmtime_jit;
+extern crate wasmtime_runtime;
+#[macro_use]
+extern crate log;
+
+mod host;
+mod instantiate;
+mod syscalls;
+mod translate;
+mod wasm32;
+
+pub use instantiate::instantiate_wasi_c;
