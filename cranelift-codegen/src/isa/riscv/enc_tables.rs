8ce66b262c958e5c95403918c714a78bac1e1760
Benjamin Bouvier <public@benj.me>
2019-07-09 11:29:48 +0200

    Build fix: add crates::predicates to the Riscv enc_tables file;

--- cranelift-codegen/src/isa/riscv/enc_tables.rs
+++ cranelift-codegen/src/isa/riscv/enc_tables.rs
@@ -6,6 +6,7 @@ use crate::isa;
 use crate::isa::constraints::*;
 use crate::isa::enc_tables::*;
 use crate::isa::encoding::{base_size, RecipeSizing};
+use crate::predicates;
 
 // Include the generated encoding tables:
 // - `LEVEL1_RV32`
