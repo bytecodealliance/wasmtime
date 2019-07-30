b5cb8556f62378b7608e2269baf063a83b25b26d
Andy Wortman <me@iximeow.net>
2019-07-30 07:32:50 -0700

    [wasm] Pass translation state along for before/after_translate_operator callbacks (#879)

--- cranelift/wasm/src/func_translator.rs
+++ cranelift/wasm/src/func_translator.rs
@@ -207,9 +207,9 @@ fn parse_function_body<FE: FuncEnvironment + ?Sized>(
     while !state.control_stack.is_empty() {
         builder.set_srcloc(cur_srcloc(&reader));
         let op = reader.read_operator()?;
-        environ.before_translate_operator(&op, builder)?;
+        environ.before_translate_operator(&op, builder, state)?;
         translate_operator(&op, builder, state, environ)?;
-        environ.after_translate_operator(&op, builder)?;
+        environ.after_translate_operator(&op, builder, state)?;
     }
 
     // The final `End` operator left us in the exit block where we need to manually add a return
