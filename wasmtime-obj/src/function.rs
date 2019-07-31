fff0198fb70c1ac4638d4265ed473776702a86da
Yury Delendik <ydelendik@mozilla.com>
2019-07-31 09:41:56 -0500

    Fix RelocationTarget::JumpTable handling in wasm2obj (#195)

--- wasmtime-obj/src/function.rs
+++ wasmtime-obj/src/function.rs
@@ -96,6 +96,9 @@ pub fn emit_functions(
                     })
                     .map_err(|err| format!("{}", err))?;
                 }
+                RelocationTarget::JumpTable(_, _) => {
+                    // ignore relocations for jump tables
+                }
                 _ => panic!("relocations target not supported yet: {:?}", r.reloc_target),
             };
         }
