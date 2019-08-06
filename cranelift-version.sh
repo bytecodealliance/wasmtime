5fc2d827b71d67db6eeb592b518cb81cc674f63e
Yury Delendik <ydelendik@mozilla.com>
2019-08-06 14:49:28 -0500

    Update cranelift requirement from 0.37.0 to 0.38.0 (#254)
    closes #248, closes #249, closes #250, closes #251, closes #252

---
+++ cranelift-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+# This is a convenience script for maintainers changing a cranelift
+# dependencies versions. To use, bump the version number below, run the
+# script.
+
+topdir=$(dirname "$0")
+cd "$topdir"
+
+# All the cranelift-* crates have the same version number
+version="0.38.0"
+
+# Update all of the Cargo.toml files.
+echo "Updating crate versions to $version"
+for crate in . wasmtime-* fuzz; do
+    # Update the version number of this crate to $version.
+    sed -i.bk -e "/^cranelift-/s/\"[^\"]*\"/\"$version\"/" \
+        "$crate/Cargo.toml"
+
+    # Update the required version number of any cranelift* dependencies.
+    sed -i.bk -e "/^cranelift-/s/version = \"[^\"]*\"/version = \"$version\"/" \
+        "$crate/Cargo.toml"
+done
