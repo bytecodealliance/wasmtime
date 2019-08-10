b980c2aa115da3aa5963079b0fabdafa70ef8eee
Jakub Konka <jakub.konka@golem.network>
2019-08-10 11:11:02 +0200

    Disable path_rename testcase on Windows

--- build.rs
+++ build.rs
@@ -136,6 +136,7 @@ cfg_if::cfg_if! {
                     "symlink_loop" => true,
                     "clock_time_get" => true,
                     "truncation_rights" => true,
+                    "path_rename" => true,
                     _ => false,
                 }
             } else {
