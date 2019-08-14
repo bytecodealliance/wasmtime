a00e954e74e993c5720b0c894ec59f422c4ed407
Andrew Brown <andrew.brown@intel.com>
2019-08-14 11:12:00 -0700

    Enable SIMD in WAST testsuites

--- tests/wast_testsuites.rs
+++ tests/wast_testsuites.rs
@@ -4,7 +4,7 @@ use cranelift_codegen::isa;
 use cranelift_codegen::settings;
 use cranelift_codegen::settings::Configurable;
 use std::path::Path;
-use wasmtime_jit::Compiler;
+use wasmtime_jit::{Compiler, Features};
 use wasmtime_wast::WastContext;
 
 include!(concat!(env!("OUT_DIR"), "/wast_testsuite_tests.rs"));
@@ -14,6 +14,7 @@ fn native_isa() -> Box<dyn isa::TargetIsa> {
     let mut flag_builder = settings::builder();
     flag_builder.enable("enable_verifier").unwrap();
     flag_builder.enable("avoid_div_traps").unwrap();
+    flag_builder.enable("enable_simd").unwrap();
 
     let isa_builder = cranelift_native::builder().unwrap_or_else(|_| {
         panic!("host machine is not a supported target");
