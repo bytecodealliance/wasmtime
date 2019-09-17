8d62d5f72445bf081d7245ce815d6c8c87489872
Till Schneidereit <till@tillschneidereit.net>
2019-09-17 15:06:37 +0900

    Revert "Continuous fuzzing (#944)" (#1036)
    This reverts commit 374fd238b06860a1e6e3f8bb9eb598afe91b90fe.

--- cranelift/fuzz/fuzz_reader_parse_test.rs
+++ cranelift/fuzz/fuzz_reader_parse_test.rs
@@ -6,7 +6,6 @@ use std::str;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = str::from_utf8(data) {
-        let options = cranelift_reader::ParseOptions::default();
-        let _ = cranelift_reader::parse_test(s, options);
+        let _ = cranelift_reader::parse_test(s, None, None);
     }
 });
