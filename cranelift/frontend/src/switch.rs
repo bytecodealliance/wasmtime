1a99ac6b4a378bac654cffb0061c96a3b5f2254a
bjorn3 <bjorn3@users.noreply.github.com>
2019-09-28 15:57:43 +0200

    Always use extern crate std in cranelift-frontend

--- cranelift/frontend/src/switch.rs
+++ cranelift/frontend/src/switch.rs
@@ -3,7 +3,7 @@ use crate::frontend::FunctionBuilder;
 use cranelift_codegen::ir::condcodes::IntCC;
 use cranelift_codegen::ir::*;
 use log::debug;
-use std::vec::Vec;
+use alloc::vec::Vec;
 
 type EntryIndex = u64;
 
@@ -291,7 +291,7 @@ mod tests {
     use super::*;
     use crate::frontend::FunctionBuilderContext;
     use cranelift_codegen::ir::Function;
-    use std::string::ToString;
+    use alloc::string::ToString;
 
     macro_rules! setup {
         ($default:expr, [$($index:expr,)*]) => {{
