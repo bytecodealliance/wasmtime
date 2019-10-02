1017cbbf6332543e6aef60cd82ec9315d501206a
Dan Gohman <sunfish@mozilla.com>
2019-10-02 14:27:21 -0700

    Update the compile fuzzer for Lightbeam changes.

--- fuzz/fuzz_targets/compile.rs
+++ fuzz/fuzz_targets/compile.rs
@@ -13,10 +13,10 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 use wasmparser::validate;
-use wasmtime_jit::{CompiledModule, Compiler, NullResolver};
+use wasmtime_jit::{CompilationStrategy, CompiledModule, Compiler, NullResolver};
 
 fuzz_target!(|data: &[u8]| {
-    if !validate(data, None) {
+    if validate(data, None).is_err() {
         return;
     }
     let flag_builder = settings::builder();
@@ -24,11 +24,32 @@ fuzz_target!(|data: &[u8]| {
         panic!("host machine is not a supported target");
     });
     let isa = isa_builder.finish(settings::Flags::new(flag_builder));
-    let mut compiler = Compiler::new(isa);
+    let mut compiler = Compiler::new(isa, CompilationStrategy::AlwaysCranelift);
     let mut resolver = NullResolver {};
     let global_exports = Rc::new(RefCell::new(HashMap::new()));
-    let _compiled = match CompiledModule::new(&mut compiler, data, &mut resolver, global_exports, false) {
-        Ok(x) => x,
-        Err(_) => return,
-    };
+    let _compiled =
+        match CompiledModule::new(&mut compiler, data, &mut resolver, global_exports, false) {
+            Ok(x) => x,
+            Err(_) => return,
+        };
+});
+
+#[cfg(feature = "lightbeam")]
+fuzz_target!(|data: &[u8]| {
+    if validate(data, None).is_err() {
+        return;
+    }
+    let flag_builder = settings::builder();
+    let isa_builder = cranelift_native::builder().unwrap_or_else(|_| {
+        panic!("host machine is not a supported target");
+    });
+    let isa = isa_builder.finish(settings::Flags::new(flag_builder));
+    let mut compiler = Compiler::new(isa, CompilationStrategy::AlwaysLightbeam);
+    let mut resolver = NullResolver {};
+    let global_exports = Rc::new(RefCell::new(HashMap::new()));
+    let _compiled =
+        match CompiledModule::new(&mut compiler, data, &mut resolver, global_exports, false) {
+            Ok(x) => x,
+            Err(_) => return,
+        };
 });
