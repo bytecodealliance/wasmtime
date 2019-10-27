11f9d73562287664a972d7de62aa5435bbafe414
Josh Triplett <josh@joshtriplett.org>
2019-10-27 13:06:45 -0700

    Fix fuzz build
    Update to binaryen 0.8.1, as 0.5.0 doesn't build on current systems.
    
    Update to match API changes in wasmtime and wasmparser.

--- fuzz/fuzz_targets/instantiate.rs
+++ fuzz/fuzz_targets/instantiate.rs
@@ -10,10 +10,10 @@ extern crate wasmtime_jit;
 
 use cranelift_codegen::settings;
 use wasmparser::validate;
-use wasmtime_jit::{instantiate, Compiler, NullResolver};
+use wasmtime_jit::{instantiate, CompilationStrategy, Compiler, NullResolver};
 
 fuzz_target!(|data: &[u8]| {
-    if !validate(data, None) {
+    if validate(data, None).is_err() {
         return;
     }
     let flag_builder = settings::builder();
@@ -21,7 +21,7 @@ fuzz_target!(|data: &[u8]| {
         panic!("host machine is not a supported target");
     });
     let isa = isa_builder.finish(settings::Flags::new(flag_builder));
-    let mut compiler = Compiler::new(isa);
+    let mut compiler = Compiler::new(isa, CompilationStrategy::Auto);
     let mut imports_resolver = NullResolver {};
     let _instance = instantiate(
         &mut compiler,
