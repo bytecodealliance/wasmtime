c28e07fbeda3e9390ff704a62ffce78fa9927780
Alex Crichton <alex@alexcrichton.com>
2019-11-13 08:20:59 -0800

    Move `*.wasm` for gcd example inline

--- crates/api/examples/gcd.rs
+++ crates/api/examples/gcd.rs
@@ -2,11 +2,40 @@
 //! invoking its exported function.
 
 use anyhow::{format_err, Result};
-use std::fs::read;
 use wasmtime_api::*;
 
+const WAT: &str = r#"
+(module
+  (func $gcd (param i32 i32) (result i32)
+    (local i32)
+    block  ;; label = @1
+      block  ;; label = @2
+        local.get 0
+        br_if 0 (;@2;)
+        local.get 1
+        local.set 2
+        br 1 (;@1;)
+      end
+      loop  ;; label = @2
+        local.get 1
+        local.get 0
+        local.tee 2
+        i32.rem_u
+        local.set 0
+        local.get 2
+        local.set 1
+        local.get 0
+        br_if 0 (;@2;)
+      end
+    end
+    local.get 2
+  )
+  (export "gcd" (func $gcd))
+)
+"#;
+
 fn main() -> Result<()> {
-    let wasm = read("examples/gcd.wasm")?;
+    let wasm = wat::parse_str(WAT)?;
 
     // Instantiate engine and store.
     let engine = HostRef::new(Engine::default());
