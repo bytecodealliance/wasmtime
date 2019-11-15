6eb7e501e234ef7e8f1e61dad274acd3ffe5220c
Dan Gohman <sunfish@mozilla.com>
2019-11-15 11:13:06 -0800

    Sort the publishing rules in topological order.
    Also, publish nightly-only crates with cargo +nightly.

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -36,23 +36,28 @@ for cargo_toml in \
     crates/wasi-common/winx/Cargo.toml \
     crates/wasi-common/wig/Cargo.toml \
     crates/wasi-common/Cargo.toml \
+    crates/lightbeam/Cargo.toml \
     crates/environ/Cargo.toml \
     crates/obj/Cargo.toml \
-    crates/wast/Cargo.toml \
     crates/runtime/Cargo.toml \
+    crates/debug/Cargo.toml \
+    crates/jit/Cargo.toml \
+    crates/wast/Cargo.toml \
     crates/wasi-c/Cargo.toml \
     crates/wasi/Cargo.toml \
-    crates/lightbeam/Cargo.toml \
-    crates/debug/Cargo.toml \
-    crates/interface-types/Cargo.toml \
-    crates/jit/Cargo.toml \
     crates/api/Cargo.toml \
+    crates/interface-types/Cargo.toml \
     crates/misc/py/Cargo.toml \
     crates/misc/rust/macro/Cargo.toml \
     crates/misc/rust/Cargo.toml \
-    Cargo.toml \
 ; do
-    echo cargo publish --manifest-path "$cargo_toml"
+    version=""
+    case $cargo_toml in
+        crates/lightbeam/Cargo.toml) version=" +nightly" ;;
+        crates/misc/py/Cargo.toml) version=" +nightly" ;;
+    esac
+
+    echo cargo$version publish --manifest-path "$cargo_toml"
 
     # Sleep for a few seconds to allow the server to update the index.
     # https://internals.rust-lang.org/t/changes-to-how-crates-io-handles-index-updates/9608
