fc23cde9436ebcc1b24f67dfb98715ffecafe8f3
Jakub Konka <jakub.konka@golem.network>
2019-11-20 11:09:29 +0100

    Unify fd_readdir impl between *nixes
    This commit unifies the implementation of `fd_readdir` between Linux
    and BSD hosts. In particular, it re-uses the `Dirent`, `Entry`, and
    `Dir` (among others) building blocks introduced recently when
    `fd_readdir` was being implemented on Windows.
    
    Notable changes:
    * on BSD, wraps `readdir` syscall in an `Iterator` of the mutex-locked
      `Dir` struct
    * on BSD, removes `DirStream` struct from `OsFile`; `OsFile` now holds a
      mutex to `Dir`
    * makes `Dir` iterators implementation specific (Linux has its own,
      and so does BSD)

--- crates/wasi-common/src/sys/unix/bsd/osfile.rs
+++ crates/wasi-common/src/sys/unix/bsd/osfile.rs
@@ -1,33 +1,18 @@
+use super::super::dir::Dir;
 use std::fs;
-use std::mem::ManuallyDrop;
 use std::ops::{Deref, DerefMut};
 use std::os::unix::prelude::{AsRawFd, RawFd};
 use std::sync::Mutex;
 
 #[derive(Debug)]
-pub(crate) struct DirStream {
-    pub(crate) file: ManuallyDrop<fs::File>,
-    pub(crate) dir_ptr: *mut libc::DIR,
-}
-
-impl Drop for DirStream {
-    fn drop(&mut self) {
-        unsafe { libc::closedir(self.dir_ptr) };
-    }
-}
-
-#[derive(Debug)]
 pub(crate) struct OsFile {
     pub(crate) file: fs::File,
-    pub(crate) dir_stream: Option<Mutex<DirStream>>,
+    pub(crate) dir: Option<Mutex<Dir>>,
 }
 
 impl From<fs::File> for OsFile {
     fn from(file: fs::File) -> Self {
-        Self {
-            file,
-            dir_stream: None,
-        }
+        Self { file, dir: None }
     }
 }
 
