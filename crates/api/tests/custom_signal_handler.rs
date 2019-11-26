d87ab2fc55b4f535fb44643050ee9ac045b80c06
Maciej Woś <maciej.wos@gmail.com>
2019-11-26 19:39:46 +0900

    test multiple instances, multiple signal handlers

--- crates/api/tests/custom_signal_handler.rs
+++ crates/api/tests/custom_signal_handler.rs
@@ -1,4 +1,6 @@
 use core::cell::Ref;
+use std::rc::Rc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use wasmtime_api::*;
 use wasmtime_interface_types::*;
 
@@ -15,16 +17,8 @@ fn invoke_export(
         .invoke(&mut context, &mut handle, func_name, &[])
 }
 
-#[test]
-fn test_custom_signal_handler() {
-    let engine = HostRef::new(Engine::new(Config::default()));
-    let store = HostRef::new(Store::new(engine));
-    let data = std::fs::read("tests/custom_signal_handler.wasm").expect("failed to read wasm file");
-    let module = HostRef::new(Module::new(store.clone(), &data).expect("failed to create module"));
-    let instance = HostRef::new(
-        Instance::new(store.clone(), module, &[]).expect("failed to instantiate module"),
-    );
-
+// Locate "memory" export, get base address and size and set memory protection to PROT_NONE
+fn set_up_memory(instance: &HostRef<Instance>) -> (*mut u8, usize) {
     let mem_export = instance.borrow().get_wasmtime_memory().expect("memory");
 
     let (base, length) = if let wasmtime_runtime::Export::Memory {
@@ -45,6 +39,23 @@ fn test_custom_signal_handler() {
     unsafe {
         libc::mprotect(base as *mut libc::c_void, length, libc::PROT_NONE);
     }
+
+    println!("memory: base={:?}, length={}", base, length);
+
+    (base, length)
+}
+
+#[test]
+fn test_custom_signal_handler_single_instance() {
+    let engine = HostRef::new(Engine::new(Config::default()));
+    let store = HostRef::new(Store::new(engine));
+    let data = std::fs::read("tests/custom_signal_handler.wasm").expect("failed to read wasm file");
+    let module = HostRef::new(Module::new(store.clone(), &data).expect("failed to create module"));
+    let instance = HostRef::new(
+        Instance::new(store.clone(), module, &[]).expect("failed to instantiate module"),
+    );
+
+    let (base, length) = set_up_memory(&instance);
 
     instance
         .borrow_mut()
@@ -109,9 +120,110 @@ fn test_custom_signal_handler() {
             .func()
             .expect("expected a 'read_out_of_bounds' func in the module");
         println!("calling read_out_of_bounds...");
-        let trap = read_out_of_bounds_func
+        let trap = read_out_of_bounds_func.borrow().call(&[]).unwrap_err();
+        assert!(trap
             .borrow()
-            .call(&[]).unwrap_err();
-        assert!(trap.borrow().message().starts_with("wasm trap: out of bounds memory access"));
+            .message()
+            .starts_with("wasm trap: out of bounds memory access"));
+    }
+}
+
+#[test]
+fn test_custom_signal_handler_multiple_instances() {
+    let engine = HostRef::new(Engine::new(Config::default()));
+    let store = HostRef::new(Store::new(engine));
+    let data = std::fs::read("tests/custom_signal_handler.wasm").expect("failed to read wasm file");
+    let module = HostRef::new(Module::new(store.clone(), &data).expect("failed to create module"));
+
+    // Set up multiple instances
+
+    let instance1 = HostRef::new(
+        Instance::new(store.clone(), module.clone(), &[]).expect("failed to instantiate module"),
+    );
+    let instance1_handler_triggered = Rc::new(AtomicBool::new(false));
+
+    {
+        let (base1, length1) = set_up_memory(&instance1);
+
+        instance1.borrow_mut().set_signal_handler({
+            let instance1_handler_triggered = instance1_handler_triggered.clone();
+            move |_signum, _siginfo, _context| {
+                // Remove protections so the execution may resume
+                unsafe {
+                    libc::mprotect(
+                        base1 as *mut libc::c_void,
+                        length1,
+                        libc::PROT_READ | libc::PROT_WRITE,
+                    );
+                }
+                instance1_handler_triggered.store(true, Ordering::SeqCst);
+                println!(
+                    "Hello from instance1 signal handler! {}",
+                    instance1_handler_triggered.load(Ordering::SeqCst)
+                );
+                true
+            }
+        });
+    }
+
+    let instance2 = HostRef::new(
+        Instance::new(store.clone(), module, &[]).expect("failed to instantiate module"),
+    );
+    let instance2_handler_triggered = Rc::new(AtomicBool::new(false));
+
+    {
+        let (base2, length2) = set_up_memory(&instance2);
+
+        instance2.borrow_mut().set_signal_handler({
+            let instance2_handler_triggered = instance2_handler_triggered.clone();
+            move |_signum, _siginfo, _context| {
+                // Remove protections so the execution may resume
+                unsafe {
+                    libc::mprotect(
+                        base2 as *mut libc::c_void,
+                        length2,
+                        libc::PROT_READ | libc::PROT_WRITE,
+                    );
+                }
+                instance2_handler_triggered.store(true, Ordering::SeqCst);
+                println!(
+                    "Hello from instance2 signal handler! {}",
+                    instance2_handler_triggered.load(Ordering::SeqCst)
+                );
+                true
+            }
+        });
+    }
+
+    // Invoke both instances and trigger both signal handlers
+
+    // First instance1
+    {
+        let exports1 = Ref::map(instance1.borrow(), |i| i.exports());
+        assert!(!exports1.is_empty());
+
+        println!("calling instance1.read...");
+        let result = invoke_export(&store, &instance1, &data, "read").expect("read succeeded");
+        assert_eq!("123", result[0].clone().to_string());
+        assert_eq!(
+            instance1_handler_triggered.load(Ordering::SeqCst),
+            true,
+            "instance1 signal handler has been triggered"
+        );
+    }
+
+    // And then instance2
+    {
+        let exports2 = Ref::map(instance2.borrow(), |i| i.exports());
+        assert!(!exports2.is_empty());
+
+        println!("calling instance2.read...");
+        let result = invoke_export(&store, &instance2, &data, "read").expect("read succeeded");
+        assert_eq!("123", result[0].clone().to_string());
+        assert_eq!(
+            instance2_handler_triggered.load(Ordering::SeqCst),
+            true,
+            "instance1 signal handler has been triggered"
+        );
     }
 }
