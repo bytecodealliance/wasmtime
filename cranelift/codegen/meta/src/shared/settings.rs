de7c0dddc673950b90d92f762ffe263a28de0da5
Benjamin Bouvier <public@benj.me>
2019-12-03 11:38:44 +0100

    Fix #1247: Tweak enable_verifier doc comment so it's true

--- cranelift/codegen/meta/src/shared/settings.rs
+++ cranelift/codegen/meta/src/shared/settings.rs
@@ -21,8 +21,8 @@ pub(crate) fn define() -> SettingGroup {
         r#"
         Run the Cranelift IR verifier at strategic times during compilation.
 
-        This makes compilation slower but catches many bugs. The verifier is
-        disabled by default, except when reading Cranelift IR from a text file.
+        This makes compilation slower but catches many bugs. The verifier is always enabled by
+        default, which is useful during development.
         "#,
         true,
     );
