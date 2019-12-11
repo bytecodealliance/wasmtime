2fbb782df72a11d47ddd2f4a5ca15f3b10789c95
Jakub Konka <jakub.konka@golem.network>
2019-12-11 16:42:02 +0100

    Draft out js-polyfill using wasi-common crate
    This commit drafts out js-polyfill using `wasi-common` crate.

---
+++ crates/wasi-common/js-polyfill/src/main.rs
@@ -0,0 +1,34 @@
+use std::cell::RefCell;
+use wasi_common::old::snapshot_0::{WasiCtx, WasiCtxBuilder};
+
+extern "C" {
+    fn loadFiles();
+}
+
+thread_local! {
+    static WASI_CTX: RefCell<Option<WasiCtx>> = RefCell::new(None);
+}
+
+fn main() {}
+
+#[no_mangle]
+unsafe fn get_wasi_context() -> *mut WasiCtx {
+    WASI_CTX.with(|ctx| {
+        ctx.borrow_mut()
+            .as_mut()
+            .expect("WasiCtx should be initialized by now") as *mut _
+    })
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+fn handleFiles() {
+    WASI_CTX.with(|ctx| {
+        let wasi_ctx = WasiCtxBuilder::new()
+            .inherit_stdio()
+            .build()
+            .expect("could build WasiCtx with stdio inherited");
+        ctx.replace(Some(wasi_ctx));
+    });
+    unsafe { loadFiles() }
+}
