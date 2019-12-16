226a8af09069fea3e9d716fd5a06b41f0cb4d5fe
Alex Crichton <alex@alexcrichton.com>
2019-12-16 08:03:19 -0800

    Don't use `libc` in wasi-tests
    We don't know what version of the wasi api it's compiled against, so
    let's avoid it.

--- crates/test-programs/wasi-tests/src/lib.rs
+++ crates/test-programs/wasi-tests/src/lib.rs
@@ -1,9 +1,6 @@
 pub mod utils;
 pub mod wasi_wrappers;
 
-use libc;
-use std::ffi::CString;
-use std::io;
 use wasi_old::wasi_unstable;
 
 /// Opens a fresh file descriptor for `path` where `path` should be a preopened
@@ -11,20 +8,27 @@ use wasi_old::wasi_unstable;
 /// `wasi_snapshot_preview1`. This is getting phased out and will likely be
 /// deleted soon.
 pub fn open_scratch_directory(path: &str) -> Result<wasi_unstable::Fd, String> {
-    // Open the scratch directory.
-    let dir_fd: wasi_unstable::Fd = unsafe {
-        let cstr = CString::new(path.as_bytes()).unwrap();
-        libc::open(cstr.as_ptr(), libc::O_RDONLY | libc::O_DIRECTORY)
-    } as wasi_unstable::Fd;
+    unsafe {
+        for i in 3.. {
+            let stat = match wasi_unstable::fd_prestat_get(i) {
+                Ok(s) => s,
+                Err(_) => break,
+            };
+            if stat.pr_type != wasi::PREOPENTYPE_DIR {
+                continue;
+            }
+            let mut dst = Vec::with_capacity(stat.u.dir.pr_name_len);
+            if wasi::fd_prestat_dir_name(i, dst.as_mut_ptr(), dst.capacity()).is_err() {
+                continue;
+            }
+            dst.set_len(stat.u.dir.pr_name_len);
+            if dst == path.as_bytes() {
+                return Ok(wasi::path_open(i, 0, ".", wasi::OFLAGS_DIRECTORY, 0, 0, 0)
+                    .expect("failed to open dir"));
+            }
+        }
 
-    if (dir_fd as std::os::raw::c_int) < 0 {
-        Err(format!(
-            "error opening scratch directory '{}': {}",
-            path,
-            io::Error::last_os_error()
-        ))
-    } else {
-        Ok(dir_fd)
+        Err(format!("failed to find scratch dir"))
     }
 }
 
