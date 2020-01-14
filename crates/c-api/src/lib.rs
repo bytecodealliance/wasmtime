aac1f1238df64985d2e4c6628daa266c537cbae0
Alex Crichton <alex@alexcrichton.com>
2020-01-14 09:19:38 -0800

    Add back in accidentally deleted comment

--- crates/c-api/src/lib.rs
+++ crates/c-api/src/lib.rs
@@ -1,4 +1,5 @@
-//! Wasmtime's embedding C API
+//! This file defines the extern "C" API, which is compatible with the
+//! [Wasm C API](https://github.com/WebAssembly/wasm-c-api).
 
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 
