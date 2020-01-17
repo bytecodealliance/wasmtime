c9caa8d40ee8bbe17ac488623b42e0e741ee32ec
Alex Crichton <alex@alexcrichton.com>
2020-01-17 14:34:28 -0800

    Update cranelift to 0.56.0

--- scripts/cranelift-version.sh
+++ scripts/cranelift-version.sh
@@ -9,7 +9,7 @@ topdir=$(dirname "$0")/..
 cd "$topdir"
 
 # All the cranelift-* crates have the same version number
-version="0.55"
+version="0.56"
 
 # Update all of the Cargo.toml files.
 echo "Updating crate versions to $version"
