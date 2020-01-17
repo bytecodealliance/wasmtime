64f07933881e11a88721dba49c3a2045715ce4fe
Jakub Konka <jakub.konka@golem.network>
2020-01-17 14:59:51 +0100

    Fix incorrect hardcoded path to witx spec

--- crates/generate/src/lib.rs
+++ crates/generate/src/lib.rs
@@ -3,8 +3,11 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 
+const WITX_PATH: &'static str = "crates/WASI/phases/snapshot/witx/wasi_snapshot_preview1.witx";
+
 #[proc_macro]
 pub fn from_witx(args: TokenStream) -> TokenStream {
+    let doc = witx::load(&[WITX_PATH]).unwrap();
     TokenStream::new()
     // TokenStream::from(raw_types::gen(
     //     TokenStream2::from(args),
