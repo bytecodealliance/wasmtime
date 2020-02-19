ea5d3d539f4ba56325674e3d753b97f56762133d
Yury Delendik <ydelendik@mozilla.com>
2020-02-19 16:02:53 -0600

    use Path in compile_cranelift

--- tests/debug/obj.rs
+++ tests/debug/obj.rs
@@ -1,11 +1,16 @@
 use anyhow::{Context as _, Result};
 use std::fs::File;
+use std::path::Path;
 use target_lexicon::Triple;
 use wasmtime::Strategy;
 use wasmtime_cli::compile_to_obj;
 use wasmtime_environ::CacheConfig;
 
-pub fn compile_cranelift(wasm: &[u8], target: Option<Triple>, output: &str) -> Result<()> {
+pub fn compile_cranelift(
+    wasm: &[u8],
+    target: Option<Triple>,
+    output: impl AsRef<Path>,
+) -> Result<()> {
     let obj = compile_to_obj(
         wasm,
         target.as_ref(),
@@ -13,7 +18,12 @@ pub fn compile_cranelift(wasm: &[u8], target: Option<Triple>, output: &str) -> R
         false,
         false,
         true,
-        output.to_string(),
+        output
+            .as_ref()
+            .file_name()
+            .unwrap()
+            .to_string_lossy()
+            .to_string(),
         &CacheConfig::new_cache_disabled(),
     )?;
 
