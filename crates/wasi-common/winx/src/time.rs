fb1a0bb6cf4903ba932d81a306bca3ee01594c42
Jakub Konka <jakub.konka@golem.network>
2020-03-06 14:19:12 +0100

    Winx now returns io::Error
    This commit is a spiritual follower of #1242 in the sense that it
    adjusts `winx` to also return `io::Error` directly rather than
    tossing a custom error type here and there.

--- crates/wasi-common/winx/src/time.rs
+++ crates/wasi-common/winx/src/time.rs
@@ -1,7 +1,8 @@
 use cvt::cvt;
+use std::io::Result;
 use winapi::um::{profileapi::QueryPerformanceFrequency, winnt::LARGE_INTEGER};
 
-pub fn perf_counter_frequency() -> std::io::Result<u64> {
+pub fn perf_counter_frequency() -> Result<u64> {
     unsafe {
         let mut frequency: LARGE_INTEGER = std::mem::zeroed();
         cvt(QueryPerformanceFrequency(&mut frequency))?;
