b0ed09d3b6ae512c8dcde299b4f10719fda5c030
Dan Gohman <sunfish@mozilla.com>
2020-03-10 12:52:45 -0700

    Exit with a more severe error code if the program traps.
    Previously, the wasmtime CLI would return with a regular failure
    error code, such as 1 on Unix. However, a program trap indicates a bug
    in the program, which can be useful to distinguish from a simple error
    status. Check for the trap case, and return an appropriate OS-specific
    exit status.

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -6,10 +6,11 @@ use std::{
     ffi::{OsStr, OsString},
     fs::File,
     path::{Component, Path, PathBuf},
+    process,
 };
 use structopt::{clap::AppSettings, StructOpt};
 use wasi_common::preopen_dir;
-use wasmtime::{Engine, Instance, Module, Store};
+use wasmtime::{Engine, Instance, Module, Store, Trap};
 use wasmtime_interface_types::ModuleData;
 use wasmtime_wasi::{old::snapshot_0::Wasi as WasiSnapshot0, Wasi};
 
@@ -113,8 +114,35 @@ impl RunCommand {
         }
 
         // Load the main wasm module.
-        self.handle_module(&store, &module_registry)
-            .with_context(|| format!("failed to run main module `{}`", self.module.display()))?;
+        match self
+            .handle_module(&store, &module_registry)
+            .with_context(|| format!("failed to run main module `{}`", self.module.display()))
+        {
+            Ok(()) => (),
+            Err(e) => {
+                // If the program exited because of a trap, return an error code
+                // to the outside environment indicating a more severe problem
+                // than a simple failure.
+                if let Some(source) = e.source() {
+                    if let Some(source) = source.source() {
+                        if source.is::<Trap>() {
+                            // Print the error message in the usual way.
+                            eprintln!("Error: {:?}", e);
+
+                            // On Unix, return the error code of an abort.
+                            #[cfg(unix)]
+                            process::exit(128 + libc::SIGABRT);
+
+                            // On Windows, return 3.
+                            // https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/abort?view=vs-2019
+                            #[cfg(windows)]
+                            process::exit(3);
+                        }
+                    }
+                }
+                return Err(e);
+            }
+        }
 
         Ok(())
     }
