e4bad23f003a071dedaceb500e4275c481e3069b
Alex Crichton <alex@alexcrichton.com>
2020-03-11 10:36:17 -0700

    Run rustfmt

--- crates/misc/run-examples/build.rs
+++ crates/misc/run-examples/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("cargo:rustc-env=TARGET={}", std::env::var("TARGET").unwrap());
+    println!(
+        "cargo:rustc-env=TARGET={}",
+        std::env::var("TARGET").unwrap()
+    );
 }
