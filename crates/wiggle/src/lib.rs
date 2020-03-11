edb39fd4a3e59c3d70bb3fae826ca48b6ceaad64
Pat Hickey <pat@moreproductive.org>
2020-03-11 14:17:35 -0700

    wiggle: make paths relative to use site of macro
    prior to this change, they were relative to CARGO_MANIFEST_DIR for the
    wiggle-generate crate.

--- crates/wiggle/src/lib.rs
+++ crates/wiggle/src/lib.rs
@@ -5,7 +5,10 @@ use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn from_witx(args: TokenStream) -> TokenStream {
-    let config = parse_macro_input!(args as wiggle_generate::Config);
+    let mut config = parse_macro_input!(args as wiggle_generate::Config);
+    config.witx.make_paths_relative_to(
+        std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var"),
+    );
     let doc = witx::load(&config.witx.paths).expect("loading witx");
     TokenStream::from(wiggle_generate::generate(&doc, &config))
 }
