a4d0a11bfd81a2ff81ca9a3117eaff136862cb26
Dan Gohman <sunfish@mozilla.com>
2020-03-26 20:43:29 -0700

    Bump Wasmtime to 0.14.0.

--- scripts/bump-wasmtime-version.sh
+++ scripts/bump-wasmtime-version.sh
@@ -9,12 +9,13 @@ topdir=$(dirname "$0")/..
 cd "$topdir"
 
 # All the wasmtime-* crates have the same version number
-version="0.13.0"
+version="0.14.0"
 
 # Update the version numbers of the crates to $version. Skip crates with
 # a version of "0.0.0", which are unpublished.
 echo "Updating crate versions to $version"
 find crates -name Cargo.toml \
+    -not -path crates/wasi-common/WASI/tools/witx/Cargo.toml \
     -not -path crates/wasi-common/wig/WASI/tools/witx/Cargo.toml \
     -exec sed -i.bk -e "s/^version = \"[.*[^0.].*\"$/version = \"$version\"/" {} \;
 
