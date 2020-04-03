56dd0d1b52b0c0c400af2d11606177b1ffdfd83d
Alex Crichton <alex@alexcrichton.com>
2020-04-03 12:05:25 -0700

    Don't pre-init backtrace things

--- crates/runtime/src/traphandlers.rs
+++ crates/runtime/src/traphandlers.rs
@@ -252,16 +252,6 @@ pub fn init() {
 }
 
 fn real_init() {
-    // This is a really weird and unfortunate function call. For all the gory
-    // details see #829, but the tl;dr; is that in a trap handler we have 2
-    // pages of stack space on Linux, and calling into libunwind which triggers
-    // the dynamic loader blows the stack.
-    //
-    // This is a dumb hack to work around this system-specific issue by
-    // capturing a backtrace once in the lifetime of a process to ensure that
-    // when we capture a backtrace in the trap handler all caches are primed,
-    // aka the dynamic loader has resolved all the relevant symbols.
-    drop(backtrace::Backtrace::new_unresolved());
     unsafe {
         platform_init();
     }
