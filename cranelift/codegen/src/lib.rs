cd69964012f132b87dcbe798a9ac7a00a9555ea8
Alex Crichton <alex@alexcrichton.com>
2020-04-17 14:18:06 -0700

    Allow some dead code in cranelift-codegen
    Prevents having to fix all warnings for all possible feature
    combinations, only the main ones which come up.

--- cranelift/codegen/src/lib.rs
+++ cranelift/codegen/src/lib.rs
@@ -39,6 +39,13 @@
     )
 )]
 #![no_std]
+// Various bits and pieces of this crate might only be used for one platform or
+// another, but it's not really too useful to learn about that all the time. On
+// CI we build at least one version of this crate with `--features all-arch`
+// which means we'll always detect truly dead code, otherwise if this is only
+// built for one platform we don't have to worry too much about trimming
+// everything down.
+#![cfg_attr(not(feature = "all-arch"), allow(dead_code))]
 
 #[allow(unused_imports)] // #[macro_use] is required for no_std
 #[macro_use]
