bdb68aab5154cad5ef55fcb319538a6cb6345fd0
Jef <jackefransham@gmail.com>
2020-04-23 14:46:01 +0200

    Fix breakages caused by rebase, remove module offset info as it is unrelated to wasmtime integration PR and was broken by rebase

--- crates/environ/src/lightbeam.rs
+++ crates/environ/src/lightbeam.rs
@@ -3,19 +3,14 @@
 use crate::cache::ModuleCacheDataTupleType;
 use crate::compilation::{Compilation, CompileError};
 use crate::func_environ::FuncEnvironment;
-use crate::module::Module;
-use crate::module_environ::FunctionBodyData;
 use crate::CacheConfig;
+use crate::ModuleTranslation;
 // TODO: Put this in `compilation`
-use crate::address_map::{
-    FunctionAddressMap, InstructionAddressMap, ModuleAddressMap, ValueLabelsRanges,
-};
+use crate::address_map::{ModuleAddressMap, ValueLabelsRanges};
 use crate::cranelift::{RelocSink, TrapSink};
-use cranelift_codegen::{ir, isa};
+use cranelift_codegen::isa;
 use cranelift_entity::{PrimaryMap, SecondaryMap};
-use cranelift_wasm::{DefinedFuncIndex, ModuleTranslationState};
-use lightbeam::{CodeGenSession, OffsetSink, Sinks};
-use std::{convert::TryFrom, mem};
+use lightbeam::{CodeGenSession, NullOffsetSink, Sinks};
 
 /// A compiler that compiles a WebAssembly module with Lightbeam, directly translating the Wasm file.
 pub struct Lightbeam;
@@ -23,135 +18,48 @@ pub struct Lightbeam;
 impl crate::compilation::Compiler for Lightbeam {
     /// Compile the module using Lightbeam, producing a compilation result with
     /// associated relocations.
-    fn compile_module<'data, 'module>(
-        module: &'module Module,
-        _module_translation: &ModuleTranslationState,
-        function_body_inputs: PrimaryMap<DefinedFuncIndex, FunctionBodyData<'data>>,
+    fn compile_module(
+        translation: &ModuleTranslation,
         isa: &dyn isa::TargetIsa,
-        // TODO
-        generate_debug_info: bool,
         _cache_config: &CacheConfig,
     ) -> Result<ModuleCacheDataTupleType, CompileError> {
-        if generate_debug_info {
+        if translation.tunables.debug_info {
             return Err(CompileError::DebugInfoNotSupported);
         }
 
-        struct WasmtimeOffsetSink {
-            offset: usize,
-            last: Option<(ir::SourceLoc, usize)>,
-            address_map: FunctionAddressMap,
-        }
-
-        impl WasmtimeOffsetSink {
-            fn new(offset: usize, start_srcloc: ir::SourceLoc, end_srcloc: ir::SourceLoc) -> Self {
-                WasmtimeOffsetSink {
-                    offset,
-                    last: None,
-                    address_map: FunctionAddressMap {
-                        instructions: vec![],
-                        start_srcloc,
-                        end_srcloc,
-                        body_offset: 0,
-                        body_len: 0,
-                    },
-                }
-            }
-
-            fn finalize(mut self, body_len: usize) -> FunctionAddressMap {
-                if let Some((srcloc, code_offset)) = self.last {
-                    self.address_map.instructions.push(InstructionAddressMap {
-                        srcloc,
-                        code_offset,
-                        code_len: body_len
-                            .checked_sub(code_offset)
-                            .expect("Code offset exceeds size of body"),
-                    });
-                }
-
-                self.address_map.body_len = body_len
-                    .checked_sub(self.address_map.body_offset)
-                    .expect("Code offset exceeds size of body");
-
-                self.address_map
-            }
-        }
-
-        impl OffsetSink for WasmtimeOffsetSink {
-            fn offset(
-                &mut self,
-                offset_in_wasm_function: ir::SourceLoc,
-                offset_in_compiled_function: usize,
-            ) {
-                if self.last.as_ref().map(|(s, _)| s) == Some(&offset_in_wasm_function) {
-                    return;
-                }
-
-                let offset_in_compiled_function = offset_in_compiled_function
-                    .checked_sub(self.offset)
-                    .expect("Code offset exceeds size of body");
-
-                let last = mem::replace(
-                    &mut self.last,
-                    Some((offset_in_wasm_function, offset_in_compiled_function)),
-                );
-
-                if let Some((srcloc, code_offset)) = last {
-                    self.address_map.instructions.push(InstructionAddressMap {
-                        srcloc,
-                        code_offset,
-                        code_len: offset_in_compiled_function
-                            .checked_sub(code_offset)
-                            .expect("Code offset exceeds size of body"),
-                    })
-                }
-            }
-        }
-
-        let env = FuncEnvironment::new(isa.frontend_config(), &module.local);
-        let mut relocations = PrimaryMap::with_capacity(function_body_inputs.len());
-        let mut traps = PrimaryMap::with_capacity(function_body_inputs.len());
-        let mut module_addresses = ModuleAddressMap::with_capacity(function_body_inputs.len());
+        let env = FuncEnvironment::new(
+            isa.frontend_config(),
+            &translation.module.local,
+            &translation.tunables,
+        );
+        let mut relocations = PrimaryMap::with_capacity(translation.function_body_inputs.len());
+        let mut traps = PrimaryMap::with_capacity(translation.function_body_inputs.len());
 
         let mut codegen_session: CodeGenSession<_> = CodeGenSession::new(
-            function_body_inputs.len() as u32,
+            translation.function_body_inputs.len() as u32,
             &env,
             lightbeam::microwasm::I32,
         );
 
-        for (i, function_body) in &function_body_inputs {
-            let func_index = module.local.func_index(i);
-
-            let start_offset = codegen_session.offset();
+        for (i, function_body) in &translation.function_body_inputs {
+            let func_index = translation.module.local.func_index(i);
 
             let mut reloc_sink = RelocSink::new(func_index);
             let mut trap_sink = TrapSink::new();
-            let mut offset_sink = WasmtimeOffsetSink::new(
-                start_offset,
-                ir::SourceLoc::new(
-                    u32::try_from(function_body.module_offset)
-                        .expect("Size of module exceeded u32"),
-                ),
-                ir::SourceLoc::new(
-                    u32::try_from(function_body.module_offset + function_body.data.len())
-                        .expect("Size of module exceeded u32"),
-                ),
-            );
-
             lightbeam::translate_function(
                 &mut codegen_session,
                 Sinks {
                     relocs: &mut reloc_sink,
                     traps: &mut trap_sink,
-                    offsets: &mut offset_sink,
+                    offsets: &mut NullOffsetSink,
                 },
                 i.as_u32(),
-                wasmparser::FunctionBody::new(function_body.module_offset, function_body.data),
+                wasmparser::FunctionBody::new(0, function_body.data),
             )
             .map_err(|e| CompileError::Codegen(format!("Failed to translate function: {}", e)))?;
 
             relocations.push(reloc_sink.func_relocs);
             traps.push(trap_sink.traps);
-            module_addresses.push(offset_sink.finalize(codegen_session.offset() - start_offset));
         }
 
         let code_section = codegen_session
@@ -164,16 +72,15 @@ impl crate::compilation::Compiler for Lightbeam {
         let code_section_ranges_and_jt = code_section
             .funcs()
             .into_iter()
-            .map(|r| (r, SecondaryMap::new(), 0..0));
+            .map(|r| (r, SecondaryMap::new()));
 
         Ok((
-            Compilation::from_buffer(code_section.buffer(), code_section_ranges_and_jt),
+            Compilation::from_buffer(code_section.buffer().to_vec(), code_section_ranges_and_jt),
             relocations,
-            module_addresses,
+            ModuleAddressMap::new(),
             ValueLabelsRanges::new(),
             PrimaryMap::new(),
             traps,
-            PrimaryMap::new(),
         ))
     }
 }
