d1be0c18e774fda93225570ab1a1bf979928c993
Alex Crichton <alex@alexcrichton.com>
2020-04-24 10:55:50 -0500

    Don't deny warnings on nightly CI (#1589)
    Too much churn!
    
    Closes #1588

--- .github/actions/install-rust/main.js
+++ .github/actions/install-rust/main.js
@@ -12,8 +12,11 @@ child_process.execFileSync('rustup', ['set', 'profile', 'minimal']);
 child_process.execFileSync('rustup', ['update', toolchain, '--no-self-update']);
 child_process.execFileSync('rustup', ['default', toolchain]);
 
-// Deny warnings on CI to keep our code warning-free as it lands in-tree
-console.log(`::set-env name=RUSTFLAGS::-D warnings`);
+// Deny warnings on CI to keep our code warning-free as it lands in-tree. Don't
+// do this on nightly though since there's a fair amount of warning churn there.
+if (!toolchain.startsWith('nightly')) {
+  console.log(`::set-env name=RUSTFLAGS::-D warnings`);
+}
 
 // Save disk space by avoiding incremental compilation, and also we don't use
 // any caching so incremental wouldn't help anyway.
