176b3a8382a8f6fa8e307560070efb9ace3c79c9
Graham✈️✈️ <plicease@cpan.org>
2020-05-05 13:57:19 -0600

    add the _set suffix to static_memory_guard_size and dynamic_memory_guard_size properties in c-api (#1662)

--- crates/c-api/src/config.rs
+++ crates/c-api/src/config.rs
@@ -153,11 +153,11 @@ pub extern "C" fn wasmtime_config_static_memory_maximum_size_set(c: &mut wasm_co
 }
 
 #[no_mangle]
-pub extern "C" fn wasmtime_config_static_memory_guard_size(c: &mut wasm_config_t, size: u64) {
+pub extern "C" fn wasmtime_config_static_memory_guard_size_set(c: &mut wasm_config_t, size: u64) {
     c.config.static_memory_guard_size(size);
 }
 
 #[no_mangle]
-pub extern "C" fn wasmtime_config_dynamic_memory_guard_size(c: &mut wasm_config_t, size: u64) {
+pub extern "C" fn wasmtime_config_dynamic_memory_guard_size_set(c: &mut wasm_config_t, size: u64) {
     c.config.dynamic_memory_guard_size(size);
 }
