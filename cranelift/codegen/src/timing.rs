15c31c1a259f05637e28cb5bebf795b8009d8a99
Julian Seward <jseward@acm.org>
2020-05-06 09:08:14 +0200

    Add a transformation pass which removes phi nodes to which it can demonstrate that only one value ever flows.  Has been observed to improve generated code run times by up to 8%.  Compilation cost increases by about 0.6%, but up to 7% total cost has been observed to be saved; iow it can be a significant win in terms of compilation time, overall.

--- cranelift/codegen/src/timing.rs
+++ cranelift/codegen/src/timing.rs
@@ -62,6 +62,7 @@ define_passes! {
     gvn: "Global value numbering",
     licm: "Loop invariant code motion",
     unreachable_code: "Remove unreachable blocks",
+    remove_constant_phis: "Remove constant phi-nodes",
 
     regalloc: "Register allocation",
     ra_liveness: "RA liveness analysis",
