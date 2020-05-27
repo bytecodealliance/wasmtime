9a3ee46fdca0fbe4eb349b62ed660448863f64a6
Leonardo Yvens <leoyvens@gmail.com>
2020-05-27 19:16:04 -0300

    Add TrapReason::Error

--- crates/wasmtime/src/linker.rs
+++ crates/wasmtime/src/linker.rs
@@ -404,11 +404,7 @@ impl Linker {
                 let export_name = export.name().to_owned();
                 let func = Func::new(&self.store, func_ty.clone(), move |_, params, results| {
                     // Create a new instance for this command execution.
-                    let instance = Instance::new(&module, &imports).map_err(|error| {
-                        error
-                            .downcast::<Trap>()
-                            .unwrap_or_else(|error| Trap::new(format!("{:?}", error)))
-                    })?;
+                    let instance = Instance::new(&module, &imports)?;
 
                     // `unwrap()` everything here because we know the instance contains a
                     // function export with the given name and signature because we're
