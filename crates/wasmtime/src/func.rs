652f47495aeb7c27496292eb9eff3944b3bff31a
Nick Fitzgerald <fitzgen@gmail.com>
2020-06-01 14:28:08 -0700

    wasmtime: Make `Func::store` a public method

--- crates/wasmtime/src/func.rs
+++ crates/wasmtime/src/func.rs
@@ -727,9 +727,7 @@ impl Func {
         (get15, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15)
     }
 
-    // This should be `pub(crate)` but the C API happens to need it in one
-    // place.
-    #[doc(hidden)]
+    /// Get a reference to this function's store.
     pub fn store(&self) -> &Store {
         &self.instance.store
     }
