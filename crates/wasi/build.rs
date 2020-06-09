caa87048abbb9a61ada05136937f8151285af5ce
Dan Gohman <sunfish@mozilla.com>
2020-06-09 10:11:49 -0700

    Wasmtime 0.18.0 and Cranelift 0.65.0.

--- crates/wasi/build.rs
+++ crates/wasi/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let wasi_root = std::env::var("DEP_WASI_COMMON_17_WASI").unwrap();
+    let wasi_root = std::env::var("DEP_WASI_COMMON_18_WASI").unwrap();
     println!("cargo:rustc-env=WASI_ROOT={}", wasi_root);
 }
