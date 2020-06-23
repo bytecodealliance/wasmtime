303f7172a146c79c83153b7c4800a990a11dee47
Pat Hickey <pat@moreproductive.org>
2020-06-22 17:23:11 -0700

    wasi: switch to use wasmtime-wiggle for wiggle integration

--- crates/wasi/src/lib.rs
+++ crates/wasi/src/lib.rs
@@ -6,7 +6,7 @@ pub use wasi_common::{WasiCtx, WasiCtxBuilder};
 
 // Defines a `struct Wasi` with member fields and appropriate APIs for dealing
 // with all the various WASI exports.
-wig::define_wasi_struct_for_wiggle!("phases/snapshot/witx/wasi_snapshot_preview1.witx");
+wasmtime_wiggle::define_struct_for_wiggle!("phases/snapshot/witx/wasi_snapshot_preview1.witx");
 
 pub fn is_wasi_module(name: &str) -> bool {
     // FIXME: this should be more conservative, but while WASI is in flux and
