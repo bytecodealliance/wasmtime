75e77f017bb3eee4bd2b1194626af74677977935
Alex Crichton <alex@alexcrichton.com>
2020-06-23 08:04:27 -0700

    Bind `Trap::i32_exit_status` in C API
    This allows embedders to take a different action when a WASI program
    exits depending on whether it exited with an exit status or exited with
    a trap.
    
    cc bytecodealliance/wasmtime-py#30

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -169,6 +169,15 @@ WASM_API_EXTERN void wasmtime_interrupt_handle_interrupt(wasmtime_interrupt_hand
 
 ///////////////////////////////////////////////////////////////////////////////
 //
+// Extensions to `wasm_trap_t`
+
+// Returns `true` if the trap is a WASI "exit" trap and has a return status. If
+// `true` is returned then the exit status is returned through the `status`
+// pointer. If `false` is returned then this is not a wasi exit trap.
+WASM_API_EXTERN bool wasmtime_trap_exit_status(const wasm_trap_t*, int *status);
+
+///////////////////////////////////////////////////////////////////////////////
+//
 // Extensions to `wasm_frame_t`
 
 WASM_API_EXTERN const wasm_name_t *wasmtime_frame_func_name(const wasm_frame_t*);
