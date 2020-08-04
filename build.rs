2154b76fa82044bacdd7320aa0b7f4967c5f8a25
Andrew Brown <andrew.brown@intel.com>
2020-08-04 14:33:04 -0700

    machinst x64: enable simd_const.wast spec test

--- build.rs
+++ build.rs
@@ -181,6 +181,7 @@ fn experimental_x64_should_panic(testsuite: &str, testname: &str, strategy: &str
 
     match (testsuite, testname) {
         ("simd", "simd_address") => return false,
+        ("simd", "simd_const") => return false,
         ("simd", "simd_f32x4_arith") => return false,
         ("simd", "simd_f32x4_cmp") => return false,
         ("simd", "simd_f64x2_arith") => return false,
