ee76e01efc88db29e1eefdeed9c29cd441c92493
Benjamin Bouvier <public@benj.me>
2020-08-19 14:52:42 +0200

    machinst: fix the pinned reg hack;
    The pinned register hack didn't work because the GetPinnedReg is marked
    as having side-effects, so that GVN wouldn't try to common it out.
    
    This commit tweaks the function used during lowering to vcode, so that
    the GetPinnedReg opcode is specially handled. It's a bit lame, but it
    makes the hack work again.
    
    Also, use_input needs to be a no-op for real registers.

--- cranelift/codegen/src/machinst/lower.rs
+++ cranelift/codegen/src/machinst/lower.rs
@@ -4,7 +4,7 @@
 
 use crate::entity::SecondaryMap;
 use crate::fx::{FxHashMap, FxHashSet};
-use crate::inst_predicates::{has_side_effect_or_load, is_constant_64bit};
+use crate::inst_predicates::{has_side_effect_or_load_not_get_pinned_reg, is_constant_64bit};
 use crate::ir::instructions::BranchInfo;
 use crate::ir::types::I64;
 use crate::ir::{
@@ -372,7 +372,7 @@ impl<'func, I: VCodeInst> Lower<'func, I> {
         for bb in f.layout.blocks() {
             cur_color += 1;
             for inst in f.layout.block_insts(bb) {
-                let side_effect = has_side_effect_or_load(f, inst);
+                let side_effect = has_side_effect_or_load_not_get_pinned_reg(f, inst);
 
                 // Assign colors. A new color is chosen *after* any side-effecting instruction.
                 inst_colors[inst] = InstColor::new(cur_color);
@@ -800,14 +800,14 @@ impl<'func, I: VCodeInst> Lower<'func, I> {
                 debug!(" -> src inst {}", src_inst);
                 debug!(
                     " -> has side effect: {}",
-                    has_side_effect_or_load(self.f, src_inst)
+                    has_side_effect_or_load_not_get_pinned_reg(self.f, src_inst)
                 );
                 debug!(
                     " -> our color is {:?}, src inst is {:?}",
                     self.inst_color(at_inst),
                     self.inst_color(src_inst)
                 );
-                if !has_side_effect_or_load(self.f, src_inst)
+                if !has_side_effect_or_load_not_get_pinned_reg(self.f, src_inst)
                     || self.inst_color(at_inst) == self.inst_color(src_inst)
                 {
                     Some((src_inst, result_idx))
@@ -989,7 +989,9 @@ impl<'func, I: VCodeInst> LowerCtx for Lower<'func, I> {
 
     fn use_input_reg(&mut self, input: LowerInput) {
         debug!("use_input_reg: vreg {:?} is needed", input.reg);
-        self.vreg_needed[input.reg.get_index()] = true;
+        if input.reg.is_virtual() {
+            self.vreg_needed[input.reg.get_index()] = true;
+        }
     }
 
     fn is_reg_needed(&self, ir_inst: Inst, reg: Reg) -> bool {
