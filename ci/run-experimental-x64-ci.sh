c29f4599ac29783f15e2334e08d7762b8f80e7ba
Benjamin Bouvier <public@benj.me>
2020-09-22 10:43:28 +0200

    Add a script to run CI for the new x64 backend (#2215)
    Co-authored-by: Andrew Brown <andrew.brown@intel.com>

---
+++ ci/run-experimental-x64-ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cargo +nightly \
+            -Zfeatures=all -Zpackage-features \
+            test \
+            --features test-programs/test_programs \
+            --features experimental_x64 \
+            --all \
+            --exclude wasmtime-lightbeam \
+            --exclude peepmatic \
+            --exclude peepmatic-automata \
+            --exclude peepmatic-fuzzing \
+            --exclude peepmatic-macro \
+            --exclude peepmatic-runtime \
+            --exclude peepmatic-test \
+            --exclude peepmatic-souper \
+            --exclude lightbeam
