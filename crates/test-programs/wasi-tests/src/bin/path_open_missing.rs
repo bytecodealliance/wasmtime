d947010181efac43e4601e6a608560bfb52581cd
Joshua Warner <joshuawarner32@gmail.com>
2020-09-25 19:52:13 -0700

    Don't implicitly create empty files in VirtualDir::openat (#2235)
    * Don't implicitly create empty files in VirtualDir::openat
    
    * Add test
    
    * Add note on how to run test-program tests to the README

---
+++ crates/test-programs/wasi-tests/src/bin/path_open_missing.rs
@@ -0,0 +1,43 @@
+use std::{env, process};
+use wasi_tests::{open_scratch_directory};
+
+unsafe fn test_path_open_missing(dir_fd: wasi::Fd) {
+    assert_eq!(
+        wasi::path_open(
+            dir_fd,
+            0,
+            "file",
+            0, // not passing O_CREAT here
+            0,
+            0,
+            0,
+        )
+        .expect_err("trying to open a file that doesn't exist")
+        .raw_error(),
+        wasi::ERRNO_NOENT,
+        "errno should be ERRNO_NOENT"
+    );
+}
+
+fn main() {
+    let mut args = env::args();
+    let prog = args.next().unwrap();
+    let arg = if let Some(arg) = args.next() {
+        arg
+    } else {
+        eprintln!("usage: {} <scratch directory>", prog);
+        process::exit(1);
+    };
+
+    // Open scratch directory
+    let dir_fd = match open_scratch_directory(&arg) {
+        Ok(dir_fd) => dir_fd,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1)
+        }
+    };
+
+    // Run the tests.
+    unsafe { test_path_open_missing(dir_fd) }
+}
