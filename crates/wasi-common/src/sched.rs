6e72a8fa9dcd4816e786c82406ab14df0a105d83
Pat Hickey <pat@moreproductive.org>
2020-09-30 17:33:39 -0700

    scheduler: introduce a new intermediate abstraction.
    Still need to translate the sys stuff to use it.
    
    Also, broke some of the exports related to clocks... those should be
    their own top level, not under sched.

--- crates/wasi-common/src/sched.rs
+++ crates/wasi-common/src/sched.rs
@@ -1,77 +1,214 @@
 use crate::ctx::WasiCtx;
 use crate::entry::EntryHandle;
 use crate::fs::Fd;
-use crate::handle::{HandleRights, Rights};
-use crate::Result;
+use crate::handle::{Filesize, HandleRights, Rights};
+use crate::Error;
+use std::convert::TryInto;
+use std::time::{Duration, SystemTime};
 
-pub use crate::wasi::types::{
-    Clockid, Errno, Event, EventFdReadwrite, Eventrwflags, Eventtype, Subclockflags,
-    SubscriptionClock, Timestamp, Userdata,
-};
+pub use crate::wasi::types::{Eventrwflags, Timestamp, Userdata};
 
-#[derive(Debug, Copy, Clone)]
-pub struct ClockEventData {
-    pub delay: u128, // delay is expressed in nanoseconds
-    pub userdata: Userdata,
+pub enum SchedResult<A, E> {
+    /// The subscribed event did not happen
+    None,
+    /// The subscribed event did happen
+    Ok(A),
+    /// The subscribed event gave an error
+    Err(E),
 }
 
-#[derive(Debug)]
-pub struct FdEventData {
+impl<A, E> SchedResult<A, E> {
+    pub fn is_not_none(&self) -> bool {
+        match self {
+            Self::None => false,
+            _ => true,
+        }
+    }
+}
+
+pub struct RwSubscription {
     pub handle: EntryHandle,
-    pub r#type: Eventtype,
-    pub userdata: Userdata,
+    status: SchedResult<(Filesize, Eventrwflags), Error>,
 }
 
-pub struct PollBuilder<'a> {
+impl RwSubscription {
+    pub fn new(handle: EntryHandle) -> Self {
+        Self {
+            handle,
+            status: SchedResult::None,
+        }
+    }
+    pub fn complete(&mut self, size: Filesize, flags: Eventrwflags) {
+        self.status = SchedResult::Ok((size, flags))
+    }
+    pub fn error(&mut self, error: Error) {
+        self.status = SchedResult::Err(error)
+    }
+    pub fn result(self) -> SchedResult<(Filesize, Eventrwflags), Error> {
+        self.status
+    }
+}
+
+pub struct TimerSubscription {
+    pub deadline: SystemTime,
+}
+
+impl TimerSubscription {
+    pub fn from_absolute(ts: Timestamp) -> Result<Self, Error> {
+        let epoch = SystemTime::UNIX_EPOCH;
+        // this line better not be in production by the year 2200:
+        let ts: u64 = ts.try_into().map_err(|_| Error::TooBig)?;
+        let deadline = epoch
+            .checked_add(Duration::from_nanos(ts))
+            .ok_or(Error::TooBig)?;
+        Ok(Self { deadline })
+    }
+    pub fn from_relative(ts: Timestamp) -> Result<Self, Error> {
+        let now = SystemTime::now();
+        // this line better not be in production by the year 2200:
+        let ts: u64 = ts.try_into().map_err(|_| Error::TooBig)?;
+        let deadline = now
+            .checked_add(Duration::from_nanos(ts))
+            .ok_or(Error::TooBig)?;
+        Ok(Self { deadline })
+    }
+    /// Ok indicates deadline in the past, None indicates deadline yet to be reached
+    pub fn result(&self) -> SchedResult<(), Error> {
+        if self.deadline.duration_since(SystemTime::now()).is_ok() {
+            SchedResult::Ok(())
+        } else {
+            SchedResult::None
+        }
+    }
+}
+
+pub enum Subscription {
+    Read(RwSubscription),
+    Write(RwSubscription),
+    Timer(TimerSubscription),
+}
+
+impl Subscription {
+    pub fn earliest_deadline<'a>(
+        subs: impl Iterator<Item = &'a Subscription> + 'a,
+    ) -> Option<SystemTime> {
+        subs.filter_map(|s| match s {
+            Subscription::Timer(ts) => Some(ts.deadline),
+            _ => None,
+        })
+        .fold(None, |early, ts| {
+            if let Some(early) = early {
+                Some(early.min(ts))
+            } else {
+                Some(ts)
+            }
+        })
+    }
+}
+
+pub enum SubscriptionResult {
+    Read(SchedResult<(Filesize, Eventrwflags), Error>),
+    Write(SchedResult<(Filesize, Eventrwflags), Error>),
+    Timer(SchedResult<(), Error>),
+}
+
+impl SubscriptionResult {
+    pub fn is_not_none(&self) -> bool {
+        match self {
+            SubscriptionResult::Read(r) => r.is_not_none(),
+            SubscriptionResult::Write(r) => r.is_not_none(),
+            SubscriptionResult::Timer(r) => r.is_not_none(),
+        }
+    }
+}
+
+impl From<Subscription> for SubscriptionResult {
+    fn from(s: Subscription) -> SubscriptionResult {
+        match s {
+            Subscription::Read(s) => SubscriptionResult::Read(s.result()),
+            Subscription::Write(s) => SubscriptionResult::Write(s.result()),
+            Subscription::Timer(s) => SubscriptionResult::Timer(s.result()),
+        }
+    }
+}
+
+pub struct Poll<'a> {
     ctx: &'a WasiCtx,
-    timeout: Option<ClockEventData>,
-    fd_events: Vec<FdEventData>,
+    subscriptions: Vec<(Subscription, Userdata)>,
 }
 
-impl<'a> PollBuilder<'a> {
+impl<'a> Poll<'a> {
     pub fn new(ctx: &'a WasiCtx) -> Self {
-        PollBuilder {
+        Poll {
             ctx,
-            timeout: None,
-            fd_events: Vec::new(),
+            subscriptions: Vec::new(),
         }
     }
 
-    pub fn subscribe_clock(&mut self, clock: SubscriptionClock, userdata: Userdata) -> Result<()> {
-        let delay = crate::sys::clock::to_relative_ns_delay(&clock)?;
-        let current = ClockEventData { delay, userdata };
-        let timeout = self.timeout.get_or_insert(current);
-        if current.delay < timeout.delay {
-            *timeout = current;
-        }
+    pub fn subscribe_timer(
+        &mut self,
+        at: Timestamp,
+        ud: Userdata,
+        is_absolute: bool,
+    ) -> Result<(), Error> {
+        self.subscriptions.push((
+            Subscription::Timer(if is_absolute {
+                TimerSubscription::from_absolute(at)?
+            } else {
+                TimerSubscription::from_relative(at)?
+            }),
+            ud,
+        ));
         Ok(())
     }
 
-    pub fn subscribe_fd_read(&mut self, fd: Fd, userdata: Userdata) -> Result<()> {
+    pub fn subscribe_relative_time(&mut self, at: Timestamp, ud: Userdata) -> Result<(), Error> {
+        self.subscriptions.push((
+            Subscription::Timer(TimerSubscription::from_relative(at)?),
+            ud,
+        ));
+        Ok(())
+    }
+
+    pub fn subscribe_absolute_time(&mut self, at: Timestamp, ud: Userdata) -> Result<(), Error> {
+        self.subscriptions.push((
+            Subscription::Timer(TimerSubscription::from_absolute(at)?),
+            ud,
+        ));
+        Ok(())
+    }
+
+    pub fn subscribe_fd_read(&mut self, fd: Fd, ud: Userdata) -> Result<(), Error> {
         let entry = self.ctx.get_entry(fd)?;
         let required_rights = HandleRights::from_base(Rights::FD_READ | Rights::POLL_FD_READWRITE);
         let handle = entry.as_handle(&required_rights)?;
-        self.fd_events.push(FdEventData {
-            handle,
-            r#type: Eventtype::FdRead,
-            userdata,
-        });
+        self.subscriptions
+            .push((Subscription::Read(RwSubscription::new(handle)), ud));
         Ok(())
     }
 
-    pub fn subscribe_fd_write(&mut self, fd: Fd, userdata: Userdata) -> Result<()> {
+    pub fn subscribe_fd_write(&mut self, fd: Fd, ud: Userdata) -> Result<(), Error> {
         let entry = self.ctx.get_entry(fd)?;
         let required_rights = HandleRights::from_base(Rights::FD_WRITE | Rights::POLL_FD_READWRITE);
         let handle = entry.as_handle(&required_rights)?;
-        self.fd_events.push(FdEventData {
-            handle,
-            r#type: Eventtype::FdWrite,
-            userdata,
-        });
+        self.subscriptions
+            .push((Subscription::Write(RwSubscription::new(handle)), ud));
         Ok(())
     }
 
-    pub fn poll(self) -> Result<Vec<Event>> {
-        crate::sys::poll::oneoff(self.timeout, self.fd_events)
+    pub fn subsciptions(&mut self) -> Vec<&mut Subscription> {
+        self.subscriptions.iter_mut().map(|(s, _ud)| s).collect()
+    }
+
+    pub fn earliest_deadline(&self) -> Option<SystemTime> {
+        Subscription::earliest_deadline(self.subscriptions.iter().map(|(s, _ud)| s))
+    }
+
+    pub fn results(self) -> Vec<(SubscriptionResult, Userdata)> {
+        self.subscriptions
+            .into_iter()
+            .map(|(s, ud)| (SubscriptionResult::from(s), ud))
+            .filter(|(r, ud)| r.is_not_none())
+            .collect()
     }
 }
--- crates/wasi-common/src/snapshots/wasi_snapshot_preview1.rs
+++ crates/wasi-common/src/snapshots/wasi_snapshot_preview1.rs
@@ -1,9 +1,8 @@
-use crate::sched::PollBuilder;
+use crate::sched::{Poll, SchedResult, SubscriptionResult, Userdata};
 use crate::sys::clock;
 use crate::wasi::types;
 use crate::wasi::wasi_snapshot_preview1::WasiSnapshotPreview1;
-use crate::{Error, Result, WasiCtx};
-use std::convert::TryInto;
+use crate::{Error, WasiCtx};
 use std::ops::{Deref, DerefMut};
 use wiggle::{GuestPtr, GuestSlice};
 
@@ -12,11 +11,11 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         &self,
         argv: &GuestPtr<'b, GuestPtr<'b, u8>>,
         argv_buf: &GuestPtr<'b, u8>,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         self.args.write_to_guest(argv_buf, argv)
     }
 
-    fn args_sizes_get(&self) -> Result<(types::Size, types::Size)> {
+    fn args_sizes_get(&self) -> Result<(types::Size, types::Size), Error> {
         Ok((self.args.number_elements, self.args.cumulative_size))
     }
 
@@ -24,15 +23,15 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         &self,
         environ: &GuestPtr<'b, GuestPtr<'b, u8>>,
         environ_buf: &GuestPtr<'b, u8>,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         self.env.write_to_guest(environ_buf, environ)
     }
 
-    fn environ_sizes_get(&self) -> Result<(types::Size, types::Size)> {
+    fn environ_sizes_get(&self) -> Result<(types::Size, types::Size), Error> {
         Ok((self.env.number_elements, self.env.cumulative_size))
     }
 
-    fn clock_res_get(&self, id: types::Clockid) -> Result<types::Timestamp> {
+    fn clock_res_get(&self, id: types::Clockid) -> Result<types::Timestamp, Error> {
         let resolution = clock::res_get(id)?;
         Ok(resolution)
     }
@@ -41,7 +40,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         &self,
         id: types::Clockid,
         _precision: types::Timestamp,
-    ) -> Result<types::Timestamp> {
+    ) -> Result<types::Timestamp, Error> {
         let time = clock::time_get(id)?;
         Ok(time)
     }
@@ -52,7 +51,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         offset: types::Filesize,
         len: types::Filesize,
         advice: types::Advice,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         self.get_entry(fd)?.fd_advise(offset, len, advice)
     }
 
@@ -61,25 +60,25 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fd: types::Fd,
         offset: types::Filesize,
         len: types::Filesize,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         self.get_entry(fd)?.fd_allocate(offset, len)
     }
 
-    fn fd_close(&self, fd: types::Fd) -> Result<()> {
+    fn fd_close(&self, fd: types::Fd) -> Result<(), Error> {
         self.get_entry(fd)?.fd_close()?;
         self.remove_entry(fd)?;
         Ok(())
     }
 
-    fn fd_datasync(&self, fd: types::Fd) -> Result<()> {
+    fn fd_datasync(&self, fd: types::Fd) -> Result<(), Error> {
         self.get_entry(fd)?.fd_datasync()
     }
 
-    fn fd_fdstat_get(&self, fd: types::Fd) -> Result<types::Fdstat> {
+    fn fd_fdstat_get(&self, fd: types::Fd) -> Result<types::Fdstat, Error> {
         self.get_entry(fd)?.fd_fdstat_get()
     }
 
-    fn fd_fdstat_set_flags(&self, fd: types::Fd, flags: types::Fdflags) -> Result<()> {
+    fn fd_fdstat_set_flags(&self, fd: types::Fd, flags: types::Fdflags) -> Result<(), Error> {
         self.get_entry(fd)?.fd_fdstat_set_flags(flags)
     }
 
@@ -88,16 +87,16 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fd: types::Fd,
         fs_rights_base: types::Rights,
         fs_rights_inheriting: types::Rights,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         self.get_entry(fd)?
             .fd_fdstat_set_rights(fs_rights_base, fs_rights_inheriting)
     }
 
-    fn fd_filestat_get(&self, fd: types::Fd) -> Result<types::Filestat> {
+    fn fd_filestat_get(&self, fd: types::Fd) -> Result<types::Filestat, Error> {
         self.get_entry(fd)?.fd_filestat_get()
     }
 
-    fn fd_filestat_set_size(&self, fd: types::Fd, size: types::Filesize) -> Result<()> {
+    fn fd_filestat_set_size(&self, fd: types::Fd, size: types::Filesize) -> Result<(), Error> {
         self.get_entry(fd)?.fd_filestat_set_size(size)
     }
 
@@ -107,12 +106,12 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         atim: types::Timestamp,
         mtim: types::Timestamp,
         fst_flags: types::Fstflags,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         self.get_entry(fd)?
             .fd_filestat_set_times(atim, mtim, fst_flags)
     }
 
-    fn fd_read(&self, fd: types::Fd, iovs: &types::IovecArray<'_>) -> Result<types::Size> {
+    fn fd_read(&self, fd: types::Fd, iovs: &types::IovecArray<'_>) -> Result<types::Size, Error> {
         let entry = self.get_entry(fd)?;
 
         let mut guest_slices = Vec::new();
@@ -130,7 +129,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fd: types::Fd,
         iovs: &types::IovecArray<'_>,
         offset: types::Filesize,
-    ) -> Result<types::Size> {
+    ) -> Result<types::Size, Error> {
         let entry = self.get_entry(fd)?;
 
         // Rather than expose the details of our IovecArray to the Entry, it accepts a
@@ -145,7 +144,11 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         entry.fd_pread(guest_slices, offset)
     }
 
-    fn fd_write(&self, fd: types::Fd, ciovs: &types::CiovecArray<'_>) -> Result<types::Size> {
+    fn fd_write(
+        &self,
+        fd: types::Fd,
+        ciovs: &types::CiovecArray<'_>,
+    ) -> Result<types::Size, Error> {
         let entry = self.get_entry(fd)?;
 
         let mut guest_slices = Vec::new();
@@ -161,7 +164,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fd: types::Fd,
         ciovs: &types::CiovecArray<'_>,
         offset: types::Filesize,
-    ) -> Result<types::Size> {
+    ) -> Result<types::Size, Error> {
         let entry = self.get_entry(fd)?;
 
         let mut guest_slices = Vec::new();
@@ -174,7 +177,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         entry.fd_pwrite(guest_slices, offset)
     }
 
-    fn fd_prestat_get(&self, fd: types::Fd) -> Result<types::Prestat> {
+    fn fd_prestat_get(&self, fd: types::Fd) -> Result<types::Prestat, Error> {
         self.get_entry(fd)?.fd_prestat_get()
     }
 
@@ -183,7 +186,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fd: types::Fd,
         path: &GuestPtr<u8>,
         path_len: types::Size,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         let entry = self.get_entry(fd)?;
         let mut path = path.as_array(path_len).as_slice()?;
         entry.fd_prestat_dir_name(path.deref_mut())
@@ -194,13 +197,13 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         buf: &GuestPtr<u8>,
         buf_len: types::Size,
         cookie: types::Dircookie,
-    ) -> Result<types::Size> {
+    ) -> Result<types::Size, Error> {
         let entry = self.get_entry(fd)?;
         let mut buf = buf.as_array(buf_len).as_slice()?;
         entry.fd_readdir(buf.deref_mut(), cookie)
     }
 
-    fn fd_renumber(&self, from: types::Fd, to: types::Fd) -> Result<()> {
+    fn fd_renumber(&self, from: types::Fd, to: types::Fd) -> Result<(), Error> {
         if !self.contains_entry(from) {
             return Err(Error::Badf);
         }
@@ -228,19 +231,23 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fd: types::Fd,
         offset: types::Filedelta,
         whence: types::Whence,
-    ) -> Result<types::Filesize> {
+    ) -> Result<types::Filesize, Error> {
         self.get_entry(fd)?.fd_seek(offset, whence)
     }
 
-    fn fd_sync(&self, fd: types::Fd) -> Result<()> {
+    fn fd_sync(&self, fd: types::Fd) -> Result<(), Error> {
         self.get_entry(fd)?.fd_sync()
     }
 
-    fn fd_tell(&self, fd: types::Fd) -> Result<types::Filesize> {
+    fn fd_tell(&self, fd: types::Fd) -> Result<types::Filesize, Error> {
         self.get_entry(fd)?.fd_tell()
     }
 
-    fn path_create_directory(&self, dirfd: types::Fd, path: &GuestPtr<'_, str>) -> Result<()> {
+    fn path_create_directory(
+        &self,
+        dirfd: types::Fd,
+        path: &GuestPtr<'_, str>,
+    ) -> Result<(), Error> {
         let entry = self.get_entry(dirfd)?;
         let path = path.as_str()?;
         entry.path_create_directory(path.deref())
@@ -251,7 +258,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         dirfd: types::Fd,
         flags: types::Lookupflags,
         path: &GuestPtr<'_, str>,
-    ) -> Result<types::Filestat> {
+    ) -> Result<types::Filestat, Error> {
         let entry = self.get_entry(dirfd)?;
         let path = path.as_str()?;
         entry.path_filestat_get(flags, path.deref())
@@ -265,7 +272,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         atim: types::Timestamp,
         mtim: types::Timestamp,
         fst_flags: types::Fstflags,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         let entry = self.get_entry(dirfd)?;
         let path = path.as_str()?;
         entry.path_filestat_set_times(flags, path.deref(), atim, mtim, fst_flags)
@@ -278,7 +285,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         old_path: &GuestPtr<'_, str>,
         new_fd: types::Fd,
         new_path: &GuestPtr<'_, str>,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         let old_entry = self.get_entry(old_fd)?;
         let new_entry = self.get_entry(new_fd)?;
         old_entry.path_link(old_flags, old_path, &new_entry, new_path)
@@ -293,7 +300,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         fs_rights_base: types::Rights,
         fs_rights_inheriting: types::Rights,
         fdflags: types::Fdflags,
-    ) -> Result<types::Fd> {
+    ) -> Result<types::Fd, Error> {
         let entry = self.get_entry(dirfd)?;
         let path = path.as_str()?;
         let opened_entry = entry.path_open(
@@ -314,13 +321,17 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         path: &GuestPtr<'_, str>,
         buf: &GuestPtr<u8>,
         buf_len: types::Size,
-    ) -> Result<types::Size> {
+    ) -> Result<types::Size, Error> {
         let entry = self.get_entry(dirfd)?;
         let buf = buf.as_array(buf_len);
         entry.path_readlink(path, &buf)
     }
 
-    fn path_remove_directory(&self, dirfd: types::Fd, path: &GuestPtr<'_, str>) -> Result<()> {
+    fn path_remove_directory(
+        &self,
+        dirfd: types::Fd,
+        path: &GuestPtr<'_, str>,
+    ) -> Result<(), Error> {
         let entry = self.get_entry(dirfd)?;
         let path = path.as_str()?;
         entry.path_remove_directory(path.deref())
@@ -332,7 +343,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         old_path: &GuestPtr<'_, str>,
         new_fd: types::Fd,
         new_path: &GuestPtr<'_, str>,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         let old_entry = self.get_entry(old_fd)?;
         let new_entry = self.get_entry(new_fd)?;
         old_entry.path_rename(old_path, &new_entry, new_path)
@@ -343,12 +354,12 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         old_path: &GuestPtr<'_, str>,
         dirfd: types::Fd,
         new_path: &GuestPtr<'_, str>,
-    ) -> Result<()> {
+    ) -> Result<(), Error> {
         let entry = self.get_entry(dirfd)?;
         entry.path_symlink(old_path, new_path)
     }
 
-    fn path_unlink_file(&self, dirfd: types::Fd, path: &GuestPtr<'_, str>) -> Result<()> {
+    fn path_unlink_file(&self, dirfd: types::Fd, path: &GuestPtr<'_, str>) -> Result<(), Error> {
         let entry = self.get_entry(dirfd)?;
         let path = path.as_str()?;
         entry.path_unlink_file(path.deref())
@@ -356,98 +367,48 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
 
     fn poll_oneoff(
         &self,
-        in_: &GuestPtr<types::Subscription>,
-        out: &GuestPtr<types::Event>,
+        subs: &GuestPtr<types::Subscription>,
+        events: &GuestPtr<types::Event>,
         nsubscriptions: types::Size,
-    ) -> Result<types::Size> {
-        let write_results = move |values: Vec<types::Event>| -> Result<types::Size> {
-            let nevents = values
-                .len()
-                .try_into()
-                .expect("nevents will always fit in Size");
-            let out_array = out.as_array(nevents);
-            for (event, event_ptr) in values.into_iter().zip(out_array.iter()) {
-                let event_ptr = event_ptr?;
-                event_ptr.write(event)?;
-            }
-            Ok(nevents)
-        };
-
+    ) -> Result<types::Size, Error> {
         if nsubscriptions == 0 {
             // As mandated by the WASI spec:
             // > If `nsubscriptions` is 0, returns `errno::inval`.
             return Err(Error::Inval);
         }
+        let subs = subs.as_array(nsubscriptions);
+        let events = events.as_array(nsubscriptions);
 
-        let mut poll_builder = PollBuilder::new(&self);
-        let mut error_events = Vec::new();
-
-        let subs = in_.as_array(nsubscriptions);
-        for sub_ptr in subs.iter() {
-            let sub_ptr = sub_ptr?;
-            let sub: types::Subscription = sub_ptr.read()?;
-            match sub.u {
-                types::SubscriptionU::Clock(clock) => {
-                    poll_builder.subscribe_clock(clock, sub.userdata)?;
-                }
-                types::SubscriptionU::FdRead(fd_read) => {
-                    if let Err(e) =
-                        poll_builder.subscribe_fd_read(fd_read.file_descriptor, sub.userdata)
-                    {
-                        error_events.push(types::Event {
-                            userdata: sub.userdata,
-                            error: types::Errno::from(e),
-                            type_: types::Eventtype::FdRead,
-                            fd_readwrite: types::EventFdReadwrite {
-                                nbytes: 0,
-                                flags: types::Eventrwflags::empty(),
-                            },
-                        });
-                    }
-                }
-                types::SubscriptionU::FdWrite(fd_write) => {
-                    if let Err(e) =
-                        poll_builder.subscribe_fd_write(fd_write.file_descriptor, sub.userdata)
-                    {
-                        error_events.push(types::Event {
-                            userdata: sub.userdata,
-                            error: types::Errno::from(e),
-                            type_: types::Eventtype::FdWrite,
-                            fd_readwrite: types::EventFdReadwrite {
-                                nbytes: 0,
-                                flags: types::Eventrwflags::empty(),
-                            },
-                        });
-                    }
-                }
+        let poll = {
+            let mut error_events = Vec::new();
+            let poll = poll_builder(&self, subs, &mut error_events)?;
+            if !error_events.is_empty() {
+                return poll_writer(error_events, events);
             }
-        }
+            poll
+        };
 
-        if !error_events.is_empty() {
-            return write_results(error_events);
-        }
+        // TODO actually run the poll here
 
-        let polled_events = poll_builder.poll()?;
-
-        write_results(polled_events)
+        poll_writer(poll.results(), events)
     }
 
-    fn proc_exit(&self, _rval: types::Exitcode) -> std::result::Result<(), ()> {
+    fn proc_exit(&self, _rval: types::Exitcode) -> Result<(), ()> {
         // proc_exit is special in that it's expected to unwind the stack, which
         // typically requires runtime-specific logic.
         unimplemented!("runtimes are expected to override this implementation")
     }
 
-    fn proc_raise(&self, _sig: types::Signal) -> Result<()> {
+    fn proc_raise(&self, _sig: types::Signal) -> Result<(), Error> {
         unimplemented!("proc_raise")
     }
 
-    fn sched_yield(&self) -> Result<()> {
+    fn sched_yield(&self) -> Result<(), Error> {
         std::thread::yield_now();
         Ok(())
     }
 
-    fn random_get(&self, buf: &GuestPtr<u8>, buf_len: types::Size) -> Result<()> {
+    fn random_get(&self, buf: &GuestPtr<u8>, buf_len: types::Size) -> Result<(), Error> {
         let mut slice = buf.as_array(buf_len).as_slice()?;
         getrandom::getrandom(&mut *slice)?;
         Ok(())
@@ -458,7 +419,7 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         _fd: types::Fd,
         _ri_data: &types::IovecArray<'_>,
         _ri_flags: types::Riflags,
-    ) -> Result<(types::Size, types::Roflags)> {
+    ) -> Result<(types::Size, types::Roflags), Error> {
         unimplemented!("sock_recv")
     }
 
@@ -467,11 +428,54 @@ impl<'a> WasiSnapshotPreview1 for WasiCtx {
         _fd: types::Fd,
         _si_data: &types::CiovecArray<'_>,
         _si_flags: types::Siflags,
-    ) -> Result<types::Size> {
+    ) -> Result<types::Size, Error> {
         unimplemented!("sock_send")
     }
 
-    fn sock_shutdown(&self, _fd: types::Fd, _how: types::Sdflags) -> Result<()> {
+    fn sock_shutdown(&self, _fd: types::Fd, _how: types::Sdflags) -> Result<(), Error> {
         unimplemented!("sock_shutdown")
     }
+}
+
+pub fn poll_builder<'a>(
+    ctx: &'a WasiCtx,
+    subs: GuestPtr<[types::Subscription]>,
+    // Errors get written to the events if subscriptions are invalid:
+    errors: &mut Vec<(SubscriptionResult, Userdata)>,
+) -> Result<Poll<'a>, Error> {
+    let mut poll = Poll::new(ctx);
+
+    for sub_ptr in subs.iter() {
+        let sub_ptr = sub_ptr?;
+        let sub: types::Subscription = sub_ptr.read()?;
+        match sub.u {
+            types::SubscriptionU::Clock(clock) => {
+                let r = if clock.flags == types::Subclockflags::SUBSCRIPTION_CLOCK_ABSTIME {
+                    poll.subscribe_absolute_time(clock.timeout, sub.userdata)
+                } else {
+                    poll.subscribe_relative_time(clock.timeout, sub.userdata)
+                };
+                if let Err(e) = r {
+                    errors.push((SubscriptionResult::Timer(SchedResult::Err(e)), sub.userdata));
+                }
+            }
+            types::SubscriptionU::FdRead(fd_read) => {
+                if let Err(e) = poll.subscribe_fd_read(fd_read.file_descriptor, sub.userdata) {
+                    errors.push((SubscriptionResult::Read(SchedResult::Err(e)), sub.userdata));
+                }
+            }
+            types::SubscriptionU::FdWrite(fd_write) => {
+                if let Err(e) = poll.subscribe_fd_write(fd_write.file_descriptor, sub.userdata) {
+                    errors.push((SubscriptionResult::Write(SchedResult::Err(e)), sub.userdata));
+                }
+            }
+        }
+    }
+    Ok(poll)
+}
+
+fn poll_writer(
+    results: Vec<(SubscriptionResult, Userdata)>,
+    events: GuestPtr<[types::Event]>,
+) -> Result<types::Size, Error> {
 }
