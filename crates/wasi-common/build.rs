c535b94c73b35a5b004512f0ee94c47b5f6b52d3
Pat Hickey <pat@moreproductive.org>
2021-02-04 15:38:02 -0800

    theres the bug

--- crates/wasi-common/build.rs
+++ crates/wasi-common/build.rs
@@ -2,7 +2,7 @@
 // use the same witx files if they want.
 fn main() {
     let cwd = std::env::current_dir().unwrap();
-    let wasi = cwd.join("..").join("wasi-common").join("WASI");
+    let wasi = cwd.join("WASI");
     // this will be available to dependent crates via the DEP_WASI_COMMON_19_WASI env var:
     println!("cargo:wasi={}", wasi.display());
     // and available to our own crate as WASI_ROOT:
