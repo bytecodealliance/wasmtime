c361485c9ed4e0d62db202bee7cd301f93ee3b87
Nick Fitzgerald <fitzgen@gmail.com>
2021-03-01 09:55:38 -0800

    fuzz: Remove peepmatic fuzz targets
    There are occasional timeouts in type checking where Z3 hangs. This is a known
    issue[0] with the implementation of type checking in Peepmatic, and getting
    these timeouts in the fuzzers is just annoying and adds noise to our fuzzing
    results. When we fix [0] we can reintroduce these fuzz targets.
    
    [0]: https://github.com/bytecodealliance/wasmtime/issues/2695

--- fuzz/fuzz_targets/peepmatic_parser.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use peepmatic_fuzzing::parser::parse;
-
-fuzz_target!(|data: &[u8]| {
-    parse(data);
-});
--- fuzz/fuzz_targets/peepmatic_simple_automata.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use peepmatic_fuzzing::automata::simple_automata;
-
-fuzz_target!(|input_output_pairs: Vec<Vec<(u8, Vec<u8>)>>| {
-    simple_automata(input_output_pairs);
-});
