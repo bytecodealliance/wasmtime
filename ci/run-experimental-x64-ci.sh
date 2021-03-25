58437431a69126b0d9f14a1601a2a57d6acf5eab
Alex Crichton <alex@alexcrichton.com>
2021-03-25 07:55:02 -0700

    Use stable Rust on CI to test the x64 backend
    This commit leverages the newly-released 1.51.0 compiler to test the
    new backend on Windows and Linux with a stable compiler instead of a
    nightly compiler. This isolates the nightly build to just the nightly
    documentation generation and fuzzing, both of which rely on nightly for
    the best results right now.

--- ci/run-experimental-x64-ci.sh
+++ ci/run-experimental-x64-ci.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Use the Nightly variant of the compiler to properly unify the
-# experimental_x64 feature across all crates.  Once the feature has stabilized
-# and become the default, we can remove this.
-CARGO_VERSION=${CARGO_VERSION:-"+nightly"}
-
-# Some WASI tests seem to have an issue on Windows with symlinks if we run them
-# with this particular invocation. It's unclear why (nightly toolchain?) but
-# we're moving to the new backend by default soon enough, and all tests seem to
-# work with the main test setup, so let's just work around this by skipping
-# the tests for now.
-MINGW_EXTRA=""
-if [ `uname -o` == "Msys" ]; then
-	MINGW_EXTRA="-- --skip wasi_cap_std_sync"
-fi
-
-cargo $CARGO_VERSION \
+cargo test \
             --locked \
-            -Zfeatures=all -Zpackage-features \
-            test \
             --features test-programs/test_programs \
             --features experimental_x64 \
             --all \
@@ -32,5 +15,4 @@ cargo $CARGO_VERSION \
             --exclude peepmatic-runtime \
             --exclude peepmatic-test \
             --exclude peepmatic-souper \
-            --exclude lightbeam \
-	    $MINGW_EXTRA
+            --exclude lightbeam
