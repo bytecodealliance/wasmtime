37bb7af454dc7f25a18824ec8b2da5c006aee9eb
Peter Huene <peter@huene.dev>
2021-04-02 16:12:19 -0700

    Fix incorrect range in `ininitialize_instance`.
    This commit fixes a bug where the wrong destination range was used when copying
    data from the module's memory initialization upon instance initialization.
    
    This affects the on-demand allocator only when using the `uffd` feature on
    Linux and when the Wasm page being initialized is not the last in the module's
    initial pages.
    
    Fixes #2784.

--- tests/all/instance.rs
+++ tests/all/instance.rs
@@ -11,3 +11,23 @@ fn wrong_import_numbers() -> Result<()> {
     assert!(Instance::new(&store, &module, &[func.clone().into(), func.into()]).is_err());
     Ok(())
 }
+
+#[test]
+fn initializes_linear_memory() -> Result<()> {
+    // Test for https://github.com/bytecodealliance/wasmtime/issues/2784
+    let wat = r#"
+        (module
+            (memory (export "memory") 2)
+            (data (i32.const 0) "Hello World!")
+        )"#;
+    let module = Module::new(&Engine::default(), wat)?;
+
+    let store = Store::new(module.engine());
+    let instance = Instance::new(&store, &module, &[])?;
+    let memory = instance.get_memory("memory").unwrap();
+
+    let mut bytes = [0; 12];
+    memory.read(0, &mut bytes)?;
+    assert_eq!(bytes, "Hello World!".as_bytes());
+    Ok(())
+}
