5351fed0bcb4873c03249786270acfb44d97c386
Pat Hickey <pat@moreproductive.org>
2021-04-27 14:52:21 -0700

    wasi-nn spec path: dont use canonicalize

--- crates/wasi-nn/build.rs
+++ crates/wasi-nn/build.rs
@@ -5,7 +5,8 @@ use std::path::PathBuf;
 
 fn main() {
     // This is necessary for Wiggle/Witx macros.
-    let wasi_root = PathBuf::from("./spec").canonicalize().unwrap();
+    let cwd = std::env::current_dir().unwrap();
+    let wasi_root = cwd.join("spec");
     println!("cargo:rustc-env=WASI_ROOT={}", wasi_root.display());
 
     // Also automatically rebuild if the Witx files change
