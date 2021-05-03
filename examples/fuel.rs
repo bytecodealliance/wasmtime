7d5d94a6cbb646038b3c1bd6bf8a5a05144c6aa7
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-05-03 14:54:10 -0400

    Catch error and return cleanly

--- examples/fuel.rs
+++ examples/fuel.rs
@@ -18,9 +18,14 @@ fn main() -> Result<()> {
     let fibonacci = instance.get_typed_func::<i32, i32>("fibonacci")?;
     for n in 1.. {
         let fuel_before = store.fuel_consumed().unwrap();
-        let output = fibonacci.call(n)?;
+        let output = match fibonacci.call(n) {
+            Ok(v) => v,
+            Err(_) => {
+                println!("Exhausted fuel computing fib({})", n);
+                break;
+            }
+        };
         let fuel_consumed = store.fuel_consumed().unwrap() - fuel_before;
-
         println!("fib({}) = {} [consumed {} fuel]", n, output, fuel_consumed);
         store.add_fuel(fuel_consumed)?;
     }
