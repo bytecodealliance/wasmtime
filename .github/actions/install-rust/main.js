8d83b088b855ca380ea81442055955aea8937cd0
Alex Crichton <alex@alexcrichton.com>
2021-05-20 15:24:11 -0700

    Try to fix CI
    Fixes a few issues that have been cropping up:
    
    * Update `rustup` on Windows to latest to skip over the 1.24.1 installed
      on GitHub Actions which can fail to install.
    * Remove the no-longer-needed `define-llvm-env` action
    * Install generic llvm/lldb packges instead of specific ones that may
      migrate in versions over time.

--- .github/actions/install-rust/main.js
+++ .github/actions/install-rust/main.js
@@ -6,11 +6,10 @@ function set_env(name, val) {
   fs.appendFileSync(process.env['GITHUB_ENV'], `${name}=${val}\n`)
 }
 
-if (process.platform === 'darwin') {
-  child_process.execSync(`curl https://sh.rustup.rs | sh -s -- -y --default-toolchain=none --profile=minimal`);
-  const bindir = `${process.env.HOME}/.cargo/bin`;
-  fs.appendFileSync(process.env['GITHUB_PATH'], `${bindir}\n`);
-  process.env.PATH = `${process.env.PATH}:${bindir}`;
+// Needed for now to get 1.24.2 which fixes a bug in 1.24.1 that causes issues
+// on Windows.
+if (process.platform === 'win32') {
+  child_process.execFileSync('rustup', ['self', 'update']);
 }
 
 child_process.execFileSync('rustup', ['set', 'profile', 'minimal']);
