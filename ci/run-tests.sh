a6d790f0c5014bc1bddd3c91c9b8dedf9510e193
Benjamin Bouvier <public@benj.me>
2021-06-01 16:46:31 +0200

    propagate supplementary arguments to `run-tests.sh`

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-EXTRA_FEATURE=""
-
-if [[ $1 != "" ]]
-then
-    EXTRA_FEATURE="--features $1"
-fi
-
 cargo test \
     --features "test-programs/test_programs" \
-    $EXTRA_FEATURE \
-    --locked \
     --workspace \
     --exclude '*lightbeam*' \
     --exclude 'wasmtime-wasi-*' \
     --exclude 'peepmatic*' \
-    --exclude wasi-crypto
+    --exclude wasi-crypto \
+    $@
