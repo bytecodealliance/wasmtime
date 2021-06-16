5140fd251a10df1f2d212ad4c0012631dd9b4906
Alex Crichton <alex@alexcrichton.com>
2021-06-15 22:56:10 -0500

    Update wasm-tools crates (#2989)
    * Update wasm-tools crates
    
    This brings in recent updates, notably including more improvements to
    wasm-smith which will hopefully help exercise non-trapping wasm more.
    
    * Fix some wat

--- crates/wiggle/tests/wasmtime_async.rs
+++ crates/wiggle/tests/wasmtime_async.rs
@@ -136,10 +136,11 @@ fn shim_module(engine: &Engine) -> Module {
         engine,
         r#"
         (module
-            (memory 1)
-            (export "memory" (memory 0))
             (import "atoms" "int_float_args" (func $int_float_args (param i32 f32) (result i32)))
             (import "atoms" "double_int_return_float" (func $double_int_return_float (param i32 i32) (result i32)))
+
+            (memory 1)
+            (export "memory" (memory 0))
 
             (func $int_float_args_shim (param i32 f32) (result i32)
                 local.get 0
@@ -154,7 +155,7 @@ fn shim_module(engine: &Engine) -> Module {
             (export "int_float_args_shim" (func $int_float_args_shim))
             (export "double_int_return_float_shim" (func $double_int_return_float_shim))
         )
-    "#,
+        "#,
     )
     .unwrap()
 }
--- crates/wiggle/tests/wasmtime_integration.rs
+++ crates/wiggle/tests/wasmtime_integration.rs
@@ -110,10 +110,11 @@ fn shim_module(engine: &Engine) -> Module {
         engine,
         r#"
         (module
-            (memory 1)
-            (export "memory" (memory 0))
             (import "atoms" "int_float_args" (func $int_float_args (param i32 f32) (result i32)))
             (import "atoms" "double_int_return_float" (func $double_int_return_float (param i32 i32) (result i32)))
+
+            (memory 1)
+            (export "memory" (memory 0))
 
             (func $int_float_args_shim (param i32 f32) (result i32)
                 local.get 0
