273cb8953831ce0c9fdd4ac82ef86a5d8fe69673
Martin Kolb <martin.kolb@sap.com>
2021-06-22 17:55:15 +0200

    Explain uasge of SONAME

--- crates/c-api/build.rs
+++ crates/c-api/build.rs
@@ -1,5 +1,11 @@
 fn main() {
-    // Set SONAME in shared library on Linux
+    // Set SONAME in shared library on Linux.
+    // 
+    // A missing SONAME hinders full C-ABI compatibility.
+    // Libraries without SONAME can produce unwanted NEEDED entries in 
+    // executables linked to this shared library.
+    // Libraries without SONAME need special treatment in the CMake
+    // build system (i.e. IMPORTED_NO_SONAME).
     if cfg!(target_os = "linux") {
         println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,libwasmtime.so");
     }
