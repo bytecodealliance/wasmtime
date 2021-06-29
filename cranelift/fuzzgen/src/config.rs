c808f09f88b7e78c95f0c36c3c9ca283cd3dda7c
Afonso Bordado <afonsobordado@az8.co>
2021-06-29 16:59:10 +0100

    cranelift: Generate variables upfront per function

--- cranelift/fuzzgen/src/config.rs
+++ cranelift/fuzzgen/src/config.rs
@@ -6,15 +6,19 @@ pub struct Config {
     pub signature_params: RangeInclusive<usize>,
     pub signature_rets: RangeInclusive<usize>,
     pub instructions_per_block: RangeInclusive<usize>,
+    /// Number of variables that we allocate per function
+    /// This value does not include the signature params
+    pub vars_per_function: RangeInclusive<usize>,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
             test_case_inputs: 1..=10,
-            signature_params: 1..=8,
-            signature_rets: 1..=8,
-            instructions_per_block: 1..=16,
+            signature_params: 0..=16,
+            signature_rets: 0..=16,
+            instructions_per_block: 0..=64,
+            vars_per_function: 0..=16,
         }
     }
 }
--- cranelift/fuzzgen/src/function_generator.rs
+++ cranelift/fuzzgen/src/function_generator.rs
@@ -2,7 +2,7 @@ use crate::config::Config;
 use anyhow::Result;
 use arbitrary::Unstructured;
 use cranelift::codegen::ir::types::*;
-use cranelift::codegen::ir::{AbiParam, ExternalName, Function, Opcode, Signature, Type};
+use cranelift::codegen::ir::{AbiParam, ExternalName, Function, Opcode, Signature, Type, Value};
 use cranelift::codegen::isa::CallConv;
 use cranelift::frontend::{FunctionBuilder, FunctionBuilderContext, Variable};
 use cranelift::prelude::{EntityRef, InstBuilder};
@@ -25,19 +25,20 @@ fn insert_opcode_arity_2(
     args: &'static [Type],
     rets: &'static [Type],
 ) -> Result<()> {
-    let arg0 = fgen.get_variable_of_type(builder, args[0])?;
+    let arg0 = fgen.get_variable_of_type(args[0])?;
     let arg0 = builder.use_var(arg0);
 
-    let arg1 = fgen.get_variable_of_type(builder, args[1])?;
+    let arg1 = fgen.get_variable_of_type(args[1])?;
     let arg1 = builder.use_var(arg1);
 
     let typevar = rets[0];
     let (inst, dfg) = builder.ins().Binary(opcode, typevar, arg0, arg1);
+    let results = dfg.inst_results(inst).to_vec();
 
-    let val = dfg.first_result(inst);
-    let var = fgen.create_var(builder, typevar)?;
-    builder.def_var(var, val);
-
+    for (val, ty) in results.into_iter().zip(rets) {
+        let var = fgen.get_variable_of_type(*ty)?;
+        builder.def_var(var, val);
+    }
     Ok(())
 }
 
@@ -164,50 +165,25 @@ where
             .collect()
     }
 
-    /// Get a variable of type `ty`, either reusing a old var, or generating a new one with a
-    /// `iconst`/`fconst`.
-    fn get_variable_of_type(
-        &mut self,
-        builder: &mut FunctionBuilder,
-        ty: Type,
-    ) -> Result<Variable> {
-        // TODO: global vars
+    /// Get a variable of type `ty` from the current function
+    fn get_variable_of_type(&mut self, ty: Type) -> Result<Variable> {
+        let opts = self.vars_of_type(ty);
+        let var = self.u.choose(&opts[..])?;
+        Ok(*var)
+    }
 
-        let mut opts: Vec<
-            fn(
-                fgen: &mut FunctionGenerator,
-                builder: &mut FunctionBuilder,
-                ty: Type,
-            ) -> Result<Variable>,
-        > = Vec::with_capacity(2);
+    /// Generates an instruction(`iconst`/`fconst`/etc...) to introduce a constant value
+    fn generate_const(&mut self, builder: &mut FunctionBuilder, ty: Type) -> Result<Value> {
+        let imm64 = match ty {
+            I8 => self.u.arbitrary::<i8>()? as i64,
+            I16 => self.u.arbitrary::<i16>()? as i64,
+            I32 => self.u.arbitrary::<i32>()? as i64,
+            I64 => self.u.arbitrary::<i64>()?,
+            _ => unreachable!(),
+        };
+        let val = builder.ins().iconst(ty, imm64);
 
-        // Generate new
-        opts.push(|fgen, builder, ty| {
-            let imm64 = match ty {
-                I8 => fgen.u.arbitrary::<i8>()? as i64,
-                I16 => fgen.u.arbitrary::<i16>()? as i64,
-                I32 => fgen.u.arbitrary::<i32>()? as i64,
-                I64 => fgen.u.arbitrary::<i64>()?,
-                _ => unreachable!(),
-            };
-            let var = fgen.create_var(builder, ty)?;
-            let val = builder.ins().iconst(ty, imm64);
-            builder.def_var(var, val);
-
-            Ok(var)
-        });
-
-        // Reuse var
-        if self.vars_of_type(ty).len() != 0 {
-            opts.push(|fg, _, ty| {
-                let opts = fg.vars_of_type(ty);
-                let var = fg.u.choose(&opts[..])?;
-                Ok(*var)
-            });
-        }
-
-        let f = self.u.choose(&opts[..])?;
-        f(self, builder, ty)
+        Ok(val)
     }
 
     fn generate_return(&mut self, builder: &mut FunctionBuilder) -> Result<()> {
@@ -215,7 +191,7 @@ where
 
         let vars = ret_params
             .iter()
-            .map(|p| self.get_variable_of_type(builder, p.value_type))
+            .map(|p| self.get_variable_of_type(p.value_type))
             .collect::<Result<Vec<_>>>()?;
 
         let vals = vars
@@ -241,21 +217,23 @@ where
 
         let mut builder = FunctionBuilder::new(&mut func, &mut fn_builder_ctx);
         let block0 = builder.create_block();
-
-        let mut block_vars = vec![];
-        for param in &sig.params {
-            let var = self.create_var(&mut builder, param.value_type)?;
-            block_vars.push(var);
-        }
         builder.append_block_params_for_function_params(block0);
         builder.switch_to_block(block0);
         builder.seal_block(block0);
 
-        for (i, _) in sig.params.iter().enumerate() {
-            let var = block_vars[i];
+        // Define variables for the function signature
+        for (i, param) in sig.params.iter().enumerate() {
+            let var = self.create_var(&mut builder, param.value_type)?;
             let block_param = builder.block_params(block0)[i];
             builder.def_var(var, block_param);
-            let _ = builder.use_var(block_vars[i]);
+        }
+
+        // Create a pool of vars that are going to be used in this function
+        for _ in 0..self.u.int_in_range(self.config.vars_per_function.clone())? {
+            let ty = self.generate_type()?;
+            let var = self.create_var(&mut builder, ty)?;
+            let value = self.generate_const(&mut builder, ty)?;
+            builder.def_var(var, value);
         }
 
         for _ in 0..self
