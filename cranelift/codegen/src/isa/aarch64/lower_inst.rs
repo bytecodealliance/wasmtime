d8e813204e32ba32c830ce551bc9837ea048c45b
Johnnie Birch <johnnie.l.birch.jr@intel.com>
2021-07-08 07:15:54 -0700

    Fold fcvt_low_from_uinit into previously existing clif instructions

--- cranelift/codegen/src/isa/aarch64/lower_inst.rs
+++ cranelift/codegen/src/isa/aarch64/lower_inst.rs
@@ -3557,7 +3557,6 @@ pub(crate) fn lower_insn_to_regs<C: LowerCtx<I = Inst>>(
 
         Opcode::ConstAddr
         | Opcode::FcvtLowFromSint
-        | Opcode::FcvtLowFromUint
         | Opcode::Fvdemote
         | Opcode::FvpromoteLow
         | Opcode::Vconcat
