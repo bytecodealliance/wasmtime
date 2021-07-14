8f57963790067a12efe4f419b7ff6d61c3366a24
Dan Gohman <dev@sunfishcode.online>
2021-07-14 14:41:32 -0700

    Clean up a few more unneeded `unsafe` blocks.

--- crates/wasi-common/cap-std-sync/src/clocks.rs
+++ crates/wasi-common/cap-std-sync/src/clocks.rs
@@ -1,13 +1,13 @@
-use cap_std::ambient_authority;
 use cap_std::time::{Duration, Instant, SystemTime};
+use cap_std::{ambient_authority, AmbientAuthority};
 use cap_time_ext::{MonotonicClockExt, SystemClockExt};
 use wasi_common::clocks::{WasiClocks, WasiMonotonicClock, WasiSystemClock};
 
 pub struct SystemClock(cap_std::time::SystemClock);
 
 impl SystemClock {
-    pub unsafe fn new() -> Self {
-        SystemClock(cap_std::time::SystemClock::new(ambient_authority()))
+    pub fn new(ambient_authority: AmbientAuthority) -> Self {
+        SystemClock(cap_std::time::SystemClock::new(ambient_authority))
     }
 }
 impl WasiSystemClock for SystemClock {
@@ -21,8 +21,8 @@ impl WasiSystemClock for SystemClock {
 
 pub struct MonotonicClock(cap_std::time::MonotonicClock);
 impl MonotonicClock {
-    pub unsafe fn new() -> Self {
-        MonotonicClock(cap_std::time::MonotonicClock::new(ambient_authority()))
+    pub fn new(ambient_authority: AmbientAuthority) -> Self {
+        MonotonicClock(cap_std::time::MonotonicClock::new(ambient_authority))
     }
 }
 impl WasiMonotonicClock for MonotonicClock {
@@ -35,7 +35,7 @@ impl WasiMonotonicClock for MonotonicClock {
 }
 
 pub fn clocks_ctx() -> WasiClocks {
-    let system = Box::new(unsafe { SystemClock::new() });
+    let system = Box::new(SystemClock::new(ambient_authority()));
     let monotonic = cap_std::time::MonotonicClock::new(ambient_authority());
     let creation_time = monotonic.now();
     let monotonic = Box::new(MonotonicClock(monotonic));
