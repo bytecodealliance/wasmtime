08e7b5d0ecd3ed529ca2c70079ad17ea336b4921
Andrew Brown <andrew.brown@intel.com>
2021-07-28 13:19:09 -0700

    refactor: rename `DifferentialWasmiModuleConfig` to `SingleFunctionModuleConfig`
    Since we plan to reuse this configuration, we rename it and ensure it
    has at least 1 type (this resulted in invalid modules).

--- fuzz/fuzz_targets/differential_wasmi.rs
+++ fuzz/fuzz_targets/differential_wasmi.rs
@@ -5,7 +5,7 @@ use wasmtime_fuzzing::{generators, oracles};
 
 fuzz_target!(|data: (
     generators::Config,
-    wasm_smith::ConfiguredModule<oracles::DifferentialWasmiModuleConfig>
+    wasm_smith::ConfiguredModule<oracles::SingleFunctionModuleConfig>
 )| {
     let (config, mut wasm) = data;
     wasm.ensure_termination(1000);
