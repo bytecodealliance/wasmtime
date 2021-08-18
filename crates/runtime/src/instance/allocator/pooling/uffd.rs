121f5da3070d24fe2cb16d62ab1ee2173f3408af
Alex Crichton <alex@alexcrichton.com>
2021-08-18 07:02:34 -0700

    Fix more uffd tests

--- crates/runtime/src/instance/allocator/pooling/uffd.rs
+++ crates/runtime/src/instance/allocator/pooling/uffd.rs
@@ -441,9 +441,7 @@ mod test {
         PoolingAllocationStrategy, VMSharedSignatureIndex,
     };
     use std::sync::Arc;
-    use wasmtime_environ::{
-        entity::PrimaryMap, wasm::Memory, MemoryPlan, MemoryStyle, Module, Tunables,
-    };
+    use wasmtime_environ::{Memory, MemoryPlan, MemoryStyle, Module, PrimaryMap, Tunables};
 
     #[cfg(target_pointer_width = "64")]
     #[test]
