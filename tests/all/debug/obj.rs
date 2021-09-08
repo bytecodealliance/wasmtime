44ce44c1431354e9141d4cc4969e5de8fd81c759
Alex Crichton <alex@alexcrichton.com>
2021-09-08 08:08:01 -0700

    Try to fix debuginfo tests

--- tests/all/debug/obj.rs
+++ tests/all/debug/obj.rs
@@ -3,25 +3,24 @@ use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use target_lexicon::Triple;
-use wasmtime::Strategy;
-use wasmtime_cli::compile_to_obj;
+use wasmtime::{Config, Engine, Module};
 
 pub fn compile_cranelift(
     wasm: &[u8],
     target: Option<Triple>,
     output: impl AsRef<Path>,
 ) -> Result<()> {
-    let obj = compile_to_obj(
-        wasm,
-        target.as_ref(),
-        Strategy::Cranelift,
-        false,
-        wasmtime::OptLevel::None,
-        true,
-    )?;
+    let mut config = Config::new();
+    config.debug_info(true);
+    if let Some(target) = target {
+        config.target(&target.to_string())?;
+    }
+    let engine = Engine::new(&config)?;
+    let module = Module::new(&engine, wasm)?;
+    let bytes = module.serialize()?;
 
     let mut file = File::create(output).context("failed to create object file")?;
-    file.write_all(&obj)
+    file.write_all(&bytes)
         .context("failed to write object file")?;
 
     Ok(())
