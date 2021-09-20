fc6328ae069f091a8cd53702d1f9876045d564e6
Alex Crichton <alex@alexcrichton.com>
2021-09-20 14:17:19 -0500

    Temporarily disable SIMD fuzzing on CI (#3376)
    We've got a large crop of fuzz-bugs from fuzzing with enabled-with-SIMD
    on oss-fuzz but at this point the fuzz stats from oss-fuzz say that the
    fuzzers like v8 are spending less than 50% of its time actually fuzzing
    and presumably mostly hitting crashes and such. While we fix the other
    issues this disables simd for fuzzing with v8 so we can try to see if we
    can weed out other issues.

--- fuzz/fuzz_targets/differential_v8.rs
+++ fuzz/fuzz_targets/differential_v8.rs
@@ -5,7 +5,7 @@ use wasmtime_fuzzing::{generators, oracles};
 
 fuzz_target!(|data: (
     generators::Config,
-    wasm_smith::ConfiguredModule<oracles::SingleFunctionModuleConfig<true, true>>
+    wasm_smith::ConfiguredModule<oracles::SingleFunctionModuleConfig<false, true>>
 )| {
     let (config, mut wasm) = data;
     wasm.module.ensure_termination(1000);
