972dc00a9221fffdf77a7109ec4f990b3465a6c1
Nick Fitzgerald <fitzgen@gmail.com>
2021-09-27 15:57:32 -0700

    Don't use `&mut dyn Write` in codegen
    We always use a `String` which has infallible writing so we can remove all these
    `Result`s and pretend-fallible methods.

--- cranelift/isle/isle/src/compile.rs
+++ cranelift/isle/isle/src/compile.rs
@@ -6,6 +6,6 @@ use crate::{ast, codegen, sema};
 pub fn compile(defs: &ast::Defs) -> Result<String, Vec<Error>> {
     let mut typeenv = sema::TypeEnv::from_ast(defs)?;
     let termenv = sema::TermEnv::from_ast(&mut typeenv, defs)?;
-    let codegen = codegen::Codegen::compile(&typeenv, &termenv).map_err(|e| vec![e])?;
-    codegen.generate_rust().map_err(|e| vec![e])
+    let codegen = codegen::Codegen::compile(&typeenv, &termenv);
+    Ok(codegen.generate_rust())
 }
