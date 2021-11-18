4c75616a7cebf01cd26f7b0f32ac52aa2493576c
bjorn3 <bjorn3@users.noreply.github.com>
2021-11-18 18:51:35 +0100

    Remove unused constants from cranelift-codegen-shared (#3479)

--- cranelift/codegen/shared/src/constants.rs
+++ cranelift/codegen/shared/src/constants.rs
@@ -20,11 +20,3 @@ pub const REFERENCE_BASE: u8 = 0x7E;
 
 /// Start of the 2-lane vector types.
 pub const VECTOR_BASE: u8 = 0x80;
-
-// Some constants about register classes and types.
-
-/// Guaranteed maximum number of top-level register classes with pressure tracking in any ISA.
-pub const MAX_TRACKED_TOP_RCS: usize = 4;
-
-/// Guaranteed maximum number of register classes in any ISA.
-pub const MAX_NUM_REG_CLASSES: usize = 32;
