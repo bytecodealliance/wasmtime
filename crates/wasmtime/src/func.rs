6e0f7329936f7040808aa750ae82bd74a15aef26
Nick Fitzgerald <fitzgen@gmail.com>
2022-01-06 10:46:09 -0800

    Fix warning about deprecated `std::array::IntoIter`

--- crates/wasmtime/src/func.rs
+++ crates/wasmtime/src/func.rs
@@ -1459,7 +1459,7 @@ macro_rules! impl_wasm_host_results {
             fn func_type(params: impl Iterator<Item = ValType>) -> FuncType {
                 FuncType::new(
                     params,
-                    std::array::IntoIter::new([$($t::valtype(),)*]),
+                    IntoIterator::into_iter([$($t::valtype(),)*]),
                 )
             }
 
