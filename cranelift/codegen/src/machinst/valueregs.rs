b9928baa8705dcd43aee829e56a8b568541ec367
Chris Fallin <chris@cfallin.org>
2022-02-14 10:46:07 -0800

    Remove incomplete/unmaintained ARM32 backend (for now).
    In #3721, we have been discussing what to do about the ARM32 backend in
    Cranelift. Currently, this backend supports only 32-bit types, which is
    insufficient for full Wasm-MVP; it's missing other critical bits, like
    floating-point support; and it has only ever been exercised, AFAIK, via
    the filetests for the individual CLIF instructions that are implemented.
    
    We were very very thankful for the original contribution of this
    backend, even in its partial state, and we had hoped at the time that we
    could eventually mature it in-tree until it supported e.g. Wasm and
    other use-cases. But that hasn't yet happened -- to the blame of no-one,
    to be clear, we just haven't had a contributor with sufficient time.
    
    Unfortunately, the existence of the backend and lack of active
    maintainer now potentially pose a bit of a burden as we hope to make
    continuing changes to the backend framework. For example, the ISLE
    migration, and the use of regalloc2 that it will allow, would need all
    of the existing lowering patterns in the hand-written ARM32 backend to
    be rewritten as ISLE rules.
    
    Given that we don't currently have the resources to do this, we think
    it's probably best if we, sadly, for now remove this partial backend.
    This is not in any way a statement of what we might accept in the
    future, though. If, in the future, an ARM32 backend updated to our
    latest codebase with an active maintainer were to appear, we'd be happy
    to merge it (and likewise for any other architecture!). But for now,
    this is probably the best path. Thanks again to the original contributor
    @jmkrauz and we hope that this work can eventually be brought back and
    reused if someone has the time to do so!

--- cranelift/codegen/src/machinst/valueregs.rs
+++ cranelift/codegen/src/machinst/valueregs.rs
@@ -4,10 +4,6 @@
 use regalloc::{RealReg, Reg, VirtualReg, Writable};
 use std::fmt::Debug;
 
-#[cfg(feature = "arm32")]
-const VALUE_REGS_PARTS: usize = 4;
-
-#[cfg(not(feature = "arm32"))]
 const VALUE_REGS_PARTS: usize = 2;
 
 /// Location at which a `Value` is stored in register(s): the value is located
@@ -90,59 +86,6 @@ impl<R: Clone + Copy + Debug + PartialEq + Eq + InvalidSentinel> ValueRegs<R> {
     }
 }
 
-#[cfg(feature = "arm32")]
-impl<R: Clone + Copy + Debug + PartialEq + Eq + InvalidSentinel> ValueRegs<R> {
-    /// Create a Value-in-R location for a value stored in one register.
-    pub fn one(reg: R) -> Self {
-        ValueRegs {
-            parts: [
-                reg,
-                R::invalid_sentinel(),
-                R::invalid_sentinel(),
-                R::invalid_sentinel(),
-            ],
-        }
-    }
-    /// Create a Value-in-R location for a value stored in two registers.
-    pub fn two(r1: R, r2: R) -> Self {
-        ValueRegs {
-            parts: [r1, r2, R::invalid_sentinel(), R::invalid_sentinel()],
-        }
-    }
-    /// Create a Value-in-R location for a value stored in four registers.
-    pub fn four(r1: R, r2: R, r3: R, r4: R) -> Self {
-        ValueRegs {
-            parts: [r1, r2, r3, r4],
-        }
-    }
-
-    /// Return the number of registers used.
-    pub fn len(self) -> usize {
-        // If rustc/LLVM is smart enough, this might even be vectorized...
-        (self.parts[0] != R::invalid_sentinel()) as usize
-            + (self.parts[1] != R::invalid_sentinel()) as usize
-            + (self.parts[2] != R::invalid_sentinel()) as usize
-            + (self.parts[3] != R::invalid_sentinel()) as usize
-    }
-
-    /// Map individual registers via a map function.
-    pub fn map<NewR, F>(self, f: F) -> ValueRegs<NewR>
-    where
-        NewR: Clone + Copy + Debug + PartialEq + Eq + InvalidSentinel,
-        F: Fn(R) -> NewR,
-    {
-        ValueRegs {
-            parts: [
-                f(self.parts[0]),
-                f(self.parts[1]),
-                f(self.parts[2]),
-                f(self.parts[3]),
-            ],
-        }
-    }
-}
-
-#[cfg(not(feature = "arm32"))]
 impl<R: Clone + Copy + Debug + PartialEq + Eq + InvalidSentinel> ValueRegs<R> {
     /// Create a Value-in-R location for a value stored in one register.
     pub fn one(reg: R) -> Self {
