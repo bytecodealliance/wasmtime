08dcdd1cb4dec2e582a668b329d28cc982777fbf
Alex Crichton <alex@alexcrichton.com>
2022-03-03 07:14:44 -0800

    fuzz: Increase table limit in differential_v8 fuzzer
    When manually increasing the table limit in this specific fuzzer we also
    need to increase the limit in the pooling allocator itself if
    configured.

--- fuzz/fuzz_targets/differential_v8.rs
+++ fuzz/fuzz_targets/differential_v8.rs
@@ -2,6 +2,7 @@
 
 use libfuzzer_sys::arbitrary::{Result, Unstructured};
 use libfuzzer_sys::fuzz_target;
+use wasmtime_fuzzing::generators::InstanceAllocationStrategy;
 use wasmtime_fuzzing::{generators, oracles};
 
 fuzz_target!(|data: &[u8]| {
@@ -23,6 +24,13 @@ fn run(data: &[u8]) -> Result<()> {
     // Allow multiple tables, as set_differential_config() assumes reference
     // types are disabled and therefore sets max_tables to 1
     config.module_config.config.max_tables = 4;
+    if let InstanceAllocationStrategy::Pooling {
+        instance_limits: limits,
+        ..
+    } = &mut config.wasmtime.strategy
+    {
+        limits.tables = 4;
+    }
 
     let module = config.generate(&mut u, Some(1000))?;
     oracles::differential_v8_execution(&module.to_bytes(), &config);
