bytecodealliance/wasmtime

commit 5fe06f7345481eae57c8b8a9c37813a6e6b75d28

Update to clap 3.* (#4082)
* Update to clap 3.0

This commit migrates all CLI commands internally used in this project
from structopt/clap2 to clap 3. The intent here is to ensure that we're
using maintained versions of the dependencies as structopt and clap 2
are less maintained nowadays. Most transitions were pretty
straightforward and mostly dealing with structopt/clap3 differences.

* Fix a number of `cargo deny` errors

This commit fixes a few errors around duplicate dependencies which
arose from the prior update to clap3. This also uses a new feature in
`deny.toml`, `skip-tree`, which allows having a bit more targeted
ignores for skips of duplicate version checks. This showed a few more
locations in Wasmtime itself where we could update some dependencies.

src/bin/wasmtime.rs
@@ -4,20 +4,15 @@
 //! See `wasmtime --help` for usage.
 
 use anyhow::Result;
-use structopt::{clap::AppSettings, clap::ErrorKind, StructOpt};
+use clap::{ErrorKind, Parser};
 use wasmtime_cli::commands::{
     CompileCommand, ConfigCommand, RunCommand, SettingsCommand, WastCommand,
 };
 
 /// Wasmtime WebAssembly Runtime
-#[derive(StructOpt)]
-#[structopt(
-    name = "wasmtime",
-    version = env!("CARGO_PKG_VERSION"),
-    global_settings = &[
-        AppSettings::VersionlessSubcommands,
-        AppSettings::ColoredHelp
-    ],
+#[derive(Parser)]
+#[clap(
+    version,
     after_help = "If a subcommand is not provided, the `run` subcommand will be used.\n\
                   \n\
                   Usage examples:\n\
@@ -34,7 +29,7 @@ use wasmtime_cli::commands::{
                   \n  \
                   wasmtime example.wasm --invoke add 1 2\n"
 )]
-enum WasmtimeApp {
+enum Wasmtime {
     // !!! IMPORTANT: if subcommands are added or removed, update `parse_module` in `src/commands/run.rs`. !!!
     /// Controls Wasmtime configuration settings
     Config(ConfigCommand),
@@ -48,7 +43,7 @@ enum WasmtimeApp {
     Wast(WastCommand),
 }
 
-impl WasmtimeApp {
+impl Wasmtime {
     /// Executes the command.
     pub fn execute(self) -> Result<()> {
         match self {
@@ -62,14 +57,13 @@ impl WasmtimeApp {
 }
 
 fn main() -> Result<()> {
-    WasmtimeApp::from_iter_safe(std::env::args())
-        .unwrap_or_else(|e| match e.kind {
-            ErrorKind::HelpDisplayed
-            | ErrorKind::VersionDisplayed
-            | ErrorKind::MissingArgumentOrSubcommand => e.exit(),
-            _ => WasmtimeApp::Run(
-                RunCommand::from_iter_safe(std::env::args()).unwrap_or_else(|_| e.exit()),
-            ),
+    Wasmtime::try_parse()
+        .unwrap_or_else(|e| match e.kind() {
+            ErrorKind::DisplayHelp
+            | ErrorKind::DisplayVersion
+            | ErrorKind::MissingSubcommand
+            | ErrorKind::MissingRequiredArgument => e.exit(),
+            _ => Wasmtime::Run(RunCommand::parse()),
         })
         .execute()
 }

Cargo.toml
@@ -29,7 +29,7 @@ wasmtime-wast = { path = "crates/wast", version = "=0.37.0" }
 wasmtime-wasi = { path = "crates/wasi", version = "0.37.0" }
 wasmtime-wasi-crypto = { path = "crates/wasi-crypto", version = "0.37.0", optional = true }
 wasmtime-wasi-nn = { path = "crates/wasi-nn", version = "0.37.0", optional = true }
-structopt = { version = "0.3.5", features = ["color", "suggestions"] }
+clap = { version = "3.1.12", features = ["color", "suggestions", "derive"] }
 anyhow = "1.0.19"
 target-lexicon = { version = "0.12.0", default-features = false }
 pretty_env_logger = "0.4.0"
@@ -47,7 +47,7 @@ rustix = "0.33.7"
 [dev-dependencies]
 # depend again on wasmtime to activate its default features for tests
 wasmtime = { path = "crates/wasmtime", version = "0.37.0" }
-env_logger = "0.8.1"
+env_logger = "0.9.0"
 filecheck = "0.5.0"
 more-asserts = "0.2.1"
 tempfile = "3.1.0"
@@ -55,7 +55,7 @@ test-programs = { path = "crates/test-programs" }
 wasmtime-runtime = { path = "crates/runtime" }
 tokio = { version = "1.8.0", features = ["rt", "time", "macros", "rt-multi-thread"] }
 tracing-subscriber = "0.3.1"
-wast = "39.0.0"
+wast = "40.0.0"
 criterion = "0.3.4"
 num_cpus = "1.13.0"
 winapi = { version = "0.3.9", features = ['memoryapi'] }

cranelift/Cargo.toml
@@ -41,7 +41,7 @@ indicatif = "0.13.0"
 thiserror = "1.0.15"
 walkdir = "2.2"
 anyhow = "1.0.32"
-structopt = "0.3.17"
+clap = { version = "3.1.12", features = ["derive"] }
 
 [features]
 default = ["disas", "wasm", "cranelift-codegen/all-arch", "souper-harvest"]

cranelift/isle/islec/Cargo.toml
@@ -8,6 +8,6 @@ publish = false
 
 [dependencies]
 cranelift-isle = { version = "*", path = "../isle/" }
-env_logger = { version = "0.8", default-features = false }
+env_logger = { version = "0.9", default-features = false }
 miette = { version = "3.0.0", features = ["fancy"] }
-structopt = "0.3.23"
+clap = { version = "3.1.12", features = ["derive"] }

cranelift/isle/islec/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use cranelift_isle::{compile, lexer, parser};
 use miette::{Context, IntoDiagnostic, Result};
 use std::{
@@ -5,17 +6,16 @@ use std::{
     io::{self, Write},
     path::PathBuf,
 };
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 struct Opts {
     /// The output file to write the generated Rust code to. `stdout` is used if
     /// this is not given.
-    #[structopt(short, long, parse(from_os_str))]
+    #[clap(short, long)]
     output: Option<PathBuf>,
 
     /// The input ISLE DSL source files.
-    #[structopt(parse(from_os_str), required(true))]
+    #[clap(required = true)]
     inputs: Vec<PathBuf>,
 }
 
@@ -32,7 +32,7 @@ fn main() -> Result<()> {
         )
     }));
 
-    let opts = Opts::from_args();
+    let opts = Opts::parse();
 
     let lexer = lexer::Lexer::from_files(opts.inputs)?;
     let defs = parser::parse(lexer)?;

cranelift/serde/Cargo.toml
@@ -14,7 +14,7 @@ name = "clif-json"
 path = "src/clif-json.rs"
 
 [dependencies]
-clap = "2.32.0"
+clap = { version = "3.1.12", features = ["derive"] }
 serde_json = "1.0.26"
 cranelift-codegen = { path = "../codegen", version = "0.84.0", features = ["enable-serde"] }
 cranelift-reader = { path = "../reader", version = "0.84.0" }

cranelift/serde/src/clif-json.rs
@@ -21,7 +21,7 @@
     )
 )]
 
-use clap::{App, Arg, SubCommand};
+use clap::Parser;
 use cranelift_codegen::ir::Function;
 use cranelift_reader::parse_functions;
 use std::fs::File;
@@ -54,50 +54,40 @@ fn call_de(file: &File) -> Result<(), String> {
     Ok(())
 }
 
-fn main() {
-    let matches = App::new("Cranelift JSON serializer/deserializer utility")
-        .subcommand(
-            SubCommand::with_name("serialize")
-                .display_order(1)
-                .about("Serializes Cranelift IR into JSON.")
-                .arg(Arg::with_name("pretty").short("p").help("pretty json"))
-                .arg(
-                    Arg::with_name("FILE")
-                        .required(true)
-                        .value_name("FILE")
-                        .help("Input file for serialization"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("deserialize")
-                .about("Deserializes Cranelift IR into JSON.")
-                .arg(
-                    Arg::with_name("FILE")
-                        .required(true)
-                        .value_name("FILE")
-                        .help("Input file for deserialization"),
-                ),
-        )
-        .get_matches();
+/// Cranelift JSON serializer/deserializer utility
+#[derive(Parser, Debug)]
+#[clap(about)]
+enum Args {
+    /// Serializes Cranelift IR into JSON
+    Serialize {
+        /// Generate pretty json
+        #[clap(long, short)]
+        pretty: bool,
 
-    let res_serde = match matches.subcommand() {
-        ("serialize", Some(m)) => {
-            let mut file =
-                File::open(m.value_of("FILE").unwrap()).expect("Unable to open the file");
+        /// Input file for serialization
+        file: String,
+    },
+    /// Deserializes Cranelift IR into JSON
+    Deserialize {
+        /// Input file for deserialization
+        file: String,
+    },
+}
+
+fn main() {
+    let res_serde = match Args::parse() {
+        Args::Serialize { pretty, file } => {
             let mut contents = String::new();
+            let mut file = File::open(file).expect("Unable to open the file");
             file.read_to_string(&mut contents)
                 .expect("Unable to read the file");
 
-            match m.occurrences_of("pretty") {
-                0 => call_ser(&contents, false),
-                _ => call_ser(&contents, true),
-            }
+            call_ser(&contents, pretty)
         }
-        ("deserialize", Some(m)) => {
-            let file = File::open(m.value_of("FILE").unwrap()).expect("Unable to open the file");
+        Args::Deserialize { file } => {
+            let file = File::open(file).expect("Unable to open the file");
             call_de(&file)
         }
-        _ => Err("Invalid subcommand.".to_string()),
     };
 
     if let Err(mut msg) = res_serde {

cranelift/src/bugpoint.rs
@@ -2,6 +2,7 @@
 
 use crate::utils::{parse_sets_and_triple, read_to_string};
 use anyhow::{Context as _, Result};
+use clap::Parser;
 use cranelift_codegen::cursor::{Cursor, FuncCursor};
 use cranelift_codegen::flowgraph::ControlFlowGraph;
 use cranelift_codegen::ir::types::{F32, F64};
@@ -16,24 +17,22 @@ use cranelift_reader::{parse_test, ParseOptions};
 use indicatif::{ProgressBar, ProgressDrawTarget, ProgressStyle};
 use std::collections::HashMap;
 use std::path::PathBuf;
-use structopt::StructOpt;
 
 /// Reduce size of clif file causing panic during compilation.
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct Options {
     /// Specify an input file to be used. Use '-' for stdin.
-    #[structopt(parse(from_os_str))]
     file: PathBuf,
 
     /// Configure Cranelift settings
-    #[structopt(long("set"))]
+    #[clap(long = "set")]
     settings: Vec<String>,
 
     /// Specify the target architecture.
     target: String,
 
     /// Be more verbose
-    #[structopt(short = "v", long = "verbose")]
+    #[clap(short, long)]
     verbose: bool,
 }
 

commit 47cd2312a3be1c2416b41e2225a3fe1ed0053411

Tweak CLI fallback to the `run` command
I ran across a case in Wasmtime today where a poor error message came
out of the CLI. For example before this commit you would get:

    $ cargo run wast --wasm-features component-model foo.wast
    error: Invalid value "wast" for '<MODULE>': module name cannot be the same as a subcommand

and now after this commit you get:

    $ cargo run wast --wasm-features component-model foo.wast
    error: Invalid value "component-model" for '--wasm-features <FEATURE,FEATURE,...>': unsupported WebAssembly feature 'component-model'

I believe this was an accidental regression from #4082 since Wasmtime
0.36.0 produces the error message as expected.

I opted to invert the conditional logic for falling back to the `run`
subcommand. Instead of having a small set of error kinds that print the
first-level error a small set of error kinds are now used to fall back
to the `run` subcommand by default. My hope is that as `ErrorKind` is
extended over time with various sorts of errors of parsing argumenst
this'll be more robust because most of the time we want the CLI
invocation to print out the normal CLI error, it's only in a select few
cases that using `run` is likely to succeed.

src/bin/wasmtime.rs
@@ -59,11 +59,10 @@ impl Wasmtime {
 fn main() -> Result<()> {
     Wasmtime::try_parse()
         .unwrap_or_else(|e| match e.kind() {
-            ErrorKind::DisplayHelp
-            | ErrorKind::DisplayVersion
-            | ErrorKind::MissingSubcommand
-            | ErrorKind::MissingRequiredArgument => e.exit(),
-            _ => Wasmtime::Run(RunCommand::parse()),
+            ErrorKind::UnrecognizedSubcommand | ErrorKind::UnknownArgument => {
+                Wasmtime::Run(RunCommand::parse())
+            }
+            _ => e.exit(),
         })
         .execute()
 }