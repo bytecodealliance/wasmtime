b519c975cb4949bf01733b576504742e5b6d40a5
Trevor Elliott <telliott@fastly.com>
2022-07-18 14:26:23 -0700

    x64: Port fdemote and fvdemote to ISLE (#4449)
    https://github.com/bytecodealliance/wasmtime/pull/4449

--- cranelift/codegen/src/isa/x64/lower.rs
+++ cranelift/codegen/src/isa/x64/lower.rs
@@ -893,13 +893,14 @@ fn lower_insn_to_regs<C: LowerCtx<I = Inst>>(
         | Opcode::Fmin
         | Opcode::Fmax
         | Opcode::FminPseudo
-        | Opcode::FmaxPseudo => implemented_in_isle(ctx),
-
-        Opcode::Icmp => {
-            implemented_in_isle(ctx);
-        }
-
-        Opcode::Fcmp => {
+        | Opcode::FmaxPseudo
+        | Opcode::Sqrt
+        | Opcode::Fpromote
+        | Opcode::FvpromoteLow
+        | Opcode::Fdemote
+        | Opcode::Fvdemote
+        | Opcode::Icmp
+        | Opcode::Fcmp => {
             implemented_in_isle(ctx);
         }
 
@@ -1018,36 +1019,6 @@ fn lower_insn_to_regs<C: LowerCtx<I = Inst>>(
                     FcmpCondResult::InvertedEqualOrConditions(_, _) => unreachable!(),
                 };
             };
-        }
-
-        Opcode::Sqrt => {
-            implemented_in_isle(ctx);
-        }
-
-        Opcode::Fpromote => {
-            implemented_in_isle(ctx);
-        }
-
-        Opcode::FvpromoteLow => {
-            implemented_in_isle(ctx);
-        }
-
-        Opcode::Fdemote => {
-            // We can't guarantee the RHS (if a load) is 128-bit aligned, so we
-            // must avoid merging a load here.
-            let src = RegMem::reg(put_input_in_reg(ctx, inputs[0]));
-            let dst = get_output_reg(ctx, outputs[0]).only_reg().unwrap();
-            ctx.emit(Inst::xmm_unary_rm_r(SseOpcode::Cvtsd2ss, src, dst));
-        }
-
-        Opcode::Fvdemote => {
-            let src = RegMem::reg(put_input_in_reg(ctx, inputs[0]));
-            let dst = get_output_reg(ctx, outputs[0]).only_reg().unwrap();
-            ctx.emit(Inst::xmm_unary_rm_r(
-                SseOpcode::Cvtpd2ps,
-                RegMem::from(src),
-                dst,
-            ));
         }
 
         Opcode::FcvtFromSint => {
