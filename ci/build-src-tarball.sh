a690cbaf3a71c4361fc8bb9e9217d6bcd8c5db8f
Alex Crichton <alex@alexcrichton.com>
2022-08-11 09:43:57 -0700

    Review comments

--- ci/build-src-tarball.sh
+++ ci/build-src-tarball.sh
@@ -8,15 +8,13 @@ if [[ $GITHUB_REF == refs/tags/v* ]]; then
   tag=${GITHUB_REF#refs/tags/}
 fi
 pkgname=wasmtime-$tag-src
-rm -rf /tmp/$pkgname
-mkdir /tmp/$pkgname
 
 # Vendor all crates.io dependencies since this is supposed to be an
 # offline-only-compatible tarball
-mkdir /tmp/$pkgname/.cargo
-cargo vendor > /tmp/$pkgname/.cargo/config.toml
+mkdir .cargo
+cargo vendor > .cargo/config.toml
 
 # Create the tarball from the destination
-tar -czf /tmp/$pkgname.tar.gz --transform "s/^\./$pkgname/S" --exclude-vcs .
+tar -czf /tmp/$pkgname.tar.gz --transform "s/^\./$pkgname/S" --exclude=.git .
 mkdir -p dist
 mv /tmp/$pkgname.tar.gz dist/
