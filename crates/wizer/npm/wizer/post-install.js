1f0b8bb563c2daa20ad4fa24dd02bd7a84bd404b
Jake Champion <jchampion@fastly.com>
2022-08-26 15:46:45 +0100

    ok

--- crates/wizer/npm/wizer/post-install.js
+++ crates/wizer/npm/wizer/post-install.js
@@ -1,24 +1,43 @@
+import { execFileSync } from "child_process";
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
-import { copyFile } from "node:fs/promises";
-
+import { copyFile, writeFile } from "node:fs/promises";
+import { endianness } from "node:os";
+import { platform, arch } from "node:process";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const platformPackages = [
-  "@bytecode-alliance/wizer-win32-x64",
-  "@bytecode-alliance/wizer-linux-x64",
-  "@bytecode-alliance/wizer-darwin-x64",
-  "@bytecode-alliance/wizer-darwin-arm64",
-]
+const knownPackages = {
+  "win32 x64 LE": "@bytecode-alliance/wizer-win32-x64",
+  "darwin arm64 LE": "@bytecode-alliance/wizer-darwin-arm64",
+  "darwin x64 LE": "@bytecode-alliance/wizer-darwin-x64",
+  "linux x64 LE": "@bytecode-alliance/wizer-linux-x64",
+};
 
+function pkgForCurrentPlatform() {
+  let platformKey = `${platform} ${arch} ${endianness()}`;
 
-for (const pkg of platformPackages) {
-  try {
-    const location = await import(pkg);
-    await copyFile(location, join(__dirname, 'wizer'))
-    process.exit()
-  } catch {}
+  if (platformKey in knownPackages) {
+    return knownPackages[platformKey];
+  }
+  throw new Error(`Unsupported platform: "${platformKey}". "@bytecode-alliance/wizer does not have a precompiled binary for the platform/architecture you are using. You can open an issue on https://github.com/bytecodealliance/wizer/issues to request for your platform/architecture to be included."`);
 }
-throw new Error(
-  "@bytecode-alliance/wizer does not have a precompiled binary for the platform/architecture you are using. You can open an issue on https://github.com/bytecodealliance/wizer/issues to request for your platform/architecture to be included."
-);
+
+const pkg = pkgForCurrentPlatform();
+
+try {
+  // First check for the binary package from our "optionalDependencies". This
+  // package should have been installed alongside this package at install time.
+  console.log({ pkg })
+  const location = await import(pkg);
+  console.log({ location: location.default })
+  await copyFile(location.default, join(__dirname, 'wizer'))
+  const contents = `export default "${location.default}";`
+  console.log({ contents })
+  await writeFile(join(__dirname, 'index.js'), contents, { encoding: 'utf-8' })
+} catch (e) {
+  console.error(e);
+  throw new Error(`The package "${pkg}" could not be found, and is needed by @bytecode-alliance/wizer.
+If you are installing @bytecode-alliance/wizer with npm, make sure that you don't specify the
+"--no-optional" flag. The "optionalDependencies" package.json feature is used
+by @bytecode-alliance/wizer to install the correct binary executable for your current platform.`);
+}
