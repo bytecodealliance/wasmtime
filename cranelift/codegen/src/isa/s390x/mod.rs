aaabdb55d7dbae4ad45193b51ef170de850a684f
Nick Fitzgerald <fitzgen@gmail.com>
2022-08-31 12:22:53 -0700

    cargo fmt

--- cranelift/codegen/src/isa/s390x/mod.rs
+++ cranelift/codegen/src/isa/s390x/mod.rs
@@ -73,11 +73,7 @@ impl TargetIsa for S390xBackend {
         let flags = self.flags();
         let (vcode, regalloc_result) = self.compile_vcode(func)?;
 
-        let emit_result = vcode.emit(
-            &regalloc_result,
-            want_disasm,
-            flags.machine_code_cfg_info(),
-        );
+        let emit_result = vcode.emit(&regalloc_result, want_disasm, flags.machine_code_cfg_info());
         let frame_size = emit_result.frame_size;
         let value_labels_ranges = emit_result.value_labels_ranges;
         let buffer = emit_result.buffer.finish();
