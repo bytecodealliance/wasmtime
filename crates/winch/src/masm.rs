1bf580e43db21c0feca7aec078116516cfb6e26f
Saúl Cabrera <saulecabrera@gmail.com>
2022-09-27 11:13:45 -0400

    Add new methods to the masm interface
    This commit introduces the ability for the MacroAssembler to reserve
    stack space, get the address of a given local and perform a stack
    store based on the concept of `Operand`s.
    
    There are several motivating factors to introduce the concept of an
    Operand:
    
    - Make the translation between Winch and Cranelift easier;
    - Make dispatching from the MacroAssembler to the underlying Assembler
    - easier by minimizing the amount of functions that we need to define
    - in order to satisfy the store/load combinations
    
    This commit also introduces the concept of a memory address, which
    essentially describes the addressing modes; as of this commit only one
    addressing mode is supported. We'll also need to verify that this
    structure will play nicely with arm64.

--- crates/winch/src/masm.rs
+++ crates/winch/src/masm.rs
@@ -1,8 +1,18 @@
+use crate::abi::{addressing_mode::Address, local::LocalSlot};
+use crate::isa::reg::Reg;
+
+/// Operand size, in bits
+pub(crate) enum OperandSize {
+    S32,
+    S64,
+}
+
 /// Generic MacroAssembler interface used by the compilation environment
 ///
 /// The MacroAssembler trait aims to expose a high-level enough interface so that
 /// each ISA can define and use their own low-level Assembler implementation
 /// to fulfill the interface
+
 pub(crate) trait MacroAssembler {
     /// Emit the function prologue
     fn prologue(&mut self);
@@ -10,7 +20,21 @@ pub(crate) trait MacroAssembler {
     /// Emit the function epilogue
     fn epilogue(&mut self);
 
+    /// Reserve stack space
+    fn reserve_stack(&mut self, bytes: u32);
+
+    /// Get the address of a local slot
+    fn local_address(&mut self, local: &LocalSlot) -> Address;
+
+    /// Get stack pointer offset
+    fn sp_offset(&mut self) -> u32;
+
+    /// Perform a sized register stack store
+    // TODO augent the src argument to be an enum;
+    //      in this case the src can also be an immediate
+    fn store(&mut self, src: Reg, dst: Address, size: OperandSize);
+
     /// Finalize the assembly and return the result
-    // NB: Interim, debug approach
+    // NOTE Interim, debug approach
     fn finalize(self) -> Vec<String>;
 }
