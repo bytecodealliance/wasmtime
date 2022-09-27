55024112e7c14efdc736a4ea34d041e2ce5fd7e5
Saúl Cabrera <saulecabrera@gmail.com>
2022-09-27 11:19:55 -0400

    Blank masm implementation for arm64

--- crates/winch/src/isa/aarch64/masm.rs
+++ crates/winch/src/isa/aarch64/masm.rs
@@ -1,4 +1,8 @@
-use crate::masm::MacroAssembler as Masm;
+use crate::{
+    abi::{addressing_mode::Address, local::LocalSlot},
+    isa::reg::Reg,
+    masm::{MacroAssembler as Masm, OperandSize},
+};
 
 #[derive(Default)]
 pub(crate) struct MacroAssembler;
@@ -10,6 +14,22 @@ impl Masm for MacroAssembler {
 
     fn epilogue(&mut self) {
         todo!()
+    }
+
+    fn reserve_stack(&mut self, bytes: u32) {
+        todo!()
+    }
+
+    fn local_address(&mut self, local: &LocalSlot) -> Address {
+        todo!()
+    }
+
+    fn store(&mut self, src: Reg, dst: Address, size: OperandSize) {
+        todo!()
+    }
+
+    fn sp_offset(&mut self) -> u32 {
+        0u32
     }
 
     fn finalize(self) -> Vec<String> {
