c3c806d1426982ba0570b7e72efcfa8be4be3e43
Dan Gohman <dev@sunfishcode.online>
2022-10-03 13:34:43 -0700

    An initial preview1 polyfill stub.

---
+++ src/lib.rs
@@ -0,0 +1,460 @@
+#![allow(unused_variables)]
+
+wit_bindgen_guest_rust::import!("wit/wasi-clocks.wit.md");
+wit_bindgen_guest_rust::import!("wit/wasi-default-clocks.wit.md");
+wit_bindgen_guest_rust::import!("wit/wasi-filesystem.wit.md");
+wit_bindgen_guest_rust::import!("wit/wasi-logging.wit.md");
+wit_bindgen_guest_rust::import!("wit/wasi-poll.wit.md");
+wit_bindgen_guest_rust::import!("wit/wasi-random.wit.md");
+
+use wasi::*;
+
+/// Read command-line argument data.
+/// The size of the array should match that returned by `args_sizes_get`
+#[no_mangle]
+pub extern "C" fn args_get(argv: *mut *mut u8, argv_buf: *mut u8) -> Errno {
+    todo!()
+}
+
+/// Return command-line argument data sizes.
+#[no_mangle]
+pub extern "C" fn args_sizes_get(argc: *mut Size, argv_buf_size: *mut Size) -> Errno {
+    todo!()
+}
+
+/// Read environment variable data.
+/// The sizes of the buffers should match that returned by `environ_sizes_get`.
+#[no_mangle]
+pub extern "C" fn environ_get(environ: *mut *mut u8, environ_buf: *mut u8) -> Errno {
+    todo!()
+}
+
+/// Return environment variable data sizes.
+#[no_mangle]
+pub extern "C" fn environ_sizes_get(environc: *mut Size, environ_buf_size: *mut Size) -> Errno {
+    todo!()
+}
+
+/// Return the resolution of a clock.
+/// Implementations are required to provide a non-zero value for supported clocks. For unsupported clocks,
+/// return `errno::inval`.
+/// Note: This is similar to `clock_getres` in POSIX.
+#[no_mangle]
+pub extern "C" fn clock_res_get(id: Clockid, resolution: *mut Timestamp) -> Errno {
+    todo!()
+}
+
+/// Return the time value of a clock.
+/// Note: This is similar to `clock_gettime` in POSIX.
+#[no_mangle]
+pub extern "C" fn clock_time_get(id: Clockid, precision: Timestamp, time: *mut Timestamp) -> Errno {
+    todo!()
+}
+
+/// Provide file advisory information on a file descriptor.
+/// Note: This is similar to `posix_fadvise` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_advise(fd: Fd, offset: Filesize, len: Filesize, advice: Advice) -> Errno {
+    todo!()
+}
+
+/// Force the allocation of space in a file.
+/// Note: This is similar to `posix_fallocate` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_allocate(fd: Fd, offset: Filesize, len: Filesize) -> Errno {
+    todo!()
+}
+
+/// Close a file descriptor.
+/// Note: This is similar to `close` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_close(fd: Fd) -> Errno {
+    todo!()
+}
+
+/// Synchronize the data of a file to disk.
+/// Note: This is similar to `fdatasync` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_datasync(fd: Fd) -> Errno {
+    todo!()
+}
+
+/// Get the attributes of a file descriptor.
+/// Note: This returns similar flags to `fsync(fd, F_GETFL)` in POSIX, as well as additional fields.
+#[no_mangle]
+pub extern "C" fn fd_fdstat_get(fd: Fd, stat: *mut Fdstat) -> Errno {
+    todo!()
+}
+
+/// Adjust the flags associated with a file descriptor.
+/// Note: This is similar to `fcntl(fd, F_SETFL, flags)` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_fdstat_set_flags(fd: Fd, flags: Fdflags) -> Errno {
+    todo!()
+}
+
+/// Adjust the rights associated with a file descriptor.
+/// This can only be used to remove rights, and returns `errno::notcapable` if called in a way that would attempt to add rights
+#[no_mangle]
+pub extern "C" fn fd_fdstat_set_rights(
+    fd: Fd,
+    fs_rights_base: Rights,
+    fs_rights_inheriting: Rights,
+) -> Errno {
+    todo!()
+}
+
+/// Return the attributes of an open file.
+#[no_mangle]
+pub extern "C" fn fd_filestat_get(fd: Fd, buf: *mut Filestat) -> Errno {
+    todo!()
+}
+
+/// Adjust the size of an open file. If this increases the file's size, the extra bytes are filled with zeros.
+/// Note: This is similar to `ftruncate` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_filestat_set_size(fd: Fd, size: Filesize) -> Errno {
+    todo!()
+}
+
+/// Adjust the timestamps of an open file or directory.
+/// Note: This is similar to `futimens` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_filestat_set_times(
+    fd: Fd,
+    atim: Timestamp,
+    mtim: Timestamp,
+    fst_flags: Fstflags,
+) -> Errno {
+    todo!()
+}
+
+/// Read from a file descriptor, without using and updating the file descriptor's offset.
+/// Note: This is similar to `preadv` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_pread(
+    fd: Fd,
+    iovs_ptr: *const Iovec,
+    iovs_len: usize,
+    offset: Filesize,
+    nread: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Return a description of the given preopened file descriptor.
+#[no_mangle]
+pub extern "C" fn fd_prestat_get(fd: Fd, buf: *mut Prestat) -> Errno {
+    todo!()
+}
+
+/// Return a description of the given preopened file descriptor.
+#[no_mangle]
+pub extern "C" fn fd_prestat_dir_name(fd: Fd, path: *mut u8, path_len: Size) -> Errno {
+    todo!()
+}
+
+/// Write to a file descriptor, without using and updating the file descriptor's offset.
+/// Note: This is similar to `pwritev` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_pwrite(
+    fd: Fd,
+    iovs_ptr: *const Ciovec,
+    iovs_len: usize,
+    offset: Filesize,
+    nwritten: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Read from a file descriptor.
+/// Note: This is similar to `readv` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_read(
+    fd: Fd,
+    iovs_ptr: *const Iovec,
+    iovs_len: usize,
+    nread: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Read directory entries from a directory.
+/// When successful, the contents of the output buffer consist of a sequence of
+/// directory entries. Each directory entry consists of a `dirent` object,
+/// followed by `dirent::d_namlen` bytes holding the name of the directory
+/// entry.
+/// This function fills the output buffer as much as possible, potentially
+/// truncating the last directory entry. This allows the caller to grow its
+/// read buffer size in case it's too small to fit a single large directory
+/// entry, or skip the oversized directory entry.
+#[no_mangle]
+pub extern "C" fn fd_readdir(
+    fd: Fd,
+    buf: *mut u8,
+    buf_len: Size,
+    cookie: Dircookie,
+    bufused: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Atomically replace a file descriptor by renumbering another file descriptor.
+/// Due to the strong focus on thread safety, this environment does not provide
+/// a mechanism to duplicate or renumber a file descriptor to an arbitrary
+/// number, like `dup2()`. This would be prone to race conditions, as an actual
+/// file descriptor with the same number could be allocated by a different
+/// thread at the same time.
+/// This function provides a way to atomically renumber file descriptors, which
+/// would disappear if `dup2()` were to be removed entirely.
+#[no_mangle]
+pub extern "C" fn fd_renumber(fd: Fd, to: Fd) -> Errno {
+    todo!()
+}
+
+/// Move the offset of a file descriptor.
+/// Note: This is similar to `lseek` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_seek(
+    fd: Fd,
+    offset: Filedelta,
+    whence: Whence,
+    newoffset: *mut Filesize,
+) -> Errno {
+    todo!()
+}
+
+/// Synchronize the data and metadata of a file to disk.
+/// Note: This is similar to `fsync` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_sync(fd: Fd) -> Errno {
+    todo!()
+}
+
+/// Return the current offset of a file descriptor.
+/// Note: This is similar to `lseek(fd, 0, SEEK_CUR)` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_tell(fd: Fd, offset: *mut Filesize) -> Errno {
+    todo!()
+}
+
+/// Write to a file descriptor.
+/// Note: This is similar to `writev` in POSIX.
+#[no_mangle]
+pub extern "C" fn fd_write(
+    fd: Fd,
+    iovs_ptr: *const Ciovec,
+    iovs_len: usize,
+    nwritten: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Create a directory.
+/// Note: This is similar to `mkdirat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_create_directory(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
+    todo!()
+}
+
+/// Return the attributes of a file or directory.
+/// Note: This is similar to `stat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_filestat_get(
+    fd: Fd,
+    flags: Lookupflags,
+    path_ptr: *const u8,
+    path_len: usize,
+    buf: *mut Filestat,
+) -> Errno {
+    todo!()
+}
+
+/// Adjust the timestamps of a file or directory.
+/// Note: This is similar to `utimensat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_filestat_set_times(
+    fd: Fd,
+    flags: Lookupflags,
+    path_ptr: *const u8,
+    path_len: usize,
+    atim: Timestamp,
+    mtim: Timestamp,
+    fst_flags: Fstflags,
+) -> Errno {
+    todo!()
+}
+
+/// Create a hard link.
+/// Note: This is similar to `linkat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_link(
+    old_fd: Fd,
+    old_flags: Lookupflags,
+    old_path_ptr: *const u8,
+    old_path_len: usize,
+    new_fd: Fd,
+    new_path_ptr: *const u8,
+    new_path_len: usize,
+) -> Errno {
+    todo!()
+}
+
+/// Open a file or directory.
+/// The returned file descriptor is not guaranteed to be the lowest-numbered
+/// file descriptor not currently open; it is randomized to prevent
+/// applications from depending on making assumptions about indexes, since this
+/// is error-prone in multi-threaded contexts. The returned file descriptor is
+/// guaranteed to be less than 2**31.
+/// Note: This is similar to `openat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_open(
+    fd: Fd,
+    dirflags: Lookupflags,
+    path_ptr: *const u8,
+    path_len: usize,
+    oflags: Oflags,
+    fs_rights_base: Rights,
+    fs_rights_inheriting: Rights,
+    fdflags: Fdflags,
+    opened_fd: *mut Fd,
+) -> Errno {
+    todo!()
+}
+
+/// Read the contents of a symbolic link.
+/// Note: This is similar to `readlinkat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_readlink(
+    fd: Fd,
+    path_ptr: *const u8,
+    path_len: usize,
+    buf: *mut u8,
+    buf_len: Size,
+    bufused: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Remove a directory.
+/// Return `errno::notempty` if the directory is not empty.
+/// Note: This is similar to `unlinkat(fd, path, AT_REMOVEDIR)` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_remove_directory(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
+    todo!()
+}
+
+/// Rename a file or directory.
+/// Note: This is similar to `renameat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_rename(
+    fd: Fd,
+    old_path_ptr: *const u8,
+    old_path_len: usize,
+    new_fd: Fd,
+    new_path_ptr: *const u8,
+    new_path_len: usize,
+) -> Errno {
+    todo!()
+}
+
+/// Create a symbolic link.
+/// Note: This is similar to `symlinkat` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_symlink(
+    old_path_ptr: *const u8,
+    old_path_len: usize,
+    fd: Fd,
+    new_path_ptr: *const u8,
+    new_path_len: usize,
+) -> Errno {
+    todo!()
+}
+
+/// Unlink a file.
+/// Return `errno::isdir` if the path refers to a directory.
+/// Note: This is similar to `unlinkat(fd, path, 0)` in POSIX.
+#[no_mangle]
+pub extern "C" fn path_unlink_file(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
+    todo!()
+}
+
+/// Concurrently poll for the occurrence of a set of events.
+#[no_mangle]
+pub extern "C" fn poll_oneoff(
+    r#in: *const Subscription,
+    out: *mut Event,
+    nsubscriptions: Size,
+    nevents: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Terminate the process normally. An exit code of 0 indicates successful
+/// termination of the program. The meanings of other values is dependent on
+/// the environment.
+#[no_mangle]
+pub extern "C" fn proc_exit(rval: Exitcode) -> ! {
+    todo!()
+}
+
+/// Send a signal to the process of the calling thread.
+/// Note: This is similar to `raise` in POSIX.
+#[no_mangle]
+pub extern "C" fn proc_raise(sig: Signal) -> Errno {
+    todo!()
+}
+
+/// Temporarily yield execution of the calling thread.
+/// Note: This is similar to `sched_yield` in POSIX.
+#[no_mangle]
+pub extern "C" fn sched_yield() -> Errno {
+    todo!()
+}
+
+/// Write high-quality random data into a buffer.
+/// This function blocks when the implementation is unable to immediately
+/// provide sufficient high-quality random data.
+/// This function may execute slowly, so when large mounts of random data are
+/// required, it's advisable to use this function to seed a pseudo-random
+/// number generator, rather than to provide the random data directly.
+#[no_mangle]
+pub extern "C" fn random_get(buf: *mut u8, buf_len: Size) -> Errno {
+    todo!()
+}
+
+/// Receive a message from a socket.
+/// Note: This is similar to `recv` in POSIX, though it also supports reading
+/// the data into multiple buffers in the manner of `readv`.
+#[no_mangle]
+pub extern "C" fn sock_recv(
+    fd: Fd,
+    ri_data_ptr: *const Iovec,
+    ri_data_len: usize,
+    ri_flags: Riflags,
+    ro_datalen: *mut Size,
+    ro_flags: *mut Roflags,
+) -> Errno {
+    todo!()
+}
+
+/// Send a message on a socket.
+/// Note: This is similar to `send` in POSIX, though it also supports writing
+/// the data from multiple buffers in the manner of `writev`.
+#[no_mangle]
+pub extern "C" fn sock_send(
+    fd: Fd,
+    si_data_ptr: *const Ciovec,
+    si_data_len: usize,
+    si_flags: Siflags,
+    so_datalen: *mut Size,
+) -> Errno {
+    todo!()
+}
+
+/// Shut down socket send and receive channels.
+/// Note: This is similar to `shutdown` in POSIX.
+#[no_mangle]
+pub extern "C" fn sock_shutdown(fd: Fd, how: Sdflags) -> Errno {
+    todo!()
+}
