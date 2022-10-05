1b3377011e3085be05323b0756614de790050b89
Dan Gohman <dev@sunfishcode.online>
2022-10-05 06:51:15 -0700

    Mark the WASI functions `unsafe`.

--- src/lib.rs
+++ src/lib.rs
@@ -35,26 +35,29 @@ pub unsafe extern "C" fn cabi_realloc(
 /// Read command-line argument data.
 /// The size of the array should match that returned by `args_sizes_get`
 #[no_mangle]
-pub extern "C" fn args_get(argv: *mut *mut u8, argv_buf: *mut u8) -> Errno {
+pub unsafe extern "C" fn args_get(argv: *mut *mut u8, argv_buf: *mut u8) -> Errno {
     unreachable()
 }
 
 /// Return command-line argument data sizes.
 #[no_mangle]
-pub extern "C" fn args_sizes_get(argc: *mut Size, argv_buf_size: *mut Size) -> Errno {
+pub unsafe extern "C" fn args_sizes_get(argc: *mut Size, argv_buf_size: *mut Size) -> Errno {
     unreachable()
 }
 
 /// Read environment variable data.
 /// The sizes of the buffers should match that returned by `environ_sizes_get`.
 #[no_mangle]
-pub extern "C" fn environ_get(environ: *mut *mut u8, environ_buf: *mut u8) -> Errno {
+pub unsafe extern "C" fn environ_get(environ: *mut *mut u8, environ_buf: *mut u8) -> Errno {
     unreachable()
 }
 
 /// Return environment variable data sizes.
 #[no_mangle]
-pub extern "C" fn environ_sizes_get(environc: *mut Size, environ_buf_size: *mut Size) -> Errno {
+pub unsafe extern "C" fn environ_sizes_get(
+    environc: *mut Size,
+    environ_buf_size: *mut Size,
+) -> Errno {
     unreachable()
 }
 
@@ -63,63 +66,72 @@ pub extern "C" fn environ_sizes_get(environc: *mut Size, environ_buf_size: *mut 
 /// return `errno::inval`.
 /// Note: This is similar to `clock_getres` in POSIX.
 #[no_mangle]
-pub extern "C" fn clock_res_get(id: Clockid, resolution: *mut Timestamp) -> Errno {
+pub unsafe extern "C" fn clock_res_get(id: Clockid, resolution: *mut Timestamp) -> Errno {
     unreachable()
 }
 
 /// Return the time value of a clock.
 /// Note: This is similar to `clock_gettime` in POSIX.
 #[no_mangle]
-pub extern "C" fn clock_time_get(id: Clockid, precision: Timestamp, time: *mut Timestamp) -> Errno {
+pub unsafe extern "C" fn clock_time_get(
+    id: Clockid,
+    precision: Timestamp,
+    time: *mut Timestamp,
+) -> Errno {
     unreachable()
 }
 
 /// Provide file advisory information on a file descriptor.
 /// Note: This is similar to `posix_fadvise` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_advise(fd: Fd, offset: Filesize, len: Filesize, advice: Advice) -> Errno {
+pub unsafe extern "C" fn fd_advise(
+    fd: Fd,
+    offset: Filesize,
+    len: Filesize,
+    advice: Advice,
+) -> Errno {
     unreachable()
 }
 
 /// Force the allocation of space in a file.
 /// Note: This is similar to `posix_fallocate` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_allocate(fd: Fd, offset: Filesize, len: Filesize) -> Errno {
+pub unsafe extern "C" fn fd_allocate(fd: Fd, offset: Filesize, len: Filesize) -> Errno {
     unreachable()
 }
 
 /// Close a file descriptor.
 /// Note: This is similar to `close` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_close(fd: Fd) -> Errno {
+pub unsafe extern "C" fn fd_close(fd: Fd) -> Errno {
     unreachable()
 }
 
 /// Synchronize the data of a file to disk.
 /// Note: This is similar to `fdatasync` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_datasync(fd: Fd) -> Errno {
+pub unsafe extern "C" fn fd_datasync(fd: Fd) -> Errno {
     unreachable()
 }
 
 /// Get the attributes of a file descriptor.
 /// Note: This returns similar flags to `fsync(fd, F_GETFL)` in POSIX, as well as additional fields.
 #[no_mangle]
-pub extern "C" fn fd_fdstat_get(fd: Fd, stat: *mut Fdstat) -> Errno {
+pub unsafe extern "C" fn fd_fdstat_get(fd: Fd, stat: *mut Fdstat) -> Errno {
     unreachable()
 }
 
 /// Adjust the flags associated with a file descriptor.
 /// Note: This is similar to `fcntl(fd, F_SETFL, flags)` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_fdstat_set_flags(fd: Fd, flags: Fdflags) -> Errno {
+pub unsafe extern "C" fn fd_fdstat_set_flags(fd: Fd, flags: Fdflags) -> Errno {
     unreachable()
 }
 
 /// Adjust the rights associated with a file descriptor.
 /// This can only be used to remove rights, and returns `errno::notcapable` if called in a way that would attempt to add rights
 #[no_mangle]
-pub extern "C" fn fd_fdstat_set_rights(
+pub unsafe extern "C" fn fd_fdstat_set_rights(
     fd: Fd,
     fs_rights_base: Rights,
     fs_rights_inheriting: Rights,
@@ -129,21 +141,21 @@ pub extern "C" fn fd_fdstat_set_rights(
 
 /// Return the attributes of an open file.
 #[no_mangle]
-pub extern "C" fn fd_filestat_get(fd: Fd, buf: *mut Filestat) -> Errno {
+pub unsafe extern "C" fn fd_filestat_get(fd: Fd, buf: *mut Filestat) -> Errno {
     unreachable()
 }
 
 /// Adjust the size of an open file. If this increases the file's size, the extra bytes are filled with zeros.
 /// Note: This is similar to `ftruncate` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_filestat_set_size(fd: Fd, size: Filesize) -> Errno {
+pub unsafe extern "C" fn fd_filestat_set_size(fd: Fd, size: Filesize) -> Errno {
     unreachable()
 }
 
 /// Adjust the timestamps of an open file or directory.
 /// Note: This is similar to `futimens` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_filestat_set_times(
+pub unsafe extern "C" fn fd_filestat_set_times(
     fd: Fd,
     atim: Timestamp,
     mtim: Timestamp,
@@ -155,7 +167,7 @@ pub extern "C" fn fd_filestat_set_times(
 /// Read from a file descriptor, without using and updating the file descriptor's offset.
 /// Note: This is similar to `preadv` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_pread(
+pub unsafe extern "C" fn fd_pread(
     fd: Fd,
     iovs_ptr: *const Iovec,
     iovs_len: usize,
@@ -167,20 +179,20 @@ pub extern "C" fn fd_pread(
 
 /// Return a description of the given preopened file descriptor.
 #[no_mangle]
-pub extern "C" fn fd_prestat_get(fd: Fd, buf: *mut Prestat) -> Errno {
+pub unsafe extern "C" fn fd_prestat_get(fd: Fd, buf: *mut Prestat) -> Errno {
     unreachable()
 }
 
 /// Return a description of the given preopened file descriptor.
 #[no_mangle]
-pub extern "C" fn fd_prestat_dir_name(fd: Fd, path: *mut u8, path_len: Size) -> Errno {
+pub unsafe extern "C" fn fd_prestat_dir_name(fd: Fd, path: *mut u8, path_len: Size) -> Errno {
     unreachable()
 }
 
 /// Write to a file descriptor, without using and updating the file descriptor's offset.
 /// Note: This is similar to `pwritev` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_pwrite(
+pub unsafe extern "C" fn fd_pwrite(
     fd: Fd,
     iovs_ptr: *const Ciovec,
     iovs_len: usize,
@@ -193,7 +205,7 @@ pub extern "C" fn fd_pwrite(
 /// Read from a file descriptor.
 /// Note: This is similar to `readv` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_read(
+pub unsafe extern "C" fn fd_read(
     fd: Fd,
     iovs_ptr: *const Iovec,
     iovs_len: usize,
@@ -212,7 +224,7 @@ pub extern "C" fn fd_read(
 /// read buffer size in case it's too small to fit a single large directory
 /// entry, or skip the oversized directory entry.
 #[no_mangle]
-pub extern "C" fn fd_readdir(
+pub unsafe extern "C" fn fd_readdir(
     fd: Fd,
     buf: *mut u8,
     buf_len: Size,
@@ -231,14 +243,14 @@ pub extern "C" fn fd_readdir(
 /// This function provides a way to atomically renumber file descriptors, which
 /// would disappear if `dup2()` were to be removed entirely.
 #[no_mangle]
-pub extern "C" fn fd_renumber(fd: Fd, to: Fd) -> Errno {
+pub unsafe extern "C" fn fd_renumber(fd: Fd, to: Fd) -> Errno {
     unreachable()
 }
 
 /// Move the offset of a file descriptor.
 /// Note: This is similar to `lseek` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_seek(
+pub unsafe extern "C" fn fd_seek(
     fd: Fd,
     offset: Filedelta,
     whence: Whence,
@@ -250,21 +262,21 @@ pub extern "C" fn fd_seek(
 /// Synchronize the data and metadata of a file to disk.
 /// Note: This is similar to `fsync` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_sync(fd: Fd) -> Errno {
+pub unsafe extern "C" fn fd_sync(fd: Fd) -> Errno {
     unreachable()
 }
 
 /// Return the current offset of a file descriptor.
 /// Note: This is similar to `lseek(fd, 0, SEEK_CUR)` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_tell(fd: Fd, offset: *mut Filesize) -> Errno {
+pub unsafe extern "C" fn fd_tell(fd: Fd, offset: *mut Filesize) -> Errno {
     unreachable()
 }
 
 /// Write to a file descriptor.
 /// Note: This is similar to `writev` in POSIX.
 #[no_mangle]
-pub extern "C" fn fd_write(
+pub unsafe extern "C" fn fd_write(
     fd: Fd,
     iovs_ptr: *const Ciovec,
     iovs_len: usize,
@@ -276,14 +288,18 @@ pub extern "C" fn fd_write(
 /// Create a directory.
 /// Note: This is similar to `mkdirat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_create_directory(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
+pub unsafe extern "C" fn path_create_directory(
+    fd: Fd,
+    path_ptr: *const u8,
+    path_len: usize,
+) -> Errno {
     unreachable()
 }
 
 /// Return the attributes of a file or directory.
 /// Note: This is similar to `stat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_filestat_get(
+pub unsafe extern "C" fn path_filestat_get(
     fd: Fd,
     flags: Lookupflags,
     path_ptr: *const u8,
@@ -296,7 +312,7 @@ pub extern "C" fn path_filestat_get(
 /// Adjust the timestamps of a file or directory.
 /// Note: This is similar to `utimensat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_filestat_set_times(
+pub unsafe extern "C" fn path_filestat_set_times(
     fd: Fd,
     flags: Lookupflags,
     path_ptr: *const u8,
@@ -311,7 +327,7 @@ pub extern "C" fn path_filestat_set_times(
 /// Create a hard link.
 /// Note: This is similar to `linkat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_link(
+pub unsafe extern "C" fn path_link(
     old_fd: Fd,
     old_flags: Lookupflags,
     old_path_ptr: *const u8,
@@ -331,7 +347,7 @@ pub extern "C" fn path_link(
 /// guaranteed to be less than 2**31.
 /// Note: This is similar to `openat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_open(
+pub unsafe extern "C" fn path_open(
     fd: Fd,
     dirflags: Lookupflags,
     path_ptr: *const u8,
@@ -348,12 +364,12 @@ pub extern "C" fn path_open(
 /// Read the contents of a symbolic link.
 /// Note: This is similar to `readlinkat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_readlink(
+pub unsafe extern "C" fn path_readlink(
     fd: Fd,
     path_ptr: *const u8,
     path_len: usize,
     buf: *mut u8,
-    buf_len: Size,
+    _buf_len: Size,
     bufused: *mut Size,
 ) -> Errno {
     unreachable()
@@ -363,14 +379,18 @@ pub extern "C" fn path_readlink(
 /// Return `errno::notempty` if the directory is not empty.
 /// Note: This is similar to `unlinkat(fd, path, AT_REMOVEDIR)` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_remove_directory(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
+pub unsafe extern "C" fn path_remove_directory(
+    fd: Fd,
+    path_ptr: *const u8,
+    path_len: usize,
+) -> Errno {
     unreachable()
 }
 
 /// Rename a file or directory.
 /// Note: This is similar to `renameat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_rename(
+pub unsafe extern "C" fn path_rename(
     fd: Fd,
     old_path_ptr: *const u8,
     old_path_len: usize,
@@ -384,7 +404,7 @@ pub extern "C" fn path_rename(
 /// Create a symbolic link.
 /// Note: This is similar to `symlinkat` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_symlink(
+pub unsafe extern "C" fn path_symlink(
     old_path_ptr: *const u8,
     old_path_len: usize,
     fd: Fd,
@@ -398,13 +418,13 @@ pub extern "C" fn path_symlink(
 /// Return `errno::isdir` if the path refers to a directory.
 /// Note: This is similar to `unlinkat(fd, path, 0)` in POSIX.
 #[no_mangle]
-pub extern "C" fn path_unlink_file(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
+pub unsafe extern "C" fn path_unlink_file(fd: Fd, path_ptr: *const u8, path_len: usize) -> Errno {
     unreachable()
 }
 
 /// Concurrently poll for the occurrence of a set of events.
 #[no_mangle]
-pub extern "C" fn poll_oneoff(
+pub unsafe extern "C" fn poll_oneoff(
     r#in: *const Subscription,
     out: *mut Event,
     nsubscriptions: Size,
@@ -417,21 +437,21 @@ pub extern "C" fn poll_oneoff(
 /// termination of the program. The meanings of other values is dependent on
 /// the environment.
 #[no_mangle]
-pub extern "C" fn proc_exit(rval: Exitcode) -> ! {
+pub unsafe extern "C" fn proc_exit(rval: Exitcode) -> ! {
     unreachable()
 }
 
 /// Send a signal to the process of the calling thread.
 /// Note: This is similar to `raise` in POSIX.
 #[no_mangle]
-pub extern "C" fn proc_raise(sig: Signal) -> Errno {
+pub unsafe extern "C" fn proc_raise(sig: Signal) -> Errno {
     unreachable()
 }
 
 /// Temporarily yield execution of the calling thread.
 /// Note: This is similar to `sched_yield` in POSIX.
 #[no_mangle]
-pub extern "C" fn sched_yield() -> Errno {
+pub unsafe extern "C" fn sched_yield() -> Errno {
     unreachable()
 }
 
@@ -442,7 +462,7 @@ pub extern "C" fn sched_yield() -> Errno {
 /// required, it's advisable to use this function to seed a pseudo-random
 /// number generator, rather than to provide the random data directly.
 #[no_mangle]
-pub extern "C" fn random_get(buf: *mut u8, buf_len: Size) -> Errno {
+pub unsafe extern "C" fn random_get(buf: *mut u8, buf_len: Size) -> Errno {
     unreachable()
 }
 
@@ -450,7 +470,7 @@ pub extern "C" fn random_get(buf: *mut u8, buf_len: Size) -> Errno {
 /// Note: This is similar to `recv` in POSIX, though it also supports reading
 /// the data into multiple buffers in the manner of `readv`.
 #[no_mangle]
-pub extern "C" fn sock_recv(
+pub unsafe extern "C" fn sock_recv(
     fd: Fd,
     ri_data_ptr: *const Iovec,
     ri_data_len: usize,
@@ -465,7 +485,7 @@ pub extern "C" fn sock_recv(
 /// Note: This is similar to `send` in POSIX, though it also supports writing
 /// the data from multiple buffers in the manner of `writev`.
 #[no_mangle]
-pub extern "C" fn sock_send(
+pub unsafe extern "C" fn sock_send(
     fd: Fd,
     si_data_ptr: *const Ciovec,
     si_data_len: usize,
@@ -478,6 +498,6 @@ pub extern "C" fn sock_send(
 /// Shut down socket send and receive channels.
 /// Note: This is similar to `shutdown` in POSIX.
 #[no_mangle]
-pub extern "C" fn sock_shutdown(fd: Fd, how: Sdflags) -> Errno {
+pub unsafe extern "C" fn sock_shutdown(fd: Fd, how: Sdflags) -> Errno {
     unreachable()
 }
