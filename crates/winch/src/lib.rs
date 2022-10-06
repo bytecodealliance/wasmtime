fef75840cc62f293f5a21c1c095214983706131a
Saúl Cabrera <saulecabrera@gmail.com>
2022-10-06 17:52:11 -0400

    Add a regset abstraction to track register availability
    Adds a bit set abstraction to track register availability for register
    allocation.
    
    The bit set has no specific knowledge about physical registers, it
    works on the register's hardware encoding as the source of truth.
    
    Each RegSet is expected to be created with the universe of allocatable
    registers per ISA when starting the compilation of a particular function.

--- crates/winch/src/lib.rs
+++ crates/winch/src/lib.rs
@@ -4,5 +4,7 @@ mod compiler;
 mod frame;
 pub mod isa;
 mod masm;
+mod regset;
+mod stack;
 
 pub use compiler::builder::builder;
---
+++ crates/winch/src/regset.rs
@@ -0,0 +1,37 @@
+use crate::isa::reg::Reg;
+
+/// A bit set to track regiter availability
+pub(crate) struct RegSet {
+    /// Bitset to track general purpose register availability
+    gpr: u32,
+    /// Bitset to track floating-point register availability
+    fpr: u32,
+}
+
+impl RegSet {
+    /// Create a new register allocator
+    pub fn new(gpr: u32, fpr: u32) -> Self {
+        Self { gpr, fpr }
+    }
+
+    /// Request a general purpose register
+    pub fn any_gpr(&mut self) -> Option<Reg> {
+	self.gpr_available().then(|| {
+	    let index = self.gpr.trailing_zeros();
+	    self.allocate(index);
+	    Reg::int(index as usize)
+	})
+    }
+
+    pub fn free_gpr(&mut self, reg: Reg) {
+        self.gpr |= reg.hw_enc() as u32;
+    }
+
+    fn gpr_available(&self) -> bool {
+        !self.gpr == 0
+    }
+
+    fn allocate(&mut self, index: u32) {
+        self.gpr &= !(1 << index);
+    }
+}
