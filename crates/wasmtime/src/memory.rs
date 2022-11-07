508dd81928ef7610bd15f5bb92b1857edf1671e1
Alphyr <47725341+a1phyr@users.noreply.github.com>
2022-11-07 16:05:59 +0100

    Impl `Debug` for `SharedMemory` and `Extern` (#5211)

--- crates/wasmtime/src/memory.rs
+++ crates/wasmtime/src/memory.rs
@@ -818,6 +818,12 @@ impl SharedMemory {
     }
 }
 
+impl std::fmt::Debug for SharedMemory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SharedMemory").finish_non_exhaustive()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
