940d85c56a9a8d14139ab64a0c5fb789793f182a
Pat Hickey <phickey@fastly.com>
2022-11-07 16:25:35 -0800

    test programs: only use main, dont bindgen export an interface

--- test-programs/src/bin/hello_stdout.rs
+++ test-programs/src/bin/hello_stdout.rs
@@ -1,16 +1,4 @@
-wit_bindgen_guest_rust::generate!({
-    default: "../wit/command.wit.md",
-    name: "app",
-});
-
-struct Exports;
-export_app!(Exports);
-
-impl app::App for Exports {
-    fn command(_: u32, _: u32) {
-        println!("hello, world");
-        eprintln!("hello, world");
-    }
+fn main() {
+    println!("hello, world");
+    eprintln!("hello, world");
 }
-
-fn main() {}
--- test-programs/src/bin/panic.rs
+++ test-programs/src/bin/panic.rs
@@ -1,15 +1,3 @@
-wit_bindgen_guest_rust::generate!({
-    default: "../wit/command.wit.md",
-    name: "app",
-});
-
-struct Exports;
-export_app!(Exports);
-
-impl app::App for Exports {
-    fn command(_: u32, _: u32) {
-        panic!("idk");
-    }
+fn main() {
+    panic!("idk!!!");
 }
-
-fn main() {}
