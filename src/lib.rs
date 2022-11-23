28cb8bd9325811f07dd992bb5de70f3e430fbd22
Dan Gohman <dev@sunfishcode.online>
2022-11-23 11:05:49 -0800

    Swap `info` and `fd-info`
    See the last commit in https://github.com/WebAssembly/wasi-filesystem/pull/66 for details.

--- src/lib.rs
+++ src/lib.rs
@@ -248,7 +248,7 @@ pub unsafe extern "C" fn fd_datasync(fd: Fd) -> Errno {
 pub unsafe extern "C" fn fd_fdstat_get(fd: Fd, stat: *mut Fdstat) -> Errno {
     match Descriptor::get(fd) {
         Descriptor::File(file) => {
-            let info = match wasi_filesystem::fd_info(file.fd) {
+            let info = match wasi_filesystem::info(file.fd) {
                 Ok(info) => info,
                 Err(err) => return errno_from_wasi_filesystem(err),
             };
