3d7e56aa2cd43737a7d3858163ccb4690eebf171
Nick Fitzgerald <fitzgen@gmail.com>
2022-12-01 09:25:53 -0800

    cranelift-wasm: move `funct_state.rs` sub module to `state.rs`
    Since it is the only submodule of `state` it can just be the whole `state`
    module.

--- cranelift/wasm/src/state/mod.rs
+++
@@ -1,9 +0,0 @@
-//! WebAssembly module and function translation state.
-//!
-//! The `FuncTranslationState` struct defined in this module is used to keep track of the WebAssembly
-//! value and control stacks during the translation of a single function.
-
-pub(crate) mod func_state;
-
-// Re-export for convenience.
-pub(crate) use func_state::*;
