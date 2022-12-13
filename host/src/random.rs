a2acf7c7b76a78911567af244d4d861c49b50f24
Ari Seyhun <ariseyhun@live.com.au>
2022-12-14 00:57:50 +0800

    Implement `getrandom` using `Standard` distribution (#24)

--- host/src/random.rs
+++ host/src/random.rs
@@ -1,9 +1,14 @@
 #![allow(unused_variables)]
+use cap_rand::{distributions::Standard, Rng};
+
 use crate::{wasi_random, WasiCtx};
 
 #[async_trait::async_trait]
 impl wasi_random::WasiRandom for WasiCtx {
     async fn getrandom(&mut self, len: u32) -> anyhow::Result<Vec<u8>> {
-        todo!()
+        Ok((&mut self.random)
+            .sample_iter(Standard)
+            .take(len as usize)
+            .collect())
     }
 }
