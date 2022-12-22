fa34a13ecc4ec1cfde75d5f44b0132da6d8c363a
Dan Gohman <dev@sunfishcode.online>
2022-12-22 08:47:27 -0800

    Add a way to get a random `u64`. (#36)
    This anticipates https://github.com/WebAssembly/wasi-random/pull/18.

--- host/src/random.rs
+++ host/src/random.rs
@@ -4,10 +4,14 @@ use crate::{wasi_random, WasiCtx};
 
 #[async_trait::async_trait]
 impl wasi_random::WasiRandom for WasiCtx {
-    async fn getrandom(&mut self, len: u32) -> anyhow::Result<Vec<u8>> {
+    async fn get_random_bytes(&mut self, len: u32) -> anyhow::Result<Vec<u8>> {
         Ok((&mut self.random)
             .sample_iter(Standard)
             .take(len as usize)
             .collect())
+    }
+
+    async fn get_random_u64(&mut self) -> anyhow::Result<u64> {
+        Ok((&mut self.random).sample(Standard))
     }
 }
