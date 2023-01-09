8ac04612ae5b4bb22a9359da6c595840ba10edb6
Afonso Bordado <afonso360@users.noreply.github.com>
2023-01-09 20:19:30 +0000

    cranelift: Remove predicate not macro branch (#5552)

--- cranelift/codegen/meta/src/cdsl/mod.rs
+++ cranelift/codegen/meta/src/cdsl/mod.rs
@@ -17,15 +17,6 @@ macro_rules! predicate {
     ($a:ident && $($b:tt)*) => {
         PredicateNode::And(Box::new($a.into()), Box::new(predicate!($($b)*)))
     };
-    (!$a:ident && $($b:tt)*) => {
-        PredicateNode::And(
-            Box::new(PredicateNode::Not(Box::new($a.into()))),
-            Box::new(predicate!($($b)*))
-        )
-    };
-    (!$a:ident) => {
-        PredicateNode::Not(Box::new($a.into()))
-    };
     ($a:ident) => {
         $a.into()
     };
