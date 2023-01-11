2eaaad3e968595da38cb9feb442b023f663d7b98
Dan Gohman <dev@sunfishcode.online>
2023-01-11 14:56:24 -0800

    Treat `wasmtime::component::Val::Float{32,64}` zero and negative zero as inequal
    Following up on #5535, treat positive and negative zero as inequal in
    wasmtime::component::Val::Float{32,64}'s `PartialEq` logic. IEEE 754
    equality considers these values equal, but they are semantically
    distinct values, and testing and fuzzing should be aware of the
    difference.

--- crates/wasmtime/src/component/values.rs
+++ crates/wasmtime/src/component/values.rs
@@ -1008,9 +1008,17 @@ impl PartialEq for Val {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             // This breaks conformance with IEEE-754 equality to simplify testing logic.
-            (Self::Float32(l), Self::Float32(r)) => l == r || (l.is_nan() && r.is_nan()),
+            (Self::Float32(l), Self::Float32(r)) => {
+                (l != 0.0 && l == r)
+                    || (l == 0 && l.to_bits() == r.to_bits())
+                    || (l.is_nan() && r.is_nan())
+            }
             (Self::Float32(_), _) => false,
-            (Self::Float64(l), Self::Float64(r)) => l == r || (l.is_nan() && r.is_nan()),
+            (Self::Float64(l), Self::Float64(r)) => {
+                (l != 0.0 && l == r)
+                    || (l == 0 && l.to_bits() == r.to_bits())
+                    || (l.is_nan() && r.is_nan())
+            }
             (Self::Float64(_), _) => false,
 
             (Self::Bool(l), Self::Bool(r)) => l == r,
