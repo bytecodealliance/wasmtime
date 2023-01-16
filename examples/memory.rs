10bbe635b8c956e8052da4920df6f63d3dc1afb1
Daniel Marin <danielmarin0051@gmail.com>
2023-01-15 17:09:13 -0800

    fixed typo in examples/memory.rs

--- examples/memory.rs
+++ examples/memory.rs
@@ -4,7 +4,7 @@
 //! read and write memory through the `Memory` object, and how wasm functions
 //! can trap when dealing with out-of-bounds addresses.
 
-// You can execute this example with `cargo run --example example`
+// You can execute this example with `cargo run --example memory`
 
 use anyhow::Result;
 use wasmtime::*;
