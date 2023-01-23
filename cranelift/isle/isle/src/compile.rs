bfc6aad184df4ac02d5337b63357b1adbcade801
Jamey Sharp <jsharp@fastly.com>
2023-01-23 12:27:51 -0800

    cranelift-isle: codegen from new IR (#5435)
    ISLE's existing code-generation strategy doesn't generate the most
    efficient matching order for rules. This PR completely replaces it.
    
    With this PR applied, wasmtime compile retires 2% fewer instructions on
    the pulldown-cmark and spidermonkey benchmarks from Sightglass.
    
    A dev build of cranelift-codegen from an empty target/ directory takes
    2% less time. The build script, invoking ISLE, takes a little longer,
    but Rust can compile the generated code faster, so it balances out.

--- cranelift/isle/isle/src/compile.rs
+++ cranelift/isle/isle/src/compile.rs
@@ -3,15 +3,14 @@
 use std::path::Path;
 
 use crate::error::Errors;
-use crate::{ast, codegen, sema, trie};
+use crate::{ast, codegen, sema};
 
 /// Compile the given AST definitions into Rust source code.
 pub fn compile(defs: &ast::Defs, options: &codegen::CodegenOptions) -> Result<String, Errors> {
     let mut typeenv = sema::TypeEnv::from_ast(defs)?;
     let termenv = sema::TermEnv::from_ast(&mut typeenv, defs)?;
-    crate::overlap::check(&mut typeenv, &termenv)?;
-    let tries = trie::build_tries(&termenv);
-    Ok(codegen::codegen(&typeenv, &termenv, &tries, options))
+    let terms = crate::overlap::check(&typeenv, &termenv)?;
+    Ok(codegen::codegen(&typeenv, &termenv, &terms, options))
 }
 
 /// Compile the given files into Rust source code.
