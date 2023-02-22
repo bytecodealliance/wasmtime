242eede5ed319fb91febc0a848fb78053bd8d67b
Ryan Levick <me@ryanlevick.com>
2023-02-22 19:06:08 +0100

    Address small PR feedback

--- crates/wasmtime/src/lib.rs
+++ crates/wasmtime/src/lib.rs
@@ -424,10 +424,11 @@ pub use crate::trap::*;
 pub use crate::types::*;
 pub use crate::values::*;
 
-/// A convience wrapper for `Result<T, anyhow::Error>`.
+/// A convenience wrapper for `Result<T, anyhow::Error>`.
 ///
 /// This type can be used to interact with `wasmtimes`'s extensive use
 /// of `anyhow::Error` while still not directly depending on `anyhow`.
+/// This type alias is identical to `anyhow::Result`.
 pub type Result<T> = std::result::Result<T, anyhow::Error>;
 
 #[cfg(feature = "component-model")]
