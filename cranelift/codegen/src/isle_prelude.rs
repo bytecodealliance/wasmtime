5decec80738b9e40853efbc8d9f1248e031c630e
Chris Fallin <chris@cfallin.org>
2023-03-09 10:24:08 -0800

    Add an explicit extractor to match 64-bit address types.

--- cranelift/codegen/src/isle_prelude.rs
+++ cranelift/codegen/src/isle_prelude.rs
@@ -413,6 +413,14 @@ macro_rules! isle_common_prelude_methods {
         }
 
         #[inline]
+        fn ty_addr64(&mut self, ty: Type) -> Option<Type> {
+            match ty {
+                I64 | R64 => Some(ty),
+                _ => None,
+            }
+        }
+
+        #[inline]
         fn u64_from_imm64(&mut self, imm: Imm64) -> u64 {
             imm.bits() as u64
         }
