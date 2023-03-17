2df107e517c6e1cc0d4c6b17c7cd15478abf9378
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-03-17 09:32:28 +0000

    Remove ProgramPoint

--- cranelift/codegen/src/ir/progpoint.rs
+++ cranelift/codegen/src/ir/progpoint.rs
@@ -1,45 +1,7 @@
 //! Program points.
 
-use crate::entity::EntityRef;
 use crate::ir::{Block, Inst, ValueDef};
 use core::fmt;
-use core::u32;
-
-/// A `ProgramPoint` represents a position in a function where the live range of an SSA value can
-/// begin or end. It can be either:
-///
-/// 1. An instruction or
-/// 2. A block header.
-///
-/// This corresponds more or less to the lines in the textual form of Cranelift IR.
-#[derive(PartialEq, Eq, Clone, Copy)]
-pub struct ProgramPoint(u32);
-
-impl From<Inst> for ProgramPoint {
-    fn from(inst: Inst) -> Self {
-        let idx = inst.index();
-        debug_assert!(idx < (u32::MAX / 2) as usize);
-        Self((idx * 2) as u32)
-    }
-}
-
-impl From<Block> for ProgramPoint {
-    fn from(block: Block) -> Self {
-        let idx = block.index();
-        debug_assert!(idx < (u32::MAX / 2) as usize);
-        Self((idx * 2 + 1) as u32)
-    }
-}
-
-impl From<ValueDef> for ProgramPoint {
-    fn from(def: ValueDef) -> Self {
-        match def {
-            ValueDef::Result(inst, _) => inst.into(),
-            ValueDef::Param(block, _) => block.into(),
-            ValueDef::Union(_, _) => panic!("Union does not have a single program point"),
-        }
-    }
-}
 
 /// An expanded program point directly exposes the variants, but takes twice the space to
 /// represent.
@@ -83,16 +45,6 @@ impl From<ValueDef> for ExpandedProgramPoint {
     }
 }
 
-impl From<ProgramPoint> for ExpandedProgramPoint {
-    fn from(pp: ProgramPoint) -> Self {
-        if pp.0 & 1 == 0 {
-            Self::Inst(Inst::from_u32(pp.0 / 2))
-        } else {
-            Self::Block(Block::from_u32(pp.0 / 2))
-        }
-    }
-}
-
 impl fmt::Display for ExpandedProgramPoint {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -102,22 +54,9 @@ impl fmt::Display for ExpandedProgramPoint {
     }
 }
 
-impl fmt::Display for ProgramPoint {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let epp: ExpandedProgramPoint = (*self).into();
-        epp.fmt(f)
-    }
-}
-
 impl fmt::Debug for ExpandedProgramPoint {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "ExpandedProgramPoint({})", self)
-    }
-}
-
-impl fmt::Debug for ProgramPoint {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "ProgramPoint({})", self)
     }
 }
 
@@ -133,8 +72,8 @@ mod tests {
         let i5 = Inst::new(5);
         let b3 = Block::new(3);
 
-        let pp1: ProgramPoint = i5.into();
-        let pp2: ProgramPoint = b3.into();
+        let pp1: ExpandedProgramPoint = i5.into();
+        let pp2: ExpandedProgramPoint = b3.into();
 
         assert_eq!(pp1.to_string(), "inst5");
         assert_eq!(pp2.to_string(), "block3");
