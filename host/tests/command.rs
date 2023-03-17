9f4984501af1e656fa02a6ea2d3ee13c197f1f7f
Pat Hickey <phickey@fastly.com>
2023-03-16 17:45:42 -0700

    look at all these new tests that pass!!!

--- host/tests/command.rs
+++ host/tests/command.rs
@@ -507,7 +507,7 @@ async fn run_dangling_fd(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
 }
 
 async fn run_dangling_symlink(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_directory_seek(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -555,7 +555,7 @@ async fn run_file_unbuffered_write(store: Store<WasiCtx>, wasi: Command) -> Resu
 }
 
 async fn run_interesting_paths(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_isatty(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -563,7 +563,7 @@ async fn run_isatty(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
 }
 
 async fn run_nofollow_errors(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_path_exists(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -575,11 +575,11 @@ async fn run_path_filestat(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
 }
 
 async fn run_path_link(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_path_open_create_existing(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_path_open_dirfd_not_dir(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -587,7 +587,7 @@ async fn run_path_open_dirfd_not_dir(store: Store<WasiCtx>, wasi: Command) -> Re
 }
 
 async fn run_path_open_missing(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_path_open_read_without_rights(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -595,7 +595,7 @@ async fn run_path_open_read_without_rights(store: Store<WasiCtx>, wasi: Command)
 }
 
 async fn run_path_rename(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_path_rename_dir_trailing_slashes(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -607,7 +607,7 @@ async fn run_path_rename_file_trailing_slashes(store: Store<WasiCtx>, wasi: Comm
 }
 
 async fn run_path_symlink_trailing_slashes(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_poll_oneoff_files(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -627,7 +627,7 @@ async fn run_remove_directory_trailing_slashes(store: Store<WasiCtx>, wasi: Comm
 }
 
 async fn run_remove_nonempty_directory(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_renumber(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -651,7 +651,7 @@ async fn run_symlink_filestat(store: Store<WasiCtx>, wasi: Command) -> Result<()
 }
 
 async fn run_symlink_loop(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_truncation_rights(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
@@ -659,7 +659,7 @@ async fn run_truncation_rights(store: Store<WasiCtx>, wasi: Command) -> Result<(
 }
 
 async fn run_unlink_file_trailing_slashes(store: Store<WasiCtx>, wasi: Command) -> Result<()> {
-    expect_fail(run_with_temp_dir(store, wasi).await)
+    run_with_temp_dir(store, wasi).await
 }
 
 async fn run_export_cabi_realloc(mut store: Store<WasiCtx>, wasi: Command) -> Result<()> {
