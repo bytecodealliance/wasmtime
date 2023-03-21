9352401665017f9eb2d9351e8c855e6db7899e00
Pat Hickey <phickey@fastly.com>
2023-03-21 16:48:16 -0700

    host and adapter: provide stdio via preopens, dont pass stdio or arg preopens to main
    stdio never gets initialized for the reactor, tha

--- host/src/command.rs
+++ host/src/command.rs
@@ -20,7 +20,6 @@ pub fn add_to_linker<T: Send>(
     wasi::instance_monotonic_clock::add_to_linker(l, f)?;
     wasi::instance_wall_clock::add_to_linker(l, f)?;
     wasi::filesystem::add_to_linker(l, f)?;
-    wasi::stderr::add_to_linker(l, f)?;
     wasi::poll::add_to_linker(l, f)?;
     wasi::streams::add_to_linker(l, f)?;
     wasi::random::add_to_linker(l, f)?;
@@ -33,6 +32,6 @@ pub fn add_to_linker<T: Send>(
     wasi::network::add_to_linker(l, f)?;
     wasi::exit::add_to_linker(l, f)?;
     wasi::environment::add_to_linker(l, f)?;
-    wasi::environment_preopens::add_to_linker(l, f)?;
+    wasi::preopens::add_to_linker(l, f)?;
     Ok(())
 }
