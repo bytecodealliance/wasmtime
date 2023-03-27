8696d83393aab3dd09320045a34301527be0ad70
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-03-27 20:17:32 +0200

    Revert "chore: include command host implementation for http"
    This reverts commit 9eea9cebd143914b192fcf12c7c185fe199379b6.

--- host/src/default_outgoing_http.rs
+++ host/src/default_outgoing_http.rs
@@ -1,27 +1,18 @@
 use crate::{
-    command, proxy,
-    proxy::wasi::types::{FutureIncomingResponse, OutgoingRequest, RequestOptions},
+    proxy::wasi,
+    proxy::wasi::types::{
+        FutureIncomingResponse as Response, OutgoingRequest as Request, RequestOptions,
+    },
     WasiCtx,
 };
 
 #[async_trait::async_trait]
-impl command::wasi::default_outgoing_http::Host for WasiCtx {
+impl wasi::default_outgoing_http::Host for WasiCtx {
     async fn handle(
         &mut self,
-        _req: OutgoingRequest,
-        _options: Option<command::wasi::types::RequestOptions>,
-    ) -> wasmtime::Result<FutureIncomingResponse> {
-        anyhow::bail!("not implemented")
-    }
-}
-
-#[async_trait::async_trait]
-impl proxy::wasi::default_outgoing_http::Host for WasiCtx {
-    async fn handle(
-        &mut self,
-        _req: OutgoingRequest,
+        _req: Request,
         _options: Option<RequestOptions>,
-    ) -> wasmtime::Result<FutureIncomingResponse> {
+    ) -> wasmtime::Result<Response> {
         anyhow::bail!("not implemented")
     }
 }
