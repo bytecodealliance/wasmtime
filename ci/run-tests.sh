64d697980d99708564c8f445e8a0a1b96355c1b7
Pat Hickey <phickey@fastly.com>
2023-03-28 16:06:41 -0700

    CI: run the http tests

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,6 +2,7 @@
 
 cargo test \
     --features "test-programs/test_programs" \
+    --features "test-programs/test_programs_http" \
     --features wasi-threads \
     --workspace \
     --exclude 'wasmtime-wasi-*' \
