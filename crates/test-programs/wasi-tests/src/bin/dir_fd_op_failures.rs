166b7a0756737c7efde4572f60dd54520377cf29
Pat Hickey <phickey@fastly.com>
2023-04-11 13:28:33 -0700

    Add comments to all the fd op failures explaining closest linux/posix behavior

--- crates/test-programs/wasi-tests/src/bin/dir_fd_op_failures.rs
+++ crates/test-programs/wasi-tests/src/bin/dir_fd_op_failures.rs
@@ -11,9 +11,11 @@ unsafe fn test_fd_dir_ops(dir_fd: wasi::Fd) {
         buf_len: read_buf.len(),
     };
     let r = wasi::fd_read(dir_fd, &[iovec]);
+    // On posix, this fails with ERRNO_ISDIR:
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_read error");
 
     let r = wasi::fd_pread(dir_fd, &[iovec], 0);
+    // On posix, this fails with ERRNO_ISDIR
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_pread error");
 
     let write_buf = vec![0; 128].into_boxed_slice();
@@ -22,11 +24,14 @@ unsafe fn test_fd_dir_ops(dir_fd: wasi::Fd) {
         buf_len: write_buf.len(),
     };
     let r = wasi::fd_write(dir_fd, &[ciovec]);
+    // Same behavior as specified by POSIX:
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_write error");
 
     let r = wasi::fd_pwrite(dir_fd, &[ciovec], 0);
+    // Same behavior as specified by POSIX:
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_pwrite error");
 
+    // Divergence from posix: lseek(dirfd) will return 0
     let r = wasi::fd_seek(dir_fd, 0, wasi::WHENCE_CUR);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_seek WHENCE_CUR error");
     let r = wasi::fd_seek(dir_fd, 0, wasi::WHENCE_SET);
@@ -34,24 +39,37 @@ unsafe fn test_fd_dir_ops(dir_fd: wasi::Fd) {
     let r = wasi::fd_seek(dir_fd, 0, wasi::WHENCE_END);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_seek WHENCE_END error");
 
+    // Tell isnt in posix, its basically lseek with WHENCE_CUR above
     let r = wasi::fd_tell(dir_fd);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_tell error");
 
+    // posix_fadvise(dirfd, 0, 0, POSIX_FADV_DONTNEED) will return 0 on linux.
+    // not available on mac os.
     let r = wasi::fd_advise(dir_fd, 0, 0, wasi::ADVICE_DONTNEED);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_advise error");
 
+    // fallocate(dirfd, FALLOC_FL_ZERO_RANGE, 0, 1) will fail with errno EBADF on linux.
+    // not available on mac os.
     let r = wasi::fd_allocate(dir_fd, 0, 0);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_allocate error");
 
+    // fdatasync(dirfd) will return 0 on linux.
+    // not available on mac os.
     let r = wasi::fd_datasync(dir_fd);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_datasync error");
 
+    // fsync(dirfd) will return 0 on linux.
+    // not available on mac os.
     let r = wasi::fd_sync(dir_fd);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_sync error");
 
+    // fcntl(dirfd,  F_SETFL, O_NONBLOCK) will return 0 on linux.
+    // not available on mac os.
     let r = wasi::fd_fdstat_set_flags(dir_fd, wasi::FDFLAGS_NONBLOCK);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_fdstat_set_flags error");
 
+    // ftruncate(dirfd, 1) will fail with errno EINVAL on posix.
+    // here, we fail with EBADF instead:
     let r = wasi::fd_filestat_set_size(dir_fd, 0);
     assert_eq!(r, Err(wasi::ERRNO_BADF), "fd_filestat_set_size error");
 }
