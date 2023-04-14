51dc803b3b2f2bfdfde392aa4a66a1d53b7ed91e
Pat Hickey <phickey@fastly.com>
2023-04-14 08:10:23 -0700

    bump wasmtime and use new syntax, but it doesnt quite work yet

--- host/src/wasi/mod.rs
+++ host/src/wasi/mod.rs
@@ -2,10 +2,29 @@ pub mod command;
 pub mod proxy;
 
 wasmtime::component::bindgen!({
-    path: "../wit",
-    // The commmand-extended world happens to encompass all of the available interfaces:
-    world: "command-extended",
-    only_interfaces: true,
+    // path: "../wit",
+    // This is a union of the imports in the command and proxy worlds:
+    interfaces: "
+      import wall-clock: clocks.wall-clock
+      import monotonic-clock: clocks.monotonic-clock
+      import timezone: clocks.timezone
+      import filesystem: filesystem.filesystem
+      import instance-network: sockets.instance-network
+      import ip-name-lookup: sockets.ip-name-lookup
+      import network: sockets.network
+      import tcp-create-socket: sockets.tcp-create-socket
+      import tcp: sockets.tcp
+      import udp-create-socket: sockets.udp-create-socket
+      import udp: sockets.udp
+      import random: random.random
+      import poll: poll.poll
+      import streams: io.streams
+      import environment: wasi-base.environment
+      import preopens: wasi-base.preopens
+      import exit: wasi-base.exit
+      import console: logging.handler
+      import default-outgoing-HTTP: http.outgoing-handler
+    ",
     tracing: true,
     async: true,
     trappable_error_type: {
