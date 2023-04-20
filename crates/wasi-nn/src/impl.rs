1ffce303e8b1480d46daf6312b194b2202060f6c
Matthew Tamayo-Rios <matthew@getmethodic.com>
2023-04-20 04:09:55 -0600

    Add unaccessible string array classes.

--- crates/wasi-nn/src/impl.rs
+++ crates/wasi-nn/src/impl.rs
@@ -209,3 +209,52 @@ impl<'a> WasiEphemeralNn for WasiNnCtx {
         }
     }
 }
+
+pub struct StringArray {
+    elems: Vec<String>,
+}
+
+impl StringArray {
+    pub fn new() -> Self {
+        StringArray { elems: Vec::new() }
+    }
+
+    pub fn number_elements(&self) -> u32 {
+        self.elems.len() as u32
+    }
+
+    pub fn cumulative_size(&self) -> u32 {
+        self.elems
+            .iter()
+            .map(|e| e.as_bytes().len() + 1)
+            .sum::<usize>() as u32
+    }
+
+    pub fn write_to_guest<'a>(
+        &self,
+        buffer: &GuestPtr<'a, u8>,
+        element_heads: &GuestPtr<'a, GuestPtr<'a, u8>>,
+    ) -> Result<()> {
+        println!("Model names to guest: {:?}", self.elems);
+        let element_heads = element_heads.as_array(self.number_elements());
+        let buffer = buffer.as_array(self.cumulative_size());
+        let mut cursor = 0;
+        for (elem, head) in self.elems.iter().zip(element_heads.iter()) {
+            let bytes = elem.as_bytes();
+            let len = bytes.len() as u32;
+            {
+                let elem_buffer = buffer
+                    .get_range(cursor..(cursor + len))
+                    .ok_or(UsageError::InvalidContext)?; // Elements don't fit in buffer provided
+                elem_buffer.copy_from_slice(bytes)?;
+            }
+            buffer
+                .get(cursor + len)
+                .ok_or(UsageError::InvalidContext)?
+                .write(0)?; // 0 terminate
+            head?.write(buffer.get(cursor).expect("already validated"))?;
+            cursor += len + 1;
+        }
+        Ok(())
+    }
+}
