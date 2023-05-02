bytecodealliance/wasmtime

commit 913efdf24d08c44128bd05743e870161e4998663

wasmtime: Overhaul trampolines (#6262)
This commit splits `VMCallerCheckedFuncRef::func_ptr` into three new function
pointers: `VMCallerCheckedFuncRef::{wasm,array,native}_call`. Each one has a
dedicated calling convention, so callers just choose the version that works for
them. This is as opposed to the previous behavior where we would chain together
many trampolines that converted between calling conventions, sometimes up to
four on the way into Wasm and four more on the way back out. See [0] for
details.

[0] https://github.com/bytecodealliance/rfcs/blob/main/accepted/tail-calls.md#a-review-of-our-existing-trampolines-calling-conventions-and-call-paths

Thanks to @bjorn3 for the initial idea of having multiple function pointers for
different calling conventions.

This is generally a nice ~5-10% speed up to our call benchmarks across the
board: both Wasm-to-host and host-to-Wasm. The one exception is typed calls from
Wasm to the host, which have a minor regression. We hypothesize that this is
because the old hand-written assembly trampolines did not maintain a call frame
and do a tail call, but the new Cranelift-generated trampolines do maintain a
call frame and do a regular call. The regression is only a couple nanoseconds,
which seems well-explained by these differences explain, and ultimately is not a
big deal.

However, this does lead to a ~5% code size regression for compiled modules.
Before, we compiled a trampoline per escaping function's signature and we
deduplicated these trampolines by signature. Now we compile two trampolines per
escaping function: one for if the host calls via the array calling convention
and one for it the host calls via the native calling convention. Additionally,
we compile a trampoline for every type in the module, in case there is a native
calling convention function from the host that we `call_indirect` of that
type. Much of this is in the `.eh_frame` section in the compiled module, because

tests/all/async_functions.rs
@@ -539,6 +539,7 @@ async fn resume_separate_thread3() {
 
 #[tokio::test]
 async fn recursive_async() -> Result<()> {
+    let _ = env_logger::try_init();
     let mut store = async_store();
     let m = Module::new(
         store.engine(),

tests/all/traps.rs
@@ -1365,3 +1365,106 @@ fn wasm_fault_address_reported_by_default() -> Result<()> {
     );
     Ok(())
 }
+
+#[test]
+fn trap_with_array_to_wasm_stack_args() -> Result<()> {
+    let engine = Engine::default();
+    let mut store = Store::new(&engine, ());
+    let module = Module::new(
+        &engine,
+        r#"
+            (module
+                (func $trap
+                    unreachable)
+                (func $run (param i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64)
+                    call $trap)
+                (export "run" (func $run))
+            )
+        "#,
+    )?;
+
+    let instance = Instance::new(&mut store, &module, &[])?;
+    let run = instance.get_func(&mut store, "run").unwrap();
+
+    let err = run
+        .call(
+            &mut store,
+            &[
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+                Val::I64(0),
+            ],
+            &mut [],
+        )
+        .unwrap_err();
+    assert!(err.is::<Trap>());
+
+    let trace = err.downcast_ref::<WasmBacktrace>().unwrap();
+    assert_eq!(trace.frames().len(), 2);
+    assert_eq!(trace.frames()[0].func_name(), Some("trap"));
+    assert_eq!(trace.frames()[1].func_name(), Some("run"));
+
+    Ok(())
+}
+
+#[test]
+fn trap_with_native_to_wasm_stack_args() -> Result<()> {
+    let engine = Engine::default();
+    let mut store = Store::new(&engine, ());
+    let module = Module::new(
+        &engine,
+        r#"
+            (module
+                (func $trap
+                    unreachable)
+                (func $run (param i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64 i64)
+                    call $trap)
+                (export "run" (func $run))
+            )
+        "#,
+    )?;
+
+    let instance = Instance::new(&mut store, &module, &[])?;
+    let run = instance.get_func(&mut store, "run").unwrap();
+
+    let err = run
+        .typed::<(
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+            i64,
+        ), ()>(&mut store)?
+        .call(&mut store, (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+        .unwrap_err();
+    assert!(err.is::<Trap>());
+
+    let trace = err.downcast_ref::<WasmBacktrace>().unwrap();
+    assert_eq!(trace.frames().len(), 2);
+    assert_eq!(trace.frames()[0].func_name(), Some("trap"));
+    assert_eq!(trace.frames()[1].func_name(), Some("run"));
+
+    Ok(())
+}

commit b82d8047dc2a3878157249122b9128ab0467394d

wasmtime: Fix resetting stack-walking registers when entering/exiting Wasm
Fixes a regression from #6262, originally reported in
https://github.com/bytecodealliance/wasmtime-dotnet/pull/245

The issue was that we would enter Wasm and save the stack-walking registers but
never clear them after Wasm returns. Then if a host-to-host call tried to
capture a stack, we would mistakenly attempt to use those stale registers to
start the stack walk. This mistake would be caught by an assertion, triggering a
panic.

This commit fixes the issue by managing the save/restore in the
`CallThreadState` construction/drop, rather than in the old `set_prev`
method.

Co-Authored-By: Alex Crichton <alex@alexcrichton.com>

crates/runtime/src/traphandlers.rs
@@ -248,7 +248,6 @@ where
 // usage of its accessor methods.
 mod call_thread_state {
     use super::*;
-    use std::mem;
 
     /// Temporary state stored on the stack which is registered in the `tls` module
     /// below for calls into wasm.
@@ -262,16 +261,27 @@ mod call_thread_state {
 
         prev: Cell<tls::Ptr>,
 
-        // The values of `VMRuntimeLimits::last_wasm_{exit_{pc,fp},entry_sp}` for
-        // the *previous* `CallThreadState`. Our *current* last wasm PC/FP/SP are
-        // saved in `self.limits`. We save a copy of the old registers here because
-        // the `VMRuntimeLimits` typically doesn't change across nested calls into
-        // Wasm (i.e. they are typically calls back into the same store and
-        // `self.limits == self.prev.limits`) and we must to maintain the list of
+        // The values of `VMRuntimeLimits::last_wasm_{exit_{pc,fp},entry_sp}`
+        // for the *previous* `CallThreadState` for this same store/limits. Our
+        // *current* last wasm PC/FP/SP are saved in `self.limits`. We save a
+        // copy of the old registers here because the `VMRuntimeLimits`
+        // typically doesn't change across nested calls into Wasm (i.e. they are
+        // typically calls back into the same store and `self.limits ==
+        // self.prev.limits`) and we must to maintain the list of
         // contiguous-Wasm-frames stack regions for backtracing purposes.
         old_last_wasm_exit_fp: Cell<usize>,
         old_last_wasm_exit_pc: Cell<usize>,
         old_last_wasm_entry_sp: Cell<usize>,
+    }
+
+    impl Drop for CallThreadState {
+        fn drop(&mut self) {
+            unsafe {
+                *(*self.limits).last_wasm_exit_fp.get() = self.old_last_wasm_exit_fp.get();
+                *(*self.limits).last_wasm_exit_pc.get() = self.old_last_wasm_exit_pc.get();
+                *(*self.limits).last_wasm_entry_sp.get() = self.old_last_wasm_entry_sp.get();
+            }
+        }
     }
 
     impl CallThreadState {
@@ -288,9 +298,9 @@ mod call_thread_state {
                 capture_backtrace,
                 limits,
                 prev: Cell::new(ptr::null()),
-                old_last_wasm_exit_fp: Cell::new(0),
-                old_last_wasm_exit_pc: Cell::new(0),
-                old_last_wasm_entry_sp: Cell::new(0),
+                old_last_wasm_exit_fp: Cell::new(unsafe { *(*limits).last_wasm_exit_fp.get() }),
+                old_last_wasm_exit_pc: Cell::new(unsafe { *(*limits).last_wasm_exit_pc.get() }),
+                old_last_wasm_entry_sp: Cell::new(unsafe { *(*limits).last_wasm_entry_sp.get() }),
             }
         }
 
@@ -314,83 +324,15 @@ mod call_thread_state {
             self.prev.get()
         }
 
-        /// Connect the link to the previous `CallThreadState`.
-        ///
-        /// Synchronizes the last wasm FP, PC, and SP on `self` and the old
-        /// `self.prev` for the given new `prev`, and returns the old
-        /// `self.prev`.
-        pub unsafe fn set_prev(&self, prev: tls::Ptr) -> tls::Ptr {
-            let old_prev = self.prev.get();
+        pub(crate) unsafe fn push(&self) {
+            assert!(self.prev.get().is_null());
+            self.prev.set(tls::raw::replace(self));
+        }
 
-            // Restore the old `prev`'s saved registers in its
-            // `VMRuntimeLimits`. This is necessary for when we are async
-            // suspending the top `CallThreadState` and doing `set_prev(null)`
-            // on it, and so any stack walking we do subsequently will start at
-            // the old `prev` and look at its `VMRuntimeLimits` to get the
-            // initial saved registers.
-            if let Some(old_prev) = old_prev.as_ref() {
-                *(*old_prev.limits).last_wasm_exit_fp.get() = self.old_last_wasm_exit_fp();
-                *(*old_prev.limits).last_wasm_exit_pc.get() = self.old_last_wasm_exit_pc();
-                *(*old_prev.limits).last_wasm_entry_sp.get() = self.old_last_wasm_entry_sp();
-            }
-
-            self.prev.set(prev);
-
-            let mut old_last_wasm_exit_fp = 0;
-            let mut old_last_wasm_exit_pc = 0;
-            let mut old_last_wasm_entry_sp = 0;
-            if let Some(prev) = prev.as_ref() {
-                // We are entering a new `CallThreadState` or resuming a
-                // previously suspended one. This means we will push new Wasm
-                // frames that save the new Wasm FP/SP/PC registers into
-                // `VMRuntimeLimits`, we need to first save the old Wasm
-                // FP/SP/PC registers into this new `CallThreadState` to
-                // maintain our list of contiguous Wasm frame regions that we
-                // use when capturing stack traces.
-                //
-                // NB: the Wasm<--->host trampolines saved the Wasm FP/SP/PC
-                // registers in the active-at-that-time store's
-                // `VMRuntimeLimits`. For the most recent FP/PC/SP that is the
-                // `state.prev.limits` (since we haven't entered this
-                // `CallThreadState` yet). And that can be a different
-                // `VMRuntimeLimits` instance from the currently active
-                // `state.limits`, which will be used by the upcoming call into
-                // Wasm! Consider the case where we have multiple, nested calls
-                // across stores (with host code in between, by necessity, since
-                // only things in the same store can be linked directly
-                // together):
-                //
-                //     | ...             |
-                //     | Host            |  |
-                //     +-----------------+  | stack
-                //     | Wasm in store A |  | grows
-                //     +-----------------+  | down
-                //     | Host            |  |
-                //     +-----------------+  |
-                //     | Wasm in store B |  V
-                //     +-----------------+
-                //
-                // In this scenario `state.limits != state.prev.limits`,
-                // i.e. `B.limits != A.limits`! Therefore we must take care to
-                // read the old FP/SP/PC from `state.prev.limits`, rather than
-                // `state.limits`, and store those saved registers into the
-                // current `state`.
-                //
-                // See also the comment above the
-                // `CallThreadState::old_last_wasm_*` fields.
-                old_last_wasm_exit_fp =
-                    mem::replace(&mut *(*prev.limits).last_wasm_exit_fp.get(), 0);
-                old_last_wasm_exit_pc =
-                    mem::replace(&mut *(*prev.limits).last_wasm_exit_pc.get(), 0);
-                old_last_wasm_entry_sp =
-                    mem::replace(&mut *(*prev.limits).last_wasm_entry_sp.get(), 0);
-            }
-
-            self.old_last_wasm_exit_fp.set(old_last_wasm_exit_fp);
-            self.old_last_wasm_exit_pc.set(old_last_wasm_exit_pc);
-            self.old_last_wasm_entry_sp.set(old_last_wasm_entry_sp);
-
-            old_prev
+        pub(crate) unsafe fn pop(&self) {
+            let prev = self.prev.replace(ptr::null());
+            let head = tls::raw::replace(prev);
+            assert!(std::ptr::eq(head, self));
         }
     }
 }
@@ -533,7 +475,6 @@ impl<T: Copy> Drop for ResetCell<'_, T> {
 // the caller to the trap site.
 mod tls {
     use super::CallThreadState;
-    use std::ptr;
 
     pub use raw::Ptr;
 
@@ -551,7 +492,7 @@ mod tls {
     //
     // Note, though, that if async support is disabled at compile time then
     // these functions are free to be inlined.
-    mod raw {
+    pub(super) mod raw {
         use super::CallThreadState;
         use std::cell::Cell;
         use std::ptr;
@@ -625,8 +566,7 @@ mod tls {
             // accidentally used later.
             let state = raw::get();
             if let Some(state) = state.as_ref() {
-                let prev_state = state.set_prev(ptr::null());
-                raw::replace(prev_state);
+                state.pop();
             } else {
                 // Null case: we aren't in a wasm context, so theres no tls to
                 // save for restoration.
@@ -640,18 +580,12 @@ mod tls {
         /// This is unsafe because it's intended to only be used within the
         /// context of stack switching within wasmtime.
         pub unsafe fn replace(self) {
-            // Null case: we aren't in a wasm context, so theres no tls
-            // to restore.
-            if self.state.is_null() {
-                return;
+            if let Some(state) = self.state.as_ref() {
+                state.push();
+            } else {
+                // Null case: we aren't in a wasm context, so theres no tls
+                // to restore.
             }
-
-            // We need to configure our previous TLS pointer to whatever is in
-            // TLS at this time, and then we set the current state to ourselves.
-            let prev = raw::get();
-            assert!((*self.state).prev().is_null());
-            (*self.state).set_prev(prev);
-            raw::replace(self.state);
         }
     }
 
@@ -668,18 +602,13 @@ mod tls {
             #[inline]
             fn drop(&mut self) {
                 unsafe {
-                    let prev = self.state.set_prev(ptr::null());
-                    let old_state = raw::replace(prev);
-                    debug_assert!(std::ptr::eq(old_state, self.state));
+                    self.state.pop();
                 }
             }
         }
 
-        let prev = raw::replace(state);
-
         unsafe {
-            state.set_prev(prev);
-
+            state.push();
             let reset = Reset { state };
             closure(reset.state)
         }

tests/all/async_functions.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, bail, Result};
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 use wasmtime::*;
 
@@ -705,4 +706,140 @@ fn noop_waker() -> Waker {
         RawWakerVTable::new(|ptr| RawWaker::new(ptr, &VTABLE), |_| {}, |_| {}, |_| {});
     const RAW: RawWaker = RawWaker::new(0 as *const (), &VTABLE);
     unsafe { Waker::from_raw(RAW) }
+}
+
+#[tokio::test]
+async fn non_stacky_async_activations() -> Result<()> {
+    let mut config = Config::new();
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
+    let mut store1: Store<Option<Pin<Box<dyn Future<Output = Result<()>> + Send>>>> =
+        Store::new(&engine, None);
+    let mut linker1 = Linker::new(&engine);
+
+    let module1 = Module::new(
+        &engine,
+        r#"
+            (module $m1
+                (import "" "host_capture_stack" (func $host_capture_stack))
+                (import "" "start_async_instance" (func $start_async_instance))
+                (func $capture_stack (export "capture_stack")
+                    call $host_capture_stack
+                )
+                (func $run_sync (export "run_sync")
+                    call $start_async_instance
+                )
+            )
+        "#,
+    )?;
+
+    let module2 = Module::new(
+        &engine,
+        r#"
+            (module $m2
+                (import "" "yield" (func $yield))
+
+                (func $run_async (export "run_async")
+                    call $yield
+                )
+            )
+        "#,
+    )?;
+
+    let stacks = Arc::new(Mutex::new(vec![]));
+    fn capture_stack(stacks: &Arc<Mutex<Vec<WasmBacktrace>>>, store: impl AsContext) {
+        let mut stacks = stacks.lock().unwrap();
+        stacks.push(wasmtime::WasmBacktrace::force_capture(store));
+    }
+
+    linker1.func_wrap0_async("", "host_capture_stack", {
+        let stacks = stacks.clone();
+        move |caller| {
+            capture_stack(&stacks, &caller);
+            Box::new(async { Ok(()) })
+        }
+    })?;
+
+    linker1.func_wrap0_async("", "start_async_instance", {
+        let stacks = stacks.clone();
+        move |mut caller| {
+            let stacks = stacks.clone();
+            capture_stack(&stacks, &caller);
+
+            let module2 = module2.clone();
+            let mut store2 = Store::new(caller.engine(), ());
+            let mut linker2 = Linker::new(caller.engine());
+            linker2
+                .func_wrap0_async("", "yield", {
+                    let stacks = stacks.clone();
+                    move |caller| {
+                        let stacks = stacks.clone();
+                        Box::new(async move {
+                            capture_stack(&stacks, &caller);
+                            tokio::task::yield_now().await;
+                            capture_stack(&stacks, &caller);
+                            Ok(())
+                        })
+                    }
+                })
+                .unwrap();
+
+            Box::new(async move {
+                let future = PollOnce::new(Box::pin({
+                    let stacks = stacks.clone();
+                    async move {
+                        let instance2 = linker2.instantiate_async(&mut store2, &module2).await?;
+
+                        instance2
+                            .get_func(&mut store2, "run_async")
+                            .unwrap()
+                            .call_async(&mut store2, &[], &mut [])
+                            .await?;
+
+                        capture_stack(&stacks, &store2);
+                        Ok(())
+                    }
+                }) as _)
+                .await;
+                capture_stack(&stacks, &caller);
+                *caller.data_mut() = Some(future);
+                Ok(())
+            })
+        }
+    })?;
+
+    let instance1 = linker1.instantiate_async(&mut store1, &module1).await?;
+    instance1
+        .get_typed_func::<(), ()>(&mut store1, "run_sync")?
+        .call_async(&mut store1, ())
+        .await?;
+    let future = store1.data_mut().take().unwrap();
+    future.await?;
+
+    instance1
+        .get_typed_func::<(), ()>(&mut store1, "capture_stack")?
+        .call_async(&mut store1, ())
+        .await?;
+
+    let stacks = stacks.lock().unwrap();
+    eprintln!("stacks = {stacks:#?}");
+
+    assert_eq!(stacks.len(), 6);
+    for (actual, expected) in stacks.iter().zip(vec![
+        vec!["run_sync"],
+        vec!["run_async"],
+        vec!["run_sync"],
+        vec!["run_async"],
+        vec![],
+        vec!["capture_stack"],
+    ]) {
+        eprintln!("expected = {expected:?}");
+        eprintln!("actual = {actual:?}");
+        assert_eq!(actual.frames().len(), expected.len());
+        for (actual, expected) in actual.frames().iter().zip(expected) {
+            assert_eq!(actual.func_name(), Some(expected));
+        }
+    }
+
+    Ok(())
 }

tests/all/traps.rs
@@ -1468,3 +1468,53 @@ fn trap_with_native_to_wasm_stack_args() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn dont_see_stale_stack_walking_registers() -> Result<()> {
+    let engine = Engine::default();
+
+    let module = Module::new(
+        &engine,
+        r#"
+            (module
+                (import "" "host_start" (func $host_start))
+                (import "" "host_get_trap" (func $host_get_trap))
+                (export "get_trap" (func $host_get_trap))
+
+                ;; We enter and exit Wasm, which saves registers in the
+                ;; `VMRuntimeLimits`. Later, when we call a re-exported host
+                ;; function, we should not accidentally reuse those saved
+                ;; registers.
+                (start $start)
+                (func $start
+                    (call $host_start)
+                )
+            )
+        "#,
+    )?;
+
+    let mut store = Store::new(&engine, ());
+    let mut linker = Linker::new(&engine);
+
+    let host_start = Func::new(
+        &mut store,
+        FuncType::new([], []),
+        |_caller, _args, _results| Ok(()),
+    );
+    linker.define(&store, "", "host_start", host_start)?;
+
+    let host_get_trap = Func::new(
+        &mut store,
+        FuncType::new([], []),
+        |_caller, _args, _results| Err(anyhow::anyhow!("trap!!!")),
+    );
+    linker.define(&store, "", "host_get_trap", host_get_trap)?;
+
+    let instance = linker.instantiate(&mut store, &module)?;
+    let get_trap = instance.get_func(&mut store, "get_trap").unwrap();
+
+    let err = get_trap.call(&mut store, &[], &mut []).unwrap_err();
+    assert!(err.to_string().contains("trap!!!"));
+
+    Ok(())
+}