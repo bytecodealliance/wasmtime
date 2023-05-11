ca455b8405d22fea0706cdb47d93575242f5d483
Afonso Bordado <afonsobordado@az8.co>
2023-05-11 20:35:34 +0100

    riscv64: Remove unused `imm5_zero`

--- cranelift/codegen/src/isa/riscv64/lower/isle.rs
+++ cranelift/codegen/src/isa/riscv64/lower/isle.rs
@@ -206,10 +206,6 @@ impl generated_code::Context for RV64IsleContext<'_, '_, MInst, Riscv64Backend> 
         Imm5::maybe_from_i8(i8::try_from(arg0 as i64).ok()?)
     }
     #[inline]
-    fn imm5_zero(&mut self) -> Imm5 {
-        Imm5::from_bits(0)
-    }
-    #[inline]
     fn uimm5_bitcast_to_imm5(&mut self, arg0: UImm5) -> Imm5 {
         Imm5::from_bits(arg0.bits() as u8)
     }
