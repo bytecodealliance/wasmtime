ddc9e66485b20761ec9b75e6cf0afda2b3484898
Afonso Bordado <afonsobordado@az8.co>
2023-05-16 12:15:11 +0100

    fuzzgen: Add `bb_padding_log2` option

--- cranelift/fuzzgen/src/lib.rs
+++ cranelift/fuzzgen/src/lib.rs
@@ -215,6 +215,12 @@ where
             builder.set("probestack_size_log2", &format!("{}", size))?;
         }
 
+        // Generate random basic block padding
+        let bb_padding = self
+            .u
+            .int_in_range(self.config.bb_padding_log2_size.clone())?;
+        builder.set("bb_padding_log2_minus_one", &format!("{}", bb_padding))?;
+
         // Fixed settings
 
         // We need llvm ABI extensions for i128 values on x86, so enable it regardless of
