103870c57172dd9423454204165ccf667b514672
Pat Hickey <phickey@fastly.com>
2023-05-16 20:05:48 -0700

    eelete all remaining files in from-preview2-prototying: done merging these into tree

--- from-preview2-prototyping/test-programs/wasi-tests/src/bin/environment.rs
+++
@@ -1,6 +0,0 @@
-use std::env;
-
-fn main() {
-    assert_eq!(env::args().collect::<Vec<_>>(), ["program", "/foo"]);
-    assert_eq!(env::var("TEST").as_deref(), Ok("1"));
-}
--- from-preview2-prototyping/test-programs/wasi-tests/src/bin/sched_yield.rs
+++
@@ -1,8 +0,0 @@
-unsafe fn test_sched_yield() {
-    wasi::sched_yield().expect("sched_yield");
-}
-
-fn main() {
-    // Run tests
-    unsafe { test_sched_yield() }
-}
