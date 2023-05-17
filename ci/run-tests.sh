6d24e0815deacd33a9df23a5c9cdb62d44635260
Pat Hickey <phickey@fastly.com>
2023-05-16 17:04:34 -0700

    just exclude wasi-tests and wasi-http-tests in run-tests.sh

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,9 +2,10 @@
 
 cargo test \
     --features "test-programs/test_programs" \
-    --features "test-programs/test_programs_http" \
     --features wasi-threads \
     --workspace \
     --exclude 'wasmtime-wasi-*' \
     --exclude wasi-crypto \
+    --exclude wasi-tests \
+    --exclude wasi-http-tests \
     $@
