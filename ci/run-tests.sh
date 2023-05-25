8a8faa88e836f7f3e0de534b4f263510c504edaa
Roman Volosatovs <rvolosatovs@riseup.net>
2023-05-25 14:30:51 +0200

    ci: do not cancel jobs, only test `test-programs`
    Flaky tests are flaky
    
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,12 +2,5 @@
 
 cargo test \
     --features "test-programs/test_programs" \
-    --features wasi-threads \
-    --workspace \
-    --exclude 'wasmtime-wasi-*' \
-    --exclude wasi-crypto \
-    --exclude wasi-tests \
-    --exclude wasi-http-tests \
-    --exclude command-tests \
-    --exclude reactor-tests \
+    -p test-programs \
     $@
