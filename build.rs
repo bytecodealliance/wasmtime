47561149c8f3cae1150c82ef824be390c62cb273
Afonso Bordado <afonso360@users.noreply.github.com>
2023-06-17 22:27:53 +0100

    riscv64: Implement a few misc SIMD instructions (#6598)
    * riscv64: Add immediate rule to `gen_vec_mask`
    
    * riscv64: Implement `scalar_to_vector`
    
    * riscv64: Implement vector `select`
    
    * riscv64: Implement SIMD `iabs`
    
    * wasmtime: Enable SIMD memory64 tests for riscv64
    
    * cranelift: Update targets for `simd-select` tests

--- build.rs
+++ build.rs
@@ -228,11 +228,6 @@ fn ignore(testsuite: &str, testname: &str, strategy: &str) -> bool {
                 return true;
             }
 
-            // The memory64 testsuite has a single SIMD test that we don't pass yet.
-            if testname == "simd" && testsuite == "memory64" {
-                return true;
-            }
-
             let known_failure = [
                 "canonicalize_nan",
                 "cvt_from_uint",
@@ -249,18 +244,14 @@ fn ignore(testsuite: &str, testname: &str, strategy: &str) -> bool {
                 "simd_i16x8_arith2",
                 "simd_i16x8_cmp",
                 "simd_i16x8_q15mulr_sat_s",
-                "simd_i32x4_arith2",
                 "simd_i32x4_cmp",
                 "simd_i32x4_trunc_sat_f32x4",
                 "simd_i32x4_trunc_sat_f64x2",
-                "simd_i64x2_arith2",
                 "simd_i64x2_cmp",
                 "simd_i8x16_arith2",
                 "simd_i8x16_cmp",
                 "simd_load",
-                "simd_load_zero",
                 "simd_splat",
-                "v128_select",
             ]
             .contains(&testname);
 
