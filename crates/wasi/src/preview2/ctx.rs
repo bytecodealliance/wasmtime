76ec3df79aaf113b99728b2ae0943b0edf806738
Dan Gohman <dev@sunfishcode.online>
2023-06-22 09:19:04 -0700

    Add a `set_random` function to `WasiCtxBuilder`. (#6596)
    * Add a `set_random` function to `WasiCtxBuilder`.
    
    Add a `set_random` function to `WasiCtxBuilder`, allowing users that
    construct a default interface via `WasiCtxBuilder::default()` or
    `WasiCtx::builder()` to initialize the `random` state.
    
    Fixes #6576.
    
    * Rename `set_random_for_testing` to `set_secure_random_to_custom_generator`
    
    Remove the `cfg(test)`, and add comments advising users of the potential
    hazards.
    
    * rustfmt
    
    * Fix documentation links.

--- crates/wasi/src/preview2/ctx.rs
+++ crates/wasi/src/preview2/ctx.rs
@@ -34,14 +34,14 @@ impl WasiCtxBuilder {
         let insecure_random_seed =
             cap_rand::thread_rng(cap_rand::ambient_authority()).gen::<u128>();
 
-        let mut result = Self::default()
+        let result = Self::default()
             .set_clocks(clocks::host::clocks_ctx())
             .set_insecure_random(insecure_random)
             .set_insecure_random_seed(insecure_random_seed)
             .set_stdin(pipe::ReadPipe::new(std::io::empty()))
             .set_stdout(pipe::WritePipe::new(std::io::sink()))
-            .set_stderr(pipe::WritePipe::new(std::io::sink()));
-        result.random = Some(random::thread_rng());
+            .set_stderr(pipe::WritePipe::new(std::io::sink()))
+            .set_secure_random();
         result
     }
 
@@ -113,11 +113,32 @@ impl WasiCtxBuilder {
         self
     }
 
-    /// Set the generator for the secure random number generator. This
-    /// is only avalabile under `cfg(test)` as it's only intended for use
-    /// by tests.
-    #[cfg(test)]
-    pub fn set_random(mut self, random: impl RngCore + Send + Sync + 'static) -> Self {
+    /// Set the generator for the secure random number generator.
+    ///
+    /// This initializes the random number generator using
+    /// [`cap_rand::thread_rng`].
+    pub fn set_secure_random(mut self) -> Self {
+        self.random = Some(random::thread_rng());
+        self
+    }
+
+    /// Set the generator for the secure random number generator to a custom
+    /// generator.
+    ///
+    /// This function is usually not needed; use [`set_secure_random`] to
+    /// install the default generator, which is intended to be sufficient for
+    /// most use cases.
+    ///
+    /// Guest code may rely on this random number generator to produce fresh
+    /// unpredictable random data in order to maintain its security invariants,
+    /// and ideally should use the insecure random API otherwise, so using any
+    /// prerecorded or otherwise predictable data may compromise security.
+    ///
+    /// [`set_secure_random`]: Self::set_secure_random
+    pub fn set_secure_random_to_custom_generator(
+        mut self,
+        random: impl RngCore + Send + Sync + 'static,
+    ) -> Self {
         self.random = Some(Box::new(random));
         self
     }
