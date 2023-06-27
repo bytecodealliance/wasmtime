983dfdf0230adf7370857a6a5504da247eab1709
Pat Hickey <phickey@fastly.com>
2023-06-26 19:39:41 -0700

    move unix stdin to its own file

--- crates/wasi/src/preview2/stdio.rs
+++ crates/wasi/src/preview2/stdio.rs
@@ -2,61 +2,11 @@ use anyhow::Error;
 
 use crate::preview2::{AsyncWriteStream, HostInputStream, HostOutputStream, StreamState};
 
-pub use self::stdin::{stdin, Stdin};
-
-// TODO: different cfg for windows here
+// TODO: different cfg for windows support
 #[cfg(unix)]
-mod stdin {
-    use crate::preview2::{AsyncFdStream, HostInputStream, StreamState};
-    use anyhow::Error;
-    use std::sync::OnceLock;
-    use tokio::sync::Mutex;
-
-    type GlobalStdin = Mutex<AsyncFdStream<std::io::Stdin>>;
-    static STDIN: OnceLock<GlobalStdin> = OnceLock::new();
-
-    // Must be running in a tokio context to succeed.
-    fn create() -> anyhow::Result<GlobalStdin> {
-        Ok(Mutex::new(AsyncFdStream::new(std::io::stdin())?))
-    }
-
-    pub struct Stdin;
-    impl Stdin {
-        fn get_global() -> &'static GlobalStdin {
-            match tokio::runtime::Handle::try_current() {
-                Ok(_) => STDIN.get_or_init(|| {
-                    create().expect("creating AsyncFd for stdin in existing tokio context")
-                }),
-                Err(_) => STDIN.get_or_init(|| {
-                    crate::preview2::poll::sync::block_on(async {
-                        create().expect("creating AsyncFd for stdin in internal tokio context")
-                    })
-                }),
-            }
-        }
-    }
-
-    pub fn stdin() -> Stdin {
-        Stdin
-    }
-
-    #[async_trait::async_trait]
-    impl HostInputStream for Stdin {
-        fn read(&mut self, buf: &mut [u8]) -> Result<(u64, StreamState), Error> {
-            let mut r = move || Self::get_global().blocking_lock().read(buf);
-            // If we are currently in a tokio context, blocking_lock will panic unless inside a
-            // block_in_place:
-            match tokio::runtime::Handle::try_current() {
-                Ok(_) => tokio::task::block_in_place(r),
-                Err(_) => r(),
-            }
-        }
-
-        async fn ready(&mut self) -> Result<(), Error> {
-            Self::get_global().lock().await.ready().await
-        }
-    }
-}
+mod unix;
+#[cfg(unix)]
+pub use self::unix::{stdin, Stdin};
 
 pub type Stdout = AsyncWriteStream<tokio::io::Stdout>;
 
---
+++ crates/wasi/src/preview2/stdio/unix.rs
@@ -0,0 +1,54 @@
+use crate::preview2::{AsyncFdStream, HostInputStream, StreamState};
+use anyhow::Error;
+use std::sync::OnceLock;
+// We use a tokio Mutex because, in ready(), the mutex needs to be held
+// across an await.
+use tokio::sync::Mutex;
+
+// We need a single global instance of the AsyncFd<Stdin> because creating
+// this instance registers the process's stdin fd with epoll, which will
+// return an error if an fd is registered more than once.
+type GlobalStdin = Mutex<AsyncFdStream<std::io::Stdin>>;
+static STDIN: OnceLock<GlobalStdin> = OnceLock::new();
+
+fn create() -> anyhow::Result<GlobalStdin> {
+    Ok(Mutex::new(AsyncFdStream::new(std::io::stdin())?))
+}
+
+pub struct Stdin;
+impl Stdin {
+    fn get_global() -> &'static GlobalStdin {
+        // Creation must be running in a tokio context to succeed.
+        match tokio::runtime::Handle::try_current() {
+            Ok(_) => STDIN.get_or_init(|| {
+                create().expect("creating AsyncFd for stdin in existing tokio context")
+            }),
+            Err(_) => STDIN.get_or_init(|| {
+                crate::preview2::poll::sync::block_on(async {
+                    create().expect("creating AsyncFd for stdin in internal tokio context")
+                })
+            }),
+        }
+    }
+}
+
+pub fn stdin() -> Stdin {
+    Stdin
+}
+
+#[async_trait::async_trait]
+impl HostInputStream for Stdin {
+    fn read(&mut self, buf: &mut [u8]) -> Result<(u64, StreamState), Error> {
+        let mut r = move || Self::get_global().blocking_lock().read(buf);
+        // If we are currently in a tokio context, blocking_lock will panic unless inside a
+        // block_in_place:
+        match tokio::runtime::Handle::try_current() {
+            Ok(_) => tokio::task::block_in_place(r),
+            Err(_) => r(),
+        }
+    }
+
+    async fn ready(&mut self) -> Result<(), Error> {
+        Self::get_global().lock().await.ready().await
+    }
+}
