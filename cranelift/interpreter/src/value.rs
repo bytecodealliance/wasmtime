9d29752d6071209ce59cc55b75ce80da6d754260
Matthias Schmidt <matthias.schmidt@gmail.com>
2023-07-11 09:16:20 +0200

    Reformat code

--- cranelift/interpreter/src/value.rs
+++ cranelift/interpreter/src/value.rs
@@ -496,7 +496,9 @@ impl DataValueExt for DataValue {
             DataValue::I128(f) => Ok(*f == 0),
             DataValue::F32(f) => Ok(f.is_zero()),
             DataValue::F64(f) => Ok(f.is_zero()),
-            DataValue::V64(_) | DataValue::V128(_) => Err(ValueError::InvalidType(ValueTypeClass::Float, self.ty())),
+            DataValue::V64(_) | DataValue::V128(_) => {
+                Err(ValueError::InvalidType(ValueTypeClass::Float, self.ty()))
+            }
         }
     }
 
