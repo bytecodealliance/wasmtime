41cb385f955c8b5b859ecd30180e21a121b55ced
Pat Hickey <phickey@fastly.com>
2023-07-26 16:33:40 -0700

    Scaffold out changes to table to track parent and children

--- crates/wasi/src/preview2/table.rs
+++ crates/wasi/src/preview2/table.rs
@@ -9,6 +9,8 @@ pub enum TableError {
     NotPresent,
     #[error("value is of another type")]
     WrongType,
+    #[error("entry still has children")]
+    HasChildren,
 }
 
 /// The `Table` type is designed to map u32 handles to resources. The table is now part of the
@@ -20,21 +22,58 @@ pub enum TableError {
 /// up. Right now it is just an approximation.
 #[derive(Debug)]
 pub struct Table {
-    pub(crate) map: HashMap<u32, Box<dyn Any + Send + Sync>>,
+    map: HashMap<u32, TableEntry>,
     next_key: u32,
 }
 
+#[derive(Debug)]
+pub struct TableEntry {
+    pub entry: Box<dyn Any + Send + Sync>,
+    pub parent: Option<u32>,
+    pub children: Vec<u32>,
+}
+
+pub struct OccupiedEntry<'a> {
+    table: &'a mut Table,
+    index: u32,
+}
+impl<'a> OccupiedEntry<'a> {
+    pub fn get(&self) -> &(dyn Any + Send + Sync + 'static) {
+        self.table.map.get(&self.index).unwrap().entry.as_ref()
+    }
+    pub fn get_mut(&mut self) -> &mut (dyn Any + Send + Sync + 'static) {
+        self.table.map.get_mut(&self.index).unwrap().entry.as_mut()
+    }
+    pub fn remove_entry(self) -> Box<dyn Any + Send + Sync> {
+        // FIXME: deletion logic requires ref to table itself, to delete the rest
+        todo!()
+    }
+    // TODO: insert is possible to implement, but make it fail with WrongType if the repalcement
+    // value downcasts to a different type than an existing one
+    // TODO: `is` would be useful to have here, for stream.rs
+}
+
 impl Table {
-    /// Create an empty table. New insertions will begin at 3, above stdio.
+    /// Create an empty table
     pub fn new() -> Self {
         Table {
             map: HashMap::new(),
-            next_key: 3, // 0, 1 and 2 are reserved for stdio
+            // 0, 1 and 2 are formerly (preview 1) for stdio. To prevent users from assuming these
+            // indicies are still valid ways to access stdio, they are deliberately left empty.
+            next_key: 3,
         }
     }
 
     /// Insert a resource at the next available index.
-    pub fn push(&mut self, a: Box<dyn Any + Send + Sync>) -> Result<u32, TableError> {
+    pub fn push(&mut self, entry: Box<dyn Any + Send + Sync>) -> Result<u32, TableError> {
+        self.push_(TableEntry {
+            entry,
+            parent: None,
+            children: Vec::new(),
+        })
+    }
+
+    fn push_(&mut self, e: TableEntry) -> Result<u32, TableError> {
         // NOTE: The performance of this new key calculation could be very bad once keys wrap
         // around.
         if self.map.len() == u32::MAX as usize {
@@ -46,7 +85,7 @@ impl Table {
             if self.map.contains_key(&key) {
                 continue;
             }
-            self.map.insert(key, a);
+            self.map.insert(key, e);
             return Ok(key);
         }
     }
@@ -60,7 +99,7 @@ impl Table {
     /// Note: this will always fail if the resource is already borrowed.
     pub fn is<T: Any + Sized>(&self, key: u32) -> bool {
         if let Some(r) = self.map.get(&key) {
-            r.is::<T>()
+            r.entry.is::<T>()
         } else {
             false
         }
@@ -71,7 +110,9 @@ impl Table {
     /// results in a trapping error.
     pub fn get<T: Any + Sized>(&self, key: u32) -> Result<&T, TableError> {
         if let Some(r) = self.map.get(&key) {
-            r.downcast_ref::<T>().ok_or_else(|| TableError::WrongType)
+            r.entry
+                .downcast_ref::<T>()
+                .ok_or_else(|| TableError::WrongType)
         } else {
             Err(TableError::NotPresent)
         }
@@ -81,42 +122,48 @@ impl Table {
     /// reference can be borrowed at any given time. Borrow failure results in a trapping error.
     pub fn get_mut<T: Any + Sized>(&mut self, key: u32) -> Result<&mut T, TableError> {
         if let Some(r) = self.map.get_mut(&key) {
-            r.downcast_mut::<T>().ok_or_else(|| TableError::WrongType)
+            r.entry
+                .downcast_mut::<T>()
+                .ok_or_else(|| TableError::WrongType)
         } else {
             Err(TableError::NotPresent)
         }
     }
 
-    /// Get an [`std::collections::hash_map::OccupiedEntry`] corresponding to
-    /// a table entry, if it exists. This allows you to remove or replace the
-    /// entry based on its contents.
-    pub fn entry(
-        &mut self,
-        key: u32,
-    ) -> Result<
-        std::collections::hash_map::OccupiedEntry<u32, Box<dyn Any + Send + Sync + 'static>>,
-        TableError,
-    > {
-        use std::collections::hash_map::Entry;
-        match self.map.entry(key) {
-            Entry::Occupied(occ) => Ok(occ),
-            Entry::Vacant(_) => Err(TableError::NotPresent),
+    /// Get an [`OccupiedEntry`] corresponding to a table entry, if it exists. This allows you to
+    /// remove or replace the entry based on its contents. The methods available are a subset of
+    /// [`std::collections::hash_map::OccupiedEntry`] - it does not give access to the key, it
+    /// restricts replacing the entry to items of the same type, and it does not allow for deletion.
+    pub fn entry(&mut self, index: u32) -> Result<OccupiedEntry, TableError> {
+        if self.map.contains_key(&index) {
+            Ok(OccupiedEntry { table: self, index })
+        } else {
+            Err(TableError::NotPresent)
         }
     }
 
     /// Remove a resource at a given index from the table.
     pub fn delete<T: Any + Sized>(&mut self, key: u32) -> Result<T, TableError> {
         // Optimistically attempt to remove the value stored under key
-        match self
-            .map
-            .remove(&key)
-            .ok_or(TableError::NotPresent)?
-            .downcast::<T>()
-        {
-            Ok(v) => Ok(*v),
-            Err(v) => {
+        let e = self.map.remove(&key).ok_or(TableError::NotPresent)?;
+        if !e.children.is_empty() {
+            return Err(TableError::HasChildren);
+        }
+        match e.entry.downcast::<T>() {
+            Ok(v) => {
+                // TODO: if it has a parent, remove from parents child list
+                Ok(*v)
+            }
+            Err(entry) => {
                 // Insert the value back, since the downcast failed
-                self.map.insert(key, v);
+                self.map.insert(
+                    key,
+                    TableEntry {
+                        entry,
+                        children: e.children,
+                        parent: e.parent,
+                    },
+                );
                 Err(TableError::WrongType)
             }
         }
@@ -133,7 +180,7 @@ impl Table {
             let item = self
                 .map
                 .get_mut(&k)
-                .map(Box::as_mut)
+                .map(|e| Box::as_mut(&mut e.entry))
                 // Safety: extending the lifetime of the mutable reference.
                 .map(|item| unsafe { &mut *(item as *mut dyn Any) })
                 .ok_or(TableError::NotPresent);
