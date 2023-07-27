dc835d954d71b3306838868e2ba25da3d92f9065
Trevor Elliott <telliott@fastly.com>
2023-07-27 15:27:16 -0700

    Refactor the test to restart the runtime as well

--- crates/wasi/src/preview2/stdio.rs
+++ crates/wasi/src/preview2/stdio.rs
@@ -83,82 +83,87 @@ mod test {
     {
         test_child_stdin(
             |mut result_write| {
-                tokio::runtime::Builder::new_multi_thread()
-                    .enable_all()
-                    .build()
-                    .unwrap()
-                    .block_on(async {
-                        let mut running = true;
-                        while running {
-                            println!("child: creating stdin");
-                            let mut stdin = mk_stdin();
+                let mut child_running = true;
+                while child_running {
+                    tokio::runtime::Builder::new_multi_thread()
+                        .enable_all()
+                        .build()
+                        .unwrap()
+                        .block_on(async {
+                            'task: loop {
+                                println!("child: creating stdin");
+                                let mut stdin = mk_stdin();
 
-                            println!("child: checking that stdin is not ready");
-                            assert!(
-                                tokio::time::timeout(
-                                    std::time::Duration::from_millis(100),
-                                    stdin.ready()
-                                )
-                                .await
-                                .is_err(),
-                                "stdin available too soon"
-                            );
+                                println!("child: checking that stdin is not ready");
+                                assert!(
+                                    tokio::time::timeout(
+                                        std::time::Duration::from_millis(100),
+                                        stdin.ready()
+                                    )
+                                    .await
+                                    .is_err(),
+                                    "stdin available too soon"
+                                );
 
-                            writeln!(&mut result_write, "start").unwrap();
+                                writeln!(&mut result_write, "start").unwrap();
 
-                            println!("child: started");
+                                println!("child: started");
 
-                            let mut buffer = String::new();
-                            loop {
-                                println!("child: waiting for stdin to be ready");
-                                stdin.ready().await.unwrap();
+                                let mut buffer = String::new();
+                                loop {
+                                    println!("child: waiting for stdin to be ready");
+                                    stdin.ready().await.unwrap();
 
-                                println!("child: reading input");
-                                let (bytes, status) = stdin.read(1024).unwrap();
+                                    println!("child: reading input");
+                                    let (bytes, status) = stdin.read(1024).unwrap();
 
-                                println!("child: {:?}, {:?}", bytes, status);
+                                    println!("child: {:?}, {:?}", bytes, status);
 
-                                // We can't effectively test for the case where stdin was closed.
-                                assert_eq!(status, StreamState::Open);
+                                    // We can't effectively test for the case where stdin was closed.
+                                    assert_eq!(status, StreamState::Open);
 
-                                println!("sleeping");
-                                tokio::task::yield_now().await;
-                                //tokio::time::sleep(std::time::Duration::from_millis(1)).await;
-                                println!("done sleeping");
+                                    buffer.push_str(std::str::from_utf8(bytes.as_ref()).unwrap());
+                                    if let Some((line, rest)) = buffer.split_once('\n') {
+                                        if line == "all done" {
+                                            writeln!(&mut result_write, "done").unwrap();
+                                            println!("child: exiting...");
+                                            child_running = false;
+                                            break 'task;
+                                        } else if line == "restart_runtime" {
+                                            writeln!(&mut result_write, "restarting").unwrap();
+                                            println!("child: restarting runtime...");
+                                            break 'task;
+                                        } else if line == "restart_task" {
+                                            writeln!(&mut result_write, "restarting").unwrap();
+                                            println!("child: restarting task...");
+                                            continue 'task;
+                                        } else {
+                                            writeln!(&mut result_write, "{}", line).unwrap();
+                                        }
 
-                                buffer.push_str(std::str::from_utf8(bytes.as_ref()).unwrap());
-                                if let Some((line, rest)) = buffer.split_once('\n') {
-                                    if line == "all done" {
-                                        writeln!(&mut result_write, "done").unwrap();
-                                        println!("child: exiting...");
-                                        running = false;
-                                        break;
-                                    } else if line == "restart" {
-                                        writeln!(&mut result_write, "restarting").unwrap();
-                                        println!("child: restarting...");
-
-                                        break;
-                                    } else {
-                                        writeln!(&mut result_write, "{}", line).unwrap();
+                                        buffer = rest.to_owned();
                                     }
-
-                                    buffer = rest.to_owned();
                                 }
                             }
-                        }
-                    });
+                        });
+                    println!("runtime exited");
+                }
+                println!("child exited");
             },
             |mut stdin_write, mut result_read| {
                 let mut line = String::new();
                 result_read.read_line(&mut line).unwrap();
                 assert_eq!(line, "start\n");
 
-                writeln!(&mut stdin_write, "some bytes").unwrap();
-                line.clear();
-                result_read.read_line(&mut line).unwrap();
-                assert_eq!(line, "some bytes\n");
+                for i in 0..5 {
+                    let message = format!("some bytes {}\n", i);
+                    stdin_write.write_all(message.as_bytes()).unwrap();
+                    line.clear();
+                    result_read.read_line(&mut line).unwrap();
+                    assert_eq!(line, message);
+                }
 
-                writeln!(&mut stdin_write, "restart").unwrap();
+                writeln!(&mut stdin_write, "restart_task").unwrap();
                 line.clear();
                 result_read.read_line(&mut line).unwrap();
                 assert_eq!(line, "restarting\n");
@@ -167,11 +172,31 @@ mod test {
                 result_read.read_line(&mut line).unwrap();
                 assert_eq!(line, "start\n");
 
-                println!("parent: writing `more bytes`");
-                writeln!(&mut stdin_write, "more bytes").unwrap();
+                for i in 0..10 {
+                    let message = format!("more bytes {}\n", i);
+                    stdin_write.write_all(message.as_bytes()).unwrap();
+                    line.clear();
+                    result_read.read_line(&mut line).unwrap();
+                    assert_eq!(line, message);
+                }
+
+                writeln!(&mut stdin_write, "restart_runtime").unwrap();
                 line.clear();
                 result_read.read_line(&mut line).unwrap();
-                assert_eq!(line, "more bytes\n");
+                assert_eq!(line, "restarting\n");
+                line.clear();
+
+                result_read.read_line(&mut line).unwrap();
+                assert_eq!(line, "start\n");
+
+                for i in 0..17 {
+                    let message = format!("even more bytes {}\n", i);
+                    stdin_write.write_all(message.as_bytes()).unwrap();
+                    line.clear();
+                    result_read.read_line(&mut line).unwrap();
+                    assert_eq!(line, message);
+                }
+
 
                 writeln!(&mut stdin_write, "all done").unwrap();
 
