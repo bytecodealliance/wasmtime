959c648040dd2a4e94c1ed5ff8e906aba7b7824a
Alex Crichton <alex@alexcrichton.com>
2023-08-09 18:43:04 +0200

    Configure Mach ports vs signals via `Config` (#6807)
    * Configure Mach ports vs signals via `Config`
    
    This commit adds a `Config::macos_use_mach_ports` configuration option
    to replace the old `posix-signals-on-macos` compile-time Cargo feature.
    This'll make Wasmtime a tad larger on macOS but likely negligibly so.
    Otherwise this is intended to provide a resolution to #6785 where
    embedders will be able to use any build of Wasmtime and configure at
    runtime how trap handling should happen.
    
    Functionally this commit additionally registers a `pthread_atfork`
    handler to cause any usage of Wasmtime in the child to panic. This
    should help head off a known-invalid state in case anyone runs into it
    in the future.
    
    * Fix build on non-macOS

--- crates/wasmtime/src/config.rs
+++ crates/wasmtime/src/config.rs
@@ -111,6 +111,7 @@ pub struct Config {
     pub(crate) memory_guaranteed_dense_image_size: u64,
     pub(crate) force_memory_init_memfd: bool,
     pub(crate) coredump_on_trap: bool,
+    pub(crate) macos_use_mach_ports: bool,
 }
 
 /// User-provided configuration for the compiler.
@@ -201,6 +202,7 @@ impl Config {
             memory_guaranteed_dense_image_size: 16 << 20,
             force_memory_init_memfd: false,
             coredump_on_trap: false,
+            macos_use_mach_ports: true,
         };
         #[cfg(any(feature = "cranelift", feature = "winch"))]
         {
@@ -1689,8 +1691,41 @@ impl Config {
 
     /// Enables clif output when compiling a WebAssembly module.
     #[cfg(any(feature = "cranelift", feature = "winch"))]
-    pub fn emit_clif(&mut self, path: &Path) {
+    pub fn emit_clif(&mut self, path: &Path) -> &mut Self {
         self.compiler_config.clif_dir = Some(path.to_path_buf());
+        self
+    }
+
+    /// Configures whether, when on macOS, Mach ports are used for exception
+    /// handling instead of traditional Unix-based signal handling.
+    ///
+    /// WebAssembly traps in Wasmtime are implemented with native faults, for
+    /// example a `SIGSEGV` will occur when a WebAssembly guest accesses
+    /// out-of-bounds memory. Handling this can be configured to either use Unix
+    /// signals or Mach ports on macOS. By default Mach ports are used.
+    ///
+    /// Mach ports enable Wasmtime to work by default with foreign
+    /// error-handling systems such as breakpad which also use Mach ports to
+    /// handle signals. In this situation Wasmtime will continue to handle guest
+    /// faults gracefully while any non-guest faults will get forwarded to
+    /// process-level handlers such as breakpad. Some more background on this
+    /// can be found in #2456.
+    ///
+    /// A downside of using mach ports, however, is that they don't interact
+    /// well with `fork()`. Forking a Wasmtime process on macOS will produce a
+    /// child process that cannot successfully run WebAssembly. In this
+    /// situation traditional Unix signal handling should be used as that's
+    /// inherited and works across forks.
+    ///
+    /// If your embedding wants to use a custom error handler which leverages
+    /// Mach ports and you additionally wish to `fork()` the process and use
+    /// Wasmtime in the child process that's not currently possible. Please
+    /// reach out to us if you're in this bucket!
+    ///
+    /// This option defaults to `true`, using Mach ports by default.
+    pub fn macos_use_mach_ports(&mut self, mach_ports: bool) -> &mut Self {
+        self.macos_use_mach_ports = mach_ports;
+        self
     }
 }
 
--- crates/wasmtime/src/engine.rs
+++ crates/wasmtime/src/engine.rs
@@ -77,7 +77,7 @@ impl Engine {
         // Ensure that wasmtime_runtime's signal handlers are configured. This
         // is the per-program initialization required for handling traps, such
         // as configuring signals, vectored exception handlers, etc.
-        wasmtime_runtime::init_traps(crate::module::is_wasm_trap_pc);
+        wasmtime_runtime::init_traps(crate::module::is_wasm_trap_pc, config.macos_use_mach_ports);
         debug_builtins::ensure_exported();
 
         let registry = SignatureRegistry::new();
