3efa7f5b8550a50f7e216724ef320e2fd240ddd1
Andrew Brown <andrew.brown@intel.com>
2023-08-09 15:12:34 -0700

    fix: remove broken doc links

--- crates/wasi-nn/src/types.rs
+++ crates/wasi-nn/src/types.rs
@@ -1,13 +1,10 @@
 //! The `wasi-nn` types used internally in this crate.
 //!
-//! These types form a common "ground truth" for the [`preview1`] and
-//! [`preview2`] types to be converted from and to. As such, these types should
-//! be kept up to date with the WIT and WITX specifications; if anything changes
-//! in the specifications, we should see compile errors in the conversion
-//! functions (e.g., `impl From<witx::...> for `crate::...`).
-//!
-//! [`preview1`]: crate::preview1
-//! [`preview2`]: crate::preview2
+//! These types form a common "ground truth" for the `witx` and `wit` ABI types
+//! to be converted from and to. As such, these types should be kept up to date
+//! with the WIT and WITX specifications; if anything changes in the
+//! specifications, we should see compile errors in the conversion functions
+//! (e.g., `impl From<witx::...> for `crate::...`).
 
 pub struct Tensor<'a> {
     pub dims: &'a [usize],
