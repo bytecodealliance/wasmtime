770c5d0f3f8727d76ccb5cb1a29ab3cc6f28314a
Andrew Brown <andrew.brown@intel.com>
2023-08-22 17:11:26 -0700

    wasi-nn: add named models (#6854)
    * wasi-nn: add [named models]
    
    This change adds a way to retrieve preloaded ML models (i.e., "graphs"
    in wasi-nn terms) from a registry. The wasi-nn specification includes a
    new function, `load_by_name`, that can be used to access these models
    more efficiently than before; previously, a user's only option was to
    read/download/etc. all of the bytes of an ML model and pass them to the
    `load` function.
    
    [named models]: https://github.com/WebAssembly/wasi-nn/issues/36
    
    In Wasmtime's implementation of wasi-nn, we call the registry that holds
    the models a `GraphRegistry`. We include a simplistic `InMemoryRegistry`
    for use in the Wasmtime CLI (more on this later) but the idea is that
    production use will involve some more complex caching and thus a new
    implementation of a registry--a `Box<dyn GraphRegistry>`--passed into
    the wasi-nn context. Note that, because we now must be able to `clone` a
    graph out of the registry and into the "used graphs" table, the OpenVINO
    `BackendGraph` is updated to be easier to copy around.
    
    To allow experimentation with this "preload a named model"
    functionality, this change also adds a new Wasmtime CLI flag: `--graph
    <encoding>:<host dir>`. Wasmtime CLI users can now preload a model from
    a directory; the directory `basename` is used as the model name. Loading
    models from a directory is probably not desired in Wasmtime embeddings
    so it is cordoned off into a separate `BackendFromDir` extension trait.
    
    * wasi-nn: add "named model" test
    
    Add a new example crate which loads a model by name and performs image
    classification. It uses the same MobileNet model as the existing test
    but a new version of the Rust bindings. The new crate is built and run
    with the new CLI flag in the `ci/run-wasi-nn-example.sh` script.
    
    prtest:full
    
    * review: rename `--graph` to `--wasi-nn-graph`

---
+++ crates/wasi-nn/src/registry/mod.rs
@@ -0,0 +1,16 @@
+//! Define the registry API.
+//!
+//! A [`GraphRegistry`] is place to store backend graphs so they can be loaded
+//! by name. This API does not mandate how a graph is loaded or how it must be
+//! stored--it could be stored remotely and rematerialized when needed, e.g. A
+//! naive in-memory implementation, [`InMemoryRegistry`] is provided for use
+//! with the Wasmtime CLI.
+
+mod in_memory;
+
+use crate::Graph;
+pub use in_memory::InMemoryRegistry;
+
+pub trait GraphRegistry: Send + Sync {
+    fn get_mut(&mut self, name: &str) -> Option<&mut Graph>;
+}
