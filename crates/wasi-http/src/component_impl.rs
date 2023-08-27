b15b0a713f0948e851eb97dab4f14594cbc66e7c
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-08-27 23:00:18 +0200

    fix(wasi-http): missing response body in sync api

--- crates/wasi-http/src/component_impl.rs
+++ crates/wasi-http/src/component_impl.rs
@@ -1254,6 +1254,9 @@ pub mod sync {
                     stream
                 );
                 let result = io::streams::Host::subscribe_to_input_stream(ctx, stream)?;
+                // TODO: necessary until this PR has been merged:
+                // https://github.com/bytecodealliance/wasmtime/pull/6877
+                let _ = poll::poll::Host::poll_oneoff(ctx, vec![result])?;
                 tracing::trace!(
                     "[module='wasi:io/streams' function='subscribe-to-input-stream'] return result=Ok({:?})",
                     result
