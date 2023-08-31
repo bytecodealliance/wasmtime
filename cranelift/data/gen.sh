b129925caf82d77fe35dd0d307425b9f147f2d01
Andrei Kashin <kashin.andrej@gmail.com>
2023-08-31 12:36:00 +0100

    Update generated zkasm with conditionals

---
+++ cranelift/data/gen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cargo build
+for name in add counter add_func fibonacci locals locals_simple
+do
+	echo $name;
+	../target/debug/clif-util wasm --target sparc-unknown-unknown ../../zkwasm/data/$name.wat > data/$name.zkasm
+done
