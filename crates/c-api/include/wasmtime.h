8f917d714ef6858220810467dfa4f368975f5a40
wasmtime-publish <59749941+wasmtime-publish@users.noreply.github.com>
2023-09-14 13:29:20 -0500

    Release Wasmtime 12.0.2 (#6992)
    * Release Wasmtime 12.0.2
    
    [automatically-tag-and-release-this-commit]
    
    * Update vet metadata
    
    ---------
    
    Co-authored-by: Wasmtime Publish <wasmtime-publish@users.noreply.github.com>
    Co-authored-by: Alex Crichton <alex@alexcrichton.com>

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -199,7 +199,7 @@
 /**
  * \brief Wasmtime version string.
  */
-#define WASMTIME_VERSION "12.0.1"
+#define WASMTIME_VERSION "12.0.2"
 /**
  * \brief Wasmtime major version number.
  */
@@ -211,7 +211,7 @@
 /**
  * \brief Wasmtime patch version number.
  */
-#define WASMTIME_VERSION_PATCH 1
+#define WASMTIME_VERSION_PATCH 2
 
 #ifdef __cplusplus
 extern "C" {
