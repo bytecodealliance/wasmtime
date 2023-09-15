c0194ec1959c56b74de894e145018bce902a681e
Dan Gohman <dev@sunfishcode.online>
2023-09-15 12:37:36 -0700

    Start adapting the wasi-http code to the new bindings.

--- crates/wasi-http/src/types_impl.rs
+++ crates/wasi-http/src/types_impl.rs
@@ -20,6 +20,7 @@ use wasmtime_wasi::preview2::{
     bindings::poll::poll::Pollable,
     HostPollable, PollableFuture, TablePollableExt, TableStreamExt,
 };
+use wasmtime::component::Resource;
 
 impl<T: WasiHttpView> crate::bindings::http::types::Host for T {
     fn drop_fields(&mut self, fields: Fields) -> wasmtime::Result<()> {
@@ -118,6 +119,36 @@ impl<T: WasiHttpView> crate::bindings::http::types::Host for T {
     }
     fn drop_outgoing_request(&mut self, request: OutgoingRequest) -> wasmtime::Result<()> {
         self.table().delete_outgoing_request(request)?;
+    async fn finish_outgoing_stream(
+        &mut self,
+        _s: Resource<OutgoingStream>,
+        _trailers: Option<Trailers>,
+    ) -> wasmtime::Result<()> {
+        bail!("unimplemented: finish_outgoing_stream")
+    }
+    async fn drop_incoming_request(&mut self, _request: IncomingRequest) -> wasmtime::Result<()> {
+        bail!("unimplemented: drop_incoming_request")
+    }
+    async fn drop_outgoing_request(&mut self, request: OutgoingRequest) -> wasmtime::Result<()> {
+        let r = self
+            .table_mut()
+            .get_request(request)
+            .context("[drop_outgoing_request] getting fields")?;
+
+        // Cleanup dependent resources
+        let body = r.body();
+        let headers = r.headers();
+        if let Some(b) = body {
+            self.table_mut().delete_stream(b).ok();
+        }
+        if let Some(h) = headers {
+            self.table_mut().delete_fields(h).ok();
+        }
+
+        self.table_mut()
+            .delete_request(request)
+            .context("[drop_outgoing_request] deleting request")?;
+
         Ok(())
     }
     fn incoming_request_method(&mut self, _request: IncomingRequest) -> wasmtime::Result<Method> {
@@ -373,7 +404,7 @@ impl<T: WasiHttpView> crate::bindings::http::types::Host for T {
     fn listen_to_future_incoming_response(
         &mut self,
         id: FutureIncomingResponse,
-    ) -> wasmtime::Result<Pollable> {
+    ) -> wasmtime::Result<Resource<Pollable>> {
         let _ = self.table().get_future_incoming_response(id)?;
 
         fn make_future<'a>(elem: &'a mut dyn Any) -> PollableFuture<'a> {
@@ -394,7 +425,7 @@ impl<T: WasiHttpView> crate::bindings::http::types::Host for T {
     fn incoming_body_stream(
         &mut self,
         id: IncomingBody,
-    ) -> wasmtime::Result<Result<InputStream, ()>> {
+    ) -> wasmtime::Result<Result<Resource<InputStream>, ()>> {
         let body = self.table().get_incoming_body(id)?;
 
         if let Some(stream) = body.stream.take() {
@@ -458,5 +489,313 @@ impl<T: WasiHttpView> crate::bindings::http::types::Host for T {
     fn drop_outgoing_body(&mut self, id: OutgoingBody) -> wasmtime::Result<()> {
         let _ = self.table().delete_outgoing_body(id)?;
         Ok(())
+||||||| parent of 7880fb3ac (Start adapting the wasi-http code to the new bindings.)
+            .get_future(future)
+            .context("[listen_to_future_incoming_response] getting future")?;
+        Ok(match f.pollable_id() {
+            Some(pollable_id) => pollable_id,
+            None => {
+                tracing::debug!("including pollable id to future incoming response");
+                let pollable =
+                    HostPollable::Closure(Box::new(|| Box::pin(futures::future::ready(Ok(())))));
+                let pollable_id = self
+                    .table_mut()
+                    .push_host_pollable(pollable)
+                    .context("[listen_to_future_incoming_response] pushing host pollable")?;
+                let f = self
+                    .table_mut()
+                    .get_future_mut(future)
+                    .context("[listen_to_future_incoming_response] getting future")?;
+                f.set_pollable_id(pollable_id);
+                tracing::trace!("future incoming response details {:?}", *f);
+                pollable_id
+            }
+        })
+    }
+}
+
+#[cfg(feature = "sync")]
+pub mod sync {
+    use crate::bindings::http::types::{
+        Error as AsyncError, Host as AsyncHost, Method as AsyncMethod, Scheme as AsyncScheme,
+    };
+    use crate::bindings::sync::http::types::{
+        Error, Fields, FutureIncomingResponse, Headers, IncomingRequest, IncomingResponse,
+        IncomingStream, Method, OutgoingRequest, OutgoingResponse, OutgoingStream,
+        ResponseOutparam, Scheme, StatusCode, Trailers,
+    };
+    use crate::http_impl::WasiHttpViewExt;
+    use crate::WasiHttpView;
+    use wasmtime_wasi::preview2::{bindings::poll::poll::Pollable, in_tokio};
+
+    // same boilerplate everywhere, converting between two identical types with different
+    // definition sites. one day wasmtime-wit-bindgen will make all this unnecessary
+    impl From<AsyncError> for Error {
+        fn from(other: AsyncError) -> Self {
+            match other {
+                AsyncError::InvalidUrl(v) => Self::InvalidUrl(v),
+                AsyncError::ProtocolError(v) => Self::ProtocolError(v),
+                AsyncError::TimeoutError(v) => Self::TimeoutError(v),
+                AsyncError::UnexpectedError(v) => Self::UnexpectedError(v),
+            }
+        }
+    }
+
+    impl From<Error> for AsyncError {
+        fn from(other: Error) -> Self {
+            match other {
+                Error::InvalidUrl(v) => Self::InvalidUrl(v),
+                Error::ProtocolError(v) => Self::ProtocolError(v),
+                Error::TimeoutError(v) => Self::TimeoutError(v),
+                Error::UnexpectedError(v) => Self::UnexpectedError(v),
+            }
+        }
+    }
+
+    impl From<AsyncMethod> for Method {
+        fn from(other: AsyncMethod) -> Self {
+            match other {
+                AsyncMethod::Connect => Self::Connect,
+                AsyncMethod::Delete => Self::Delete,
+                AsyncMethod::Get => Self::Get,
+                AsyncMethod::Head => Self::Head,
+                AsyncMethod::Options => Self::Options,
+                AsyncMethod::Patch => Self::Patch,
+                AsyncMethod::Post => Self::Post,
+                AsyncMethod::Put => Self::Put,
+                AsyncMethod::Trace => Self::Trace,
+                AsyncMethod::Other(v) => Self::Other(v),
+            }
+        }
+    }
+
+    impl From<Method> for AsyncMethod {
+        fn from(other: Method) -> Self {
+            match other {
+                Method::Connect => Self::Connect,
+                Method::Delete => Self::Delete,
+                Method::Get => Self::Get,
+                Method::Head => Self::Head,
+                Method::Options => Self::Options,
+                Method::Patch => Self::Patch,
+                Method::Post => Self::Post,
+                Method::Put => Self::Put,
+                Method::Trace => Self::Trace,
+                Method::Other(v) => Self::Other(v),
+            }
+        }
+    }
+
+    impl From<AsyncScheme> for Scheme {
+        fn from(other: AsyncScheme) -> Self {
+            match other {
+                AsyncScheme::Http => Self::Http,
+                AsyncScheme::Https => Self::Https,
+                AsyncScheme::Other(v) => Self::Other(v),
+            }
+        }
+    }
+
+    impl From<Scheme> for AsyncScheme {
+        fn from(other: Scheme) -> Self {
+            match other {
+                Scheme::Http => Self::Http,
+                Scheme::Https => Self::Https,
+                Scheme::Other(v) => Self::Other(v),
+            }
+        }
+    }
+
+    impl<T: WasiHttpView + WasiHttpViewExt> crate::bindings::sync::http::types::Host for T {
+        fn drop_fields(&mut self, fields: Fields) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_fields(self, fields).await })
+        }
+        fn new_fields(&mut self, entries: Vec<(String, String)>) -> wasmtime::Result<Fields> {
+            in_tokio(async { AsyncHost::new_fields(self, entries).await })
+        }
+        fn fields_get(&mut self, fields: Fields, name: String) -> wasmtime::Result<Vec<Vec<u8>>> {
+            in_tokio(async { AsyncHost::fields_get(self, fields, name).await })
+        }
+        fn fields_set(
+            &mut self,
+            fields: Fields,
+            name: String,
+            value: Vec<Vec<u8>>,
+        ) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::fields_set(self, fields, name, value).await })
+        }
+        fn fields_delete(&mut self, fields: Fields, name: String) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::fields_delete(self, fields, name).await })
+        }
+        fn fields_append(
+            &mut self,
+            fields: Fields,
+            name: String,
+            value: Vec<u8>,
+        ) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::fields_append(self, fields, name, value).await })
+        }
+        fn fields_entries(&mut self, fields: Fields) -> wasmtime::Result<Vec<(String, Vec<u8>)>> {
+            in_tokio(async { AsyncHost::fields_entries(self, fields).await })
+        }
+        fn fields_clone(&mut self, fields: Fields) -> wasmtime::Result<Fields> {
+            in_tokio(async { AsyncHost::fields_clone(self, fields).await })
+        }
+        fn finish_incoming_stream(
+            &mut self,
+            stream_id: IncomingStream,
+        ) -> wasmtime::Result<Option<Trailers>> {
+            in_tokio(async { AsyncHost::finish_incoming_stream(self, stream_id).await })
+        }
+        fn finish_outgoing_stream(
+            &mut self,
+            stream: OutgoingStream,
+            trailers: Option<Trailers>,
+        ) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::finish_outgoing_stream(self, stream, trailers).await })
+        }
+        fn drop_incoming_request(&mut self, request: IncomingRequest) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_incoming_request(self, request).await })
+        }
+        fn drop_outgoing_request(&mut self, request: OutgoingRequest) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_outgoing_request(self, request).await })
+        }
+        fn incoming_request_method(
+            &mut self,
+            request: IncomingRequest,
+        ) -> wasmtime::Result<Method> {
+            in_tokio(async { AsyncHost::incoming_request_method(self, request).await })
+                .map(Method::from)
+        }
+        fn incoming_request_path_with_query(
+            &mut self,
+            request: IncomingRequest,
+        ) -> wasmtime::Result<Option<String>> {
+            in_tokio(async { AsyncHost::incoming_request_path_with_query(self, request).await })
+        }
+        fn incoming_request_scheme(
+            &mut self,
+            request: IncomingRequest,
+        ) -> wasmtime::Result<Option<Scheme>> {
+            Ok(
+                in_tokio(async { AsyncHost::incoming_request_scheme(self, request).await })?
+                    .map(Scheme::from),
+            )
+        }
+        fn incoming_request_authority(
+            &mut self,
+            request: IncomingRequest,
+        ) -> wasmtime::Result<Option<String>> {
+            in_tokio(async { AsyncHost::incoming_request_authority(self, request).await })
+        }
+        fn incoming_request_headers(
+            &mut self,
+            request: IncomingRequest,
+        ) -> wasmtime::Result<Headers> {
+            in_tokio(async { AsyncHost::incoming_request_headers(self, request).await })
+        }
+        fn incoming_request_consume(
+            &mut self,
+            request: IncomingRequest,
+        ) -> wasmtime::Result<Result<IncomingStream, ()>> {
+            in_tokio(async { AsyncHost::incoming_request_consume(self, request).await })
+        }
+        fn new_outgoing_request(
+            &mut self,
+            method: Method,
+            path_with_query: Option<String>,
+            scheme: Option<Scheme>,
+            authority: Option<String>,
+            headers: Headers,
+        ) -> wasmtime::Result<OutgoingRequest> {
+            in_tokio(async {
+                AsyncHost::new_outgoing_request(
+                    self,
+                    method.into(),
+                    path_with_query,
+                    scheme.map(AsyncScheme::from),
+                    authority,
+                    headers,
+                )
+                .await
+            })
+        }
+        fn outgoing_request_write(
+            &mut self,
+            request: OutgoingRequest,
+        ) -> wasmtime::Result<Result<OutgoingStream, ()>> {
+            in_tokio(async { AsyncHost::outgoing_request_write(self, request).await })
+        }
+        fn drop_response_outparam(&mut self, response: ResponseOutparam) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_response_outparam(self, response).await })
+        }
+        fn set_response_outparam(
+            &mut self,
+            outparam: ResponseOutparam,
+            response: Result<OutgoingResponse, Error>,
+        ) -> wasmtime::Result<Result<(), ()>> {
+            in_tokio(async {
+                AsyncHost::set_response_outparam(self, outparam, response.map_err(AsyncError::from))
+                    .await
+            })
+        }
+        fn drop_incoming_response(&mut self, response: IncomingResponse) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_incoming_response(self, response).await })
+        }
+        fn drop_outgoing_response(&mut self, response: OutgoingResponse) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_outgoing_response(self, response).await })
+        }
+        fn incoming_response_status(
+            &mut self,
+            response: IncomingResponse,
+        ) -> wasmtime::Result<StatusCode> {
+            in_tokio(async { AsyncHost::incoming_response_status(self, response).await })
+        }
+        fn incoming_response_headers(
+            &mut self,
+            response: IncomingResponse,
+        ) -> wasmtime::Result<Headers> {
+            in_tokio(async { AsyncHost::incoming_response_headers(self, response).await })
+        }
+        fn incoming_response_consume(
+            &mut self,
+            response: IncomingResponse,
+        ) -> wasmtime::Result<Result<IncomingStream, ()>> {
+            in_tokio(async { AsyncHost::incoming_response_consume(self, response).await })
+        }
+        fn new_outgoing_response(
+            &mut self,
+            status_code: StatusCode,
+            headers: Headers,
+        ) -> wasmtime::Result<OutgoingResponse> {
+            in_tokio(async { AsyncHost::new_outgoing_response(self, status_code, headers).await })
+        }
+        fn outgoing_response_write(
+            &mut self,
+            response: OutgoingResponse,
+        ) -> wasmtime::Result<Result<OutgoingStream, ()>> {
+            in_tokio(async { AsyncHost::outgoing_response_write(self, response).await })
+        }
+        fn drop_future_incoming_response(
+            &mut self,
+            future: FutureIncomingResponse,
+        ) -> wasmtime::Result<()> {
+            in_tokio(async { AsyncHost::drop_future_incoming_response(self, future).await })
+        }
+        fn future_incoming_response_get(
+            &mut self,
+            future: FutureIncomingResponse,
+        ) -> wasmtime::Result<Option<Result<IncomingResponse, Error>>> {
+            Ok(
+                in_tokio(async { AsyncHost::future_incoming_response_get(self, future).await })?
+                    .map(|v| v.map_err(Error::from)),
+            )
+        }
+        fn listen_to_future_incoming_response(
+            &mut self,
+            future: FutureIncomingResponse,
+        ) -> wasmtime::Result<Pollable> {
+            in_tokio(async { AsyncHost::listen_to_future_incoming_response(self, future).await })
+        }
     }
 }
