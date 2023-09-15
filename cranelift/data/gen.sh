9e4289c9af6fbb792a2e89f873de7ab715367662
Andrei Kashin <kashin.andrej@gmail.com>
2023-09-15 08:11:10 +0100

    Add example working with memory

--- cranelift/data/gen.sh
+++ cranelift/data/gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cargo build
-for name in add counter add_func fibonacci locals locals_simple fibonacci_recursive
+for name in add counter add_func add_memory fibonacci locals locals_simple fibonacci_recursive
 do
 	echo $name;
 	../target/debug/clif-util wasm --target sparc-unknown-unknown ../../zkwasm/data/$name.wat > data/$name.zkasm
