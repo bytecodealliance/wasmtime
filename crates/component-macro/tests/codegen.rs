9a0a09ced71631f460e22a16eca92fcc48b8eec1
Alex Crichton <alex@alexcrichton.com>
2023-09-20 11:30:25 -0700

    Reenable tests

--- crates/component-macro/tests/codegen.rs
+++ crates/component-macro/tests/codegen.rs
@@ -1,29 +1,29 @@
-// macro_rules! gentest {
-//     ($id:ident $name:tt $path:tt) => {
-//         mod $id {
-//             mod sugar {
-//                 wasmtime::component::bindgen!(in $path);
-//             }
-//             mod async_ {
-//                 wasmtime::component::bindgen!({
-//                     path: $path,
-//                     async: true,
-//                 });
-//             }
-//             mod tracing {
-//                 wasmtime::component::bindgen!({
-//                     path: $path,
-//                     tracing: true,
-//                     ownership: Borrowing {
-//                         duplicate_if_necessary: true
-//                     }
-//                 });
-//             }
-//         }
-//     };
-// }
+macro_rules! gentest {
+    ($id:ident $name:tt $path:tt) => {
+        mod $id {
+            mod sugar {
+                wasmtime::component::bindgen!(in $path);
+            }
+            mod async_ {
+                wasmtime::component::bindgen!({
+                    path: $path,
+                    async: true,
+                });
+            }
+            mod tracing {
+                wasmtime::component::bindgen!({
+                    path: $path,
+                    tracing: true,
+                    ownership: Borrowing {
+                        duplicate_if_necessary: true
+                    }
+                });
+            }
+        }
+    };
+}
 
-// component_macro_test_helpers::foreach!(gentest);
+component_macro_test_helpers::foreach!(gentest);
 
 mod with_key_and_resources {
     use anyhow::Result;
