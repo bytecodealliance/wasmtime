233786c257fd9f15ec6c8f69f5bccbfcf01c3fa2
Afonso Bordado <afonso360@users.noreply.github.com>
2023-10-02 18:26:08 +0100

    riscv64: Add `Zcb` extension instructions (#7123)
    * riscv64: Add `c.mul`
    
    * riscv64: Add `c.not`
    
    * riscv64: Add zbb and zba dependent compressed instructions
    
    * riscv64: Add `Zcb` loads and stores
    
    * riscv64: Restrict immediate encoding for halfword compressed stores and loads
    
    * riscv64: Reverse imm bits for bytewise compressed loads and stores

--- cranelift/codegen/src/isa/riscv64/inst/emit.rs
+++ cranelift/codegen/src/isa/riscv64/inst/emit.rs
@@ -4,7 +4,7 @@ use crate::binemit::StackMap;
 use crate::ir::{self, LibCall, RelSourceLoc, TrapCode};
 use crate::isa::riscv64::inst::*;
 use crate::isa::riscv64::lower::isle::generated_code::{
-    CaOp, CbOp, CiOp, CiwOp, ClOp, CrOp, CsOp, CssOp,
+    CaOp, CbOp, CiOp, CiwOp, ClOp, CrOp, CsOp, CssOp, CsznOp, ZcbMemOp,
 };
 use crate::machinst::{AllocationConsumer, Reg, Writable};
 use crate::trace;
@@ -465,7 +465,11 @@ impl Inst {
         state: &mut EmitState,
         start_off: &mut u32,
     ) -> Option<()> {
+        let has_m = emit_info.isa_flags.has_m();
+        let has_zba = emit_info.isa_flags.has_zba();
+        let has_zbb = emit_info.isa_flags.has_zbb();
         let has_zca = emit_info.isa_flags.has_zca();
+        let has_zcb = emit_info.isa_flags.has_zcb();
         let has_zcd = emit_info.isa_flags.has_zcd();
 
         // Currently all compressed extensions (Zcb, Zcd, Zcmp, Zcmt, etc..) require Zca
@@ -513,7 +517,8 @@ impl Inst {
                     | AluOPRRR::Xor
                     | AluOPRRR::Sub
                     | AluOPRRR::Addw
-                    | AluOPRRR::Subw),
+                    | AluOPRRR::Subw
+                    | AluOPRRR::Mul),
                 rd,
                 rs1,
                 rs2,
@@ -525,7 +530,8 @@ impl Inst {
                     AluOPRRR::Sub => CaOp::CSub,
                     AluOPRRR::Addw => CaOp::CAddw,
                     AluOPRRR::Subw => CaOp::CSubw,
-                    _ => unreachable!(),
+                    AluOPRRR::Mul if has_zcb && has_m => CaOp::CMul,
+                    _ => return None,
                 };
 
                 sink.put2(encode_ca_type(op, rd, rs2));
@@ -700,6 +706,22 @@ impl Inst {
                 sink.put2(encode_cb_type(op, rd, imm6));
             }
 
+            // c.zextb
+            //
+            // This is an alias for `andi rd, rd, 0xff`
+            Inst::AluRRImm12 {
+                alu_op: AluOPRRI::Andi,
+                rd,
+                rs,
+                imm12,
+            } if has_zcb
+                && rd.to_reg() == rs
+                && reg_is_compressible(rs)
+                && imm12.as_i16() == 0xff =>
+            {
+                sink.put2(encode_cszn_type(CsznOp::CZextb, rd));
+            }
+
             // c.andi
             Inst::AluRRImm12 {
                 alu_op: AluOPRRI::Andi,
@@ -752,7 +774,9 @@ impl Inst {
             // Regular Loads
             Inst::Load {
                 rd,
-                op: op @ (LoadOP::Lw | LoadOP::Ld | LoadOP::Fld),
+                op:
+                    op
+                    @ (LoadOP::Lw | LoadOP::Ld | LoadOP::Fld | LoadOP::Lbu | LoadOP::Lhu | LoadOP::Lh),
                 from,
                 flags,
             } if reg_is_compressible(rd.to_reg())
@@ -766,17 +790,49 @@ impl Inst {
 
                 // We encode the offset in multiples of the store size.
                 let offset = from.get_offset_with_state(state);
-                let imm5 = u8::try_from(offset / op.size())
-                    .ok()
-                    .and_then(Uimm5::maybe_from_u8)?;
+                let offset = u8::try_from(offset / op.size()).ok()?;
 
-                // Floating point loads are not included in the base Zca extension
-                // but in a separate Zcd extension. Both of these are part of the C Extension.
-                let op = match op {
-                    LoadOP::Lw => ClOp::CLw,
-                    LoadOP::Ld => ClOp::CLd,
-                    LoadOP::Fld if has_zcd => ClOp::CFld,
-                    _ => return None,
+                // We mix two different formats here.
+                //
+                // c.lw / c.ld / c.fld instructions are available in the standard Zca
+                // extension using the CL format.
+                //
+                // c.lbu / c.lhu / c.lh are only available in the Zcb extension and
+                // are also encoded differently. Technically they each have a different
+                // format, but they are similar enough that we can group them.
+                let is_zcb_load = matches!(op, LoadOP::Lbu | LoadOP::Lhu | LoadOP::Lh);
+                let encoded = if is_zcb_load {
+                    if !has_zcb {
+                        return None;
+                    }
+
+                    let op = match op {
+                        LoadOP::Lbu => ZcbMemOp::CLbu,
+                        LoadOP::Lhu => ZcbMemOp::CLhu,
+                        LoadOP::Lh => ZcbMemOp::CLh,
+                        _ => unreachable!(),
+                    };
+
+                    // Byte stores & loads have 2 bits of immediate offset. Halfword stores
+                    // and loads only have 1 bit.
+                    let imm2 = Uimm2::maybe_from_u8(offset)?;
+                    if (offset & !((1 << op.imm_bits()) - 1)) != 0 {
+                        return None;
+                    }
+
+                    encode_zcbmem_load(op, rd, base, imm2)
+                } else {
+                    // Floating point loads are not included in the base Zca extension
+                    // but in a separate Zcd extension. Both of these are part of the C Extension.
+                    let op = match op {
+                        LoadOP::Lw => ClOp::CLw,
+                        LoadOP::Ld => ClOp::CLd,
+                        LoadOP::Fld if has_zcd => ClOp::CFld,
+                        _ => return None,
+                    };
+                    let imm5 = Uimm5::maybe_from_u8(offset)?;
+
+                    encode_cl_type(op, rd, base, imm5)
                 };
 
                 let srcloc = state.cur_srcloc();
@@ -784,7 +840,7 @@ impl Inst {
                     // Register the offset at which the actual load instruction starts.
                     sink.add_trap(TrapCode::HeapOutOfBounds);
                 }
-                sink.put2(encode_cl_type(op, rd, base, imm5));
+                sink.put2(encoded);
             }
 
             // Stack Based Stores
@@ -822,7 +878,7 @@ impl Inst {
             // Regular Stores
             Inst::Store {
                 src,
-                op: op @ (StoreOP::Sw | StoreOP::Sd | StoreOP::Fsd),
+                op: op @ (StoreOP::Sw | StoreOP::Sd | StoreOP::Fsd | StoreOP::Sh | StoreOP::Sb),
                 to,
                 flags,
             } if reg_is_compressible(src)
@@ -836,17 +892,47 @@ impl Inst {
 
                 // We encode the offset in multiples of the store size.
                 let offset = to.get_offset_with_state(state);
-                let imm5 = u8::try_from(offset / op.size())
-                    .ok()
-                    .and_then(Uimm5::maybe_from_u8)?;
+                let offset = u8::try_from(offset / op.size()).ok()?;
 
-                // Floating point stores are not included in the base Zca extension
-                // but in a separate Zcd extension. Both of these are part of the C Extension.
-                let op = match op {
-                    StoreOP::Sw => CsOp::CSw,
-                    StoreOP::Sd => CsOp::CSd,
-                    StoreOP::Fsd if has_zcd => CsOp::CFsd,
-                    _ => return None,
+                // We mix two different formats here.
+                //
+                // c.sw / c.sd / c.fsd instructions are available in the standard Zca
+                // extension using the CL format.
+                //
+                // c.sb / c.sh are only available in the Zcb extension and are also
+                // encoded differently.
+                let is_zcb_store = matches!(op, StoreOP::Sh | StoreOP::Sb);
+                let encoded = if is_zcb_store {
+                    if !has_zcb {
+                        return None;
+                    }
+
+                    let op = match op {
+                        StoreOP::Sh => ZcbMemOp::CSh,
+                        StoreOP::Sb => ZcbMemOp::CSb,
+                        _ => unreachable!(),
+                    };
+
+                    // Byte stores & loads have 2 bits of immediate offset. Halfword stores
+                    // and loads only have 1 bit.
+                    let imm2 = Uimm2::maybe_from_u8(offset)?;
+                    if (offset & !((1 << op.imm_bits()) - 1)) != 0 {
+                        return None;
+                    }
+
+                    encode_zcbmem_store(op, src, base, imm2)
+                } else {
+                    // Floating point stores are not included in the base Zca extension
+                    // but in a separate Zcd extension. Both of these are part of the C Extension.
+                    let op = match op {
+                        StoreOP::Sw => CsOp::CSw,
+                        StoreOP::Sd => CsOp::CSd,
+                        StoreOP::Fsd if has_zcd => CsOp::CFsd,
+                        _ => return None,
+                    };
+                    let imm5 = Uimm5::maybe_from_u8(offset)?;
+
+                    encode_cs_type(op, src, base, imm5)
                 };
 
                 let srcloc = state.cur_srcloc();
@@ -854,7 +940,64 @@ impl Inst {
                     // Register the offset at which the actual load instruction starts.
                     sink.add_trap(TrapCode::HeapOutOfBounds);
                 }
-                sink.put2(encode_cs_type(op, src, base, imm5));
+                sink.put2(encoded);
+            }
+
+            // c.not
+            //
+            // This is an alias for `xori rd, rd, -1`
+            Inst::AluRRImm12 {
+                alu_op: AluOPRRI::Xori,
+                rd,
+                rs,
+                imm12,
+            } if has_zcb
+                && rd.to_reg() == rs
+                && reg_is_compressible(rs)
+                && imm12.as_i16() == -1 =>
+            {
+                sink.put2(encode_cszn_type(CsznOp::CNot, rd));
+            }
+
+            // c.sext.b / c.sext.h / c.zext.h
+            //
+            // These are all the extend instructions present in `Zcb`, they
+            // also require `Zbb` since they aren't available in the base ISA.
+            Inst::AluRRImm12 {
+                alu_op: alu_op @ (AluOPRRI::Sextb | AluOPRRI::Sexth | AluOPRRI::Zexth),
+                rd,
+                rs,
+                imm12,
+            } if has_zcb
+                && has_zbb
+                && rd.to_reg() == rs
+                && reg_is_compressible(rs)
+                && imm12.as_i16() == 0 =>
+            {
+                let op = match alu_op {
+                    AluOPRRI::Sextb => CsznOp::CSextb,
+                    AluOPRRI::Sexth => CsznOp::CSexth,
+                    AluOPRRI::Zexth => CsznOp::CZexth,
+                    _ => unreachable!(),
+                };
+                sink.put2(encode_cszn_type(op, rd));
+            }
+
+            // c.zext.w
+            //
+            // This is an alias for `add.uw rd, rd, zero`
+            Inst::AluRRR {
+                alu_op: AluOPRRR::Adduw,
+                rd,
+                rs1,
+                rs2,
+            } if has_zcb
+                && has_zba
+                && rd.to_reg() == rs1
+                && reg_is_compressible(rs1)
+                && rs2 == zero_reg() =>
+            {
+                sink.put2(encode_cszn_type(CsznOp::CZextw, rd));
             }
 
             _ => return None,
