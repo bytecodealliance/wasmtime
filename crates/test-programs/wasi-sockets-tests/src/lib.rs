269a9a897db072e17261c32dac7d9cb5565fdcc7
Dave Bakker <github@davebakker.io>
2023-10-09 05:22:36 +0200

    wasi-sockets: Simplify & clarify TCP errors (#7120)
    * Use Rustix::Errno to unify error code mapping.
    
    * Clarify Connect failure state
    
    * Allow accept() to return transient errors.
    
    The original provision was added to align with preview3 streams that may only fail once. However, after discussing with Dan Gohman, we came to the conclusion that a stream of result<> could do the trick fine too.
    
    Fixes: https://github.com/WebAssembly/wasi-sockets/issues/22
    
    * Fold `ephemeral-ports-exhausted` into `address-in-use`
    
    There is no cross-platform way to know the distinction between them
    
    * Clarify `local-address` behavior on unbound socket
    
    * Remove `concurrency-conflict` clutter,
    and just document it to be always possible.
    
    * Simplify state errors.
    
    They were unnecessarily detailed and mostly have no standardized equivalent in POSIX, so wasi-libc will probably just map them all back into a single EOPNOTSUPP or EINVAL or ...
    
    EISCONN/ENOTCONN can be derived in wasi-libc based on context and/or by checking `remote-address`. For example, `shutdown` can only be called on connected sockets, so if it returns `invalid-state` it can be unambiguously mapped to ENOTCONN.
    
    * Document that connect may return ECONNABORTED
    
    * Remove create-tcp/udp-socket not supported errors.
    
    These stem from back when the entire wasi-sockets proposal was one big single thing. In this day and age, when an implementation doesn't want to support TCP and/or UDP, it can simply _not_ implement that interface, rather than returning an error at runtime.
    
    * Simplify "not supported" and "invalid argument" error cases.
    
    There is a myriad of reasons why an argument might be invalid or an operation might be not supported. But there is few cross platform consistency in which of those error cases result in which error codes.
    
    The error codes that have been removed were fairly specific, but:
    - Were still missing error cases. So additional error codes would have needed to be created.
    - Implementations would have to bend over backwards to make it work cross platform, especially beyond just Win/Mac/Linux.
    - Didn't all have an equivalent in POSIX, so they would map back into a generic EINVAL anyways.
    
    * Move example_body out of lib.rs into its own test-case make room for other tests.
    
    * Refactor TCP integration tests:
    - Ad-hoc skeleton implementation of resources.
    - Add blocking wrappers around async operations.
    
    * Fix get/set_unicast_hop_limit on Linux
    
    * Test TCP socket states
    
    * Keep track of address family ourselves.
    
    Because I need the family for input validation.
    And the resulting code is more straightforward.
    
    * Add more tests and make it work on Linux
    
    * Fix Windows
    
    * Simplify integration tests.
    
    All platforms supported by wasmtime also support dualstack sockets.
    
    * Test ipv6_only inheritence
    
    * Test that socket options keep being respected, even if listen() has already been called
    
    * cargo fmt
    
    * Duplicate .wit changes to wasi-http
    
    * prtest:full
    
    * Fix BSD behavior of SO_SNDBUF/SO_RCVBUF
    
    * fmt
    
    * Fix type error
    
    * Got lost during merge
    
    * Implement listen backlog tests
    
    * Manually inherit buffer size from listener on MacOS.
    
    I suspect that these changes apply to any BSD platform, but I can't test that.
    
    * Keep track of IPV6_V6ONLY ourselves.
    
    - This provides cross-platform behaviour for `ipv6-only`
    - This eliminates the syscall in `validate_address_family`
    
    * Reject IPv4-compatible IPv6 addresses.
    
    * Remove intermediate SystemError trait
    
    * Fix ambiguous .into()'s
    
    * Fix IPV6_UNICAST_HOPS inheritance on MacOS

--- crates/test-programs/wasi-sockets-tests/src/lib.rs
+++ crates/test-programs/wasi-sockets-tests/src/lib.rs
@@ -1,97 +1,191 @@
 wit_bindgen::generate!("test-command-with-sockets" in "../../wasi/wit");
 
-use wasi::io::poll;
-use wasi::io::streams;
-use wasi::sockets::{network, tcp, tcp_create_socket};
+use wasi::io::poll::{self, Pollable};
+use wasi::io::streams::{InputStream, OutputStream, StreamError};
+use wasi::sockets::instance_network;
+use wasi::sockets::network::{
+    ErrorCode, IpAddress, IpAddressFamily, IpSocketAddress, Ipv4SocketAddress, Ipv6SocketAddress,
+    Network,
+};
+use wasi::sockets::tcp::TcpSocket;
+use wasi::sockets::tcp_create_socket;
 
-pub fn write(output: &streams::OutputStream, mut bytes: &[u8]) -> Result<(), streams::StreamError> {
-    let pollable = output.subscribe();
-
-    while !bytes.is_empty() {
-        poll::poll_list(&[&pollable]);
-
-        let permit = output.check_write()?;
-
-        let len = bytes.len().min(permit as usize);
-        let (chunk, rest) = bytes.split_at(len);
-
-        output.write(chunk)?;
-
-        output.blocking_flush()?;
-
-        bytes = rest;
+impl Pollable {
+    pub fn wait(&self) {
+        poll::poll_one(self);
     }
-    Ok(())
 }
 
-pub fn example_body(net: tcp::Network, sock: tcp::TcpSocket, family: network::IpAddressFamily) {
-    let first_message = b"Hello, world!";
-    let second_message = b"Greetings, planet!";
+impl OutputStream {
+    pub fn blocking_write_util(&self, mut bytes: &[u8]) -> Result<(), StreamError> {
+        let pollable = self.subscribe();
 
-    let sub = sock.subscribe();
+        while !bytes.is_empty() {
+            pollable.wait();
 
-    sock.set_listen_backlog_size(32).unwrap();
+            let permit = self.check_write()?;
 
-    sock.start_listen().unwrap();
-    poll::poll_one(&sub);
-    sock.finish_listen().unwrap();
+            let len = bytes.len().min(permit as usize);
+            let (chunk, rest) = bytes.split_at(len);
 
-    let addr = sock.local_address().unwrap();
+            self.write(chunk)?;
 
-    let client = tcp_create_socket::create_tcp_socket(family).unwrap();
-    let client_sub = client.subscribe();
+            self.blocking_flush()?;
 
-    client.start_connect(&net, addr).unwrap();
-    poll::poll_one(&client_sub);
-    let (client_input, client_output) = client.finish_connect().unwrap();
+            bytes = rest;
+        }
+        Ok(())
+    }
+}
 
-    write(&client_output, &[]).unwrap();
+impl Network {
+    pub fn default() -> Network {
+        instance_network::instance_network()
+    }
+}
 
-    write(&client_output, first_message).unwrap();
+impl TcpSocket {
+    pub fn new(address_family: IpAddressFamily) -> Result<TcpSocket, ErrorCode> {
+        tcp_create_socket::create_tcp_socket(address_family)
+    }
 
-    drop(client_input);
-    drop(client_output);
-    drop(client_sub);
-    drop(client);
+    pub fn blocking_bind(
+        &self,
+        network: &Network,
+        local_address: IpSocketAddress,
+    ) -> Result<(), ErrorCode> {
+        let sub = self.subscribe();
 
-    poll::poll_one(&sub);
-    let (accepted, input, output) = sock.accept().unwrap();
+        self.start_bind(&network, local_address)?;
 
-    let empty_data = input.read(0).unwrap();
-    assert!(empty_data.is_empty());
+        loop {
+            match self.finish_bind() {
+                Err(ErrorCode::WouldBlock) => sub.wait(),
+                result => return result,
+            }
+        }
+    }
 
-    let data = input.blocking_read(first_message.len() as u64).unwrap();
+    pub fn blocking_listen(&self) -> Result<(), ErrorCode> {
+        let sub = self.subscribe();
 
-    drop(input);
-    drop(output);
-    drop(accepted);
+        self.start_listen()?;
 
-    // Check that we sent and recieved our message!
-    assert_eq!(data, first_message); // Not guaranteed to work but should work in practice.
+        loop {
+            match self.finish_listen() {
+                Err(ErrorCode::WouldBlock) => sub.wait(),
+                result => return result,
+            }
+        }
+    }
 
-    // Another client
-    let client = tcp_create_socket::create_tcp_socket(family).unwrap();
-    let client_sub = client.subscribe();
+    pub fn blocking_connect(
+        &self,
+        network: &Network,
+        remote_address: IpSocketAddress,
+    ) -> Result<(InputStream, OutputStream), ErrorCode> {
+        let sub = self.subscribe();
 
-    client.start_connect(&net, addr).unwrap();
-    poll::poll_one(&client_sub);
-    let (client_input, client_output) = client.finish_connect().unwrap();
+        self.start_connect(&network, remote_address)?;
 
-    write(&client_output, second_message).unwrap();
+        loop {
+            match self.finish_connect() {
+                Err(ErrorCode::WouldBlock) => sub.wait(),
+                result => return result,
+            }
+        }
+    }
 
-    drop(client_input);
-    drop(client_output);
-    drop(client_sub);
-    drop(client);
+    pub fn blocking_accept(&self) -> Result<(TcpSocket, InputStream, OutputStream), ErrorCode> {
+        let sub = self.subscribe();
 
-    poll::poll_one(&sub);
-    let (accepted, input, output) = sock.accept().unwrap();
-    let data = input.blocking_read(second_message.len() as u64).unwrap();
+        loop {
+            match self.accept() {
+                Err(ErrorCode::WouldBlock) => sub.wait(),
+                result => return result,
+            }
+        }
+    }
+}
 
-    drop(input);
-    drop(output);
-    drop(accepted);
+impl IpAddress {
+    pub const IPV4_BROADCAST: IpAddress = IpAddress::Ipv4((255, 255, 255, 255));
 
-    // Check that we sent and recieved our message!
-    assert_eq!(data, second_message); // Not guaranteed to work but should work in practice.
+    pub const IPV4_LOOPBACK: IpAddress = IpAddress::Ipv4((127, 0, 0, 1));
+    pub const IPV6_LOOPBACK: IpAddress = IpAddress::Ipv6((0, 0, 0, 0, 0, 0, 0, 1));
+
+    pub const IPV4_UNSPECIFIED: IpAddress = IpAddress::Ipv4((0, 0, 0, 0));
+    pub const IPV6_UNSPECIFIED: IpAddress = IpAddress::Ipv6((0, 0, 0, 0, 0, 0, 0, 0));
+
+    pub const IPV4_MAPPED_LOOPBACK: IpAddress =
+        IpAddress::Ipv6((0, 0, 0, 0, 0, 0xFFFF, 0x7F00, 0x0001));
+
+    pub const fn new_loopback(family: IpAddressFamily) -> IpAddress {
+        match family {
+            IpAddressFamily::Ipv4 => Self::IPV4_LOOPBACK,
+            IpAddressFamily::Ipv6 => Self::IPV6_LOOPBACK,
+        }
+    }
+
+    pub const fn new_unspecified(family: IpAddressFamily) -> IpAddress {
+        match family {
+            IpAddressFamily::Ipv4 => Self::IPV4_UNSPECIFIED,
+            IpAddressFamily::Ipv6 => Self::IPV6_UNSPECIFIED,
+        }
+    }
+
+    pub const fn family(&self) -> IpAddressFamily {
+        match self {
+            IpAddress::Ipv4(_) => IpAddressFamily::Ipv4,
+            IpAddress::Ipv6(_) => IpAddressFamily::Ipv6,
+        }
+    }
+}
+
+impl PartialEq for IpAddress {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Ipv4(left), Self::Ipv4(right)) => left == right,
+            (Self::Ipv6(left), Self::Ipv6(right)) => left == right,
+            _ => false,
+        }
+    }
+}
+
+impl IpSocketAddress {
+    pub const fn new(ip: IpAddress, port: u16) -> IpSocketAddress {
+        match ip {
+            IpAddress::Ipv4(addr) => IpSocketAddress::Ipv4(Ipv4SocketAddress {
+                port: port,
+                address: addr,
+            }),
+            IpAddress::Ipv6(addr) => IpSocketAddress::Ipv6(Ipv6SocketAddress {
+                port: port,
+                address: addr,
+                flow_info: 0,
+                scope_id: 0,
+            }),
+        }
+    }
+
+    pub const fn ip(&self) -> IpAddress {
+        match self {
+            IpSocketAddress::Ipv4(addr) => IpAddress::Ipv4(addr.address),
+            IpSocketAddress::Ipv6(addr) => IpAddress::Ipv6(addr.address),
+        }
+    }
+
+    pub const fn port(&self) -> u16 {
+        match self {
+            IpSocketAddress::Ipv4(addr) => addr.port,
+            IpSocketAddress::Ipv6(addr) => addr.port,
+        }
+    }
+
+    pub const fn family(&self) -> IpAddressFamily {
+        match self {
+            IpSocketAddress::Ipv4(_) => IpAddressFamily::Ipv4,
+            IpSocketAddress::Ipv6(_) => IpAddressFamily::Ipv6,
+        }
+    }
 }
