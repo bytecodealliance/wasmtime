9ab2e0a65f718dab12976ae0f37b8e1dba43e9eb
Jeffrey Charles <jeff.charles@shopify.com>
2023-11-01 11:23:01 -0700

    popcnt should check for sse4.2 support in Winch (#7449)

--- winch/codegen/src/isa/x64/masm.rs
+++ winch/codegen/src/isa/x64/masm.rs
@@ -799,7 +799,7 @@ impl Masm for MacroAssembler {
 
     fn popcnt(&mut self, context: &mut CodeGenContext, size: OperandSize) {
         let src = context.pop_to_reg(self, None);
-        if self.flags.has_popcnt() {
+        if self.flags.has_popcnt() && self.flags.has_sse42() {
             self.asm.popcnt(src.into(), size);
             context.stack.push(src.into());
         } else {
