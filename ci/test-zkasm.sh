e59b3fc86b1396f0d43acc662fac94022b53ed7a
Andrei Kashin <kashin.andrej@gmail.com>
2023-11-16 19:48:19 +0000

    Run `npm install` only if there are no node_modules

--- ci/test-zkasm.sh
+++ ci/test-zkasm.sh
@@ -3,6 +3,10 @@
 set -o pipefail
 set -eux
 
-npm install --prefix tests/zkasm
+# NB: This might have false-positives locally, but it's worth it for iteration speed.
+# If you ever run into a situation when modules are out of date, run this command manually.
+if [ ! -d "tests/zkasm/node_modules" ]; then
+	npm install --prefix tests/zkasm
+fi
 TEST_PATH=../../${1:-"cranelift/zkasm_data/generated"}
 npm test --prefix tests/zkasm $TEST_PATH
