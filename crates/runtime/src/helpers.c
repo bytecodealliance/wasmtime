e0bb6ccea0d63d48faf8f8d709e5b608b455bd0f
Tyler Rockwood <rockwood@redpanda.com>
2023-11-29 11:37:36 -0600

    disable formatting
    To prevent a link from being broken
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- crates/runtime/src/helpers.c
+++ crates/runtime/src/helpers.c
@@ -15,6 +15,8 @@ typedef jmp_buf platform_jmp_buf;
 
 #elif defined(WASMTIME_GCC) || defined(__x86_64__)
 
+// clang-format off
+
 // GCC and Clang on x86_64 provide `__builtin_setjmp`/`__builtin_longjmp`, which
 // differ from plain `setjmp` and `longjmp` in that they're implemented by
 // the compiler inline rather than in libc, and the compiler can avoid saving
@@ -27,8 +29,9 @@ typedef jmp_buf platform_jmp_buf;
 // runtime) is assuming is callee-saved.
 //
 // [GCC docs]: https://gcc.gnu.org/onlinedocs/gcc/Nonlocal-Gotos.html
-// [clang docs]:
-// https://llvm.org/docs/ExceptionHandling.html#llvm-eh-sjlj-setjmp
+// [clang docs]: https://llvm.org/docs/ExceptionHandling.html#llvm-eh-sjlj-setjmp
+
+// clang-format on
 #define platform_setjmp(buf) __builtin_setjmp(buf)
 #define platform_longjmp(buf, arg) __builtin_longjmp(buf, arg)
 typedef void *platform_jmp_buf[5]; // this is the documented size; see the docs
