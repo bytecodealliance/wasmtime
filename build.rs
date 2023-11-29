ab50122101fd669d00cc2845c576958353195a37
Alex Crichton <alex@alexcrichton.com>
2023-11-29 13:50:53 -0800

    Add extended version information to `wasmtime --version`
    This commit adds some more information to `wasmtime --version` which
    includes the git commit plus the git commit's date. This matches `rustc
    -V` for example which was additionally copied to `wasm-tools` and
    mirrored as `wasm-tools -V`.
    
    Personally I've found this useful since it can help point to exact
    commits and additionally quickly get a sense of how old a version is
    based on its commit date presented.

--- build.rs
+++ build.rs
@@ -13,6 +13,8 @@ use std::process::Command;
 fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
 
+    set_commit_info_for_rustc();
+
     let out_dir = PathBuf::from(
         env::var_os("OUT_DIR").expect("The OUT_DIR environment variable must be set"),
     );
@@ -280,4 +282,31 @@ fn ignore(testsuite: &str, testname: &str, strategy: &str) -> bool {
 
         _ => false,
     }
+}
+
+fn set_commit_info_for_rustc() {
+    if !Path::new(".git").exists() {
+        return;
+    }
+    let output = match Command::new("git")
+        .arg("log")
+        .arg("-1")
+        .arg("--date=short")
+        .arg("--format=%H %h %cd")
+        .arg("--abbrev=9")
+        .output()
+    {
+        Ok(output) if output.status.success() => output,
+        _ => return,
+    };
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let mut parts = stdout.split_whitespace();
+    let mut next = || parts.next().unwrap();
+    println!("cargo:rustc-env=WASMTIME_GIT_HASH={}", next());
+    println!(
+        "cargo:rustc-env=WASMTIME_VERSION_INFO={} ({} {})",
+        env!("CARGO_PKG_VERSION"),
+        next(),
+        next()
+    );
 }
