38f5bb88ebc778c6e6eba4d6bcdc00b16bac0342
Tyler Rockwood <rockwood@redpanda.com>
2023-11-29 11:34:14 -0600

    manually reformat wasmtime.h
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -227,9 +227,10 @@ extern "C" {
  * \brief Converts from the text format of WebAssembly to to the binary format.
  *
  * \param wat this it the input pointer with the WebAssembly Text Format inside
- * of it. This will be parsed and converted to the binary format. \param wat_len
- * this it the length of `wat`, in bytes. \param ret if the conversion is
- * successful, this byte vector is filled in with the WebAssembly binary format.
+ *        of it. This will be parsed and converted to the binary format.
+ * \param wat_len this it the length of `wat`, in bytes.
+ * \param ret if the conversion is successful, this byte vector is filled in
+ *        with the WebAssembly binary format.
  *
  * \return a non-null error if parsing fails, or returns `NULL`. If parsing
  * fails then `ret` isn't touched.
