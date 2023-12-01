03a14ac64fcf35421570b7310ffe58d34525974b
Andrew Brown <andrew.brown@intel.com>
2023-12-01 10:21:39 -0800

    mpk: optimize layout of protected stripes, again (#7622)
    * mpk: optimize layout of protected stripes, again
    
    This is another attempt at #7603, attempting reduce the slab layout
    sizes of MPK-protected stripes. While experimenting with the limits of
    MPK-protected memory pools, @alexcrichton and I discovered that the
    current slab layout calculations were too conservative. This meant that
    the memory pool could not pack in as many memories as it should have
    been able: we were expecting, but not seeing, ~15x more memory slots
    over non-MPK memory pools.
    
    This change brings together several fixes:
    - it more aggressively divides up the stripes (as in b212152)
    - it eliminates an extra stripe (as in 8813a30)
    - it replaces some uses of `checked_*` with `saturating_*` (as in
      fb22a20)
    - it improves some documentation
    - and, crucially, it reports back a larger value for
      `memory_and_guard_size`
    
    The failures observed with #7603 when run with MPK
    (`WASMTIME_TEST_FORCE_MPK=1 cargo test`) were due to `Store::wasm_fault`
    not being able to identify which memory an OOB address belonged to.
    This is because the `MemoryPool` was underreporting the size of the
    region in which OOB accesses would fault. The correct value is provided
    by the new `SlabLayout::bytes_to_next_stripe_slot`: any OOB access
    within that larger region must fault because (1) the other stripes have
    different protection keys and (2) a `Store` can only use one protection
    key. We also use (2) to guarantee that `Store::wasm_fault` will be able
    to calculate the Wasm address from the raw address.
    
    This change also provides a new `traps` test that will reproduce the
    failures from #7603; if we observe `SIGABRT` from that test, it will be
    a regression.
    
    * fix: make test x86-specific

--- tests/all/traps.rs
+++ tests/all/traps.rs
@@ -1349,18 +1349,57 @@ fn wasm_fault_address_reported_by_default() -> Result<()> {
 
     // NB: at this time there's no programmatic access to the fault address
     // because it's not always available for load/store traps. Only static
-    // memories on 32-bit have this information, but bounds-checked memories
-    // use manual trapping instructions and otherwise don't have a means of
+    // memories on 32-bit have this information, but bounds-checked memories use
+    // manual trapping instructions and otherwise don't have a means of
     // communicating the faulting address at this time.
     //
     // It looks like the exact reported fault address may not be deterministic,
-    // so assert that we have the right error message, but not the exact address.
+    // so assert that we have the right error message, but not the exact
+    // address.
     let err = format!("{err:?}");
     assert!(
         err.contains("memory fault at wasm address ")
             && err.contains(" in linear memory of size 0x10000"),
         "bad error: {err}"
     );
+    Ok(())
+}
+
+#[cfg(target_arch = "x86_64")]
+#[test]
+fn wasm_fault_address_reported_from_mpk_protected_memory() -> Result<()> {
+    // Trigger the case where an OOB memory access causes a segfault and the
+    // store attempts to convert it into a `WasmFault`, calculating the Wasm
+    // address from the raw faulting address. Previously, a store could not do
+    // this calculation for MPK-protected, causing an abort.
+    let mut pool = crate::small_pool_config();
+    pool.total_memories(16);
+    pool.memory_protection_keys(MpkEnabled::Auto);
+    let mut config = Config::new();
+    config.allocation_strategy(InstanceAllocationStrategy::Pooling(pool));
+    let engine = Engine::new(&config)?;
+
+    let mut store = Store::new(&engine, ());
+    let module = Module::new(
+        &engine,
+        r#"
+            (module
+                (memory 1)
+                (func $start
+                    i32.const 0xdeadbeef
+                    i32.load
+                    drop)
+                (start $start)
+            )
+        "#,
+    )?;
+    let err = Instance::new(&mut store, &module, &[]).unwrap_err();
+
+    // We expect an error here, not an abort; but we also check that the store
+    // can now calculate the correct Wasm address. If this test is failing with
+    // an abort, use `--nocapture` to see more details.
+    let err = format!("{err:?}");
+    assert!(err.contains("0xdeadbeef"), "bad error: {err}");
     Ok(())
 }
 
