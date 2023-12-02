fa9a6622f363c85e1bde961ac3e451ffeb565875
Trevor Elliott <telliott@fastly.com>
2023-12-01 16:19:42 -0800

    Make `fields.get` return an option

--- crates/wasi-http/src/types_impl.rs
+++ crates/wasi-http/src/types_impl.rs
@@ -144,19 +144,24 @@ impl<T: WasiHttpView> crate::bindings::http::types::HostFields for T {
         &mut self,
         fields: Resource<HostFields>,
         name: String,
-    ) -> wasmtime::Result<Vec<Vec<u8>>> {
+    ) -> wasmtime::Result<Option<Vec<Vec<u8>>>> {
+        let fields = get_fields(self.table(), &fields).context("[fields_get] getting fields")?;
+
         let header = match hyper::header::HeaderName::from_bytes(name.as_bytes()) {
             Ok(header) => header,
-            Err(_) => return Ok(vec![]),
+            Err(_) => return Ok(None),
         };
 
-        let res = get_fields(self.table(), &fields)
-            .context("[fields_get] getting fields")?
-            .get_all(header)
+        if !fields.contains_key(&header) {
+            return Ok(None);
+        }
+
+        let res = fields
+            .get_all(&header)
             .into_iter()
             .map(|val| val.as_bytes().to_owned())
             .collect();
-        Ok(res)
+        Ok(Some(res))
     }
 
     fn set(
