64c2a247edd6bfc41bcfccb61848077c116bd9d6
Joel Dice <joel.dice@fermyon.com>
2023-12-12 08:07:42 -0700

    [16.0.0] Backport `adapter_{open|close}_badfd` exports (#7675)
    * add `adapter_{open|close}_badfd` exports to Preview 1 adapter (#7663)
    
    * add `adapter_{open|close}_badfd` exports to Preview 1 adapter
    
    This is to be used by `wasi-libc` to reserve file descriptors for its own use
    (e.g. for sockets), ensuring that any attempt to pass them directly to Preview 1
    functions will consistently return an error.
    
    See https://github.com/WebAssembly/wasi-libc/issues/447 for further details.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    
    * add `preview2_adapter_badfd` test
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    
    ---------
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    
    * Update `*_badfd` methods in the adapter to return an errno (#7672)
    
    Makes them a bit more consistent with the rest of the WASI functions as
    opposed to returning a boolean.
    
    ---------
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    Co-authored-by: Alex Crichton <alex@alexcrichton.com>

--- crates/wasi-preview1-component-adapter/src/descriptors.rs
+++ crates/wasi-preview1-component-adapter/src/descriptors.rs
@@ -20,6 +20,8 @@ pub enum Descriptor {
 
     /// Input and/or output wasi-streams, along with stream metadata.
     Streams(Streams),
+
+    Bad,
 }
 
 /// Input and/or output wasi-streams, along with a stream type that
@@ -358,7 +360,7 @@ impl Descriptors {
     ) -> Result<&mut Streams, Errno> {
         match self.get_mut(fd)? {
             Descriptor::Streams(streams) => Ok(streams),
-            Descriptor::Closed(_) => Err(error),
+            Descriptor::Closed(_) | Descriptor::Bad => Err(error),
         }
     }
 
@@ -420,14 +422,14 @@ impl Descriptors {
     pub fn get_read_stream(&self, fd: Fd) -> Result<&InputStream, Errno> {
         match self.get(fd)? {
             Descriptor::Streams(streams) => streams.get_read_stream(),
-            Descriptor::Closed(_) => Err(wasi::ERRNO_BADF),
+            Descriptor::Closed(_) | Descriptor::Bad => Err(wasi::ERRNO_BADF),
         }
     }
 
     pub fn get_write_stream(&self, fd: Fd) -> Result<&OutputStream, Errno> {
         match self.get(fd)? {
             Descriptor::Streams(streams) => streams.get_write_stream(),
-            Descriptor::Closed(_) => Err(wasi::ERRNO_BADF),
+            Descriptor::Closed(_) | Descriptor::Bad => Err(wasi::ERRNO_BADF),
         }
     }
 }
