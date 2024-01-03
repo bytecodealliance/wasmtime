7a68d58ee8138d851301dc727316976be330d9d9
Saúl Cabrera <saulecabrera@gmail.com>
2024-01-03 08:46:54 -0500

    Emit right shift instead of division to obtain the memory size in pages

--- winch/codegen/src/isa/x64/masm.rs
+++ winch/codegen/src/isa/x64/masm.rs
@@ -27,7 +27,7 @@ use cranelift_codegen::{
     isa::x64::settings as x64_settings, settings, Final, MachBufferFinalized, MachLabel,
 };
 
-use wasmtime_environ::{PtrSize, WASM_PAGE_SIZE};
+use wasmtime_environ::{PtrSize, WasmType, WASM_PAGE_SIZE};
 
 /// x64 MacroAssembler.
 pub(crate) struct MacroAssembler {
@@ -221,13 +221,27 @@ impl Masm for MacroAssembler {
 
         let size_addr = Address::offset(base, heap_data.current_length_offset);
         self.asm.mov_mr(&size_addr, size_reg, self.ptr_size);
-        // Prepare the stack to emit division and get the size in pages rather
+        // Prepare the stack to emit a shift to get the size in pages rather
         // than in bytes.
         context
             .stack
             .push(TypedReg::new(heap_data.ty, size_reg).into());
-        context.stack.push(Val::i64(WASM_PAGE_SIZE as i64));
-        self.div(context, DivKind::Unsigned, heap_data.ty.into());
+
+        // Since the page size is a power-of-two, verify that 2^16, equals the
+        // defined constant. This is mostly a safeguard in case the constant
+        // value ever changes.
+        let pow = 16;
+        debug_assert_eq!(2u32.pow(pow), WASM_PAGE_SIZE);
+
+        // Ensure that the constant is correctly typed according to the heap
+        // type to reduce register pressure when emitting the shift operation.
+        match heap_data.ty {
+            WasmType::I32 => context.stack.push(Val::i32(pow as i32)),
+            WasmType::I64 => context.stack.push(Val::i64(pow as i64)),
+            _ => unreachable!(),
+        }
+
+        self.shift(context, ShiftKind::ShrU, heap_data.ty.into());
     }
 
     fn address_from_sp(&self, offset: SPOffset) -> Self::Address {
