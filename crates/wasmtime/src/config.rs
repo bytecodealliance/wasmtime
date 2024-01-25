fac3b9c2d1373c8bdf10b53f2f6e96c01c90afd8
Alex Crichton <alex@alexcrichton.com>
2024-01-25 09:50:19 -0800

    Enable the component model by default
    This commit enables the component model by default in the embedding API
    and the CLI. This means that an opt-in of `-W component-model` is no
    longer required and additionally `.wasm_component_model(true)` is no
    longer required. Note that this won't impact existing embeddings since
    the component model feature doesn't do much less `wasmtime::component`
    is used, and if that's being used this is probably good news for you.

--- crates/wasmtime/src/config.rs
+++ crates/wasmtime/src/config.rs
@@ -228,6 +228,7 @@ impl Config {
         ret.wasm_multi_value(true);
         ret.wasm_bulk_memory(true);
         ret.wasm_simd(true);
+        ret.wasm_component_model(true);
         ret.wasm_backtrace_details(WasmBacktraceDetails::Environment);
 
         // This is on-by-default in `wasmparser` since it's a stage 4+ proposal
