c03f99a237c70f9cfc4d672078db44c81bde1818
Miłosz Rachwał <me@milek7.pl>
2024-02-02 18:21:35 +0100

    GuestProfiler C API: remove unsafe and add docs

--- crates/c-api/include/wasmtime/profiling.h
+++ crates/c-api/include/wasmtime/profiling.h
@@ -14,17 +14,83 @@
 extern "C" {
 #endif
 
-typedef struct wasmtime_guestprofiler_t wasmtime_guestprofiler_t;
+/**
+ * \brief Collects basic profiling data for a single WebAssembly guest.
+ *
+ * To use this, you’ll need to arrange to call #wasmtime_guestprofiler_sample at
+ * regular intervals while the guest is on the stack. The most straightforward
+ * way to do that is to call it from a callback registered with
+ * #wasmtime_store_epoch_deadline_callback.
+ *
+ * For more information see the Rust documentation at:
+ * https://docs.wasmtime.dev/api/wasmtime/struct.GuestProfiler.html
+ */
+typedef struct wasmtime_guestprofiler wasmtime_guestprofiler_t;
 
+/**
+ * \brief Deletes profiler without finishing it.
+ *
+ * \param guestprofiler profiler that is being deleted
+ */
 WASM_API_EXTERN void wasmtime_guestprofiler_delete(
     /* own */ wasmtime_guestprofiler_t *guestprofiler);
-WASM_API_EXTERN /* own */ wasmtime_guestprofiler_t *
-wasmtime_guestprofiler_new(wasm_name_t *module_name, uint64_t interval_nanos,
-                           size_t modules_size, wasm_name_t **modules_name,
-                           wasmtime_module_t **modules_module);
+
+/**
+ * \brief Begin profiling a new guest.
+ *
+ * \param module_name    name recorded in the profile
+ * \param interval_nanos intended sampling interval in nanoseconds recorded in
+ *                       the profile
+ * \param modules_len    count of tuples passed in `modules_name` and
+ *                       `modules_module`
+ * \param modules_name   names recorded in the profile associated with provided
+ *                       modules, pointer to the first element
+ * \param modules_module modules that will appear in captured stack traces,
+ *                       pointer to the first element
+ *
+ * \return Created profiler that is owned by the caller
+ *
+ * List of (#wasm_name_t*, #wasmtime_module_t*) tuples of `modules_len` length is
+ * passed column-major in `modules_name` and `modules_module`.
+ * This function does not take ownership of the arguments.
+ *
+ * For more information see the Rust documentation at:
+ * https://docs.wasmtime.dev/api/wasmtime/struct.GuestProfiler.html#method.new
+ */
+WASM_API_EXTERN /* own */ wasmtime_guestprofiler_t *wasmtime_guestprofiler_new(
+    const wasm_name_t *module_name, uint64_t interval_nanos, size_t modules_len,
+    const wasm_name_t **modules_name, const wasmtime_module_t **modules_module);
+
+/**
+ * \brief Add a sample to the profile.
+ *
+ * \param guestprofiler the profiler the sample is being added to
+ * \param store         that is being used to collect the backtraces
+ *
+ * This function does not take ownership of the arguments.
+ *
+ * For more information see the Rust documentation at:
+ * https://docs.wasmtime.dev/api/wasmtime/struct.GuestProfiler.html#method.sample
+ */
 WASM_API_EXTERN void
 wasmtime_guestprofiler_sample(wasmtime_guestprofiler_t *guestprofiler,
-                              wasmtime_store_t *store);
+                              const wasmtime_store_t *store);
+
+/**
+ * \brief Writes out the captured profile.
+ *
+ * \param guestprofiler the profiler which is being finished and deleted
+ * \param out           byte vector that receives the generated file
+ *
+ * \return Returns #wasmtime_error_t owned by the caller in case of error,
+ * `NULL` otherwise.
+ *
+ * This function takes ownership of `guestprofiler`, even when error is
+ * returned.
+ *
+ * For more information see the Rust documentation at:
+ * https://docs.wasmtime.dev/api/wasmtime/struct.GuestProfiler.html#method.finish
+ */
 WASM_API_EXTERN /* own */ wasmtime_error_t *
 wasmtime_guestprofiler_finish(/* own */ wasmtime_guestprofiler_t *guestprofiler,
                               wasm_byte_vec_t *out);
--- crates/c-api/src/profiling.rs
+++ crates/c-api/src/profiling.rs
@@ -1,5 +1,4 @@
 use crate::{wasm_byte_vec_t, wasm_name_t, wasmtime_error_t, wasmtime_module_t, wasmtime_store_t};
-use std::mem::MaybeUninit;
 use std::str::from_utf8;
 use std::time::Duration;
 use wasmtime::GuestProfiler;
@@ -14,13 +13,13 @@ wasmtime_c_api_macros::declare_own!(wasm_guestprofiler_t);
 pub unsafe extern "C" fn wasmtime_guestprofiler_new(
     module_name: &wasm_name_t,
     interval_nanos: u64,
-    modules_size: usize,
+    modules_len: usize,
     modules_name: *const &wasm_name_t,
     modules_module: *const &wasmtime_module_t,
 ) -> Box<wasm_guestprofiler_t> {
-    let modules_size = modules_size.try_into().unwrap();
+    let modules_len = modules_len.try_into().unwrap();
     let module_name = from_utf8(&module_name.as_slice()).expect("not valid utf-8");
-    let list = (0..modules_size)
+    let list = (0..modules_len)
         .map(|i| {
             (
                 from_utf8(modules_name.offset(i).as_ref().unwrap().as_slice())
@@ -36,24 +35,20 @@ pub unsafe extern "C" fn wasmtime_guestprofiler_new(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn wasmtime_guestprofiler_sample(
+pub extern "C" fn wasmtime_guestprofiler_sample(
     guestprofiler: &mut wasm_guestprofiler_t,
-    store: &mut wasmtime_store_t,
+    store: &wasmtime_store_t,
 ) {
     guestprofiler.guest_profiler.sample(&store.store);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn wasmtime_guestprofiler_finish(
-    guestprofiler: &mut MaybeUninit<wasm_guestprofiler_t>,
+pub extern "C" fn wasmtime_guestprofiler_finish(
+    guestprofiler: Box<wasm_guestprofiler_t>,
     out: &mut wasm_byte_vec_t,
 ) -> Option<Box<wasmtime_error_t>> {
     let mut buf = vec![];
-    match guestprofiler
-        .assume_init_read()
-        .guest_profiler
-        .finish(&mut buf)
-    {
+    match guestprofiler.guest_profiler.finish(&mut buf) {
         Ok(()) => {
             out.set_buffer(buf);
             None
