cad2c1377f006398bca27a5716f0230afde3cccd
Saúl Cabrera <saulecabrera@gmail.com>
2024-02-14 06:28:25 -0500

    Expect `CallInfo` where applicable and add a comment about the type definition

--- cranelift/codegen/src/isa/x64/inst/mod.rs
+++ cranelift/codegen/src/isa/x64/inst/mod.rs
@@ -2351,36 +2351,34 @@ fn x64_get_operands<F: Fn(VReg) -> VReg>(inst: &Inst, collector: &mut OperandCol
             // regalloc, so we do not need to represent its ABI to the
             // register allocator. Assert that we don't alter that
             // arrangement.
-            if let Some(info) = info {
-                debug_assert_ne!(*dest, ExternalName::LibCall(LibCall::Probestack));
-                for u in &info.uses {
-                    collector.reg_fixed_use(u.vreg, u.preg);
-                }
-                for d in &info.defs {
-                    collector.reg_fixed_def(d.vreg, d.preg);
-                }
-                collector.reg_clobbers(info.clobbers);
+            let info = info.as_ref().expect("CallInfo is expected in this path");
+            debug_assert_ne!(*dest, ExternalName::LibCall(LibCall::Probestack));
+            for u in &info.uses {
+                collector.reg_fixed_use(u.vreg, u.preg);
             }
+            for d in &info.defs {
+                collector.reg_fixed_def(d.vreg, d.preg);
+            }
+            collector.reg_clobbers(info.clobbers);
         }
 
         Inst::CallUnknown { info, dest, .. } => {
-            if let Some(info) = info {
-                match dest {
-                    RegMem::Reg { reg } if info.callee_conv == CallConv::Tail => {
-                        // TODO(https://github.com/bytecodealliance/regalloc2/issues/145):
-                        // This shouldn't be a fixed register constraint.
-                        collector.reg_fixed_use(*reg, regs::r15())
-                    }
-                    _ => dest.get_operands(collector),
+            let info = info.as_ref().expect("CallInfo is expected in this path");
+            match dest {
+                RegMem::Reg { reg } if info.callee_conv == CallConv::Tail => {
+                    // TODO(https://github.com/bytecodealliance/regalloc2/issues/145):
+                    // This shouldn't be a fixed register constraint.
+                    collector.reg_fixed_use(*reg, regs::r15())
                 }
-                for u in &info.uses {
-                    collector.reg_fixed_use(u.vreg, u.preg);
-                }
-                for d in &info.defs {
-                    collector.reg_fixed_def(d.vreg, d.preg);
-                }
-                collector.reg_clobbers(info.clobbers);
+                _ => dest.get_operands(collector),
             }
+            for u in &info.uses {
+                collector.reg_fixed_use(u.vreg, u.preg);
+            }
+            for d in &info.defs {
+                collector.reg_fixed_def(d.vreg, d.preg);
+            }
+            collector.reg_clobbers(info.clobbers);
         }
 
         Inst::ReturnCallKnown { callee, info } => {
