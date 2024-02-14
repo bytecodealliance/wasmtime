d098873607499c5e9fc283e9a662f4193e397f7d
Alex Crichton <alex@alexcrichton.com>
2024-02-14 07:21:30 -0800

    Fix running MIRI tests on macOS
    Closes #7936

--- crates/runtime/src/sys/miri/traphandlers.rs
+++ crates/runtime/src/sys/miri/traphandlers.rs
@@ -40,3 +40,7 @@ pub type SignalHandler<'a> = dyn Fn() + Send + Sync + 'a;
 pub fn platform_init(_macos_use_mach_ports: bool) {}
 
 pub fn lazy_per_thread_init() {}
+
+pub fn using_mach_ports() -> bool {
+    false
+}
