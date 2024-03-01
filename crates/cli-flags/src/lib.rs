480fb0e1a87bc4b4e72a85135c9ff4341bf78e8a
Andrew Brown <andrew.brown@intel.com>
2024-03-01 11:49:50 -0800

    Add CLI flags for instance limits (#8027)
    * Add CLI flags for instance limits
    
    While experimenting with `wasmtime serve`, I found it helpful to be able
    to configure some of the maximum limits of the various objects stored in
    the pooling allocator. This change surfaces some new `-O total-*` flags
    for controlling these limits from the command line.
    
    * review: prepend flags with `pooling_*`

--- crates/cli-flags/src/lib.rs
+++ crates/cli-flags/src/lib.rs
@@ -81,6 +81,26 @@ wasmtime_option_group! {
         /// Configure attempting to initialize linear memory via a
         /// copy-on-write mapping (default: yes)
         pub memory_init_cow: Option<bool>,
+
+        /// The maximum number of WebAssembly instances which can be created
+        /// with the pooling allocator.
+        pub pooling_total_core_instances: Option<u32>,
+
+        /// The maximum number of WebAssembly components which can be created
+        /// with the pooling allocator.
+        pub pooling_total_component_instances: Option<u32>,
+
+        /// The maximum number of WebAssembly memories which can be created with
+        /// the pooling allocator.
+        pub pooling_total_memories: Option<u32>,
+
+        /// The maximum number of WebAssembly tables which can be created with
+        /// the pooling allocator.
+        pub pooling_total_tables: Option<u32>,
+
+        /// The maximum number of WebAssembly stacks which can be created with
+        /// the pooling allocator.
+        pub pooling_total_stacks: Option<u32>,
     }
 
     enum Optimize {
@@ -527,6 +547,21 @@ impl CommonOptions {
                     }
                     if let Some(size) = self.opts.pooling_table_keep_resident {
                         cfg.table_keep_resident(size);
+                    }
+                    if let Some(limit) = self.opts.pooling_total_core_instances {
+                        cfg.total_core_instances(limit);
+                    }
+                    if let Some(limit) = self.opts.pooling_total_component_instances {
+                        cfg.total_component_instances(limit);
+                    }
+                    if let Some(limit) = self.opts.pooling_total_memories {
+                        cfg.total_memories(limit);
+                    }
+                    if let Some(limit) = self.opts.pooling_total_tables {
+                        cfg.total_tables(limit);
+                    }
+                    if let Some(limit) = self.opts.pooling_total_stacks {
+                        cfg.total_stacks(limit);
                     }
                     if let Some(enable) = self.opts.memory_protection_keys {
                         if enable {
