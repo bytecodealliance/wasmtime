19394e39d50a10e872e455f48e9c7fc47530a2b1
Alex Crichton <alex@alexcrichton.com>
2024-03-13 08:19:33 -0700

    Review comments

--- ci/vendor-wit.sh
+++ ci/vendor-wit.sh
@@ -5,15 +5,22 @@
 #
 # This script is executed on CI to ensure that everything is up-to-date.
 
+# Space-separated list of wasi proposals that are vendored here along with the
+# tag that they're all vendored at.
+#
+# This assumes that the repositories all have the pattern:
+# https://github.com/WebAssembly/wasi-$repo
+# and every repository has a tag `v$tag` here. That is currently done as part
+# of the WASI release process.
+repos="cli clocks filesystem http io random sockets"
+tag=0.2.0
+
 set -ex
 
 dst=crates/wasi/wit/deps
 
 rm -rf $dst
 mkdir -p $dst
-
-repos="cli clocks filesystem http io random sockets"
-tag=0.2.0
 
 for repo in $repos; do
   mkdir $dst/$repo
