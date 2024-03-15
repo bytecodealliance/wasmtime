d08d016ac97c21f145ed96dc06368c36072749a7
Alex Crichton <alex@alexcrichton.com>
2024-03-15 10:53:32 -0500

    Canonicalize floats produced through bitcasts (#8146)
    This commit updates the nan-canonicalization pass that Cranelift does to
    canonicalize bitcasts from arbitrary integers in addition to floating
    point arithmetic operations.
    
    Closes #8145

--- cranelift/codegen/src/nan_canonicalization.rs
+++ cranelift/codegen/src/nan_canonicalization.rs
@@ -48,6 +48,24 @@ fn is_fp_arith(pos: &mut FuncCursor, inst: Inst) -> bool {
                 || opcode == Opcode::Fsub
         }
         InstructionData::Ternary { opcode, .. } => opcode == Opcode::Fma,
+
+        // bitcasts coming from arbitrary integers need canonicalization as the
+        // integer isn't known to necessarily be a canonical NaN
+        InstructionData::LoadNoOffset {
+            opcode: Opcode::Bitcast,
+            arg,
+            flags: _,
+        } => {
+            let ret = pos.func.dfg.first_result(inst);
+            let ret_type = pos.func.dfg.value_type(ret);
+            let arg_type = pos.func.dfg.value_type(arg);
+            match (arg_type, ret_type) {
+                (types::I32, types::F32) => true,
+                (types::I64, types::F64) => true,
+                _ => false,
+            }
+        }
+
         _ => false,
     }
 }
