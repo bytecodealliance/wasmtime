8eb7914e87d7b47c3b19e78869f3c9d5a410c3af
Trevor Elliott <telliott@fastly.com>
2024-03-22 10:33:38 -0400

    cranelift: Handle argument extension in the Winch calling convention (#8215)
    * Disable argument packing with the winch cc if extension is present
    
    * Don't generate functions that use SIMD and the Winch calling convention
    
    * Handle i128 values for the winch calling convention

--- cranelift/fuzzgen/src/cranelift_arbitrary.rs
+++ cranelift/fuzzgen/src/cranelift_arbitrary.rs
@@ -98,12 +98,19 @@ impl<'a> CraneliftArbitrary for &mut Unstructured<'a> {
 
     fn signature(
         &mut self,
-        simd_enabled: bool,
+        mut simd_enabled: bool,
         architecture: Architecture,
         max_params: usize,
         max_rets: usize,
     ) -> Result<Signature> {
         let callconv = self.callconv(architecture)?;
+
+        // Winch doesn't support SIMD yet
+        // https://github.com/bytecodealliance/wasmtime/issues/8093
+        if callconv == CallConv::Winch {
+            simd_enabled = false;
+        }
+
         let mut sig = Signature::new(callconv);
 
         for _ in 0..max_params {
