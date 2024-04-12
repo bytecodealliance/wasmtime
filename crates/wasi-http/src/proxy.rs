03918ce1c0bf41481a839e326001a4399d2551d7
Ryan Levick <ryan.levick@fermyon.com>
2024-04-12 10:14:18 +0200

    Document the proxy module
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- crates/wasi-http/src/proxy.rs
+++ crates/wasi-http/src/proxy.rs
@@ -1,15 +1,31 @@
-use crate::{bindings, WasiHttpView};
+//! Implementation of the `wasi:http/proxy` world.
+//!
+//! The implementation at the top of the module for use in async contexts,
+//! while the `sync` module provides implementation for use in sync contexts.
 
-wasmtime::component::bindgen!({
-    world: "wasi:http/proxy",
-    tracing: true,
-    async: true,
-    with: {
-        "wasi:http": bindings::http,
-        "wasi": wasmtime_wasi::bindings,
-    },
-});
+use crate::WasiHttpView;
 
+mod bindings {
+    wasmtime::component::bindgen!({
+        world: "wasi:http/proxy",
+        tracing: true,
+        async: true,
+        with: {
+            "wasi:http": crate::bindings::http,
+            "wasi": wasmtime_wasi::bindings,
+        },
+    });
+}
+
+/// Raw bindings to the `wasi:http/proxy` exports.
+pub use bindings::exports;
+
+/// Bindings to the `wasi:http/proxy` world.
+pub use bindings::Proxy;
+
+/// Add support for the `wasi:http/proxy` world to a [`wasmtime::component::Linker`].
+///
+/// This should be used in async contexts. For sync contexts, use [`sync::add_to_linker`].
 pub fn add_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
 where
     T: WasiHttpView + wasmtime_wasi::WasiView,
@@ -30,28 +46,32 @@ where
 #[doc(hidden)]
 pub fn add_only_http_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
 where
-    T: WasiHttpView + wasmtime_wasi::WasiView + bindings::http::types::Host,
+    T: WasiHttpView + wasmtime_wasi::WasiView + crate::bindings::http::types::Host,
 {
-    bindings::http::outgoing_handler::add_to_linker(l, |t| t)?;
-    bindings::http::types::add_to_linker(l, |t| t)?;
+    crate::bindings::http::outgoing_handler::add_to_linker(l, |t| t)?;
+    crate::bindings::http::types::add_to_linker(l, |t| t)?;
 
     Ok(())
 }
 
+/// Sync implementation of the `wasi:http/proxy` world.
 pub mod sync {
-    use crate::{bindings, WasiHttpView};
+    use crate::WasiHttpView;
 
     wasmtime::component::bindgen!({
         world: "wasi:http/proxy",
         tracing: true,
         async: false,
         with: {
-            "wasi:http": bindings::http, // http is in this crate
+            "wasi:http": crate::bindings::http, // http is in this crate
             "wasi:io": wasmtime_wasi::bindings::sync, // io is sync
             "wasi": wasmtime_wasi::bindings, // everything else
         },
     });
 
+    /// Add support for the `wasi:http/proxy` world to a [`wasmtime::component::Linker`].
+    ///
+    /// This should be used in sync contexts. For async contexts, use [`super::add_to_linker`].
     pub fn add_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
     where
         T: WasiHttpView + wasmtime_wasi::WasiView,
@@ -75,10 +95,10 @@ pub mod sync {
     // TODO: This is temporary solution until the wasmtime_wasi command functions can be removed
     pub fn add_only_http_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
     where
-        T: WasiHttpView + wasmtime_wasi::WasiView + bindings::http::types::Host,
+        T: WasiHttpView + wasmtime_wasi::WasiView + crate::bindings::http::types::Host,
     {
-        bindings::http::outgoing_handler::add_to_linker(l, |t| t)?;
-        bindings::http::types::add_to_linker(l, |t| t)?;
+        crate::bindings::http::outgoing_handler::add_to_linker(l, |t| t)?;
+        crate::bindings::http::types::add_to_linker(l, |t| t)?;
 
         Ok(())
     }
