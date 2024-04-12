7d39d3be94ec26d73d8647bc5177dcff928473d9
Ryan Levick <ryan.levick@fermyon.com>
2024-04-12 11:11:36 +0200

    Document proxy add_to_linker with example
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- crates/wasi-http/src/proxy.rs
+++ crates/wasi-http/src/proxy.rs
@@ -23,9 +23,61 @@ pub use bindings::exports;
 /// Bindings to the `wasi:http/proxy` world.
 pub use bindings::Proxy;
 
-/// Add support for the `wasi:http/proxy` world to a [`wasmtime::component::Linker`].
+/// Add all of the `wasi:http/proxy` world's interfaces to a [`wasmtime::component::Linker`].
 ///
-/// This should be used in async contexts. For sync contexts, use [`sync::add_to_linker`].
+/// This function will add the `async` variant of all interfaces into the
+/// `Linker` provided. By `async` this means that this function is only
+/// compatible with [`Config::async_support(true)`][async]. For embeddings with
+/// async support disabled see [`sync::add_to_linker`] instead.
+///
+/// [async]: wasmtime::Config::async_support
+///
+/// # Example
+///
+/// ```
+/// use wasmtime::{Engine, Result, Store, Config};
+/// use wasmtime::component::{ResourceTable, Linker};
+/// use wasmtime_wasi::{WasiCtx, WasiView, WasiCtxBuilder};
+/// use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
+///
+/// fn main() -> Result<()> {
+///     let mut config = Config::new();
+///     config.async_support(true);
+///     let engine = Engine::new(&config)?;
+///
+///     let mut linker = Linker::<MyState>::new(&engine);
+///     wasmtime_wasi_http::proxy::add_to_linker(&mut linker)?;
+///     // ... add any further functionality to `linker` if desired ...
+///
+///     let mut store = Store::new(
+///         &engine,
+///         MyState {
+///             ctx: WasiCtxBuilder::new().build(),
+///             http_ctx: WasiHttpCtx::new(),
+///             table: ResourceTable::new(),
+///         },
+///     );
+///
+///     // use `linker.instantiate_async` to instantiate within `store`
+///
+///     Ok(())
+/// }
+///
+/// struct MyState {
+///     ctx: WasiCtx,
+///     http_ctx: WasiHttpCtx,
+///     table: ResourceTable,
+/// }
+///
+/// impl WasiHttpView for MyState {
+///     fn ctx(&mut self) -> &mut WasiHttpCtx { &mut self.http_ctx }
+///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+/// }
+/// impl WasiView for MyState {
+///     fn ctx(&mut self) -> &mut WasiCtx { &mut self.ctx }
+///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+/// }
+/// ```
 pub fn add_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
 where
     T: WasiHttpView + wasmtime_wasi::WasiView,
@@ -58,20 +110,75 @@ where
 pub mod sync {
     use crate::WasiHttpView;
 
-    wasmtime::component::bindgen!({
-        world: "wasi:http/proxy",
-        tracing: true,
-        async: false,
-        with: {
-            "wasi:http": crate::bindings::http, // http is in this crate
-            "wasi:io": wasmtime_wasi::bindings::sync, // io is sync
-            "wasi": wasmtime_wasi::bindings, // everything else
-        },
-    });
+    mod bindings {
+        wasmtime::component::bindgen!({
+            world: "wasi:http/proxy",
+            tracing: true,
+            async: false,
+            with: {
+                "wasi:http": crate::bindings::http, // http is in this crate
+                "wasi:io": wasmtime_wasi::bindings::sync, // io is sync
+                "wasi": wasmtime_wasi::bindings, // everything else
+            },
+        });
+    }
 
-    /// Add support for the `wasi:http/proxy` world to a [`wasmtime::component::Linker`].
+    /// Raw bindings to the `wasi:http/proxy` exports.
+    pub use bindings::exports;
+
+    /// Bindings to the `wasi:http/proxy` world.
+    pub use bindings::Proxy;
+
+    /// Add all of the `wasi:http/proxy` world's interfaces to a [`wasmtime::component::Linker`].
     ///
-    /// This should be used in sync contexts. For async contexts, use [`super::add_to_linker`].
+    /// This function will add the `sync` variant of all interfaces into the
+    /// `Linker` provided. For embeddings with async support see [`super::add_to_linker`] instead.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use wasmtime::{Engine, Result, Store, Config};
+    /// use wasmtime::component::{ResourceTable, Linker};
+    /// use wasmtime_wasi::{WasiCtx, WasiView, WasiCtxBuilder};
+    /// use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
+    ///
+    /// fn main() -> Result<()> {
+    ///     let config = Config::default();
+    ///     let engine = Engine::new(&config)?;
+    ///
+    ///     let mut linker = Linker::<MyState>::new(&engine);
+    ///     wasmtime_wasi_http::proxy::sync::add_to_linker(&mut linker)?;
+    ///     // ... add any further functionality to `linker` if desired ...
+    ///
+    ///     let mut store = Store::new(
+    ///         &engine,
+    ///         MyState {
+    ///             ctx: WasiCtxBuilder::new().build(),
+    ///             http_ctx: WasiHttpCtx::new(),
+    ///             table: ResourceTable::new(),
+    ///         },
+    ///     );
+    ///
+    ///     // use `linker.instantiate` to instantiate within `store`
+    ///
+    ///     Ok(())
+    /// }
+    ///
+    /// struct MyState {
+    ///     ctx: WasiCtx,
+    ///     http_ctx: WasiHttpCtx,
+    ///     table: ResourceTable,
+    /// }
+    ///
+    /// impl WasiHttpView for MyState {
+    ///     fn ctx(&mut self) -> &mut WasiHttpCtx { &mut self.http_ctx }
+    ///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+    /// }
+    /// impl WasiView for MyState {
+    ///     fn ctx(&mut self) -> &mut WasiCtx { &mut self.ctx }
+    ///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+    /// }
+    /// ```
     pub fn add_to_linker<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
     where
         T: WasiHttpView + wasmtime_wasi::WasiView,
