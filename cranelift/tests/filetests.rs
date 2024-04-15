9e02fe1a06a42c8c858fe7b93fa32191307174e7
Trevor Elliott <telliott@fastly.com>
2024-04-15 13:56:15 -0700

    Use libtest-mimic to make filetests behave like a normal test

--- cranelift/tests/filetests.rs
+++ cranelift/tests/filetests.rs
@@ -1,5 +1,16 @@
-fn main() -> anyhow::Result<()> {
+use libtest_mimic::{Arguments, Failed, Trial};
+
+fn main() -> std::process::ExitCode {
+    let args = Arguments::from_args();
+
+    let tests = vec![Trial::test("filetests", filetests)];
+
+    libtest_mimic::run(&args, tests).exit()
+}
+
+fn filetests() -> Result<(), Failed> {
     // Run all the filetests in the following directories.
-    cranelift_filetests::run(false, false, &["filetests".into(), "docs".into()])?;
+    cranelift_filetests::run(false, false, &["filetests".into(), "docs".into()])
+        .map_err(Failed::from)?;
     Ok(())
 }
