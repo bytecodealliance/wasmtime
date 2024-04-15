d17621d7a3451ab1ee9acf499e901c66882b05ec
Trevor Elliott <telliott@fastly.com>
2024-04-15 14:07:54 -0700

    Skip using libtest-mimic

--- cranelift/tests/filetests.rs
+++ cranelift/tests/filetests.rs
@@ -1,16 +1,6 @@
-use libtest_mimic::{Arguments, Failed, Trial};
-
-fn main() -> std::process::ExitCode {
-    let args = Arguments::from_args();
-
-    let tests = vec![Trial::test("filetests", filetests)];
-
-    libtest_mimic::run(&args, tests).exit()
-}
-
-fn filetests() -> Result<(), Failed> {
+#[test]
+fn filetests() -> anyhow::Result<()> {
     // Run all the filetests in the following directories.
-    cranelift_filetests::run(false, false, &["filetests".into(), "docs".into()])
-        .map_err(Failed::from)?;
+    cranelift_filetests::run(false, false, &["filetests".into(), "docs".into()])?;
     Ok(())
 }
