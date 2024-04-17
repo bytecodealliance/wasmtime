9bc5c773bcaa317f1fd6a2325bc183c4b6f875b5
Trevor Elliott <telliott@fastly.com>
2024-04-17 14:50:51 -0700

    Remove nextest

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-set -euo pipefail
-
-cargo_flags=(
-    --features wasmtime-cli/wasi-threads
-    --features wasmtime-cli/wasi-http
-    --features wasmtime-cli/component-model
-    --features wasmtime-cli/serve
-    --workspace
-    --exclude test-programs
-)
-
-if [ "${USE_NEXTEST:-0}" = "1" ]; then
-  cargo nextest run "${cargo_flags[@]}" "$@"
-else
-  cargo test "${cargo_flags[@]}" "$@"
-fi
+cargo test \
+    --features wasi-threads \
+    --features wasi-http \
+    --features component-model \
+    --features serve \
+    --workspace \
+    --exclude test-programs \
+    $@
