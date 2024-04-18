86806a88d74b7cc7a3f5f32c6ee96e1e719b2f71
Jamey Sharp <jsharp@fastly.com>
2024-04-18 13:02:50 -0700

    cranelift: Emit argument location uses eagerly in gen_arg (#8398)
    This reverts the key parts of e3a08d4c40f64ff7a2113d23af73f40af0859a10
    (#8151), because it turns out that we didn't need that abstraction.
    
    Several changes in the last month have enabled this:
    
    - #8292 and then #8316 allow us to refer to either incoming or outgoing
      argument areas in a (mostly) consistent way
    
    - #8327, #8377, and #8383 demonstrate that we never need to delay
      writing stack arguments directly to their final location
    
    prtest:full

--- cranelift/codegen/src/machinst/isle.rs
+++ cranelift/codegen/src/machinst/isle.rs
@@ -826,8 +826,7 @@ macro_rules! isle_prelude_method_helpers {
                 call_site.emit_copy_regs_to_buffer(self.lower_ctx, i, *arg_regs);
             }
             for (i, arg_regs) in arg_regs.iter().enumerate() {
-                let moves = call_site.gen_arg(self.lower_ctx, i, *arg_regs);
-                call_site.emit_arg_moves(self.lower_ctx, moves);
+                call_site.gen_arg(self.lower_ctx, i, *arg_regs);
             }
         }
 
