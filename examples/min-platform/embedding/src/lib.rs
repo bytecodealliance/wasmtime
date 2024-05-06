5054d400ba797d2cb697b5babdcf17b460a2a061
Alex Crichton <alex@alexcrichton.com>
2024-05-06 12:23:40 -0700

    Update documentation and example for no_std (#8555)
    * Update Wasmtime's tier stability documentation
    
    Move some items between tiers and add a few misc items here and there.
    
    * Update platform support documentation
    
    Re-word lots of this since it was originally written, link to the tiers
    of support page, and rewrite the section on `no_std`.
    
    * Update the `min-platform` example with no_std
    
    This commit updates the preexisting `min-platform` example to no longer
    require Nightly Rust and instead use the `no_std` support now added to
    Wasmtime. This involved:
    
    * Change the build process to produce a staticlib which is then manually
      converted via `cc` into a shared library for the native Linux platform.
    * Compile the modules outside of the embedding and only `deserialize`
      within the embedding.
    * Update the `indexmap` dependency to pick up a bug fix required in
      `no_std` mode (apparently, it fails on indexmap@2.0.0 and passes at
      2.2.6, I didn't dig much further).
    
    This commit additionally makes the `wasmtime-platform.h` header file
    generated by the example a release artifact for Wasmtime itself. The
    header itself is touched up a bit by configuring some more `cbindgen`
    options as well.
    
    * Fix clippy build
    
    prtest:full
    
    * Review comments
    
    * Pass gc-sections to linking the library

--- examples/min-platform/embedding/src/lib.rs
+++ examples/min-platform/embedding/src/lib.rs
@@ -1,12 +1,35 @@
+#![no_std]
+
+#[macro_use]
+extern crate alloc;
+
 use anyhow::Result;
 use wasmtime::{Engine, Instance, Linker, Module, Store};
 
 mod allocator;
+mod panic;
 
+/// Entrypoint of this embedding.
+///
+/// This takes a number of parameters which are the precompiled module AOT
+/// images that are run for each of the various tests below. The first parameter
+/// is also where to put an error string, if any, if anything fails.
 #[no_mangle]
-pub unsafe extern "C" fn run(buf: *mut u8, size: usize) -> usize {
-    let buf = std::slice::from_raw_parts_mut(buf, size);
-    match run_result() {
+pub unsafe extern "C" fn run(
+    error_buf: *mut u8,
+    error_size: usize,
+    smoke_module: *const u8,
+    smoke_size: usize,
+    simple_add_module: *const u8,
+    simple_add_size: usize,
+    simple_host_fn_module: *const u8,
+    simple_host_fn_size: usize,
+) -> usize {
+    let buf = core::slice::from_raw_parts_mut(error_buf, error_size);
+    let smoke = core::slice::from_raw_parts(smoke_module, smoke_size);
+    let simple_add = core::slice::from_raw_parts(simple_add_module, simple_add_size);
+    let simple_host_fn = core::slice::from_raw_parts(simple_host_fn_module, simple_host_fn_size);
+    match run_result(smoke, simple_add, simple_host_fn) {
         Ok(()) => 0,
         Err(e) => {
             let msg = format!("{e:?}");
@@ -17,50 +40,37 @@ pub unsafe extern "C" fn run(buf: *mut u8, size: usize) -> usize {
     }
 }
 
-fn run_result() -> Result<()> {
-    smoke()?;
-    simple_add()?;
-    simple_host_fn()?;
+fn run_result(
+    smoke_module: &[u8],
+    simple_add_module: &[u8],
+    simple_host_fn_module: &[u8],
+) -> Result<()> {
+    smoke(smoke_module)?;
+    simple_add(simple_add_module)?;
+    simple_host_fn(simple_host_fn_module)?;
     Ok(())
 }
 
-fn smoke() -> Result<()> {
+fn smoke(module: &[u8]) -> Result<()> {
     let engine = Engine::default();
-    let module = Module::new(&engine, "(module)")?;
+    let module = unsafe { Module::deserialize(&engine, module)? };
     Instance::new(&mut Store::new(&engine, ()), &module, &[])?;
     Ok(())
 }
 
-fn simple_add() -> Result<()> {
+fn simple_add(module: &[u8]) -> Result<()> {
     let engine = Engine::default();
-    let module = Module::new(
-        &engine,
-        r#"
-            (module
-                (func (export "add") (param i32 i32) (result i32)
-                    (i32.add (local.get 0) (local.get 1)))
-            )
-        "#,
-    )?;
+    let module = unsafe { Module::deserialize(&engine, module)? };
     let mut store = Store::new(&engine, ());
-    let instance = Instance::new(&mut store, &module, &[])?;
+    let instance = Linker::new(&engine).instantiate(&mut store, &module)?;
     let func = instance.get_typed_func::<(u32, u32), u32>(&mut store, "add")?;
     assert_eq!(func.call(&mut store, (2, 3))?, 5);
     Ok(())
 }
 
-fn simple_host_fn() -> Result<()> {
+fn simple_host_fn(module: &[u8]) -> Result<()> {
     let engine = Engine::default();
-    let module = Module::new(
-        &engine,
-        r#"
-            (module
-                (import "host" "multiply" (func $multiply (param i32 i32) (result i32)))
-                (func (export "add_and_mul") (param i32 i32 i32) (result i32)
-                    (i32.add (call $multiply (local.get 0) (local.get 1)) (local.get 2)))
-            )
-        "#,
-    )?;
+    let module = unsafe { Module::deserialize(&engine, module)? };
     let mut linker = Linker::<()>::new(&engine);
     linker.func_wrap("host", "multiply", |a: u32, b: u32| a.saturating_mul(b))?;
     let mut store = Store::new(&engine, ());
