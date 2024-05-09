330eb20c05d9e05a833a522122a59046083d81f3
Alex Crichton <alex@alexcrichton.com>
2024-05-09 09:03:38 -0700

    Disable fuzzing of memory64 (#8589)
    The proposal has been updated to include 64-bit tables which aren't yet
    implemented in Wasmtime, so disable the fuzz test case generation for
    now.

--- crates/fuzzing/src/generators/config.rs
+++ crates/fuzzing/src/generators/config.rs
@@ -399,6 +399,11 @@ impl<'a> Arbitrary<'a> for Config {
         // doesn't implement this yet, so forcibly always disable it.
         config.module_config.config.tail_call_enabled = false;
 
+        // Wasm-smith implements the most up-to-date version of memory64 where
+        // it supports 64-bit tables as well, but Wasmtime doesn't support that
+        // yet, so disable the memory64 proposal in fuzzing for now.
+        config.module_config.config.memory64_enabled = false;
+
         // If using the pooling allocator, constrain the memory and module configurations
         // to the module limits.
         if let InstanceAllocationStrategy::Pooling(pooling) = &mut config.wasmtime.strategy {
