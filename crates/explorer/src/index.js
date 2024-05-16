6fb2e90882e71a7374e1e0e3918a9874f39c371a
L. Pereira <l.pereira@fastly.com>
2024-05-16 16:09:47 -0700

    Rebase, fix conflicts, and remove the bridge because it doesn't support the CLIF view
    Co-Authored-By: Nick Fitzgerald <fitzgen@gmail.com>
    
    Auto-format explorer index.js
    
    Use CRC24 to pick colors for offsets in explore
    
    Make better use of the DOM
    
    In the ASM view, instead of creating one element per instruction,
    create one per WASM chunk.  This significantly reduces the amount
    of elements we have to take care of, including event listeners.
    
    For both views, get rid of all the maps to look up DOM elements by
    WASM offset and use CSS selectors to find them.  This made things
    quite a bit smoother.  To highlight items, we now add a class to
    elements with the same WASM offset, and remove it when we don't
    want to highlight them anymore.
    
    In addition to this, use a bit more tricks to get brighter colors
    from the CRC24 algorithm we're now using to pick the colors.  (Since
    the colors can now be very dark, we get the luminance by using the
    NTSC color space, or YIQ, and use contrasting colors.)
    
    Draw polygon bridging WAT and ASM views
    
    Scale luma calculation to a power of two to avoid integer division
    
    Outline hovered lines
    
    Instead of using opacity, which slow down rendering, use a solid
    outline, which looks nicer and is more efficient!
    
    Adjust bridge width to account for different scrollbar widths
    
    Use simpler math in rgbToLuma() to avoid multiplications too
    
    Slight improvement to crc24 calculation
    
    Draw multiple conections from WAT to ASM views
    
    Sometimes the ASM block contains disjoint sets of instructions
    that relate to one WASM instruction, so link all of them at the
    same time.
    
    Show WASM offset on hover in the ASM view
    
    Fix grouping of ASM instructions
    
    Some instructions with NULL wasm_offset were being group together
    with the first instruction in the stream that had an offset.
    
    Only set the title attribute to show WASM offset on hover
    
    This saves quite a bit of memory!
    
    Fix grouping of instructions in ASM mode
    
    Add note about the origin of the CRC-24 calculation
    
    Tweak color generation (let some brighter colors pass the filter)
    
    Ensure we have enough elements to compute the bridge polygon
    
    I don't know why this happens, but sometimes, a block of instructions
    in the ASM side, that have a WASM offset in the generated JSON, won't
    have an equivalent in the WAT side.  Guard against this.
    
    Don't obscure the hovered line with the outline
    
    Simplify bridge width calculation
    
    Optimize hovering of blocks when one 1 WAT element matches with 1 ASM element
    
    Address review comments
    
    Make eslint happy
    
    Since this code runs in a browser, we need to tell eslint that
    it's going to run in a browser enviroment so things like `window`
    and `document` are defined.
    
    Signed-off-by: L. Pereira <l.pereira@fastly.com>

--- crates/explorer/src/index.js
+++ crates/explorer/src/index.js
@@ -1,7 +1,3 @@
-/* global window, document */
-
-/*** State *********************************************************************/
-
 class State {
   constructor(wat, clif, asm) {
     this.wat = wat;
@@ -12,203 +8,177 @@ class State {
 
 const state = (window.STATE = new State(window.WAT, window.CLIF, window.ASM));
 
-/*** Hues for Offsets **********************************************************/
+/*** LRU Cache *****************************************************************/
 
-const hues = [
-  80, 160, 240, 320, 40, 120, 200, 280, 20, 100, 180, 260, 340, 60, 140, 220,
-  300,
-];
+class LruCache {
+  constructor(maxSize, getFunc) {
+    // Maps preserve the insertion order, so we can use it to implement a naïve LRU
+    // cache.
+    this.cache = new Map();
+    this.maxSize = maxSize;
+    this.getFunc = getFunc;
+  }
 
-const nextHue = (function () {
-  let i = 0;
-  return () => {
-    return hues[++i % hues.length];
+  get(key) {
+    let v = this.cache.get(key);
+    if (v !== undefined) {
+      // Remove the found element from the cache so it can be inserted it again
+      // at the end before returning.
+      this.cache.delete(key);
+    } else {
+      v = this.getFunc(key);
+      if (this.cache.size > this.cache.maxSize) {
+        // Evict the oldest item from the cache.
+        this.cache.delete(this.cache.keys().next().value);
+      }
+    }
+    this.cache.set(key, v);
+    return v;
+  }
+}
+
+/*** Colors for Offsets **********************************************************/
+
+const rgbToLuma = rgb => {
+  // Use the NTSC color space (https://en.wikipedia.org/wiki/YIQ) to determine
+  // the luminance (Y) of this color.  (This is an approximation using powers of two,
+  // to avoid multiplications and divisions. It's not accurate, but it's good enough
+  // for our purposes.)
+  let [r, g, b] = rgbToTriple(rgb);
+  return (((r << 8) + (g << 9) + (b << 7)) >> 10) + (g & 31);
+};
+
+// Convert a color as a 24-bit number into a list with 3 elements: R, G, and B,
+// each ranging [0, 255].
+const rgbToTriple = rgb => [(rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff];
+
+// Use CRC24 as a way to calculate a color for a given Wasm offset. This
+// particular algorithm has been chosen because it produces bright, vibrant
+// colors, that don't repeat often, and is easily implementable.
+const calculateRgbForOffset = offset => {
+  const crc24 = (crc, byte) => {
+    // CRC computation adapted from Wikipedia[1] (shift-register based division versions.)
+    // [1] https://en.m.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
+    crc ^= byte << 16;
+    for (let bit = 0; bit < 8; bit++) {
+      crc = ((crc << 1) ^ (crc & 0x800000 ? 0xfa5711 : 0)) & 0xffffff;
+    }
+    return crc;
   };
-})();
 
-// NB: don't just assign hues based on something simple like `hues[offset %
-// hues.length]` since that can suffer from bias due to certain alignments
-// happening more or less frequently.
-const offsetToHue = new Map();
-
-// Get the hue for the given offset, or assign it a new one if it doesn't have
-// one already.
-const hueForOffset = offset => {
-  if (offsetToHue.has(offset)) {
-    return offsetToHue.get(offset);
-  } else {
-    let hue = nextHue();
-    offsetToHue.set(offset, hue);
-    return hue;
+  // Feed the offset into the CRC24 algorithm, one byte at a time.
+  let color = offset;
+  while (offset) {
+    color = crc24(color, offset & 0xff);
+    offset >>= 8;
   }
+
+  // Avoid colors that are too close to white.  Flip some bits around
+  // so that the color components are more pronounced.
+  return rgbToLuma(color) > 200 ? color ^ 0xa5a5a5 : color;
 };
 
-// Get the hue for the given offset, only if the offset has already been
-// assigned a hue.
-const existingHueForOffset = offset => {
-  return offsetToHue.get(offset);
+// Memoize all colors for a given Wasm offset. Cache isn't used here since,
+// when rendering the Wat side, we use the fact that if a color has not been
+// assigned during the rendering of the Native Asm side, that block of Wasm
+// instructions isn't colored.
+let offsetToRgb = new Map();
+const rgbForOffset = offset => {
+  let rgb = offsetToRgb.get(offset);
+  if (rgb === undefined) {
+    rgb = calculateRgbForOffset(offset);
+    offsetToRgb.set(offset, rgb);
+  }
+  return rgb;
 };
 
-// Get WAT chunk elements by Wasm offset.
-const watByOffset = new Map();
+// Convert a color in a 24-bit number to a string suitable for CSS styling.
+const rgbToCss = rgb => `rgba(${rgbToTriple(rgb).join(",")})`;
 
-// Get asm instruction elements by Wasm offset.
-const asmByOffset = new Map();
-
-// Get clif instruction elements by Wasm offset.
-const clifByOffset = new Map();
-
-// Get all (WAT chunk or asm instruction) elements by offset.
-const anyByOffset = new Map();
-
-const addWatElem = (offset, elem) => {
-  if (!watByOffset.has(offset)) {
-    watByOffset.set(offset, []);
-  }
-  watByOffset.get(offset).push(elem);
-
-  if (!anyByOffset.has(offset)) {
-    anyByOffset.set(offset, []);
-  }
-  anyByOffset.get(offset).push(elem);
+// Darkens a color in a 24-bit number slightly by subtracting at most 0x20
+// from each color component; e.g. RGB(175, 161, 10) becomes RGB(143, 129, 0).
+// This loses some color information, but it's good enough for our use case here.
+const rgbDarken = rgb => {
+  let [r, g, b] = rgbToTriple(rgb);
+  return (
+    ((r - Math.min(r, 0x20)) << 16) |
+    ((g - Math.min(g, 0x20)) << 8) |
+    (b - Math.min(b, 0x20))
+  );
 };
 
-const addAsmElem = (offset, elem) => {
-  if (!asmByOffset.has(offset)) {
-    asmByOffset.set(offset, []);
-  }
-  asmByOffset.get(offset).push(elem);
-
-  if (!anyByOffset.has(offset)) {
-    anyByOffset.set(offset, []);
-  }
-  anyByOffset.get(offset).push(elem);
-};
-
-const addClifElem = (offset, elem) => {
-  if (!clifByOffset.has(offset)) {
-    clifByOffset.set(offset, []);
-  }
-  clifByOffset.get(offset).push(elem);
-
-  if (!anyByOffset.has(offset)) {
-    anyByOffset.set(offset, []);
-  }
-  anyByOffset.get(offset).push(elem);
+// Adjust the color styles of a DOM element for a given Wasm offset.
+const adjustColorForOffset = (element, offset) => {
+  let backgroundColor = rgbForOffset(offset);
+  element.style.backgroundColor = rgbToCss(backgroundColor);
+  element.classList.add(
+    rgbToLuma(backgroundColor) > 128 ? "dark-text" : "light-text",
+  );
 };
 
 /*** Event Handlers ************************************************************/
 
-const watElem = document.getElementById("wat");
-watElem.addEventListener(
-  "click",
-  event => {
-    if (event.target.dataset.wasmOffset == null) {
-      return;
-    }
-
-    const offset = parseInt(event.target.dataset.wasmOffset);
-    if (!asmByOffset.get(offset)) {
-      return;
-    }
-
-    const firstClifElem = clifByOffset.get(offset)[0];
-    firstClifElem.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
-    });
-    const firstAsmElem = asmByOffset.get(offset)[0];
-    firstAsmElem.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
-    });
-  },
-  { passive: true },
+// Connects callbacks to mouse hovering events so elements are properly highlighted when
+// hovered, and the bridging element is drawn between the instruction lists.
+const linkedElementCache = new LruCache(256, offset =>
+  document.querySelectorAll(`[data-wasm-offset="${offset}"]`),
 );
 
-const asmElem = document.getElementById("asm");
-asmElem.addEventListener(
-  "click",
-  event => {
-    if (event.target.dataset.wasmOffset == null) {
-      return;
-    }
-
-    const offset = parseInt(event.target.dataset.wasmOffset);
-    if (!watByOffset.get(offset)) {
-      return;
-    }
-
-    const firstWatElem = watByOffset.get(offset)[0];
-    firstWatElem.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
+const eachElementWithSameWasmOff = (event, closure) => {
+  let offset = event.target.dataset.wasmOffset;
+  if (offset !== null) {
+    // Run the loop inside an animation frame.  Since we're modifying the DOM,
+    // do so when the browser has some breathing room.
+    window.requestAnimationFrame(() => {
+      linkedElementCache.get(offset).forEach(closure);
     });
-    const firstClifElem = clifByOffset.get(offset)[0];
-    firstClifElem.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
-    });
-  },
-  { passive: true },
-);
-const clifElem = document.getElementById("clif");
-if (clifElem) {
-  clifElem.addEventListener(
+  }
+};
+
+const linkElements = element => {
+  element.addEventListener(
     "click",
     event => {
-      if (event.target.dataset.wasmOffset == null) {
-        return;
-      }
-
-      const offset = parseInt(event.target.dataset.wasmOffset);
-      if (!watByOffset.get(offset)) {
-        return;
-      }
-
-      const firstWatElem = watByOffset.get(offset)[0];
-      firstWatElem.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "nearest",
-      });
-      const firstAsmElem = asmByOffset.get(offset)[0];
-      firstAsmElem.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "nearest",
+      eachElementWithSameWasmOff(event, elem => {
+        if (elem === event.target) return; // Only scroll into view the other elements.
+        elem.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+          inline: "nearest",
+        });
       });
     },
     { passive: true },
   );
-}
 
-const onMouseEnter = event => {
-  if (event.target.dataset.wasmOffset == null) {
-    return;
-  }
+  element.addEventListener("mouseenter", event => {
+    let offset = event.target.dataset.wasmOffset;
+    if (offset === null) return;
 
-  const offset = parseInt(event.target.dataset.wasmOffset);
-  const hue = hueForOffset(offset);
-  for (const elem of anyByOffset.get(offset)) {
-    elem.style.backgroundColor = `hsl(${hue} 75% 80%)`;
-  }
-};
+    // Gather all elements related to the desired offset.
+    let elems = linkedElementCache.get(offset);
 
-const onMouseLeave = event => {
-  if (event.target.dataset.wasmOffset == null) {
-    return;
-  }
+    // Perform the DOM modification inside an animation frame to give the browser a bit of
+    // a breathing room.
+    window.requestAnimationFrame(() => {
+      // Draw a 2px dark outline in each block of instructions so it stands out a bit better
+      // when hovered.
+      let outline = `2px solid ${rgbToCss(rgbDarken(rgbForOffset(offset)))}`;
+      for (const elem of elems) {
+        elem.setAttribute("title", `Wasm offset @ ${offset}`);
+        elem.classList.add("hovered");
+        elem.style.outline = outline;
+      }
+    });
+  });
 
-  const offset = parseInt(event.target.dataset.wasmOffset);
-  const hue = hueForOffset(offset);
-  for (const elem of anyByOffset.get(offset)) {
-    elem.style.backgroundColor = `hsl(${hue} 50% 95%)`;
-  }
+  element.addEventListener("mouseleave", event => {
+    eachElementWithSameWasmOff(event, elem => {
+      elem.removeAttribute("title");
+      elem.classList.remove("hovered");
+      elem.style.outline = "";
+    });
+  });
 };
 
 /*** Rendering *****************************************************************/
@@ -242,8 +212,14 @@ const renderInst = (mnemonic, operands) => {
   }
 };
 
-// Render the CLIF.
+const createDivForCode = () => {
+  let div = document.createElement("div");
+  div.classList.add("highlight");
+  return div;
+};
 
+// Render the CLIF (if any).
+const clifElem = document.getElementById("clif");
 if (clifElem) {
   for (const func of state.clif.functions) {
     const funcElem = document.createElement("div");
@@ -257,73 +233,79 @@ if (clifElem) {
     funcHeader.title = `Function ${func.func_index}: ${func_name}`;
     funcElem.appendChild(funcHeader);
 
-    const bodyElem = document.createElement("pre");
     for (const inst of func.instructions) {
-      const instElem = document.createElement("span");
+      const instElem = createDivForCode();
       instElem.textContent = `${inst.clif}\n`;
       if (inst.wasm_offset != null) {
-        instElem.setAttribute("data-wasm-offset", inst.wasm_offset);
-        const hue = hueForOffset(inst.wasm_offset);
-        instElem.style.backgroundColor = `hsl(${hue} 50% 90%)`;
-        instElem.addEventListener("mouseenter", onMouseEnter);
-        instElem.addEventListener("mouseleave", onMouseLeave);
-        addClifElem(inst.wasm_offset, instElem);
+        instElem.dataset.wasmOffset = inst.wasm_offset;
+        adjustColorForOffset(instElem, inst.wasm_offset);
+        linkElements(instElem);
       }
-      bodyElem.appendChild(instElem);
+      funcElem.appendChild(instElem);
     }
-    funcElem.appendChild(bodyElem);
 
     clifElem.appendChild(funcElem);
   }
 }
 
 // Render the ASM.
-
+const asmElem = document.getElementById("asm");
 for (const func of state.asm.functions) {
   const funcElem = document.createElement("div");
 
   const funcHeader = document.createElement("h3");
-  let func_name =
+  let functionName =
     func.name === null ? `function[${func.func_index}]` : func.name;
-  let demangled_name =
-    func.demangled_name !== null ? func.demangled_name : func_name;
-  funcHeader.textContent = `Disassembly of function <${demangled_name}>:`;
-  funcHeader.title = `Function ${func.func_index}: ${func_name}`;
+  let demangledName =
+    func.demangled_name !== null ? func.demangled_name : functionName;
+  funcHeader.textContent = `Disassembly of function <${demangledName}>:`;
+  funcHeader.title = `Function ${func.func_index}: ${functionName}`;
   funcElem.appendChild(funcHeader);
 
-  const bodyElem = document.createElement("pre");
-  for (const inst of func.instructions) {
-    const instElem = document.createElement("span");
-    instElem.textContent = `${renderAddress(inst.address)}    ${renderBytes(inst.bytes)}    ${renderInst(inst.mnemonic, inst.operands)}\n`;
-    if (inst.wasm_offset != null) {
-      instElem.setAttribute("data-wasm-offset", inst.wasm_offset);
-      const hue = hueForOffset(inst.wasm_offset);
-      instElem.style.backgroundColor = `hsl(${hue} 50% 90%)`;
-      instElem.addEventListener("mouseenter", onMouseEnter);
-      instElem.addEventListener("mouseleave", onMouseLeave);
-      addAsmElem(inst.wasm_offset, instElem);
+  let currentBlock = createDivForCode();
+  let disasmBuffer = [];
+  let lastOffset = null;
+
+  const addCurrentBlock = offset => {
+    currentBlock.dataset.wasmOffset = offset;
+
+    if (offset !== null) {
+      adjustColorForOffset(currentBlock, offset);
+      linkElements(currentBlock);
     }
-    bodyElem.appendChild(instElem);
+
+    currentBlock.innerText = disasmBuffer.join("\n");
+    funcElem.appendChild(currentBlock);
+    disasmBuffer = [];
+  };
+
+  for (const inst of func.instructions) {
+    if (lastOffset !== inst.wasm_offset) {
+      addCurrentBlock(lastOffset);
+      currentBlock = createDivForCode();
+      lastOffset = inst.wasm_offset;
+    }
+    disasmBuffer.push(
+      `${renderAddress(inst.address)}    ${renderBytes(inst.bytes)}    ${renderInst(inst.mnemonic, inst.operands)}`,
+    );
   }
-  funcElem.appendChild(bodyElem);
+  addCurrentBlock(lastOffset);
 
   asmElem.appendChild(funcElem);
 }
 
 // Render the WAT.
-
+const watElem = document.getElementById("wat");
 for (const chunk of state.wat.chunks) {
-  const chunkElem = document.createElement("span");
-  if (chunk.wasm_offset != null) {
-    chunkElem.dataset.wasmOffset = chunk.wasm_offset;
-    const hue = existingHueForOffset(chunk.wasm_offset);
-    if (hue) {
-      chunkElem.style.backgroundColor = `hsl(${hue} 50% 95%)`;
-      chunkElem.addEventListener("mouseenter", onMouseEnter);
-      chunkElem.addEventListener("mouseleave", onMouseLeave);
-      addWatElem(chunk.wasm_offset, chunkElem);
-    }
+  if (chunk.wasm_offset === null) continue;
+  const block = createDivForCode();
+  block.dataset.wasmOffset = chunk.wasm_offset;
+  block.innerText = chunk.wat;
+
+  if (offsetToRgb.get(chunk.wasm_offset) !== undefined) {
+    adjustColorForOffset(block, chunk.wasm_offset);
+    linkElements(block);
   }
-  chunkElem.textContent = chunk.wat;
-  watElem.appendChild(chunkElem);
+
+  watElem.appendChild(block);
 }
