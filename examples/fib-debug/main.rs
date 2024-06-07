482afd6e85defffab283cbc538bacfd622186241
Alex Crichton <alex@alexcrichton.com>
2024-06-06 17:51:24 -0700

    Recommend `-O opt-level=0` when debugging wasm
    This improves inspection of local variables by avoiding the egraph pass
    which doesn't have full fidelity in terms of preserving debug
    information.

--- examples/fib-debug/main.rs
+++ examples/fib-debug/main.rs
@@ -14,7 +14,7 @@ fn main() -> Result<()> {
     // Load our previously compiled wasm file (built previously with Cargo) and
     // also ensure that we generate debuginfo so this executable can be
     // debugged in GDB.
-    let engine = Engine::new(Config::new().debug_info(true))?;
+    let engine = Engine::new(Config::new().debug_info(true).opt_level(OptLevel::None))?;
     let mut store = Store::new(&engine, ());
     let module = Module::from_file(&engine, "target/wasm32-unknown-unknown/debug/fib.wasm")?;
     let instance = Instance::new(&mut store, &module, &[])?;
