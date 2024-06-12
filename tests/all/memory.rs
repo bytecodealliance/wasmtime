67c35062b2efbdb288132fb5852275bc123a7b26
Nick Fitzgerald <fitzgen@gmail.com>
2024-06-12 14:08:43 -0700

    Revert "Avoid actually trying to allocate the whole address space in the `massive_64_bit_still_limited` test"
    This reverts commit ccfa34a78dd3d53e49a6158ca03077d42ce8bcd7.

--- tests/all/memory.rs
+++ tests/all/memory.rs
@@ -375,7 +375,7 @@ fn massive_64_bit_still_limited() -> Result<()> {
             _max: Option<usize>,
         ) -> Result<bool> {
             self.hit = true;
-            Ok(false)
+            Ok(true)
         }
         fn table_growing(
             &mut self,
