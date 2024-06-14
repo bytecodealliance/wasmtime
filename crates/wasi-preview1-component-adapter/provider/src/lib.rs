3e2cadb7ab99a022d87c6c0e3e46b1b53ecc4e6c
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-06-14 07:20:26 +0000

    Update adapter docs

--- crates/wasi-preview1-component-adapter/provider/src/lib.rs
+++ crates/wasi-preview1-component-adapter/provider/src/lib.rs
@@ -1,8 +1,8 @@
 //! This crate contains the binaries of three WebAssembly modules:
 //!
+//! - [`WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER`]
 //! - [`WASI_SNAPSHOT_PREVIEW1_COMMAND_ADAPTER`]
 //! - [`WASI_SNAPSHOT_PREVIEW1_PROXY_ADAPTER`]
-//! - [`WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER`]
 //!
 //! These three modules bridge the wasip1 ABI to the wasip2 ABI of the component
 //! model.
@@ -12,25 +12,33 @@
 //!
 //! [`wit_component::ComponentEncoder::adapter`]: https://docs.rs/wit-component/latest/wit_component/struct.ComponentEncoder.html#method.adapter
 
+/// The "reactor" adapter provides the default adaptation from preview1 to
+/// preview2.
+///
+/// This adapter implements the [`wasi:cli/imports`] world.
+///
+/// [`wasi:cli/imports`]: https://github.com/WebAssembly/WASI/blob/01bb90d8b66cbc1d50349aaaab9ac5b143c9c98c/preview2/cli/imports.wit
+pub const WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER: &[u8] =
+    include_bytes!("../artefacts/wasi_snapshot_preview1.reactor.wasm");
+
 /// The "command" adapter extends the ["reactor" adapter] and additionally
 /// exports a `run` function entrypoint.
 ///
 /// This adapter implements the [`wasi:cli/command`] world.
 ///
 /// ["reactor" adapter]: WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER
-/// [`wasi:cli/command`]: https://github.com/WebAssembly/wasi-cli/blob/6ae82617096e83e6606047736e84ac397b788631/wit/command.wit
+/// [`wasi:cli/command`]: https://github.com/WebAssembly/WASI/blob/01bb90d8b66cbc1d50349aaaab9ac5b143c9c98c/preview2/cli/command.wit
 pub const WASI_SNAPSHOT_PREVIEW1_COMMAND_ADAPTER: &[u8] =
     include_bytes!("../artefacts/wasi_snapshot_preview1.command.wasm");
 
-/// The "proxy" adapter provides implements a HTTP proxy.
+/// The "proxy" adapter provides implements a HTTP proxy which is more
+/// restricted than the ["reactor" adapter] adapter, as it lacks filesystem,
+/// socket, environment, exit, and terminal support, but includes HTTP handlers
+/// for incoming and outgoing requests.
+///
+/// This adapter implements the [`wasi:http/proxy`] world.
+///
+/// ["reactor" adapter]: WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER
+/// [`wasi:http/proxy`]: https://github.com/WebAssembly/WASI/blob/01bb90d8b66cbc1d50349aaaab9ac5b143c9c98c/preview2/http/proxy.wit
 pub const WASI_SNAPSHOT_PREVIEW1_PROXY_ADAPTER: &[u8] =
     include_bytes!("../artefacts/wasi_snapshot_preview1.proxy.wasm");
-
-/// The "reactor" adapter provides the default adaptation from preview1 to
-/// preview2.
-///
-/// This adapter implements the [`wasi:cli/imports`] world.
-///
-/// [`wasi:cli/imports`]: https://github.com/WebAssembly/wasi-cli/blob/6ae82617096e83e6606047736e84ac397b788631/wit/imports.wit
-pub const WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER: &[u8] =
-    include_bytes!("../artefacts/wasi_snapshot_preview1.reactor.wasm");
