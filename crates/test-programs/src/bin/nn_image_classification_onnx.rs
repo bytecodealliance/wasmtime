d8e07c512b035bfb09f8e466618ad5c3c1adcf9e
Andrew Brown <andrew.brown@intel.com>
2024-06-14 15:47:22 -0700

    wasi-nn: remove some unncecessary panics from test programs

--- crates/test-programs/src/bin/nn_image_classification_onnx.rs
+++ crates/test-programs/src/bin/nn_image_classification_onnx.rs
@@ -1,15 +1,15 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::fs;
 use test_programs::nn::{classify, sort_results};
 use wasi_nn::{ExecutionTarget, GraphBuilder, GraphEncoding};
 
 pub fn main() -> Result<()> {
     let model = fs::read("fixture/model.onnx")
-        .expect("the model file to be mapped to the fixture directory");
+        .context("the model file to be mapped to the fixture directory")?;
     let graph =
         GraphBuilder::new(GraphEncoding::Onnx, ExecutionTarget::CPU).build_from_bytes([&model])?;
     let tensor = fs::read("fixture/000000062808.rgb")
-        .expect("the tensor file to be mapped to the fixture directory");
+        .context("the tensor file to be mapped to the fixture directory")?;
     let results = classify(graph, tensor)?;
     let top_five = &sort_results(&results)[..5];
     // 963 is meat loaf, meatloaf.
