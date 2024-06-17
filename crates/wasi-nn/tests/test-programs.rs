8d7373065308460318046b492d684246b50a7db4
Andrew Brown <andrew.brown@intel.com>
2024-06-17 14:52:33 -0700

    fix: revert to previous winml behavior
    This test was reusing the ONNX test for some reason.

--- crates/wasi-nn/tests/test-programs.rs
+++ crates/wasi-nn/tests/test-programs.rs
@@ -160,7 +160,7 @@ fn nn_image_classification_winml() -> Result<()> {
     check::winml::is_available()?;
     check::onnx::are_artifacts_available()?;
     let backend = Backend::from(backend::winml::WinMLBackend::default());
-    exec::run(NN_IMAGE_CLASSIFICATION_WINML, backend, true)
+    exec::run(NN_IMAGE_CLASSIFICATION_ONNX, backend, false)
 }
 
 #[cfg(not(all(feature = "winml", target_os = "windows")))]
