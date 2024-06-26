feebed51b456f448e9af0f5a88b63e6ac56e5e19
Rahul <rahul.s.chaphalkar@intel.com>
2024-06-26 15:17:50 -0700

    update openvino crate to 0.7.2

--- crates/wasi-nn/src/backend/openvino.rs
+++ crates/wasi-nn/src/backend/openvino.rs
@@ -37,11 +37,9 @@ impl BackendInner for OpenvinoBackend {
         //Construct new tensor with data.
         let dims: [i64; 2] = [1, weights.len() as i64];
         let shape = Shape::new(&dims)?;
-        let mut weights_tensor = OvTensor::new(ElementType::F32, &shape)?;
-        let buffer = weights_tensor.buffer_mut()?;
-        for (index, bytes) in weights.iter().enumerate() {
-            buffer[index] = *bytes;
-        }
+        let mut weights_tensor = OvTensor::new(ElementType::U8, &shape)?;
+        let buffer = weights_tensor.get_raw_data_mut()?;
+        buffer.copy_from_slice(&weights);
 
         // Construct OpenVINO graph structures: `model` contains the graph
         // structure, `compiled_model` can perform inference.
@@ -101,10 +99,8 @@ impl BackendExecutionContext for OpenvinoExecutionContext {
             .collect::<Vec<_>>();
         let shape = Shape::new(&dimensions)?;
         let mut new_tensor = OvTensor::new(precision, &shape)?;
-        let buffer = new_tensor.buffer_mut()?;
-        for (index, bytes) in tensor.data.iter().enumerate() {
-            buffer[index] = *bytes;
-        }
+        let buffer = new_tensor.get_raw_data_mut()?;
+        buffer.copy_from_slice(&tensor.data);
 
         // Assign the tensor to the request.
         self.0
@@ -118,7 +114,7 @@ impl BackendExecutionContext for OpenvinoExecutionContext {
     }
 
     fn get_output(&mut self, index: u32, destination: &mut [u8]) -> Result<u32, BackendError> {
-        let mut tensor = self.0.get_output_tensor_by_index(index as usize)?;
+        let tensor = self.0.get_output_tensor_by_index(index as usize)?;
         let tensor_size = tensor.get_byte_size()?;
         if tensor_size > destination.len() {
             return Err(BackendError::NotEnoughMemory(tensor_size));
