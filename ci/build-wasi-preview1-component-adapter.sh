7455066be8b857dee440b1fec0bc67d36e672613
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-06-26 11:41:27 +0000

    Add wasi adapter provider template which is materialised in CI

--- ci/build-wasi-preview1-component-adapter.sh
+++ ci/build-wasi-preview1-component-adapter.sh
@@ -15,24 +15,38 @@ release="target/wasm32-unknown-unknown/release/wasi_snapshot_preview1.wasm"
 $build_adapter
 $verify $debug
 
+build() {
+  input=$1
+  flavor=$2
+  $verify $input
+  name=wasi_snapshot_preview1.$flavor.wasm
+  dst=$(dirname $input)/$name
+  provider=crates/wasi-preview1-component-adapter/provider/artefacts/$name
+  wasm-tools metadata add --name "wasi_preview1_component_adapter.$flavor.adapter" $input \
+    -o $dst
+  cp $dst $provider
+}
+
 # Debug build, command
 $build_adapter --no-default-features --features command
 $verify $debug
 
 # Release build, command
 $build_adapter --release --no-default-features --features command
-$verify $release
-wasm-tools metadata add --name "wasi_preview1_component_adapter.command.adapter:${VERSION}" $release \
-  -o target/wasm32-unknown-unknown/release/wasi_snapshot_preview1.command.wasm
+build $release command
 
 # Release build, default features (reactor)
 $build_adapter --release
-$verify $release
-wasm-tools metadata add --name "wasi_preview1_component_adapter.reactor.adapter:${VERSION}" $release \
-  -o target/wasm32-unknown-unknown/release/wasi_snapshot_preview1.reactor.wasm
+build $release reactor
 
 # Release build, proxy
 $build_adapter --release --no-default-features --features proxy
-$verify $release
-wasm-tools metadata add --name "wasi_preview1_component_adapter.proxy.adapter:${VERSION}" $release \
-  -o target/wasm32-unknown-unknown/release/wasi_snapshot_preview1.proxy.wasm
+build $release proxy
+
+# Add the adapter provider to the workspace
+cp crates/wasi-preview1-component-adapter/provider/Cargo.toml.in crates/wasi-preview1-component-adapter/provider/Cargo.toml
+sed -i '/"crates\/wasi-preview1-component-adapter",/a\ \ "crates\/wasi-preview1-component-adapter\/provider",' Cargo.toml
+
+cargo fmt -p wasi-preview1-component-adapter-provider -- --check
+cargo check -p wasi-preview1-component-adapter-provider
+cargo clippy -p wasi-preview1-component-adapter-provider
