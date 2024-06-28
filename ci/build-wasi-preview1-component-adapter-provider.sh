98202466e7c5ad19df93ca178366d588f2501a70
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-06-28 13:03:10 +0000

    Refactor to use composite actions

--- ci/build-wasi-preview1-component-adapter-provider.sh
+++ ci/build-wasi-preview1-component-adapter-provider.sh
@@ -5,11 +5,10 @@ set -ex
 cp crates/wasi-preview1-component-adapter/provider/Cargo.toml.in crates/wasi-preview1-component-adapter/provider/Cargo.toml
 sed -i '/"crates\/wasi-preview1-component-adapter",/a\ \ "crates\/wasi-preview1-component-adapter\/provider",' Cargo.toml
 
-set +x
-if [ "$CHECK" = "1" ]; then
-  cargo fmt -p wasi-preview1-component-adapter-provider -- --check
-  cargo check -p wasi-preview1-component-adapter-provider
-  cargo clippy -p wasi-preview1-component-adapter-provider
-  cargo publish -p wasi-preview1-component-adapter-provider --dry-run --allow-dirty
-fi
-set -x
+# Check the adapter provider's code formatting and style
+cargo fmt -p wasi-preview1-component-adapter-provider -- --check
+cargo check -p wasi-preview1-component-adapter-provider
+cargo clippy -p wasi-preview1-component-adapter-provider
+
+# Check that publishing the adapter provider should work
+cargo publish -p wasi-preview1-component-adapter-provider --dry-run --allow-dirty
