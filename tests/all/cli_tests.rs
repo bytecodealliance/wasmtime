bytecodealliance/wasmtime

commit f471b4dcc85a0c5ca964e8efac39f6e7a0072e90

Refactor and document the wasmtime-wasi-http more (#8861)
* Improve some documentation of the `wasmtime-wasi` crate

Show a few examples of using `with` to point to upstream `wasmtime-wasi`
for bindings.

* Refactor and document the `wasmtime-wasi-http` more

This commit primarily adds a complete example of using
`wasmtime-wasi-http` to the documentation. Along the way I've done a
number of other refactorings too:

* `bindgen!`-generated `*Pre` structures now implement `Clone`.
* `bindgen!`-generated `*Pre` structures now have an `engine` method.
* `bindgen!`-generated `*Pre` structures now have an `instance_pre` method.
* The structure of `wasmtime-wasi-http` now matches `wasmtime-wasi`,
  notably:
  * The `proxy` module is removed
  * `wasmtime_wasi_http::add_to_linker_{a,}sync` is the top level
    add-to-linker function.
  * The `bindings` module now contains `Proxy` and `ProxyPre` along with
    a `sync` submodule.
  * The `bindings` module contains all bindings for `wasi:http` things.
* The `add_only_*` methods are un-hidden and documented.
* Code processing `req` has been simplified by avoiding
  decomposing-and-reconstructing a request.
* The `new_incoming_request` method is now generic to avoid callers
  having to do boxing/mapping themselves.

* Update expanded macro expectations

* Remove unused import

crates/wasi-http/src/lib.rs
@@ -1,39 +1,220 @@
-//! Wasmtime's WASI HTTP Implementation
+//! # Wasmtime's WASI HTTP Implementation
 //!
-//! This crate's implementation is primarily built on top of [`hyper`].
+//! This crate is Wasmtime's host implementation of the `wasi:http` package as
+//! part of WASIp2. This crate's implementation is primarily built on top of
+//! [`hyper`] and [`tokio`].
 //!
 //! # WASI HTTP Interfaces
 //!
 //! This crate contains implementations of the following interfaces:
 //!
-//! * `wasi:http/incoming-handler`
-//! * `wasi:http/outgoing-handler`
-//! * `wasi:http/types`
+//! * [`wasi:http/incoming-handler`]
+//! * [`wasi:http/outgoing-handler`]
+//! * [`wasi:http/types`]
 //!
 //! The crate also contains an implementation of the [`wasi:http/proxy`] world.
 //!
-//! [`wasi:http/proxy`]: crate::proxy
-
-//! All traits are implemented in terms of a [`WasiHttpView`] trait which provides
-//! basic access to [`WasiHttpCtx`], configuration for WASI HTTP, and a [`wasmtime_wasi::ResourceTable`],
-//! the state for all host-defined component model resources.
-
+//! [`wasi:http/proxy`]: crate::bindings::Proxy
+//! [`wasi:http/outgoing-handler`]: crate::bindings::http::outgoing_handler::Host
+//! [`wasi:http/types`]: crate::bindings::http::types::Host
+//! [`wasi:http/incoming-handler`]: crate::bindings::exports::wasi::http::incoming_handler::Guest
+//!
+//! This crate is very similar to [`wasmtime-wasi`] in the it uses the
+//! `bindgen!` macro in Wasmtime to generate bindings to interfaces. Bindings
+//! are located in the [`bindings`] module.
+//!
+//! # The `WasiHttpView` trait
+//!
+//! All `bindgen!`-generated `Host` traits are implemented in terms of a
+//! [`WasiHttpView`] trait which provides basic access to [`WasiHttpCtx`],
+//! configuration for WASI HTTP, and a [`wasmtime_wasi::ResourceTable`], the
+//! state for all host-defined component model resources.
+//!
+//! The [`WasiHttpView`] trait additionally offers a few other configuration
+//! methods such as [`WasiHttpView::send_request`] to customize how outgoing
+//! HTTP requests are handled.
+//!
+//! # Async and Sync
+//!
+//! There are both asynchronous and synchronous bindings in this crate. For
+//! example [`add_to_linker_async`] is for asynchronous embedders and
+//! [`add_to_linker_sync`] is for synchronous embedders. Note that under the
+//! hood both versions are implemented with `async` on top of [`tokio`].
+//!
 //! # Examples
 //!
 //! Usage of this crate is done through a few steps to get everything hooked up:
 //!
 //! 1. First implement [`WasiHttpView`] for your type which is the `T` in
 //!    [`wasmtime::Store<T>`].
-//! 2. Add WASI HTTP interfaces to a [`wasmtime::component::Linker<T>`]. This is either
-//!    done through functions like [`proxy::add_to_linker`] (which bundles all interfaces
-//!    in the `wasi:http/proxy` world together) or through individual interfaces like the
-//!    [`bindings::http::outgoing_handler::add_to_linker_get_host`] function.
-//! 3. Use the previous [`wasmtime::component::Linker<T>::instantiate`] to instantiate
-//!    a [`wasmtime::component::Component`] within a [`wasmtime::Store<T>`]. If you're
-//!    targeting the `wasi:http/proxy` world, you can instantiate the component with
-//!    [`proxy::Proxy::instantiate_async`] or [`proxy::sync::Proxy::instantiate`] functions.
+//! 2. Add WASI HTTP interfaces to a [`wasmtime::component::Linker<T>`]. There
+//!    are a few options of how to do this:
+//!    * Use [`add_to_linker_async`] to bundle all interfaces in
+//!      `wasi:http/proxy` together
+//!    * Use [`add_only_http_to_linker_async`] to add only HTTP interfaces but
+//!      no others. This is useful when working with
+//!      [`wasmtime_wasi::add_to_linker_async`] for example.
+//!    * Add individual interfaces such as with the
+//!      [`bindings::http::outgoing_handler::add_to_linker_get_host`] function.
+//! 3. Use [`ProxyPre`](bindings::ProxyPre) to pre-instantiate a component
+//!    before serving requests.
+//! 4. When serving requests use
+//!    [`ProxyPre::instantiate_async`](bindings::ProxyPre::instantiate_async)
+//!    to create instances and handle HTTP requests.
+//!
+//! A standalone example of doing all this looks like:
+//!
+//! ```no_run
+//! use anyhow::bail;
+//! use hyper::server::conn::http1;
+//! use std::sync::Arc;
+//! use tokio::net::TcpListener;
+//! use wasmtime::component::{Component, Linker, ResourceTable};
+//! use wasmtime::{Config, Engine, Result, Store};
+//! use wasmtime_wasi::{WasiCtx, WasiCtxBuilder, WasiView};
+//! use wasmtime_wasi_http::bindings::ProxyPre;
+//! use wasmtime_wasi_http::body::HyperOutgoingBody;
+//! use wasmtime_wasi_http::io::TokioIo;
+//! use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     let component = std::env::args().nth(1).unwrap();
+//!
+//!     // Prepare the `Engine` for Wasmtime
+//!     let mut config = Config::new();
+//!     config.async_support(true);
+//!     let engine = Engine::new(&config)?;
+//!
+//!     // Compile the component on the command line to machine code
+//!     let component = Component::from_file(&engine, &component)?;
+//!
+//!     // Prepare the `ProxyPre` which is a pre-instantiated version of the
+//!     // component that we have. This will make per-request instantiation
+//!     // much quicker.
+//!     let mut linker = Linker::new(&engine);
+//!     wasmtime_wasi_http::add_to_linker_async(&mut linker)?;
+//!     let pre = ProxyPre::new(linker.instantiate_pre(&component)?)?;
+//!
+//!     // Prepare our server state and start listening for connections.
+//!     let server = Arc::new(MyServer { pre });
+//!     let listener = TcpListener::bind("127.0.0.1:8000").await?;
+//!     println!("Listening on {}", listener.local_addr()?);
+//!
+//!     loop {
+//!         // Accept a TCP connection and serve all of its requests in a separate
+//!         // tokio task. Note that for now this only works with HTTP/1.1.
+//!         let (client, addr) = listener.accept().await?;
+//!         println!("serving new client from {addr}");
+//!
+//!         let server = server.clone();
+//!         tokio::task::spawn(async move {
+//!             if let Err(e) = http1::Builder::new()
+//!                 .keep_alive(true)
+//!                 .serve_connection(
+//!                     TokioIo::new(client),
+//!                     hyper::service::service_fn(move |req| {
+//!                         let server = server.clone();
+//!                         async move { server.handle_request(req).await }
+//!                     }),
+//!                 )
+//!                 .await
+//!             {
+//!                 eprintln!("error serving client[{addr}]: {e:?}");
+//!             }
+//!         });
+//!     }
+//! }
+//!
+//! struct MyServer {
+//!     pre: ProxyPre<MyClientState>,
+//! }
+//!
+//! impl MyServer {
+//!     async fn handle_request(
+//!         &self,
+//!         req: hyper::Request<hyper::body::Incoming>,
+//!     ) -> Result<hyper::Response<HyperOutgoingBody>> {
+//!         // Create per-http-request state within a `Store` and prepare the
+//!         // initial resources  passed to the `handle` function.
+//!         let mut store = Store::new(
+//!             self.pre.engine(),
+//!             MyClientState {
+//!                 table: ResourceTable::new(),
+//!                 wasi: WasiCtxBuilder::new().inherit_stdio().build(),
+//!                 http: WasiHttpCtx::new(),
+//!             },
+//!         );
+//!         let (sender, receiver) = tokio::sync::oneshot::channel();
+//!         let req = store.data_mut().new_incoming_request(req)?;
+//!         let out = store.data_mut().new_response_outparam(sender)?;
+//!         let pre = self.pre.clone();
+//!
+//!         // Run the http request itself in a separate task so the task can
+//!         // optionally continue to execute beyond after the initial
+//!         // headers/response code are sent.
+//!         let task = tokio::task::spawn(async move {
+//!             let proxy = pre.instantiate_async(&mut store).await?;
+//!
+//!             if let Err(e) = proxy
+//!                 .wasi_http_incoming_handler()
+//!                 .call_handle(store, req, out)
+//!                 .await
+//!             {
+//!                 return Err(e);
+//!             }
+//!
+//!             Ok(())
+//!         });
+//!
+//!         match receiver.await {
+//!             // If the client calls `response-outparam::set` then one of these
+//!             // methods will be called.
+//!             Ok(Ok(resp)) => Ok(resp),
+//!             Ok(Err(e)) => Err(e.into()),
+//!
+//!             // Otherwise the `sender` will get dropped along with the `Store`
+//!             // meaning that the oneshot will get disconnected and here we can
+//!             // inspect the `task` result to see what happened
+//!             Err(_) => {
+//!                 let e = match task.await {
+//!                     Ok(r) => r.unwrap_err(),
+//!                     Err(e) => e.into(),
+//!                 };
+//!                 bail!("guest never invoked `response-outparam::set` method: {e:?}")
+//!             }
+//!         }
+//!     }
+//! }
+//!
+//! struct MyClientState {
+//!     wasi: WasiCtx,
+//!     http: WasiHttpCtx,
+//!     table: ResourceTable,
+//! }
+//!
+//! impl WasiView for MyClientState {
+//!     fn ctx(&mut self) -> &mut WasiCtx {
+//!         &mut self.wasi
+//!     }
+//!     fn table(&mut self) -> &mut ResourceTable {
+//!         &mut self.table
+//!     }
+//! }
+//!
+//! impl WasiHttpView for MyClientState {
+//!     fn ctx(&mut self) -> &mut WasiHttpCtx {
+//!         &mut self.http
+//!     }
+//!     fn table(&mut self) -> &mut ResourceTable {
+//!         &mut self.table
+//!     }
+//! }
+//! ```
 
 #![deny(missing_docs)]
+#![doc(test(attr(deny(warnings))))]
+#![doc(test(attr(allow(dead_code, unused_variables, unused_mut))))]
 
 mod error;
 mod http_impl;
@@ -41,50 +222,185 @@ mod types_impl;
 
 pub mod body;
 pub mod io;
-pub mod proxy;
 pub mod types;
 
-/// Raw bindings to the `wasi:http` package.
-pub mod bindings {
-    #![allow(missing_docs)]
-    wasmtime::component::bindgen!({
-        path: "wit",
-        interfaces: "
-            import wasi:http/incoming-handler@0.2.0;
-            import wasi:http/outgoing-handler@0.2.0;
-            import wasi:http/types@0.2.0;
-        ",
-        tracing: true,
-        async: false,
-        trappable_imports: true,
-        with: {
-            // Upstream package dependencies
-            "wasi:io": wasmtime_wasi::bindings::io,
-
-            // Configure all WIT http resources to be defined types in this
-            // crate to use the `ResourceTable` helper methods.
-            "wasi:http/types/outgoing-body": super::body::HostOutgoingBody,
-            "wasi:http/types/future-incoming-response": super::types::HostFutureIncomingResponse,
-            "wasi:http/types/outgoing-response": super::types::HostOutgoingResponse,
-            "wasi:http/types/future-trailers": super::body::HostFutureTrailers,
-            "wasi:http/types/incoming-body": super::body::HostIncomingBody,
-            "wasi:http/types/incoming-response": super::types::HostIncomingResponse,
-            "wasi:http/types/response-outparam": super::types::HostResponseOutparam,
-            "wasi:http/types/outgoing-request": super::types::HostOutgoingRequest,
-            "wasi:http/types/incoming-request": super::types::HostIncomingRequest,
-            "wasi:http/types/fields": super::types::HostFields,
-            "wasi:http/types/request-options": super::types::HostRequestOptions,
-        },
-        trappable_error_type: {
-            "wasi:http/types/error-code" => crate::HttpError,
-        },
-    });
-
-    pub use wasi::http;
-}
+pub mod bindings;
 
 pub use crate::error::{
     http_request_error, hyper_request_error, hyper_response_error, HttpError, HttpResult,
 };
 #[doc(inline)]
 pub use crate::types::{WasiHttpCtx, WasiHttpImpl, WasiHttpView};
+
+/// Add all of the `wasi:http/proxy` world's interfaces to a [`wasmtime::component::Linker`].
+///
+/// This function will add the `async` variant of all interfaces into the
+/// `Linker` provided. By `async` this means that this function is only
+/// compatible with [`Config::async_support(true)`][async]. For embeddings with
+/// async support disabled see [`add_to_linker_sync`] instead.
+///
+/// [async]: wasmtime::Config::async_support
+///
+/// # Example
+///
+/// ```
+/// use wasmtime::{Engine, Result, Config};
+/// use wasmtime::component::{ResourceTable, Linker};
+/// use wasmtime_wasi::{WasiCtx, WasiView};
+/// use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
+///
+/// fn main() -> Result<()> {
+///     let mut config = Config::new();
+///     config.async_support(true);
+///     let engine = Engine::new(&config)?;
+///
+///     let mut linker = Linker::<MyState>::new(&engine);
+///     wasmtime_wasi_http::add_to_linker_async(&mut linker)?;
+///     // ... add any further functionality to `linker` if desired ...
+///
+///     Ok(())
+/// }
+///
+/// struct MyState {
+///     ctx: WasiCtx,
+///     http_ctx: WasiHttpCtx,
+///     table: ResourceTable,
+/// }
+///
+/// impl WasiHttpView for MyState {
+///     fn ctx(&mut self) -> &mut WasiHttpCtx { &mut self.http_ctx }
+///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+/// }
+/// impl WasiView for MyState {
+///     fn ctx(&mut self) -> &mut WasiCtx { &mut self.ctx }
+///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+/// }
+/// ```
+pub fn add_to_linker_async<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
+where
+    T: WasiHttpView + wasmtime_wasi::WasiView,
+{
+    let closure = type_annotate_wasi::<T, _>(|t| wasmtime_wasi::WasiImpl(t));
+    wasmtime_wasi::bindings::clocks::wall_clock::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::clocks::monotonic_clock::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::io::poll::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::io::error::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::io::streams::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::cli::stdin::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::cli::stdout::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::cli::stderr::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::random::random::add_to_linker_get_host(l, closure)?;
+
+    add_only_http_to_linker_async(l)
+}
+
+// NB: workaround some rustc inference - a future refactoring may make this
+// obsolete.
+fn type_annotate_http<T, F>(val: F) -> F
+where
+    F: Fn(&mut T) -> WasiHttpImpl<&mut T>,
+{
+    val
+}
+fn type_annotate_wasi<T, F>(val: F) -> F
+where
+    F: Fn(&mut T) -> wasmtime_wasi::WasiImpl<&mut T>,
+{
+    val
+}
+
+/// A slimmed down version of [`add_to_linker_async`] which only adds
+/// `wasi:http` interfaces to the linker.
+///
+/// This is useful when using [`wasmtime_wasi::add_to_linker_async`] for
+/// example to avoid re-adding the same interfaces twice.
+pub fn add_only_http_to_linker_async<T>(
+    l: &mut wasmtime::component::Linker<T>,
+) -> anyhow::Result<()>
+where
+    T: WasiHttpView,
+{
+    let closure = type_annotate_http::<T, _>(|t| WasiHttpImpl(t));
+    crate::bindings::http::outgoing_handler::add_to_linker_get_host(l, closure)?;
+    crate::bindings::http::types::add_to_linker_get_host(l, closure)?;
+
+    Ok(())
+}
+
+/// Add all of the `wasi:http/proxy` world's interfaces to a [`wasmtime::component::Linker`].
+///
+/// This function will add the `sync` variant of all interfaces into the
+/// `Linker` provided. For embeddings with async support see
+/// [`add_to_linker_async`] instead.
+///
+/// # Example
+///
+/// ```
+/// use wasmtime::{Engine, Result, Config};
+/// use wasmtime::component::{ResourceTable, Linker};
+/// use wasmtime_wasi::{WasiCtx, WasiView};
+/// use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
+///
+/// fn main() -> Result<()> {
+///     let config = Config::default();
+///     let engine = Engine::new(&config)?;
+///
+///     let mut linker = Linker::<MyState>::new(&engine);
+///     wasmtime_wasi_http::add_to_linker_sync(&mut linker)?;
+///     // ... add any further functionality to `linker` if desired ...
+///
+///     Ok(())
+/// }
+///
+/// struct MyState {
+///     ctx: WasiCtx,
+///     http_ctx: WasiHttpCtx,
+///     table: ResourceTable,
+/// }
+///
+/// impl WasiHttpView for MyState {
+///     fn ctx(&mut self) -> &mut WasiHttpCtx { &mut self.http_ctx }
+///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+/// }
+/// impl WasiView for MyState {
+///     fn ctx(&mut self) -> &mut WasiCtx { &mut self.ctx }
+///     fn table(&mut self) -> &mut ResourceTable { &mut self.table }
+/// }
+/// ```
+pub fn add_to_linker_sync<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
+where
+    T: WasiHttpView + wasmtime_wasi::WasiView,
+{
+    let closure = type_annotate_wasi::<T, _>(|t| wasmtime_wasi::WasiImpl(t));
+
+    wasmtime_wasi::bindings::clocks::wall_clock::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::clocks::monotonic_clock::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::sync::io::poll::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::sync::io::streams::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::io::error::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::cli::stdin::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::cli::stdout::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::cli::stderr::add_to_linker_get_host(l, closure)?;
+    wasmtime_wasi::bindings::random::random::add_to_linker_get_host(l, closure)?;
+
+    add_only_http_to_linker_sync(l)?;
+
+    Ok(())
+}
+
+/// A slimmed down version of [`add_to_linker_sync`] which only adds
+/// `wasi:http` interfaces to the linker.
+///
+/// This is useful when using [`wasmtime_wasi::add_to_linker_sync`] for
+/// example to avoid re-adding the same interfaces twice.
+pub fn add_only_http_to_linker_sync<T>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()>
+where
+    T: WasiHttpView,
+{
+    let closure = type_annotate_http::<T, _>(|t| WasiHttpImpl(t));
+
+    crate::bindings::http::outgoing_handler::add_to_linker_get_host(l, closure)?;
+    crate::bindings::http::types::add_to_linker_get_host(l, closure)?;
+
+    Ok(())
+}

commit baf4abaed5a01883e96e2239097e145ef90c4487

Fix handling of authority/scheme in `wasmtime serve`
This commit is aimed at fixing an accidental regression from #8861 where
the `wasmtime serve` subcommand stopped reporting some instances of
authority and scheme. After discussion in #8878 the new logic
implemented is:

* Creation of an incoming request now explicitly requires specifying a
  scheme which is out-of-band information about how the surrounding
  server received the request.

* The authority is stored separately within a request and is inferred
  from the URI's authority or the `Host` header if present. If neither
  are present then an error is returned.

Closes #8878

crates/test-programs/src/bin/cli_serve_authority_and_scheme.rs
@@ -1,0 +1,26 @@
+use test_programs::proxy;
+use test_programs::wasi::http::types::{
+    Fields, IncomingRequest, OutgoingResponse, ResponseOutparam, Scheme,
+};
+
+struct T;
+
+proxy::export!(T);
+
+impl proxy::exports::wasi::http::incoming_handler::Guest for T {
+    fn handle(request: IncomingRequest, outparam: ResponseOutparam) {
+        let authority = request.authority();
+        let scheme = request.scheme();
+
+        assert_eq!(authority.as_deref(), Some("localhost"));
+        assert!(
+            matches!(scheme, Some(Scheme::Http)),
+            "bad scheme: {scheme:?}",
+        );
+
+        let resp = OutgoingResponse::new(Fields::new());
+        ResponseOutparam::set(outparam, Ok(resp));
+    }
+}
+
+fn main() {}

crates/wasi-http/src/lib.rs
@@ -73,6 +73,7 @@
 //! use wasmtime::{Config, Engine, Result, Store};
 //! use wasmtime_wasi::{WasiCtx, WasiCtxBuilder, WasiView};
 //! use wasmtime_wasi_http::bindings::ProxyPre;
+//! use wasmtime_wasi_http::bindings::http::types::Scheme;
 //! use wasmtime_wasi_http::body::HyperOutgoingBody;
 //! use wasmtime_wasi_http::io::TokioIo;
 //! use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
@@ -146,7 +147,7 @@
 //!             },
 //!         );
 //!         let (sender, receiver) = tokio::sync::oneshot::channel();
-//!         let req = store.data_mut().new_incoming_request(req)?;
+//!         let req = store.data_mut().new_incoming_request(Scheme::Http, req)?;
 //!         let out = store.data_mut().new_response_outparam(sender)?;
 //!         let pre = self.pre.clone();
 //!

crates/wasi-http/src/types.rs
@@ -8,6 +8,7 @@ use crate::{
     error::dns_error,
     hyper_request_error,
 };
+use anyhow::bail;
 use bytes::Bytes;
 use http_body_util::BodyExt;
 use hyper::body::Body;
@@ -81,6 +82,7 @@ pub trait WasiHttpView: Send {
     /// Create a new incoming request resource.
     fn new_incoming_request<B>(
         &mut self,
+        scheme: Scheme,
         req: hyper::Request<B>,
     ) -> wasmtime::Result<Resource<HostIncomingRequest>>
     where
@@ -94,7 +96,7 @@ pub trait WasiHttpView: Send {
             // TODO: this needs to be plumbed through
             std::time::Duration::from_millis(600 * 1000),
         );
-        let incoming_req = HostIncomingRequest::new(self, parts, Some(body));
+        let incoming_req = HostIncomingRequest::new(self, parts, scheme, Some(body))?;
         Ok(self.table().push(incoming_req)?)
     }
 
@@ -486,6 +488,8 @@ impl TryInto<http::Method> for types::Method {
 /// The concrete type behind a `wasi:http/types/incoming-request` resource.
 pub struct HostIncomingRequest {
     pub(crate) parts: http::request::Parts,
+    pub(crate) scheme: Scheme,
+    pub(crate) authority: String,
     /// The body of the incoming request.
     pub body: Option<HostIncomingBody>,
 }
@@ -495,10 +499,24 @@ impl HostIncomingRequest {
     pub fn new(
         view: &mut dyn WasiHttpView,
         mut parts: http::request::Parts,
+        scheme: Scheme,
         body: Option<HostIncomingBody>,
-    ) -> Self {
+    ) -> anyhow::Result<Self> {
+        let authority = match parts.uri.authority() {
+            Some(authority) => authority.to_string(),
+            None => match parts.headers.get(http::header::HOST) {
+                Some(host) => host.to_str()?.to_string(),
+                None => bail!("invalid HTTP request missing authority in URI and host header"),
+            },
+        };
+
         remove_forbidden_headers(view, &mut parts.headers);
-        Self { parts, body }
+        Ok(Self {
+            parts,
+            authority,
+            scheme,
+            body,
+        })
     }
 }
 

crates/wasi-http/src/types_impl.rs
@@ -312,25 +312,11 @@ where
     }
     fn scheme(&mut self, id: Resource<HostIncomingRequest>) -> wasmtime::Result<Option<Scheme>> {
         let req = self.table().get(&id)?;
-        Ok(req.parts.uri.scheme().map(|scheme| {
-            if scheme == &http::uri::Scheme::HTTP {
-                return Scheme::Http;
-            }
-
-            if scheme == &http::uri::Scheme::HTTPS {
-                return Scheme::Https;
-            }
-
-            Scheme::Other(req.parts.uri.scheme_str().unwrap().to_owned())
-        }))
+        Ok(Some(req.scheme.clone()))
     }
     fn authority(&mut self, id: Resource<HostIncomingRequest>) -> wasmtime::Result<Option<String>> {
         let req = self.table().get(&id)?;
-        Ok(req
-            .parts
-            .uri
-            .authority()
-            .map(|auth| auth.as_str().to_owned()))
+        Ok(Some(req.authority.clone()))
     }
 
     fn headers(

src/commands/serve.rs
@@ -12,6 +12,7 @@ use std::{
 use wasmtime::component::Linker;
 use wasmtime::{Config, Engine, Memory, MemoryType, Store, StoreLimits};
 use wasmtime_wasi::{StreamError, StreamResult, WasiCtx, WasiCtxBuilder, WasiView};
+use wasmtime_wasi_http::bindings::http::types::Scheme;
 use wasmtime_wasi_http::bindings::ProxyPre;
 use wasmtime_wasi_http::io::TokioIo;
 use wasmtime_wasi_http::{body::HyperOutgoingBody, WasiHttpCtx, WasiHttpView};
@@ -400,22 +401,21 @@ async fn handle_request(
 ) -> Result<hyper::Response<HyperOutgoingBody>> {
     let (sender, receiver) = tokio::sync::oneshot::channel();
 
+    let req_id = inner.next_req_id();
+
+    log::info!(
+        "Request {req_id} handling {} to {}",
+        req.method(),
+        req.uri()
+    );
+
+    let mut store = inner.cmd.new_store(&inner.engine, req_id)?;
+
+    let req = store.data_mut().new_incoming_request(Scheme::Http, req)?;
+    let out = store.data_mut().new_response_outparam(sender)?;
+    let proxy = inner.instance_pre.instantiate_async(&mut store).await?;
+
     let task = tokio::task::spawn(async move {
-        let req_id = inner.next_req_id();
-
-        log::info!(
-            "Request {req_id} handling {} to {}",
-            req.method(),
-            req.uri()
-        );
-
-        let mut store = inner.cmd.new_store(&inner.engine, req_id)?;
-
-        let req = store.data_mut().new_incoming_request(req)?;
-        let out = store.data_mut().new_response_outparam(sender)?;
-
-        let proxy = inner.instance_pre.instantiate_async(&mut store).await?;
-
         if let Err(e) = proxy
             .wasi_http_incoming_handler()
             .call_handle(store, req, out)

crates/wasi-http/tests/all/main.rs
@@ -13,7 +13,7 @@ use wasmtime::{
 };
 use wasmtime_wasi::{self, pipe::MemoryOutputPipe, WasiCtx, WasiCtxBuilder, WasiView};
 use wasmtime_wasi_http::{
-    bindings::http::types::ErrorCode,
+    bindings::http::types::{ErrorCode, Scheme},
     body::HyperOutgoingBody,
     io::TokioIo,
     types::{self, HostFutureIncomingResponse, IncomingResponse, OutgoingRequestConfig},
@@ -166,7 +166,7 @@ async fn run_wasi_http(
         wasmtime_wasi_http::bindings::Proxy::instantiate_async(&mut store, &component, &linker)
             .await?;
 
-    let req = store.data_mut().new_incoming_request(req)?;
+    let req = store.data_mut().new_incoming_request(Scheme::Http, req)?;
 
     let (sender, receiver) = tokio::sync::oneshot::channel();
     let out = store.data_mut().new_response_outparam(sender)?;

tests/all/cli_tests.rs
@@ -1814,6 +1814,37 @@ stderr [1] :: after empty
 
         Ok(())
     }
+
+    #[tokio::test]
+    async fn cli_serve_authority_and_scheme() -> Result<()> {
+        let server = WasmtimeServe::new(CLI_SERVE_AUTHORITY_AND_SCHEME_COMPONENT, |cmd| {
+            cmd.arg("-Scli");
+        })?;
+
+        let resp = server
+            .send_request(
+                hyper::Request::builder()
+                    .uri("/")
+                    .header("Host", "localhost")
+                    .body(String::new())
+                    .context("failed to make request")?,
+            )
+            .await?;
+        assert!(resp.status().is_success());
+
+        let resp = server
+            .send_request(
+                hyper::Request::builder()
+                    .method("CONNECT")
+                    .uri("http://localhost/")
+                    .body(String::new())
+                    .context("failed to make request")?,
+            )
+            .await?;
+        assert!(resp.status().is_success());
+
+        Ok(())
+    }
 }
 
 #[test]