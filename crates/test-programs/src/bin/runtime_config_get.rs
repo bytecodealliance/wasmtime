0a296b3e03158a357266c33b5c3561c5ae02356a
Xinzhao Xu <z2d@jifangcheng.com>
2024-07-17 23:01:28 +0800

    Implement wasi-runtime-config (#8950)
    * Implement wasi-runtime-config
    
    * Avoid clone the WasiRuntimeConfig every time

---
+++ crates/test-programs/src/bin/runtime_config_get.rs
@@ -0,0 +1,8 @@
+use test_programs::config::wasi::config::runtime;
+
+fn main() {
+    let v = runtime::get("hello").unwrap().unwrap();
+    assert_eq!(v, "world");
+    let config = runtime::get_all().unwrap();
+    assert_eq!(config, [("hello".to_string(), "world".to_string())])
+}
