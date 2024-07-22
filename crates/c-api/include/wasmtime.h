fb531d26e6c1c0302cc9006c76d2ea3dc71cf291
wasmtime-publish <59749941+wasmtime-publish@users.noreply.github.com>
2024-07-22 11:08:47 -0500

    Release Wasmtime 23.0.1 (#8988)
    * Release Wasmtime 23.0.1
    
    [automatically-tag-and-release-this-commit]
    
    * Fix publication issues of Wasmtime 23.0.0 (#8986)
    
    * Remove the `readme` directive of the `cranelift-bitset` crate as the
      file doesn't exist.
    
    * Remove invalid categories of the `wasmtime-wasi-threads` crate that
      aren't present on crates.io.
    
    * Update release notes for 23.0.1
    
    ---------
    
    Co-authored-by: Wasmtime Publish <wasmtime-publish@users.noreply.github.com>
    Co-authored-by: Alex Crichton <alex@alexcrichton.com>

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -206,7 +206,7 @@
 /**
  * \brief Wasmtime version string.
  */
-#define WASMTIME_VERSION "23.0.0"
+#define WASMTIME_VERSION "23.0.1"
 /**
  * \brief Wasmtime major version number.
  */
@@ -218,7 +218,7 @@
 /**
  * \brief Wasmtime patch version number.
  */
-#define WASMTIME_VERSION_PATCH 0
+#define WASMTIME_VERSION_PATCH 1
 
 #ifdef __cplusplus
 extern "C" {
