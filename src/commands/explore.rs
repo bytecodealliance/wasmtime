3767f76b487959194b10ec9e17ddb8d1c3a37116
Vulcain <100216770+vulc41n@users.noreply.github.com>
2024-07-27 01:53:38 +0200

    explorer with clif fix: cache does not emit clif files (#9027)

--- src/commands/explore.rs
+++ src/commands/explore.rs
@@ -55,6 +55,7 @@ impl ExploreCommand {
         let clif_dir = if let Some(Strategy::Cranelift) | None = self.common.codegen.compiler {
             let clif_dir = tempdir()?;
             config.emit_clif(clif_dir.path());
+            config.disable_cache(); // cache does not emit clif
             Some(clif_dir)
         } else {
             None
