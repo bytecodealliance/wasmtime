94f2e3445db3936536d2e8bc6ff282a84b556d74
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-07-29 10:28:15 +0600

    Use cmake to build wasmtime-c-api

--- crates/c-api/build.rs
+++ crates/c-api/build.rs
@@ -1,4 +1,10 @@
+use cmake;
+use std::{env, path::PathBuf};
+
 fn main() {
-    let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:include={dir}/include");
+    let dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let dst = cmake::Config::new(&dir).build_target("conf_h").build();
+
+    println!("cargo:conf-include={}/build/include", dst.display());
+    println!("cargo:include={}/include", dir.display());
 }
