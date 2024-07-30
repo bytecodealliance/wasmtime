c0c8d1938b8a1962fe714bf9cf91a7bf8d8d8306
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-07-30 07:01:26 +0600

    Properly expose features when building via cmake

--- crates/c-api/build.rs
+++ crates/c-api/build.rs
@@ -1,9 +1,40 @@
 use cmake;
 use std::{env, path::PathBuf};
 
+// WASMTIME_FEATURE_LIST
+const FEATURES: &[&str] = &[
+    "ASYNC",
+    "PROFILING",
+    "CACHE",
+    "PARALLEL_COMPILATION",
+    "WASI",
+    "LOGGING",
+    "DISABLE_LOGGING",
+    "COREDUMP",
+    "ADDR2LINE",
+    "DEMANGLE",
+    "THREADS",
+    "GC",
+    "CRANELIFT",
+    "WINCH",
+];
+// ... if you add a line above this be sure to change the other locations
+// marked WASMTIME_FEATURE_LIST
+
 fn main() {
+    println!("cargo:rerun-if-changed=CMakesLists.txt");
+    println!("cargo:rerun-if-changed=include/wasmtime/conf.h.in");
     let dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let dst = cmake::Config::new(&dir).build_target("conf_h").build();
+    let mut config = cmake::Config::new(&dir);
+    config
+        .define("WASMTIME_DISABLE_ALL_FEATURES", "ON")
+        .build_target("conf_h");
+    for f in FEATURES {
+        if env::var_os(format!("CARGO_FEATURE_{}", f)).is_some() {
+            config.define(format!("WASMTIME_FEATURE_{}", f), "ON");
+        }
+    }
+    let dst = config.build();
 
     println!("cargo:conf-include={}/build/include", dst.display());
     println!("cargo:include={}/include", dir.display());
