556a6a6d4972726fea347687d211a8aa4397b30f
Dave Bakker <github@davebakker.io>
2024-08-01 16:05:53 +0200

    Move generic socket setup & teardown code out of the test case.

--- crates/test-programs/src/bin/preview2_tcp_streams.rs
+++ crates/test-programs/src/bin/preview2_tcp_streams.rs
@@ -1,46 +1,64 @@
-use test_programs::wasi::io::streams::StreamError;
+use test_programs::wasi::io::streams::{InputStream, OutputStream, StreamError};
 use test_programs::wasi::sockets::network::{IpAddress, IpAddressFamily, IpSocketAddress, Network};
 use test_programs::wasi::sockets::tcp::{ShutdownType, TcpSocket};
 
 /// InputStream::read should return `StreamError::Closed` after the connection has been shut down by the server.
-fn test_tcp_read_from_closed_input_stream(net: &Network, family: IpAddressFamily) {
-    // Set up server & client sockets:
-    let bind_address = IpSocketAddress::new(IpAddress::new_loopback(family), 0);
-    let listener = TcpSocket::new(family).unwrap();
-    listener.blocking_bind(&net, bind_address).unwrap();
-    listener.blocking_listen().unwrap();
-    let bound_address = listener.local_address().unwrap();
-    let client = TcpSocket::new(family).unwrap();
-    let (connected_input, connected_output) = client.blocking_connect(net, bound_address).unwrap();
-    let (accepted, accepted_input, accepted_output) = listener.blocking_accept().unwrap();
+fn test_tcp_input_stream_should_be_closed_by_remote_shutdown(
+    net: &Network,
+    family: IpAddressFamily,
+) {
+    setup(net, family, |server, client| {
+        // Shut down the connection from the server side:
+        server.socket.shutdown(ShutdownType::Both).unwrap();
+        drop(server);
 
-    // Shut down the connection from the server side:
-    accepted.shutdown(ShutdownType::Both).unwrap();
-    drop(accepted_input);
-    drop(accepted_output);
-    drop(accepted);
+        // Wait for the shutdown signal to reach the client:
+        client.input.subscribe().block();
 
-    // Wait for the shutdown signal to reach the client:
-    connected_input.subscribe().block();
+        // The input stream should immediately signal StreamError::Closed.
+        // Notably, it should _not_ return an empty list (the wasi-io equivalent of EWOULDBLOCK)
+        // See: https://github.com/bytecodealliance/wasmtime/pull/8968
+        assert!(matches!(client.input.read(10), Err(StreamError::Closed)));
 
-    // And now the actual test:
-
-    // The input stream should immediately signal StreamError::Closed.
-    // Notably, it should _not_ return an empty list (the wasi-io equivalent of EWOULDBLOCK)
-    // See: https://github.com/bytecodealliance/wasmtime/pull/8968
-    assert!(matches!(connected_input.read(10), Err(StreamError::Closed)));
-
-    // Stream should still be closed, even when requesting 0 bytes:
-    assert!(matches!(connected_input.read(0), Err(StreamError::Closed)));
-
-    drop(connected_input);
-    drop(connected_output);
-    drop(client);
+        // Stream should still be closed, even when requesting 0 bytes:
+        assert!(matches!(client.input.read(0), Err(StreamError::Closed)));
+    });
 }
 
 fn main() {
     let net = Network::default();
 
-    test_tcp_read_from_closed_input_stream(&net, IpAddressFamily::Ipv4);
-    test_tcp_read_from_closed_input_stream(&net, IpAddressFamily::Ipv6);
+    test_tcp_input_stream_should_be_closed_by_remote_shutdown(&net, IpAddressFamily::Ipv4);
+    test_tcp_input_stream_should_be_closed_by_remote_shutdown(&net, IpAddressFamily::Ipv6);
+}
+
+struct Connection {
+    input: InputStream,
+    output: OutputStream,
+    socket: TcpSocket,
+}
+
+/// Set up a connected pair of sockets
+fn setup(net: &Network, family: IpAddressFamily, body: impl FnOnce(Connection, Connection)) {
+    let bind_address = IpSocketAddress::new(IpAddress::new_loopback(family), 0);
+    let listener = TcpSocket::new(family).unwrap();
+    listener.blocking_bind(&net, bind_address).unwrap();
+    listener.blocking_listen().unwrap();
+    let bound_address = listener.local_address().unwrap();
+    let client_socket = TcpSocket::new(family).unwrap();
+    let (client_input, client_output) = client_socket.blocking_connect(net, bound_address).unwrap();
+    let (accepted_socket, accepted_input, accepted_output) = listener.blocking_accept().unwrap();
+
+    body(
+        Connection {
+            input: accepted_input,
+            output: accepted_output,
+            socket: accepted_socket,
+        },
+        Connection {
+            input: client_input,
+            output: client_output,
+            socket: client_socket,
+        },
+    );
 }
