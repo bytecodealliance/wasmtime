3461726c21ddfd48bf64dbd5776d24352b29a508
Alex Crichton <alex@alexcrichton.com>
2024-08-05 05:00:51 -0500

    Update to WASI WITs 0.2.1 (#9063)
    No major changes happen in this release so it's just bumping version
    numbers. The goal here is to turn the crank and ensure that nothing else
    breaks as part of this release process. There's a surprising number of
    versions to update in-repo which we may want to make easier in the
    future but for now this just manually updates.
    
    It is planned that this does not actually break anyone in practice.
    Older runtimes should support newer versions and Wasmtime after this PR
    should continue to support older binaries as well.
    
    Put another way this should not break things and if it does this will
    get reverted and fixed. We've done our best to make sure this won't
    break things, so let's find out in the real world now.

--- crates/wasi-preview1-component-adapter/src/lib.rs
+++ crates/wasi-preview1-component-adapter/src/lib.rs
@@ -89,12 +89,12 @@ pub mod bindings {
             package wasmtime:adapter;
 
             world adapter {
-                import wasi:clocks/wall-clock@0.2.0;
-                import wasi:clocks/monotonic-clock@0.2.0;
-                import wasi:random/random@0.2.0;
-                import wasi:cli/stdout@0.2.0;
-                import wasi:cli/stderr@0.2.0;
-                import wasi:cli/stdin@0.2.0;
+                import wasi:clocks/wall-clock@0.2.1;
+                import wasi:clocks/monotonic-clock@0.2.1;
+                import wasi:random/random@0.2.1;
+                import wasi:cli/stdout@0.2.1;
+                import wasi:cli/stderr@0.2.1;
+                import wasi:cli/stdin@0.2.1;
             }
         "#,
         std_feature,
@@ -111,7 +111,7 @@ pub mod bindings {
     }
 }
 
-#[export_name = "wasi:cli/run@0.2.0#run"]
+#[export_name = "wasi:cli/run@0.2.1#run"]
 #[cfg(feature = "command")]
 pub unsafe extern "C" fn run() -> u32 {
     #[link(wasm_import_module = "__main_module__")]
@@ -453,7 +453,7 @@ impl BumpAlloc {
 }
 
 #[cfg(not(feature = "proxy"))]
-#[link(wasm_import_module = "wasi:cli/environment@0.2.0")]
+#[link(wasm_import_module = "wasi:cli/environment@0.2.1")]
 extern "C" {
     #[link_name = "get-arguments"]
     fn wasi_cli_get_arguments(rval: *mut WasmStrList);
@@ -2152,7 +2152,7 @@ pub unsafe extern "C" fn poll_oneoff(
             });
         }
 
-        #[link(wasm_import_module = "wasi:io/poll@0.2.0")]
+        #[link(wasm_import_module = "wasi:io/poll@0.2.1")]
         #[allow(improper_ctypes)] // FIXME(bytecodealliance/wit-bindgen#684)
         extern "C" {
             #[link_name = "poll"]
