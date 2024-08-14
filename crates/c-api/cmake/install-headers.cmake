61191a2734b88fb63259a13841fad00d6ac33795
Alex Crichton <alex@alexcrichton.com>
2024-08-14 13:30:07 -0500

    Add NEWLINE_STYLE to cmake install (#9131)
    Looks like CMake before 3.20.0 doesn't generate newlines at all without
    this configuration option. CMake 3.20.0 and prior, however, generates
    newlines by default which is why this didn't show up in CI or
    development.
    
    Closes #9126

--- crates/c-api/cmake/install-headers.cmake
+++ crates/c-api/cmake/install-headers.cmake
@@ -11,7 +11,8 @@ set(include_src "${CMAKE_CURRENT_LIST_DIR}/../include")
 
 message(STATUS "Installing: ${dst}/wasmtime/conf.h")
 file(READ "${include_src}/wasmtime/conf.h.in" conf_h)
-file(CONFIGURE OUTPUT "${dst}/wasmtime/conf.h" CONTENT "${conf_h}")
+file(CONFIGURE OUTPUT "${dst}/wasmtime/conf.h" CONTENT "${conf_h}"
+     NEWLINE_STYLE CRLF)
 file(INSTALL "${include_src}/"
      DESTINATION "${dst}"
      FILES_MATCHING REGEX "\\.hh?$")
