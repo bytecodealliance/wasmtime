c41934ed8bea9a4d1f33f90e5b3a95bbaaa413c9
Nick Fitzgerald <fitzgen@gmail.com>
2024-09-12 10:03:55 -0700

    Pulley: Allow configuring offsets and hexdumps in the disassembler (#9233)

--- pulley/tests/all/disas.rs
+++ pulley/tests/all/disas.rs
@@ -11,17 +11,23 @@ fn encoded(ops: &[Op]) -> Vec<u8> {
     encoded
 }
 
-fn assert_disas(ops: &[Op], expected: &str) {
+#[track_caller]
+fn assert_disas_with_disassembler(dis: &mut disas::Disassembler<'_>, expected: &str) {
     let expected = expected.trim();
     eprintln!("=== expected ===\n{expected}");
 
-    let bytecode = encoded(ops);
+    decode::Decoder::decode_all(dis).expect("decoding should succeed");
 
-    let actual = disas::Disassembler::disassemble_all(&bytecode).expect("decoding failed");
-    let actual = actual.trim();
+    let actual = dis.disas().trim();
     eprintln!("=== actual ===\n{actual}");
 
     assert_eq!(expected, actual);
+}
+
+#[track_caller]
+fn assert_disas(ops: &[Op], expected: &str) {
+    let bytecode = encoded(ops);
+    assert_disas_with_disassembler(&mut disas::Disassembler::new(&bytecode), expected);
 }
 
 #[test]
@@ -82,6 +88,95 @@ fn push_pop_many() {
        9: 36 1f 00 00 00                  xpop32_many x0, x1, x2, x3, x4
        e: 30                              pop_frame
        f: 00                              ret
+        "#,
+    );
+}
+
+#[test]
+fn no_offsets() {
+    let bytecode = encoded(&[
+        // Prologue.
+        Op::PushFrame(PushFrame {}),
+        // Function body.
+        Op::Xadd32(Xadd32 {
+            operands: BinaryOperands {
+                dst: XReg::x0,
+                src1: XReg::x0,
+                src2: XReg::x1,
+            },
+        }),
+        // Epilogue.
+        Op::PopFrame(PopFrame {}),
+        Op::Ret(Ret {}),
+    ]);
+
+    assert_disas_with_disassembler(
+        disas::Disassembler::new(&bytecode).offsets(false),
+        r#"
+2f                              push_frame
+12 00 04                        xadd32 x0, x0, x1
+30                              pop_frame
+00                              ret
+        "#,
+    );
+}
+
+#[test]
+fn no_hexdump() {
+    let bytecode = encoded(&[
+        // Prologue.
+        Op::PushFrame(PushFrame {}),
+        // Function body.
+        Op::Xadd32(Xadd32 {
+            operands: BinaryOperands {
+                dst: XReg::x0,
+                src1: XReg::x0,
+                src2: XReg::x1,
+            },
+        }),
+        // Epilogue.
+        Op::PopFrame(PopFrame {}),
+        Op::Ret(Ret {}),
+    ]);
+
+    assert_disas_with_disassembler(
+        disas::Disassembler::new(&bytecode).hexdump(false),
+        r#"
+       0: push_frame
+       1: xadd32 x0, x0, x1
+       4: pop_frame
+       5: ret
+        "#,
+    );
+}
+
+#[test]
+fn no_offsets_or_hexdump() {
+    let bytecode = encoded(&[
+        // Prologue.
+        Op::PushFrame(PushFrame {}),
+        // Function body.
+        Op::Xadd32(Xadd32 {
+            operands: BinaryOperands {
+                dst: XReg::x0,
+                src1: XReg::x0,
+                src2: XReg::x1,
+            },
+        }),
+        // Epilogue.
+        Op::PopFrame(PopFrame {}),
+        Op::Ret(Ret {}),
+    ]);
+
+    assert_disas_with_disassembler(
+        disas::Disassembler::new(&bytecode)
+            .offsets(false)
+            .hexdump(false),
+        r#"
+push_frame
+xadd32 x0, x0, x1
+pop_frame
+ret
         "#,
     );
 }
