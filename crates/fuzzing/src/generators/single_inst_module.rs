f72eb5b7119893bdd22c9a5458ac4979fc97cfb9
Alex Crichton <alex@alexcrichton.com>
2024-09-30 12:25:26 -0700

    Fix fuzzer tests

--- crates/fuzzing/src/generators/single_inst_module.rs
+++ crates/fuzzing/src/generators/single_inst_module.rs
@@ -589,12 +589,12 @@ mod test {
             wat,
             r#"(module
   (type (;0;) (func (param i32 i32) (result i32)))
+  (export "test" (func 0))
   (func (;0;) (type 0) (param i32 i32) (result i32)
     local.get 0
     local.get 1
     i32.add
   )
-  (export "test" (func 0))
 )
 "#
         )
