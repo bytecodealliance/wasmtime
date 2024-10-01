47124201a079ad96d1aff775e7981f52e017f60a
Trevor Elliott <telliott@fastly.com>
2024-10-01 16:58:25 -0700

    Allow alignments that eat the remaining allocation (#9356)
    * Allow alignments that eat the remaining allocation
    
    co-authored-by: Dan Gohman <dev@sunfishcode.online>
    
    * Add a test for empty reads
    
    ---------
    
    Co-authored-by: Dan Gohman <dev@sunfishcode.online>

---
+++ crates/test-programs/src/bin/cli_stdin_empty.rs
@@ -0,0 +1,16 @@
+use test_programs::preview1::STDIN_FD;
+
+fn main() {
+    let mut buffer = [0_u8; 0];
+
+    unsafe {
+        wasi::fd_read(
+            STDIN_FD,
+            &[wasi::Iovec {
+                buf: buffer.as_mut_ptr(),
+                buf_len: 0,
+            }],
+        )
+        .expect("empty read should succeed");
+    }
+}
