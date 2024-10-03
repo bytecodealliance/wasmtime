80ed395e185b7092bb0b520b67150b7b984e1148
Alex Crichton <alex@alexcrichton.com>
2024-10-02 19:57:07 -0500

    Disable tail-call/relaxed-simd when fuzzing the spec interpreter (#9360)
    * Disable tail-call when fuzzing the spec interpreter
    
    Our copy of the reference interpreter does not implement the tail-call
    proposal, so fix OSS-Fuzz by disabling it. This technically should have
    been done in #9336 when wasm-tools was updated to auto-try to enable
    this proposal by default, but alas I am not a human fuzzer so I waited
    until OSS-Fuzz found it.
    
    * Also disable relaxed-simd

--- crates/fuzzing/src/oracles/diff_spec.rs
+++ crates/fuzzing/src/oracles/diff_spec.rs
@@ -23,6 +23,8 @@ impl SpecInterpreter {
         config.threads_enabled = false;
         config.bulk_memory_enabled = false;
         config.reference_types_enabled = false;
+        config.tail_call_enabled = false;
+        config.relaxed_simd_enabled = false;
 
         Self
     }
