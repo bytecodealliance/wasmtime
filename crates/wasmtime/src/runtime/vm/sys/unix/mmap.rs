d88aeda8a09a88386b8953fc3f285fffcd141c59
Alex Crichton <alex@alexcrichton.com>
2024-10-08 10:31:51 -0500

    Increase alignment of empty mmap pointers (#9393)
    This commit updates platform implementations of `Mmap::new_empty()` to
    use a shared helper to create a pointer with a higher alignment than
    one. This fixes a debug assert tripping in niche configurations of the
    pooling allocator where if no virtual memory is given and table lazy
    init is disabled then a module could trip a debug-only assert about a
    slice being created from a raw pointer that is not properly aligned. The
    fix here is to use a helper that starts with an over-aligned empty slice
    which is then casted down to a byte slice.

--- crates/wasmtime/src/runtime/vm/sys/unix/mmap.rs
+++ crates/wasmtime/src/runtime/vm/sys/unix/mmap.rs
@@ -14,7 +14,7 @@ pub struct Mmap {
 impl Mmap {
     pub fn new_empty() -> Mmap {
         Mmap {
-            memory: SendSyncPtr::from(&mut [][..]),
+            memory: crate::vm::sys::empty_mmap(),
         }
     }
 
