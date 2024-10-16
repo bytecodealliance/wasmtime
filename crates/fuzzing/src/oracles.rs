6e920bdd1c742af67c614d9dfa7d01cd207260d0
Nick Fitzgerald <fitzgen@gmail.com>
2024-10-16 11:53:30 -0700

    Add custom-page-sizes as an expected feature for module generation

--- crates/fuzzing/src/oracles.rs
+++ crates/fuzzing/src/oracles.rs
@@ -1229,7 +1229,8 @@ mod tests {
             | WasmFeatures::TAIL_CALL
             | WasmFeatures::WIDE_ARITHMETIC
             | WasmFeatures::MEMORY64
-            | WasmFeatures::GC_TYPES;
+            | WasmFeatures::GC_TYPES
+            | WasmFeatures::CUSTOM_PAGE_SIZES;
 
         // All other features that wasmparser supports, which is presumably a
         // superset of the features that wasm-smith supports, are listed here as
