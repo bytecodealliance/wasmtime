bytecodealliance/wasmtime

commit a40776cc7928cfdae2a657d27c15b6171d499fe4

Bump MSRV to 1.80 (#9496)
* Bump MSRV to 1.80

This commit updates the minimum Rust version required to compile
Wasmtime to 1.80.0 from the previous 1.78.0.

* Update rust installation in CI

* Change MSRV window back to 2 now that oss-fuzz is resolved
* Update the nightly to the latest nightly

* Undo most of the nightly update

.github/actions/install-rust/action.yml
@@ -9,15 +9,7 @@ inputs:
   msrv_range:
     description: 'Versions later-than-latest-Rust the MSRV supports'
     required: false
-    # Note that this is currently set to 3 as the MSRV is 1.78 and current
-    # stable is 1.81. Currently MSRV cannot be updated as it would break the
-    # build on OSS-Fuzz which hasn't updated its Rust compiler in quite some time.
-    #
-    # Updating OSS-Fuzz is being done in
-    # https://github.com/google/oss-fuzz/pull/12365 but it's taking some time,
-    # so for now this'll get bumped instead of MSRV while we wait for the update
-    # to happen.
-    default: '3'
+    default: '2'
 
 runs:
   using: composite
@@ -36,7 +28,7 @@ runs:
         elif [ "${{ inputs.toolchain }}" = "msrv" ]; then
           echo "version=1.$msrv.0" >> "$GITHUB_OUTPUT"
         elif [ "${{ inputs.toolchain }}" = "wasmtime-ci-pinned-nightly" ]; then
-          echo "version=nightly-2024-10-01" >> "$GITHUB_OUTPUT"
+          echo "version=nightly-2024-10-02" >> "$GITHUB_OUTPUT"
         else
           echo "version=${{ inputs.toolchain }}" >> "$GITHUB_OUTPUT"
         fi

Cargo.toml
@@ -165,7 +165,7 @@ authors = ["The Wasmtime Project Developers"]
 edition = "2021"
 # Wasmtime's current policy is that this number can be no larger than the
 # current stable release of Rust minus 2.
-rust-version = "1.78.0"
+rust-version = "1.80.0"
 
 [workspace.lints.rust]
 # Turn on some lints which are otherwise allow-by-default in rustc.

commit 25f14821c194d4d069c191c2c17a258ce25f4402

Update nightly in CI
This commit updates nightly again in CI after failing to do so in #9496.
This fixes an issue in our release CI where CMake was misconfigured when
cross-compiling and creating `aarch64-pc-windows-msvc` artifacts.

prtest:full

.github/actions/install-rust/action.yml
@@ -28,7 +28,7 @@ runs:
         elif [ "${{ inputs.toolchain }}" = "msrv" ]; then
           echo "version=1.$msrv.0" >> "$GITHUB_OUTPUT"
         elif [ "${{ inputs.toolchain }}" = "wasmtime-ci-pinned-nightly" ]; then
-          echo "version=nightly-2024-10-02" >> "$GITHUB_OUTPUT"
+          echo "version=nightly-2024-10-22" >> "$GITHUB_OUTPUT"
         else
           echo "version=${{ inputs.toolchain }}" >> "$GITHUB_OUTPUT"
         fi

ci/build-release-artifacts.sh
@@ -45,6 +45,7 @@ cargo build --release $flags --target $target -p wasmtime-cli $bin_flags --featu
 mkdir -p target/c-api-build
 cd target/c-api-build
 cmake \
+  -G Ninja \
   ../../crates/c-api \
   $cmake_flags \
   -DCMAKE_BUILD_TYPE=Release \