8df32f7fb2c0cd6295239471822f4032b8223c0e
Alex Crichton <alex@alexcrichton.com>
2024-12-03 22:03:52 -0700

    Pin some runner images (#9717)
    For the larger test/build jobs use some variables in the `*.js` files
    calculating the matrix to pin the images to fixed versions of GitHub
    actions images. For the `main.yml` file instead of changing everything
    over to a pin only do the ones that are currently failing on the
    `ubuntu-24.04` update. The hope is that there's only a few locations to
    update pinned versions in the future, and we'll need to keep an eye on
    CI warnings and such to know when to update these pins in the future.

--- ci/build-build-matrix.js
+++ ci/build-build-matrix.js
@@ -4,69 +4,73 @@
 // This is a separate script primarily to write out all the release
 // targets/platforms once and then duplicate them all with a "min" build.
 
+const ubuntu = 'ubuntu-22.04';
+const windows = 'windows-2022';
+const macos = 'macos-14';
+
 const array = [
   {
     // The name of the build which shows up in the name of the artifact for
     // Wasmtime's github releases.
     "build": "x86_64-linux",
     // The GitHub Actions platform that this build runs on
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     // The Rust target that will be used for the build.
     "target": "x86_64-unknown-linux-gnu",
   },
   {
     "build": "aarch64-linux",
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     "target": "aarch64-unknown-linux-gnu",
   },
   {
     "build": "s390x-linux",
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     "target": "s390x-unknown-linux-gnu",
   },
   {
     "build": "riscv64gc-linux",
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     "target": "riscv64gc-unknown-linux-gnu",
   },
   {
     "build": "x86_64-macos",
-    "os": "macos-latest",
+    "os": macos,
     "target": "x86_64-apple-darwin",
   },
   {
     "build": "aarch64-macos",
-    "os": "macos-latest",
+    "os": macos,
     "target": "aarch64-apple-darwin",
   },
   {
     "build": "x86_64-windows",
-    "os": "windows-latest",
+    "os": windows,
     "target": "x86_64-pc-windows-msvc",
   },
   {
     "build": "x86_64-mingw",
-    "os": "windows-latest",
+    "os": windows,
     "target": "x86_64-pc-windows-gnu",
   },
   {
     "build": "aarch64-android",
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     "target": "aarch64-linux-android",
   },
   {
     "build": "x86_64-android",
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     "target": "x86_64-linux-android",
   },
   {
     "build": "x86_64-musl",
-    "os": "ubuntu-latest",
+    "os": ubuntu,
     "target": "x86_64-unknown-linux-musl",
   },
   {
     "build": "aarch64-windows",
-    "os": "windows-latest",
+    "os": windows,
     "target": "aarch64-pc-windows-msvc",
   },
 ];
