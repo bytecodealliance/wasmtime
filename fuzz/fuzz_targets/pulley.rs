8bbd9663de85df4686151dcf20ff348fd228782e
Alex Crichton <alex@alexcrichton.com>
2024-12-10 08:25:17 -0800

    Remove pulley interpreter fuzz target
    This is a bit onerous to keep updated and is probably best subsumed by
    the fuzzing support we have in general for wasm.

--- fuzz/fuzz_targets/pulley.rs
+++ fuzz/fuzz_targets/pulley.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use libfuzzer_sys::{arbitrary::*, fuzz_target};
-use pulley_interpreter_fuzz::{interp, roundtrip};
+use pulley_interpreter_fuzz::roundtrip;
 
 fuzz_target!(|data| {
     let _ = fuzz(data);
@@ -11,9 +11,8 @@ fn fuzz(data: &[u8]) -> Result<()> {
     let _ = env_logger::try_init();
 
     let mut u = Unstructured::new(data);
-    match u.int_in_range(0..=1)? {
+    match u.int_in_range(0..=0)? {
         0 => roundtrip(Arbitrary::arbitrary_take_rest(u)?),
-        1 => interp(Arbitrary::arbitrary_take_rest(u)?),
         _ => unreachable!(),
     }
 
