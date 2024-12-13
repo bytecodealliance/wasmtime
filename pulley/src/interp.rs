c7a70e39a68a114b5331a275dd20f40022bc37d6
Dan Gohman <dev@sunfishcode.online>
2024-12-13 12:39:47 -0800

    Port to main.

--- pulley/src/interp.rs
+++ pulley/src/interp.rs
@@ -196,6 +196,11 @@ impl Vm {
         self.state.fp
     }
 
+    /// Returns the current `sp` register value.
+    pub fn sp(&self) -> *mut u8 {
+        self.state.sp
+    }
+
     /// Returns the current `lr` register value.
     pub fn lr(&self) -> *mut u8 {
         self.state.lr
@@ -701,6 +706,7 @@ pub struct MachineState {
     f_regs: [FRegVal; FReg::RANGE.end as usize],
     v_regs: [VRegVal; VReg::RANGE.end as usize],
     fp: *mut u8,
+    sp: *mut u8,
     lr: *mut u8,
     stack: Vec<u8>,
     done_reason: Option<DoneReason<()>>,
@@ -718,6 +724,7 @@ impl fmt::Debug for MachineState {
             stack: _,
             done_reason: _,
             fp: _,
+            sp: _,
             lr: _,
         } = self;
 
@@ -799,6 +806,7 @@ impl MachineState {
             stack,
             done_reason: None,
             fp: HOST_RETURN_ADDR,
+            sp: HOST_RETURN_ADDR,
             lr: HOST_RETURN_ADDR,
         };
 
