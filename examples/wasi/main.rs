4c6cce2b88cee8e47cb3ab6b6418160846abc36f
maple@max <feng.liang@kaust.edu.sa>
2024-12-14 22:51:54 +0800

    cargo fmt

--- examples/wasi/main.rs
+++ examples/wasi/main.rs
@@ -63,10 +63,14 @@ fn main() -> Result<()> {
     // Instantiate it as a normal component
     let instance = linker.instantiate(&mut store, &component)?;
     // Get the index for the exported interface
-    let interface_idx = instance.get_export(&mut store, None, "wasi:cli/run@0.2.0").unwrap();
+    let interface_idx = instance
+        .get_export(&mut store, None, "wasi:cli/run@0.2.0")
+        .unwrap();
     // Get the index for the exported function in the exported interface
     let parent_export_idx = Some(&interface_idx);
-    let func_idx = instance.get_export(&mut store, parent_export_idx, "run").unwrap();
+    let func_idx = instance
+        .get_export(&mut store, parent_export_idx, "run")
+        .unwrap();
     let func = instance.get_func(&mut store, func_idx).unwrap();
     // As the `run` function in `wasi:cli/run@0.2.0` takes no argument and return a WASI result that correspond to a `Result<(), ()>`
     // Reference:
