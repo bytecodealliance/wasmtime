0e9a0e1c380b043a8517ab882e45f6b797dd514a
Alex Crichton <alex@alexcrichton.com>
2024-12-19 17:07:07 -0800

    pulley: Finish `simd` proposal implementation
    This commit fills out the final and miscellaneous set of opcodes for
    Pulley to have a complete implementation of the `simd` proposal for
    WebAssembly. All spec tests are now enabled and the Pulley-specific
    exceptions for `*.wast` tests are all gone.
    
    Closes #9783

---
+++ pulley/build.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // let opt_level = std::env::var("OPT_LEVEL").unwrap();
+    // let target = std::env::var("TARGET").unwrap();
+    // dbg!(&opt_level);
+    // dbg!(&target);
+
+    // if let Ok(n) = opt_level.parse::<u32>() {
+    //     if n >= 2 {
+    //         match target.as_str() {
+    //             "x86_64-unknown-linux-gnu" => {
+    //                 println!("cargo:rustc-cfg=pulley_assume_llvm_makes_tail_calls");
+    //             }
+    //             _ => panic!(),
+    //         }
+    //     } else {
+    //         // panic!()
+    //     }
+    // } else {
+    //     panic!()
+    // }
+}
