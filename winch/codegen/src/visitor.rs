f76f2edb2cf0d2819db025783bd53b0b0b721875
adhoc <postma.marin@protonmail.com>
2025-01-09 16:32:27 +0100

    implement i64.atomic.load32_u

--- winch/codegen/src/visitor.rs
+++ winch/codegen/src/visitor.rs
@@ -258,6 +258,7 @@ macro_rules! def_unsupported {
     (emit I32AtomicLoad $($rest:tt)*) => {};
     (emit I64AtomicLoad8U $($rest:tt)*) => {};
     (emit I64AtomicLoad16U $($rest:tt)*) => {};
+    (emit I64AtomicLoad32U $($rest:tt)*) => {};
 
     (emit $unsupported:tt $($rest:tt)*) => {$($rest)*};
 }
@@ -2162,6 +2163,10 @@ where
 
     fn visit_i64_atomic_load16_u(&mut self, memarg: wasmparser::MemArg) {
         self.emit_wasm_load_atomic(&memarg, WasmValType::I64, OperandSize::S16, None);
+    }
+
+    fn visit_i64_atomic_load32_u(&mut self, memarg: wasmparser::MemArg) {
+        self.emit_wasm_load_atomic(&memarg, WasmValType::I64, OperandSize::S32, None);
     }
     wasmparser::for_each_visit_operator!(def_unsupported);
 }
