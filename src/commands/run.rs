c7ff06e905751bc04a6edb3cf2476ca4712b8f59
VolodymyrBg <aqdrgg19@gmail.com>
2025-01-14 16:19:22 +0200

    Update run.rs

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -574,28 +574,29 @@ impl RunCommand {
                 "warning: using `--invoke` with a function that returns values \
                  is experimental and may break in the future"
             );
-        }
 
-        for result in results {
-            match result {
-                Val::I32(i) => {
-                    eprintln!("Debug: Result value: {i}");
-                    print!("{i}");
+            for result in results {
+                match result {
+                    Val::I32(i) => {
+                        eprintln!("Debug: Result value: {i}");
+                        print!("{i}");
+                    }
+                    Val::I64(i) => print!("{i}"),
+                    Val::F32(f) => print!("{}", f32::from_bits(f)),
+                    Val::F64(f) => print!("{}", f64::from_bits(f)),
+                    Val::V128(i) => print!("{}", i.as_u128()),
+                    Val::ExternRef(None) => print!("<null externref>"),
+                    Val::ExternRef(Some(_)) => print!("<externref>"),
+                    Val::FuncRef(None) => print!("<null funcref>"),
+                    Val::FuncRef(Some(_)) => print!("<funcref>"),
+                    Val::AnyRef(None) => print!("<null anyref>"),
+                    Val::AnyRef(Some(_)) => print!("<anyref>"),
                 }
-                Val::I64(i) => print!("{i}"),
-                Val::F32(f) => print!("{}", f32::from_bits(f)),
-                Val::F64(f) => print!("{}", f64::from_bits(f)),
-                Val::V128(i) => print!("{}", i.as_u128()),
-                Val::ExternRef(None) => print!("<null externref>"),
-                Val::ExternRef(Some(_)) => print!("<externref>"),
-                Val::FuncRef(None) => print!("<null funcref>"),
-                Val::FuncRef(Some(_)) => print!("<funcref>"),
-                Val::AnyRef(None) => print!("<null anyref>"),
-                Val::AnyRef(Some(_)) => print!("<anyref>"),
             }
-        }
-        if !self.no_newline {
-            println!();
+
+            if !self.no_newline {
+                println!();
+            }
         }
         Ok(())
     }
