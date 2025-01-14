bytecodealliance/wasmtime

commit 21ab8ea016cf6a2c25527f644773a7d25e845d8e

Emit post-optimization CLIF with `--emit-clif` (#10011)
* Emit post-optimization CLIF with `--emit-clif`

This commit updates the implementation of the `wasmtime` CLI flag
`--emit-clif` to emit the IR post-optimization rather than
pre-optimization. This is now emitting the IR that's directly fed to the
backend for lowering which reflects any mid-level transformation that's
performed. It should still be possible to recover pre-optimized CLIF
with `-O opt-level=0`.

This then additionally refactors functions to emit CLIF in a more "core"
location so all trampolines get their CLIF emitted as well in case those
are needed for debugging.

* Update disas tests

crates/cranelift/src/compiler.rs
@@ -273,18 +273,10 @@ impl wasmtime_environ::Compiler for Compiler {
             &mut func_env,
         )?;
 
-        if let Some(path) = &self.clif_dir {
-            use std::io::Write;
-
-            let mut path = path.to_path_buf();
-            path.push(format!("wasm_func_{}", func_index.as_u32()));
-            path.set_extension("clif");
-
-            let mut output = std::fs::File::create(path).unwrap();
-            write!(output, "{}", context.func.display()).unwrap();
-        }
-
-        let (info, func) = compiler.finish_with_info(Some((&body, &self.tunables)))?;
+        let (info, func) = compiler.finish_with_info(
+            Some((&body, &self.tunables)),
+            &format!("wasm_func_{}", func_index.as_u32()),
+        )?;
 
         let timing = cranelift_codegen::timing::take_current();
         log::debug!("{:?} translated in {:?}", func_index, timing.total());
@@ -363,7 +355,10 @@ impl wasmtime_environ::Compiler for Compiler {
         builder.ins().return_(&[true_return]);
         builder.finalize();
 
-        Ok(Box::new(compiler.finish()?))
+        Ok(Box::new(compiler.finish(&format!(
+            "array_to_wasm_{}",
+            func_index.as_u32(),
+        ))?))
     }
 
     fn compile_wasm_to_array_trampoline(
@@ -433,7 +428,9 @@ impl wasmtime_environ::Compiler for Compiler {
         builder.ins().return_(&results);
         builder.finalize();
 
-        Ok(Box::new(compiler.finish()?))
+        Ok(Box::new(compiler.finish(&format!(
+            "wasm_to_array_trampoline_{wasm_func_ty}"
+        ))?))
     }
 
     fn append_code(
@@ -643,7 +640,9 @@ impl wasmtime_environ::Compiler for Compiler {
         builder.ins().return_(&results);
         builder.finalize();
 
-        Ok(Box::new(compiler.finish()?))
+        Ok(Box::new(
+            compiler.finish(&format!("wasm_to_builtin_{}", index.name()))?,
+        ))
     }
 
     fn compiled_function_relocation_targets<'a>(
@@ -959,8 +958,8 @@ impl FunctionCompiler<'_> {
         (builder, block0)
     }
 
-    fn finish(self) -> Result<CompiledFunction, CompileError> {
-        let (info, func) = self.finish_with_info(None)?;
+    fn finish(self, clif_filename: &str) -> Result<CompiledFunction, CompileError> {
+        let (info, func) = self.finish_with_info(None, clif_filename)?;
         assert!(info.stack_maps.is_empty());
         Ok(func)
     }
@@ -968,11 +967,22 @@ impl FunctionCompiler<'_> {
     fn finish_with_info(
         mut self,
         body_and_tunables: Option<(&FunctionBody<'_>, &Tunables)>,
+        clif_filename: &str,
     ) -> Result<(WasmFunctionInfo, CompiledFunction), CompileError> {
         let context = &mut self.cx.codegen_context;
         let isa = &*self.compiler.isa;
         let mut compiled_code =
             compile_maybe_cached(context, isa, self.cx.incremental_cache_ctx.as_mut())?;
+
+        if let Some(path) = &self.compiler.clif_dir {
+            use std::io::Write;
+
+            let mut path = path.join(clif_filename);
+            path.set_extension("clif");
+
+            let mut output = std::fs::File::create(path).unwrap();
+            write!(output, "{}", context.func.display()).unwrap();
+        }
 
         // Give wasm functions, user defined code, a "preferred" alignment
         // instead of the minimum alignment as this can help perf in niche

commit 2ba60e4d4ef28658279cc494ff6d2d51b9dd50ac

Emit CLIF even if compilation fails
Just after #10011 I ran into a case where I wanted to peek at CLIF after
compilation failed but the structure of the code failed to enable this,
so I've refactored `--emit-clif` to emit the CLIF even if the lowering
step fails.

crates/cranelift/src/compiler.rs
@@ -971,8 +971,14 @@ impl FunctionCompiler<'_> {
     ) -> Result<(WasmFunctionInfo, CompiledFunction), CompileError> {
         let context = &mut self.cx.codegen_context;
         let isa = &*self.compiler.isa;
-        let mut compiled_code =
-            compile_maybe_cached(context, isa, self.cx.incremental_cache_ctx.as_mut())?;
+
+        // Run compilation, but don't propagate the error just yet. This'll
+        // mutate `context` and the IR contained within (optionally) but it may
+        // fail if the backend has a bug in it. Use `context` after this
+        // finishes to optionally emit CLIF and then after that's done actually
+        // propagate the error if one happened.
+        let compilation_result =
+            compile_maybe_cached(context, isa, self.cx.incremental_cache_ctx.as_mut());
 
         if let Some(path) = &self.compiler.clif_dir {
             use std::io::Write;
@@ -983,6 +989,8 @@ impl FunctionCompiler<'_> {
             let mut output = std::fs::File::create(path).unwrap();
             write!(output, "{}", context.func.display()).unwrap();
         }
+
+        let mut compiled_code = compilation_result?;
 
         // Give wasm functions, user defined code, a "preferred" alignment
         // instead of the minimum alignment as this can help perf in niche