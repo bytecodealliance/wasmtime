94172cb27c3c99fbe902ed8d15df8f1ddd750424
Pat Hickey <p.hickey@f5.com>
2025-01-16 13:05:19 -0800

    crate-level docs for wasmtime-wasi-io

--- crates/wasi-io/src/lib.rs
+++ crates/wasi-io/src/lib.rs
@@ -1,3 +1,20 @@
+//! # Wasmtime's wasi-io Implementation
+//!
+//! This crate provides a Wasmtime host implementation of the WASI 0.2 (aka
+//! WASIp2 aka Preview 2) wasi-io package. The host implementation is
+//! abstract: it is exposed as a set of traits which other crates provide
+//! impls of.
+//!
+//! The wasi-io package is the foundation which defines how WASI programs
+//! interact with the scheduler. It provides the `pollable`, `input-stream`,
+//! and `output-stream` Component Model resources, which other packages
+//! (including wasi-filesystem, wasi-sockets, wasi-cli, and wasi-http)
+//! expose as the standard way to wait for readiness, and asynchronously read
+//! and write to streams.
+//!
+//! This crate is designed to have no unnecessary dependencies and, in
+//! particular, compile without `std`.
+
 pub mod bindings;
 mod impls;
 pub mod poll;
