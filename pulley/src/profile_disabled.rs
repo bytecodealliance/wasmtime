402203558e9ce7e4d131eaa3ac05771261227493
Alex Crichton <alex@alexcrichton.com>
2025-01-16 12:44:29 -0800

    Add missing source file

---
+++ pulley/src/profile_disabled.rs
@@ -0,0 +1,26 @@
+//! Stubs for when profiling is disabled to have the "executing_pc" field
+//! basically compiled away.
+
+use core::marker;
+
+#[derive(Default, Clone)]
+pub(crate) struct ExecutingPc;
+
+impl ExecutingPc {
+    pub(crate) fn as_ref(&self) -> ExecutingPcRef<'_> {
+        ExecutingPcRef {
+            _marker: marker::PhantomData,
+        }
+    }
+}
+
+#[repr(transparent)]
+pub(crate) struct ExecutingPcRef<'a> {
+    _marker: marker::PhantomData<&'a ()>,
+}
+
+impl ExecutingPcRef<'_> {
+    pub(crate) fn record(&self, pc: usize) {
+        let _ = pc;
+    }
+}
