795f5fbf02c655006ae45cad4dea71dacc748f48
Alex Crichton <alex@alexcrichton.com>
2025-01-17 15:17:27 -0800

    Try using ARM64 runners from GitHub
    Let's see if we can run our aarch64 tests natively instead of through
    QEMU emulation...
    
    prtest:linux-arm64

--- ci/build-test-matrix.js
+++ ci/build-test-matrix.js
@@ -16,6 +16,7 @@ const GENERIC_BUCKETS = 3;
 const SINGLE_CRATE_BUCKETS = ["wasmtime", "wasmtime-cli", "wasmtime-wasi"];
 
 const ubuntu = 'ubuntu-24.04';
+const ubuntu_arm = 'ubuntu-24.04-arm';
 const windows = 'windows-2022';
 const macos = 'macos-14';
 
@@ -95,12 +96,8 @@ const FULL_MATRIX = [
     "filter": "mingw-x64"
   },
   {
-    "os": ubuntu,
+    "os": ubuntu_arm,
     "target": "aarch64-unknown-linux-gnu",
-    "gcc_package": "gcc-aarch64-linux-gnu",
-    "gcc": "aarch64-linux-gnu-gcc",
-    "qemu": "qemu-aarch64 -L /usr/aarch64-linux-gnu",
-    "qemu_target": "aarch64-linux-user",
     "name": "Test Linux arm64",
     "filter": "linux-arm64",
     "isa": "aarch64",
