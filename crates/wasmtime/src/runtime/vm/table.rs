420ef50e4d7475ed1c85a4ebd689853bb7fc2a48
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-01-31 16:54:58 +0100

    Update table.rs

--- crates/wasmtime/src/runtime/vm/table.rs
+++ crates/wasmtime/src/runtime/vm/table.rs
@@ -568,7 +568,7 @@ impl Table {
         if delta == 0 {
             return Ok(Some(old_size));
         }
-        // Cannot return `Trap::TableOutOfBounds` here becase `impl std::error::Error for Trap` is not available in no-std.
+        // Cannot return `Trap::TableOutOfBounds` here because `impl std::error::Error for Trap` is not available in no-std.
         let delta =
             usize::try_from(delta).map_err(|_| format_err!("delta exceeds host pointer size"))?;
 
