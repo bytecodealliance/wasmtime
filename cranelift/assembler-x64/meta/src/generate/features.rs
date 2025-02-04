dcd8c62ecb6ce42422ed207bccbe8394e497a9e9
Andrew Brown <andrew.brown@intel.com>
2025-02-03 19:38:47 -0800

    asm: prepare docs for publishing
    This change is motivated by staring at the output of `cargo doc` for the
    `cranelift-assembler-x64` crate. In order to write a sensible top-level
    example, I felt it was best to refactor how we construct the
    instructions: this removes the `build` module and adds conventional
    `<inst_name>::new` functions to each instruction. Also, a generated
    `From` implementation makes it easier to convert to an `Inst`.
    
    This change has other doc-related refactorings and tweaks, but should
    not change any functionality.

--- cranelift/assembler-x64/meta/src/generate/features.rs
+++ cranelift/assembler-x64/meta/src/generate/features.rs
@@ -9,7 +9,9 @@ impl dsl::Feature {
     /// This function recreates the `Feature` struct itself in the generated
     /// code.
     pub fn generate_enum(f: &mut Formatter) {
+        fmtln!(f, "#[doc(hidden)]");
         generate_derive(f);
+        fmtln!(f, "#[derive(Copy, PartialEq)]"); // Add a couple more helpful derives.
         fmtln!(f, "pub enum Feature {{");
         f.indent(|f| {
             for feature in dsl::ALL_FEATURES {
