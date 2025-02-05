6f3f3c9ebe67ec41e4b06c0d975dd5deb1de8df4
Christof Petig <christof.petig@arcor.de>
2025-02-06 00:16:33 +0100

    support pulley even if host triplet also works

--- crates/wasmtime/src/config.rs
+++ crates/wasmtime/src/config.rs
@@ -2124,6 +2124,11 @@ impl Config {
             return target_lexicon::Triple::pulley_host();
         }
 
+        #[cfg(feature = "pulley")]
+        if self.target == Some(target_lexicon::Triple::pulley_host()) {
+            return target_lexicon::Triple::pulley_host();
+        }
+
         // And at this point the target is for sure the host.
         target_lexicon::Triple::host()
     }
