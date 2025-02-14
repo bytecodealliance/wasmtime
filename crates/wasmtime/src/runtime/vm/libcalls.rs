ae58bc474f117d9550a7df188e711f0be24f07ea
Alex Crichton <alex@alexcrichton.com>
2025-02-14 09:20:30 -0800

    Some minor modifications for Pulley
    prtest:full

--- crates/wasmtime/src/runtime/vm/libcalls.rs
+++ crates/wasmtime/src/runtime/vm/libcalls.rs
@@ -1317,16 +1317,6 @@ fn i8x16_swizzle(_store: &mut dyn VMStore, _instance: &mut Instance, a: i8x16, b
     }
 }
 
-#[cfg(not(target_arch = "x86_64"))]
-fn i8x16_swizzle(
-    _store: &mut dyn VMStore,
-    _instance: &mut Instance,
-    _a: i8x16,
-    _b: i8x16,
-) -> i8x16 {
-    unreachable!()
-}
-
 // This intrinsic is only used on x86_64 platforms as an implementation of
 // the `i8x16.shuffle` instruction when `pshufb` in SSSE3 is not available.
 #[cfg(target_arch = "x86_64")]
@@ -1381,17 +1371,6 @@ fn i8x16_shuffle(
         }
         .reg
     }
-}
-
-#[cfg(not(target_arch = "x86_64"))]
-fn i8x16_shuffle(
-    _store: &mut dyn VMStore,
-    _instance: &mut Instance,
-    _a: i8x16,
-    _b: i8x16,
-    _c: i8x16,
-) -> i8x16 {
-    unreachable!()
 }
 
 /// This intrinsic is just used to record trap information.
