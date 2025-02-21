97f3eda49c394aae1ca6a30d405a734426a674fe
primoly <168267431+primoly@users.noreply.github.com>
2025-02-21 15:17:59 +0100

    add tests

---
+++ crates/test-programs/src/bin/cli_serve_trap_before_set.rs
@@ -0,0 +1,14 @@
+use test_programs::proxy;
+use test_programs::wasi::http::types::{IncomingRequest, ResponseOutparam};
+
+struct T;
+
+proxy::export!(T);
+
+impl proxy::exports::wasi::http::incoming_handler::Guest for T {
+    fn handle(_request: IncomingRequest, _outparam: ResponseOutparam) {
+        panic!()
+    }
+}
+
+fn main() {}
