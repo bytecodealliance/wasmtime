308c18dbfebd8219d45525960ee6f7cd19a3c9b5
Jeff Charles <jeff.charles@shopify.com>
2025-02-21 21:53:38 +0000

    Winch: Add min and max for SIMD floats for x64 with AVX

---
+++ tests/disas/winch/x64/f64x2_min/const_avx.wat
@@ -0,0 +1,56 @@
+;;! target = "x86_64"
+;;! test = "winch"
+;;! flags = [ "-Ccranelift-has-avx" ]
+
+(module
+    (func (result v128)
+        (f64x2.min (v128.const i64x2 1 0) (v128.const i64x2 0 1))
+    )
+)
+;; wasm[0]::function[0]:
+;;       pushq   %rbp
+;;       movq    %rsp, %rbp
+;;       movq    8(%rdi), %r11
+;;       movq    0x10(%r11), %r11
+;;       addq    $0x10, %r11
+;;       cmpq    %rsp, %r11
+;;       ja      0x64
+;;   1c: movq    %rdi, %r14
+;;       subq    $0x10, %rsp
+;;       movq    %rdi, 8(%rsp)
+;;       movq    %rsi, (%rsp)
+;;       movdqu  0x3c(%rip), %xmm0
+;;       movdqu  0x44(%rip), %xmm1
+;;       vminpd  %xmm0, %xmm1, %xmm15
+;;       vminpd  %xmm1, %xmm0, %xmm1
+;;       vorpd   %xmm1, %xmm15, %xmm1
+;;       vcmpunordpd %xmm1, %xmm0, %xmm0
+;;       vorpd   %xmm1, %xmm0, %xmm1
+;;       vpsrlq  $0xd, %xmm0, %xmm0
+;;       vandnpd %xmm1, %xmm0, %xmm1
+;;       movdqa  %xmm1, %xmm0
+;;       addq    $0x10, %rsp
+;;       popq    %rbp
+;;       retq
+;;   64: ud2
+;;   66: addb    %al, (%rax)
+;;   68: addb    %al, (%rax)
+;;   6a: addb    %al, (%rax)
+;;   6c: addb    %al, (%rax)
+;;   6e: addb    %al, (%rax)
+;;   70: addb    %al, (%rax)
+;;   72: addb    %al, (%rax)
+;;   74: addb    %al, (%rax)
+;;   76: addb    %al, (%rax)
+;;   78: addl    %eax, (%rax)
+;;   7a: addb    %al, (%rax)
+;;   7c: addb    %al, (%rax)
+;;   7e: addb    %al, (%rax)
+;;   80: addl    %eax, (%rax)
+;;   82: addb    %al, (%rax)
+;;   84: addb    %al, (%rax)
+;;   86: addb    %al, (%rax)
+;;   88: addb    %al, (%rax)
+;;   8a: addb    %al, (%rax)
+;;   8c: addb    %al, (%rax)
+;;   8e: addb    %al, (%rax)
--- winch/codegen/src/visitor.rs
+++ winch/codegen/src/visitor.rs
@@ -548,6 +548,10 @@ macro_rules! def_unsupported {
     (emit F64x2Nearest $($rest:tt)*) => {};
     (emit F32x4Trunc $($rest:tt)*) => {};
     (emit F64x2Trunc $($rest:tt)*) => {};
+    (emit F32x4Min $($rest:tt)*) => {};
+    (emit F64x2Min $($rest:tt)*) => {};
+    (emit F32x4Max $($rest:tt)*) => {};
+    (emit F64x2Max $($rest:tt)*) => {};
 
     (emit $unsupported:tt $($rest:tt)*) => {$($rest)*};
 }
@@ -4500,6 +4504,38 @@ where
     fn visit_f64x2_trunc(&mut self) -> Self::Output {
         self.masm
             .v128_trunc(&mut self.context, V128TruncKind::F64x2)
+    }
+
+    fn visit_f32x4_min(&mut self) -> Self::Output {
+        self.context
+            .binop(self.masm, OperandSize::S32, |masm, dst, src, _size| {
+                masm.v128_min(dst, src, writable!(dst), V128MinKind::F32x4)?;
+                Ok(TypedReg::v128(dst))
+            })
+    }
+
+    fn visit_f64x2_min(&mut self) -> Self::Output {
+        self.context
+            .binop(self.masm, OperandSize::S64, |masm, dst, src, _size| {
+                masm.v128_min(dst, src, writable!(dst), V128MinKind::F64x2)?;
+                Ok(TypedReg::v128(dst))
+            })
+    }
+
+    fn visit_f32x4_max(&mut self) -> Self::Output {
+        self.context
+            .binop(self.masm, OperandSize::S32, |masm, dst, src, _size| {
+                masm.v128_max(dst, src, writable!(dst), V128MaxKind::F32x4)?;
+                Ok(TypedReg::v128(dst))
+            })
+    }
+
+    fn visit_f64x2_max(&mut self) -> Self::Output {
+        self.context
+            .binop(self.masm, OperandSize::S64, |masm, dst, src, _size| {
+                masm.v128_max(dst, src, writable!(dst), V128MaxKind::F64x2)?;
+                Ok(TypedReg::v128(dst))
+            })
     }
 
     wasmparser::for_each_visit_simd_operator!(def_unsupported);
