6d028abe10e9331e7f1baf3b723223e0e2e58562
wasmtime-publish <59749941+wasmtime-publish@users.noreply.github.com>
2025-03-04 18:19:42 -0600

    Bump Wasmtime to 32.0.0 (#10330)
    Co-authored-by: Wasmtime Publish <wasmtime-publish@users.noreply.github.com>

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -206,11 +206,11 @@
 /**
  * \brief Wasmtime version string.
  */
-#define WASMTIME_VERSION "31.0.0"
+#define WASMTIME_VERSION "32.0.0"
 /**
  * \brief Wasmtime major version number.
  */
-#define WASMTIME_VERSION_MAJOR 31
+#define WASMTIME_VERSION_MAJOR 32
 /**
  * \brief Wasmtime minor version number.
  */
