e8d5e3ae2bbc1d6816134db189ed472e612563e2
James Sturtevant <jstur@microsoft.com>
2025-03-07 11:30:07 -0800

    Initial implementation of Wasi-tls (Transport Layer Security)  (#10249)
    * Initial implementation of wasi-tls
    
    This crate provides the Wasmtime host implementation for the [wasi-tls] API.
    The [wasi-tls] world allows WebAssembly modules to perform SSL/TLS operations,
    such as establishing secure connections to servers. TLS often relies on other wasi networking systems
    to provide the stream so it will be common to enable the [wasi:cli] world as well with the networking features enabled.
    
    The initial implemntation is using rustls.
    
    Signed-off-by: James Sturtevant <jsturtevant@gmail.com>
    
    * Remove configuration object for now
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Update cargo patch to use temp branch
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Rename tcp streams to wasistreams to be more generic
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * gate the wasi-tls ctx behind a feature
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * cleanup and clippy fixes
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Fix issue when another pollable cancels
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * prtest:full
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Skip test on riscv64/s390x
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Drop debug info to support tests on pulley based platforms
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Update signature of `close-notify`
    
    * Use draft version
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Remove patches
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    
    * Ungate tls on riscv64 and s390x
    
    * Un-gate wais-http on riscv64/s390x as well
    
    * Add wasmtime-wasi-tls to publish list
    
    * Add wasmtime-wasi-tls to public API crate list
    
    * Revert some changes to Cargo.lock
    
    ---------
    
    Signed-off-by: James Sturtevant <jsturtevant@gmail.com>
    Signed-off-by: James Sturtevant <jstur@microsoft.com>
    Co-authored-by: badeend <github@davebakker.io>
    Co-authored-by: Alex Crichton <alex@alexcrichton.com>

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -32,6 +32,9 @@ use wasmtime_wasi_http::{
 #[cfg(feature = "wasi-keyvalue")]
 use wasmtime_wasi_keyvalue::{WasiKeyValue, WasiKeyValueCtx, WasiKeyValueCtxBuilder};
 
+#[cfg(feature = "wasi-tls")]
+use wasmtime_wasi_tls::WasiTlsCtx;
+
 fn parse_preloads(s: &str) -> Result<(String, PathBuf)> {
     let parts: Vec<&str> = s.splitn(2, '=').collect();
     if parts.len() != 2 {
@@ -822,6 +825,32 @@ impl RunCommand {
                 }
 
                 store.data_mut().wasi_http = Some(Arc::new(WasiHttpCtx::new()));
+            }
+        }
+
+        if self.run.common.wasi.tls == Some(true) {
+            #[cfg(all(not(all(feature = "wasi-tls", feature = "component-model"))))]
+            {
+                bail!("Cannot enable wasi-tls when the binary is not compiled with this feature.");
+            }
+            #[cfg(all(feature = "wasi-tls", feature = "component-model",))]
+            {
+                match linker {
+                    CliLinker::Core(_) => {
+                        bail!("Cannot enable wasi-tls for core wasm modules");
+                    }
+                    CliLinker::Component(linker) => {
+                        let mut opts = wasmtime_wasi_tls::LinkOptions::default();
+                        opts.tls(true);
+                        wasmtime_wasi_tls::add_to_linker(linker, &mut opts, |h| {
+                            let preview2_ctx =
+                                h.preview2_ctx.as_mut().expect("wasip2 is not configured");
+                            let preview2_ctx =
+                                Arc::get_mut(preview2_ctx).unwrap().get_mut().unwrap();
+                            WasiTlsCtx::new(preview2_ctx.table())
+                        })?;
+                    }
+                }
             }
         }
 
