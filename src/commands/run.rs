7198a91c59d56f75f4b491d803041dc524a4da87
VolodymyrBg <aqdrgg19@gmail.com>
2025-03-08 21:32:13 +0200

    Update run.rs

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -526,11 +526,9 @@ impl RunCommand {
                 .to_str()
                 .ok_or_else(|| anyhow!("argument is not valid utf-8: {val:?}"))?;
             values.push(match ty {
-                // TODO: integer parsing here should handle hexadecimal notation
-                // like `0x0...`, but the Rust standard library currently only
-                // parses base-10 representations.
-                ValType::I32 => Val::I32(val.parse()?),
-                ValType::I64 => Val::I64(val.parse()?),
+                // Supports both decimal and hexadecimal notation (with 0x prefix)
+                ValType::I32 => Val::I32(val.parse::<i32>().or_else(|_| i32::from_str_radix(&val.trim_start_matches("0x"), 16))?),
+                ValType::I64 => Val::I64(val.parse::<i64>().or_else(|_| i64::from_str_radix(&val.trim_start_matches("0x"), 16))?),
                 ValType::F32 => Val::F32(val.parse::<f32>()?.to_bits()),
                 ValType::F64 => Val::F64(val.parse::<f64>()?.to_bits()),
                 t => bail!("unsupported argument type {:?}", t),
