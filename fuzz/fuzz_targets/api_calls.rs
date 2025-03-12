840f54488696ba23cb2d0f896a9df71f262ec399
Alex Crichton <alex@alexcrichton.com>
2025-03-12 11:57:57 -0500

    Consolidate some fuzzers into one (#10385)
    This commit consolidates a number of fuzzers that we have in Wasmtime
    into a single fuzzer. This follows the pattern of [wasm-tools's `run`
    fuzzer][tools] where the first byte in the fuzz input says which fuzzer
    to run and then the rest of the input is the input to the fuzzer. This
    is intended to improve our time-slicing on OSS-Fuzz where less important
    fuzzers don't hog time from the "important fuzzers" such as
    `differential`. Some minor-ish fuzzers have been folded in to this new
    fuzzer, but we can also always move things around as needed
    
    [tools]: https://github.com/bytecodealliance/wasm-tools/blob/main/fuzz/fuzz_targets/run.rs

--- fuzz/fuzz_targets/api_calls.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use wasmtime_fuzzing::{generators::api::ApiCalls, oracles};
-
-fuzz_target!(|api: ApiCalls| {
-    oracles::make_api_calls(api);
-});
--- fuzz/fuzz_targets/memory_accesses.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use wasmtime_fuzzing::{generators::MemoryAccesses, oracles::memory::check_memory_accesses};
-
-fuzz_target!(|input: MemoryAccesses| {
-    check_memory_accesses(input);
-});
