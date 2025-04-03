8804932194a7e24491584ed399f02bf3f02f5b1c
Alex Crichton <alex@alexcrichton.com>
2025-04-03 08:06:04 -0700

    Bump MSRV to 1.84.0
    Coupled with today's release of 1.86.0

--- crates/wasmtime/build.rs
+++ crates/wasmtime/build.rs
@@ -1,10 +1,7 @@
-use std::process::Command;
 use std::str;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-
-    enable_features_based_on_rustc_version();
 
     // NB: duplicating a workaround in the wasmtime-fiber build script.
     custom_cfg("asan", cfg_is("sanitize", "address"));
@@ -94,24 +91,4 @@ fn build_c_helpers() {
     println!("cargo:rerun-if-changed=src/runtime/vm/helpers.c");
     build.file("src/runtime/vm/helpers.c");
     build.compile("wasmtime-helpers");
-}
-
-fn enable_features_based_on_rustc_version() {
-    // Temporary check to see if the rustc version >= 1.84 in which case
-    // provenance-related pointer APIs are available. This is temporary because
-    // in the future the MSRV of this crate will be beyond 1.84 in which case
-    // this build script can be deleted.
-    let minor = rustc_minor_version().unwrap_or(0);
-    custom_cfg("has_provenance_apis", minor >= 84);
-}
-
-fn rustc_minor_version() -> Option<u32> {
-    let rustc = std::env::var("RUSTC").unwrap();
-    let output = Command::new(rustc).arg("--version").output().ok()?;
-    let version = str::from_utf8(&output.stdout).ok()?;
-    let mut pieces = version.split('.');
-    if pieces.next() != Some("rustc 1") {
-        return None;
-    }
-    pieces.next()?.parse().ok()
 }
