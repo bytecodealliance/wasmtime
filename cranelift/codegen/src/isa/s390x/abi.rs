1e75bc103a169a18c366754258acfe5ce1e2ef99
Alex Crichton <alex@alexcrichton.com>
2025-04-16 16:11:40 -0700

    Fix the new `logged-filetests.rs` test
    This test unfortunately didn't actually have any effect in CI. Locally
    setting `RUST_LOG` and running `cargo run test filetests` showed a panic
    in the s390x backend that this test was not uncovering. The reason that
    this test wasn't actually doing anything on CI is that it's tested
    adjacently with the `wasmtime-cli` crate. Both crates are tested with
    `--all-features` which enables the CLI's `disable-logging` feature which
    meant that all logging statements, including the ones we want to test,
    were compiled out.
    
    This commit fixes the s390x panic and then additionally adds a special
    CI job (repurposing an existing one) for testing just this test in
    isolation.

--- cranelift/codegen/src/isa/s390x/abi.rs
+++ cranelift/codegen/src/isa/s390x/abi.rs
@@ -1036,7 +1036,8 @@ impl S390xMachineDeps {
         // (but after restoring FPRs, which might clobber %r1).
         let temp_dest = match dest {
             CallInstDest::Indirect { reg }
-                if is_reg_saved_in_prologue(call_conv, reg.to_real_reg().unwrap()) =>
+                if reg.to_real_reg().is_some()
+                    && is_reg_saved_in_prologue(call_conv, reg.to_real_reg().unwrap()) =>
             {
                 insts.push(Inst::Mov64 {
                     rd: writable_gpr(1),
