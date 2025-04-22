1819edba1516021697696ce55d44ae891119c5fc
Alex Crichton <alex@alexcrichton.com>
2025-04-22 15:21:01 -0500

    Move C++ Module/Engine types to their own headers (#10642)
    * Move C++ Module/Engine types to their own headers
    
    Add some #ifdef guards for Wasmtime features as well as some small tests.
    
    * Remove trailing newline

---
+++ crates/c-api/tests/module.cc
@@ -0,0 +1,17 @@
+#include <wasmtime/module.hh>
+
+#include <gtest/gtest.h>
+
+using namespace wasmtime;
+
+TEST(Module, Simple) {
+  Engine engine;
+  Module m = Module::compile(engine, "(module)").unwrap();
+  auto wasm = wat2wasm("(module)").unwrap();
+  Module::compile(engine, wasm).unwrap();
+  Module::validate(engine, wasm).unwrap();
+
+
+  auto serialized = m.serialize().unwrap();
+  Module::deserialize(engine, serialized).unwrap();
+}
