ff087c283b4a7b860c846324f7f23a2a2e53be7b
MangoPeachGrape <191630121+MangoPeachGrape@users.noreply.github.com>
2025-04-28 22:06:08 +0300

    Remove doc link?

--- crates/c-api/include/wasmtime/component/func.h
+++ crates/c-api/include/wasmtime/component/func.h
@@ -12,10 +12,10 @@ extern "C" {
 /// \brief Representation of a function in Wasmtime.
 ///
 /// Functions in Wasmtime are represented as an index into a store and don't
-/// have any data or destructor associated with the #wasmtime_component_func_t
-/// value. Functions cannot interoperate between #wasmtime_store_t instances and
-/// if the wrong function is passed to the wrong store then it may trigger an
-/// assertion to abort the process.
+/// have any data or destructor associated with the value. Functions cannot
+/// interoperate between #wasmtime_store_t instances and if the wrong function
+/// is passed to the wrong store then it may trigger an assertion to abort the
+/// process.
 typedef struct wasmtime_component_func {
   /// Internal identifier of what store this belongs to, never zero.
   uint64_t store_id;
