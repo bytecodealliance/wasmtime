956a17b96b62d6eb53dfbc7e5a5b21bf4743c5ea
Andrew Brown <andrew.brown@intel.com>
2025-05-02 10:54:29 -0700

    winch: move reason for ignoring dead code
    This removes a clippy warning when running:
    
    ```
    RUSTFLAGS='-D warnings' cargo clippy --workspace --exclude test-programs
    ```

--- winch/codegen/src/lib.rs
+++ winch/codegen/src/lib.rs
@@ -1,10 +1,14 @@
 //! Code generation library for Winch.
 
 // Unless this library is compiled with `all-arch`, the rust compiler
-// is going to emit dead code warnings. This directive is fine as long
-// as we configure to run CI at least once with the `all-arch` feature
-// enabled.
-#![cfg_attr(not(feature = "all-arch"), allow(dead_code))]
+// is going to emit dead code warnings.
+#![cfg_attr(
+    not(feature = "all-arch"),
+    allow(
+        dead_code,
+        reason = "this is fine as long as we run CI at least once with the `all-arch` feature enabled"
+    )
+)]
 
 mod abi;
 pub use codegen::{BuiltinFunctions, FuncEnv};
