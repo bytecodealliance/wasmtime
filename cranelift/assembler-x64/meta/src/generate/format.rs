b6af9aaae97d3ef58b1bafcaa9ef02558b557e24
Andrew Brown <andrew.brown@intel.com>
2025-05-07 14:40:51 -0700

    asm: refactor how REX suffixes are emitted
    This change simplifies the generated code for encoding REX suffixes
    (ModR/M byte, SIB byte, displacement) but does not alter the underlying
    logic. As a part of this refactoring, the displacement struct is now
    appropriately named `Disp` and several public functions no longer need
    to be so.

--- cranelift/assembler-x64/meta/src/generate/format.rs
+++ cranelift/assembler-x64/meta/src/generate/format.rs
@@ -136,64 +136,18 @@ impl dsl::Format {
             [FixedReg(_), Imm(_)] => {
                 // No need to emit a ModRM byte: we know the register used.
             }
-            [Mem(dst), Imm(_)] => {
-                let digit = rex
-                    .digit
-                    .expect("REX digit must be set for operands: [RegMem, Imm]");
+            [Mem(mem), Imm(_)] | [RegMem(mem), Imm(_)] => {
+                let digit = rex.digit.unwrap();
                 fmtln!(f, "let digit = 0x{digit:x};");
-                fmtln!(
-                    f,
-                    "emit_modrm_sib_disp(buf, off, digit, &self.{dst}, 0, None);"
-                );
+                fmtln!(f, "self.{mem}.encode_rex_suffixes(buf, off, digit, 0);");
             }
-            [RegMem(dst), Imm(_)] => {
-                let digit = rex
-                    .digit
-                    .expect("REX digit must be set for operands: [RegMem, Imm]");
-                fmtln!(f, "let digit = 0x{digit:x};");
-                f.add_block(&format!("match &self.{dst}"), |f| {
-                    fmtln!(f, "GprMem::Gpr({dst}) => emit_modrm(buf, digit, {dst}.enc()),");
-                    fmtln!(f, "GprMem::Mem({dst}) => emit_modrm_sib_disp(buf, off, digit, {dst}, 0, None),");
-                });
-            }
-            [Reg(dst), RegMem(src)] => {
-                fmtln!(f, "let {dst} = self.{dst}.enc();");
-                f.add_block(&format!("match &self.{src}"), |f| {
-                    match dst.bits() {
-                        128 => {
-                            fmtln!(f, "XmmMem::Xmm({src}) => emit_modrm(buf, {dst}, {src}.enc()),");
-                            fmtln!(f, "XmmMem::Mem({src}) => emit_modrm_sib_disp(buf, off, {dst}, {src}, 0, None),");
-                        }
-                        _ => {
-                            fmtln!(f, "GprMem::Gpr({src}) => emit_modrm(buf, {dst}, {src}.enc()),");
-                            fmtln!(f, "GprMem::Mem({src}) => emit_modrm_sib_disp(buf, off, {dst}, {src}, 0, None),");
-                        }
-                    };
-                });
-            }
-            [Mem(dst), Reg(src)] => {
-                fmtln!(f, "let {src} = self.{src}.enc();");
-                fmtln!(
-                    f,
-                    "emit_modrm_sib_disp(buf, off, {src}, &self.{dst}, 0, None);"
-                );
-            }
-            [RegMem(dst), Reg(src)]
-            | [RegMem(dst), Reg(src), Imm(_)]
-            | [RegMem(dst), Reg(src), FixedReg(_)] => {
-                fmtln!(f, "let {src} = self.{src}.enc();");
-                f.add_block(&format!("match &self.{dst}"), |f| {
-                    match src.bits() {
-                        128 => {
-                            fmtln!(f, "XmmMem::Xmm({dst}) => emit_modrm(buf, {src}, {dst}.enc()),");
-                            fmtln!(f, "XmmMem::Mem({dst}) => emit_modrm_sib_disp(buf, off, {src}, {dst}, 0, None),");
-                        }
-                        _ => {
-                            fmtln!(f, "GprMem::Gpr({dst}) => emit_modrm(buf, {src}, {dst}.enc()),");
-                            fmtln!(f, "GprMem::Mem({dst}) => emit_modrm_sib_disp(buf, off, {src}, {dst}, 0, None),");
-                        }
-                    };
-                });
+            [Reg(reg), RegMem(mem)]
+            | [Mem(mem), Reg(reg)]
+            | [RegMem(mem), Reg(reg)]
+            | [RegMem(mem), Reg(reg), Imm(_)]
+            | [RegMem(mem), Reg(reg), FixedReg(_)] => {
+                fmtln!(f, "let reg = self.{reg}.enc();");
+                fmtln!(f, "self.{mem}.encode_rex_suffixes(buf, off, reg, 0);");
             }
             unknown => unimplemented!("unknown pattern: {unknown:?}"),
         }
