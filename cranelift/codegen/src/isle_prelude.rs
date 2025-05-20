bytecodealliance/wasmtime

commit 7ea7ebcc9a7d6a8bb0fd47b124094b0eb0605706

x64: convert all multiplication-related instructions (#10782)
* asm: add SSE4.1 feature

* asm: refactor format constructors with expanding structs

* asm: add implicit operands

Implicit operands are used by an instruction but not present in its
disassembled output. Instructions like `mul`, e.g., will write to the
`%rax` and `%rdx` registers, but this is all invisible in disassembly.
Implicit operands are always fixed (i.e., the register is known), but
not all fixed operands are implicit (i.e., some fixed registers _are_
disassembled).

* asm: add `mul*` instructions, vector and scalar

* asm: generate ISLE for multi-return constructors--`AssemblerOutputs::RetValueRegs`

Certain `mul*` instructions write to multiple registers. For register
allocation, Cranelift needs to know about all of these registers. This
change uses the pre-existing pattern of returning a `ValueRegs` type to
indicate this. This change is limited to what is needed now: the only
multi-return needed now uses two fixed registers.

* x64: convert SSE multiplications

* x64: convert scalar multiplications

* x64: remove old `Inst::Mul*` variants

* winch: convert multiplications

This does not include any special "small immediate resizing" rules for
Winch, so the Winch disassembly tests gain a few bytes (e.g., some
immediates that _could_ fit in 8 bits are emitted as the full 32 bits).

* refactor: move `i32`-related extractors to `prelude.isle`

* review: print the implicit registers as well

* fix: the instruction size has changed (again)

cranelift/codegen/src/isa/x64/inst.isle
@@ -62,15 +62,6 @@
              (dividend Gpr)
              (dst WritableGpr))
 
-       ;; Unsigned multiplication producing the high bits of the result in one
-       ;; register and the low bits in another register.
-       (Mul (size OperandSize)
-            (signed bool)
-            (src1 Gpr)
-            (src2 GprMem)
-            (dst_lo WritableGpr)
-            (dst_hi WritableGpr))
-
        ;; Same as `Mul`, but for the BMI2 `mulx` instruction. This is different
        ;; where the two `dst_*` registers can be arbitrary registers and it
        ;; is always unsigned multiplication. Note that this instruction does
@@ -85,26 +76,6 @@
              (src2 GprMem)
              (dst_lo WritableGpr)
              (dst_hi WritableGpr))
-
-       ;; Same as `Mul` but the 16-bit multiplication result is stored in `AX`.
-       (Mul8 (signed bool)
-             (src1 Gpr)
-             (src2 GprMem)
-             (dst WritableGpr))
-
-       ;; The two-operand form of `imul` which produces a truncated same-size
-       ;; result as the operands.
-       (IMul (size OperandSize)
-             (src1 Gpr)
-             (src2 GprMem)
-             (dst WritableGpr))
-
-       ;; The three-operand form of `imul` where the third operand must be
-       ;; a constant.
-       (IMulImm (size OperandSize)
-                (src1 GprMem)
-                (src2 i32)
-                (dst WritableGpr))
 
        ;; A synthetic instruction sequence used as part of the lowering of the
        ;; `srem` instruction which returns 0 if the divisor is -1 and
@@ -2693,6 +2664,8 @@
 (decl asm_produce_flags (AssemblerOutputs) ProducesFlags)
 (rule (asm_produce_flags (AssemblerOutputs.RetGpr inst gpr))
       (ProducesFlags.ProducesFlagsReturnsResultWithConsumer inst gpr))
+(rule (asm_produce_flags (AssemblerOutputs.RetValueRegs inst regs))
+      (ProducesFlags.ProducesFlagsReturnsResultWithConsumer inst (value_regs_get_gpr regs 0)))
 
 ;; Other operations consume _and_ produce flags--"chaining".
 (type ChainFlagsOp (enum (Adc) (Sbb)))
@@ -2883,15 +2856,18 @@
 
 
 ;; Helper for creating `mul` instructions or `imul` instructions (depending
-;; on `signed`)
-(decl x64_mul (Type bool Gpr GprMem) ValueRegs)
-(rule (x64_mul ty signed src1 src2)
-      (let ((dst_lo WritableGpr (temp_writable_gpr))
-            (dst_hi WritableGpr (temp_writable_gpr))
-            (size OperandSize (raw_operand_size_of_type ty))
-            (_ Unit (emit (MInst.Mul size signed src1 src2 dst_lo dst_hi))))
-        (value_gprs dst_lo dst_hi)))
+;; on `signed`). For the 8-bit rules, see `x64_mul8`.
+(decl x64_mul_raw (Type bool Gpr GprMem) AssemblerOutputs)
+(rule (x64_mul_raw $I16 false src1 src2) (x64_mulw_m_raw src1 src2))
+(rule (x64_mul_raw $I32 false src1 src2) (x64_mull_m_raw src1 src2))
+(rule (x64_mul_raw $I64 false src1 src2) (x64_mulq_m_raw src1 src2))
+(rule (x64_mul_raw $I16 true src1 src2)  (x64_imulw_m_raw src1 src2))
+(rule (x64_mul_raw $I32 true src1 src2)  (x64_imull_m_raw src1 src2))
+(rule (x64_mul_raw $I64 true src1 src2)  (x64_imulq_m_raw src1 src2))
 
+(decl x64_mul (Type bool Gpr GprMem) ValueRegs)
+(rule 0 (x64_mul ty signed src1 src2)
+      (emit_ret_value_regs (x64_mul_raw ty signed src1 src2)))
 ;; Special case the `mulx` pattern with the BMI2 instruction set.
 (rule 1 (x64_mul (ty_32_or_64 ty) false src1 src2)
   (if-let true (use_bmi2))
@@ -2908,49 +2884,43 @@
         (_ Unit (emit (MInst.MulX size src1 src2 (writable_invalid_gpr) dst))))
     dst))
 
+(decl x64_mul_lo_with_flags_paired (Type bool Gpr GprMem) ProducesFlags)
+(rule (x64_mul_lo_with_flags_paired ty signed src1 src2)
+      (asm_produce_flags (x64_mul_raw ty signed src1 src2)))
+
 ;; Get the invalid register as writable
 (decl writable_invalid_gpr () WritableGpr)
 (extern constructor writable_invalid_gpr writable_invalid_gpr)
 
-;; Helper for creating `mul` instructions or `imul` instructions (depending
-;; on `signed`) for 8-bit operands.
-(decl x64_mul8 (bool Gpr GprMem) Gpr)
-(rule (x64_mul8 signed src1 src2)
-      (let ((dst WritableGpr (temp_writable_gpr))
-            (_ Unit (emit (MInst.Mul8 signed src1 src2 dst))))
-        dst))
-
 ;; Helper for creating `imul` instructions.
 (decl x64_imul (Type Gpr GprMem) Gpr)
-(rule (x64_imul ty src1 src2)
-      (let ((dst WritableGpr (temp_writable_gpr))
-            (size OperandSize (raw_operand_size_of_type ty))
-            (_ Unit (emit (MInst.IMul size src1 src2 dst))))
-        dst))
+(rule (x64_imul $I16 src1 src2) (x64_imulw_rm src1 src2))
+(rule (x64_imul $I32 src1 src2) (x64_imull_rm src1 src2))
+(rule (x64_imul $I64 src1 src2) (x64_imulq_rm src1 src2))
 
-;; Helper for creating `imul` instructions with an immediate operand.
+;; Helper for creating `imul` instructions with an immediate operand. Match
+;; 8-bit immediates first to allow a smaller instruction encoding.
 (decl x64_imul_imm (Type GprMem i32) Gpr)
-(rule (x64_imul_imm ty src1 src2)
-      (let ((dst WritableGpr (temp_writable_gpr))
-            (size OperandSize (raw_operand_size_of_type ty))
-            (_ Unit (emit (MInst.IMulImm size src1 src2 dst))))
-        dst))
+(rule 2 (x64_imul_imm $I16 src1 (i8_try_from_i32 src2))  (x64_imulw_rmi_sxb src1 src2))
+(rule 2 (x64_imul_imm $I32 src1 (i8_try_from_i32 src2))  (x64_imull_rmi_sxb src1 src2))
+(rule 2 (x64_imul_imm $I64 src1 (i8_try_from_i32 src2))  (x64_imulq_rmi_sxb src1 src2))
+(rule 1 (x64_imul_imm $I16 src1 (u16_try_from_i32 src2)) (x64_imulw_rmi src1 src2))
+(rule 1 (x64_imul_imm $I32 src1 (i32_as_u32 src2))       (x64_imull_rmi src1 src2))
+(rule 1 (x64_imul_imm $I64 src1 src2)                    (x64_imulq_rmi_sxl src1 src2))
+
+;; Helper for creating `mul` instructions or `imul` instructions (depending
+;; on `signed`) for 8-bit operands.
+(decl x64_mul8_raw (bool Gpr GprMem) AssemblerOutputs)
+(rule (x64_mul8_raw false src1 src2) (x64_mulb_m_raw src1 src2))
+(rule (x64_mul8_raw true src1 src2)  (x64_imulb_m_raw src1 src2))
+
+(decl x64_mul8 (bool Gpr GprMem) Gpr)
+(rule (x64_mul8 signed src1 src2)
+      (emit_ret_gpr (x64_mul8_raw signed src1 src2)))
 
 (decl x64_mul8_with_flags_paired (bool Gpr GprMem) ProducesFlags)
 (rule (x64_mul8_with_flags_paired signed src1 src2)
-      (let ((dst WritableGpr (temp_writable_gpr)))
-           (ProducesFlags.ProducesFlagsReturnsResultWithConsumer
-                 (MInst.Mul8 signed src1 src2 dst)
-                 dst)))
-
-(decl x64_mul_lo_with_flags_paired (Type bool Gpr GprMem) ProducesFlags)
-(rule (x64_mul_lo_with_flags_paired ty signed src1 src2)
-      (let ((dst_lo WritableGpr (temp_writable_gpr))
-            (dst_hi WritableGpr (temp_writable_gpr))
-            (size OperandSize (raw_operand_size_of_type ty)))
-           (ProducesFlags.ProducesFlagsReturnsResultWithConsumer
-                 (MInst.Mul size signed src1 src2 dst_lo dst_hi)
-                 dst_lo)))
+      (asm_produce_flags (x64_mul8_raw signed src1 src2)))
 
 
 
@@ -3587,59 +3557,52 @@
 
 ;; Helper for creating `pmullw` instructions.
 (decl x64_pmullw (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmullw src1 src2)
-      (xmm_rm_r (SseOpcode.Pmullw) src1 src2))
 (rule 1 (x64_pmullw src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmullw) src1 src2))
+(rule 0 (x64_pmullw src1 src2) (x64_pmullw_a src1 src2))
 
 ;; Helper for creating `pmulld` instructions.
 (decl x64_pmulld (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmulld src1 src2)
-      (xmm_rm_r (SseOpcode.Pmulld) src1 src2))
 (rule 1 (x64_pmulld src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmulld) src1 src2))
+(rule 0 (x64_pmulld src1 src2) (x64_pmulld_a src1 src2))
 
 ;; Helper for creating `pmulhw` instructions.
 (decl x64_pmulhw (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmulhw src1 src2)
-      (xmm_rm_r (SseOpcode.Pmulhw) src1 src2))
 (rule 1 (x64_pmulhw src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmulhw) src1 src2))
+(rule 0 (x64_pmulhw src1 src2) (x64_pmulhw_a src1 src2))
 
 ;; Helper for creating `pmulhrsw` instructions.
 (decl x64_pmulhrsw (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmulhrsw src1 src2)
-      (xmm_rm_r (SseOpcode.Pmulhrsw) src1 src2))
 (rule 1 (x64_pmulhrsw src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmulhrsw) src1 src2))
+(rule 0 (x64_pmulhrsw src1 src2) (x64_pmulhrsw_a src1 src2))
 
 ;; Helper for creating `pmulhuw` instructions.
 (decl x64_pmulhuw (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmulhuw src1 src2)
-      (xmm_rm_r (SseOpcode.Pmulhuw) src1 src2))
 (rule 1 (x64_pmulhuw src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmulhuw) src1 src2))
+(rule 0 (x64_pmulhuw src1 src2) (x64_pmulhuw_a src1 src2))
 
 ;; Helper for creating `pmuldq` instructions.
 (decl x64_pmuldq (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmuldq src1 src2)
-      (xmm_rm_r (SseOpcode.Pmuldq) src1 src2))
 (rule 1 (x64_pmuldq src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmuldq) src1 src2))
+(rule 0 (x64_pmuldq src1 src2) (x64_pmuldq_a src1 src2))
 
 ;; Helper for creating `pmuludq` instructions.
 (decl x64_pmuludq (Xmm XmmMem) Xmm)
-(rule 0 (x64_pmuludq src1 src2)
-      (xmm_rm_r (SseOpcode.Pmuludq) src1 src2))
 (rule 1 (x64_pmuludq src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vpmuludq) src1 src2))
+(rule 0 (x64_pmuludq src1 src2) (x64_pmuludq_a src1 src2))
 
 ;; Helper for creating `punpckhwd` instructions.
 (decl x64_punpckhwd (Xmm XmmMem) Xmm)
@@ -3792,35 +3755,31 @@
 
 ;; Helper for creating `mulss` instructions.
 (decl x64_mulss (Xmm XmmMem) Xmm)
-(rule (x64_mulss src1 src2)
-      (xmm_rm_r_unaligned (SseOpcode.Mulss) src1 src2))
 (rule 1 (x64_mulss src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vmulss) src1 src2))
+(rule 0 (x64_mulss src1 src2) (x64_mulss_a src1 src2))
 
 ;; Helper for creating `mulsd` instructions.
 (decl x64_mulsd (Xmm XmmMem) Xmm)
-(rule (x64_mulsd src1 src2)
-      (xmm_rm_r_unaligned (SseOpcode.Mulsd) src1 src2))
 (rule 1 (x64_mulsd src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vmulsd) src1 src2))
+(rule 0 (x64_mulsd src1 src2) (x64_mulsd_a src1 src2))
 
 ;; Helper for creating `mulps` instructions.
 (decl x64_mulps (Xmm XmmMem) Xmm)
-(rule 0 (x64_mulps src1 src2)
-      (xmm_rm_r (SseOpcode.Mulps) src1 src2))
 (rule 1 (x64_mulps src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vmulps) src1 src2))
+(rule 0 (x64_mulps src1 src2) (x64_mulps_a src1 src2))
 
 ;; Helper for creating `mulpd` instructions.
 (decl x64_mulpd (Xmm XmmMem) Xmm)
-(rule (x64_mulpd src1 src2)
-      (xmm_rm_r (SseOpcode.Mulpd) src1 src2))
 (rule 1 (x64_mulpd src1 src2)
       (if-let true (use_avx))
       (xmm_rmir_vex (AvxOpcode.Vmulpd) src1 src2))
+(rule 0 (x64_mulpd src1 src2) (x64_mulpd_a src1 src2))
 
 ;; Helper for creating `divss` instructions.
 (decl x64_divss (Xmm XmmMem) Xmm)

cranelift/codegen/src/isle_prelude.rs
@@ -59,6 +59,11 @@ macro_rules! isle_common_prelude_methods {
         }
 
         #[inline]
+        fn i32_as_u32(&mut self, x: i32) -> Option<u32> {
+            Some(x as u32)
+        }
+
+        #[inline]
         fn i32_as_i64(&mut self, x: i32) -> i64 {
             x.into()
         }
@@ -933,12 +938,20 @@ macro_rules! isle_common_prelude_methods {
             u64::try_from(val).ok()
         }
 
+        fn u16_try_from_i32(&mut self, val: i32) -> Option<u16> {
+            u16::try_from(val).ok()
+        }
+
         fn u16_try_from_u64(&mut self, val: u64) -> Option<u16> {
             u16::try_from(val).ok()
         }
 
         fn u32_try_from_u64(&mut self, val: u64) -> Option<u32> {
             u32::try_from(val).ok()
+        }
+
+        fn i8_try_from_i32(&mut self, val: i32) -> Option<i8> {
+            i8::try_from(val).ok()
         }
 
         fn i8_try_from_u64(&mut self, val: u64) -> Option<i8> {

cranelift/codegen/src/prelude.isle
@@ -124,6 +124,9 @@
 (decl pure partial u8_try_from_i32 (i32) u8)
 (extern constructor u8_try_from_i32 u8_try_from_i32)
 
+(decl u16_try_from_i32 (u16) i32)
+(extern extractor u16_try_from_i32 u16_try_from_i32)
+
 (decl pure partial u16_try_from_u64 (u64) u16)
 (extern constructor u16_try_from_u64 u16_try_from_u64)
 
@@ -132,6 +135,9 @@
 
 (decl pure partial u64_try_from_i64 (i64) u64)
 (extern constructor u64_try_from_i64 u64_try_from_i64)
+
+(decl i8_try_from_i32 (i8) i32)
+(extern extractor i8_try_from_i32 i8_try_from_i32)
 
 (decl pure partial i8_try_from_u64 (u64) i8)
 (extern constructor i8_try_from_u64 i8_try_from_u64)
@@ -145,6 +151,9 @@
 (decl pure u32_as_u64 (u32) u64)
 (extern constructor u32_as_u64 u32_as_u64)
 (convert u32 u64 u32_as_u64)
+
+(decl i32_as_u32 (u32) i32)
+(extern extractor i32_as_u32 i32_as_u32)
 
 (decl pure i32_as_i64 (i32) i64)
 (extern constructor i32_as_i64 i32_as_i64)

cranelift/assembler-x64/meta/src/dsl.rs
@@ -13,8 +13,8 @@ pub use encoding::{
 };
 pub use encoding::{rex, vex};
 pub use features::{ALL_FEATURES, Feature, Features};
-pub use format::{Extension, Format, Location, Mutability, Operand, OperandKind};
-pub use format::{align, fmt, r, rw, sxl, sxq, sxw, w};
+pub use format::{Extension, Format, Location, Mutability, Operand, OperandKind, RegClass};
+pub use format::{align, fmt, implicit, r, rw, sxl, sxq, sxw, w};
 
 /// Abbreviated constructor for an x64 instruction.
 pub fn inst(

cranelift/assembler-x64/meta/src/dsl/features.rs
@@ -65,6 +65,7 @@ pub enum Feature {
     sse,
     sse2,
     ssse3,
+    sse41,
 }
 
 /// List all CPU features.
@@ -80,6 +81,7 @@ pub const ALL_FEATURES: &[Feature] = &[
     Feature::sse,
     Feature::sse2,
     Feature::ssse3,
+    Feature::sse41,
 ];
 
 impl fmt::Display for Feature {
@@ -90,6 +92,7 @@ impl fmt::Display for Feature {
             Feature::sse => write!(f, "sse"),
             Feature::sse2 => write!(f, "sse2"),
             Feature::ssse3 => write!(f, "ssse3"),
+            Feature::sse41 => write!(f, "sse41"),
         }
     }
 }

cranelift/assembler-x64/meta/src/dsl/format.rs
@@ -34,13 +34,12 @@ pub fn fmt(
 /// This function panics if the location is an immediate (i.e., an immediate
 /// cannot be written to).
 #[must_use]
-pub fn rw(location: Location) -> Operand {
-    assert!(!matches!(location.kind(), OperandKind::Imm(_)));
+pub fn rw(op: impl Into<Operand>) -> Operand {
+    let op = op.into();
+    assert!(!matches!(op.location.kind(), OperandKind::Imm(_)));
     Operand {
-        location,
         mutability: Mutability::ReadWrite,
-        extension: Extension::default(),
-        align: false,
+        ..op
     }
 }
 
@@ -54,12 +53,11 @@ pub fn r(op: impl Into<Operand>) -> Operand {
 
 /// An abbreviated constructor for a "write" operand.
 #[must_use]
-pub fn w(location: Location) -> Operand {
+pub fn w(op: impl Into<Operand>) -> Operand {
+    let op = op.into();
     Operand {
-        location,
         mutability: Mutability::Write,
-        extension: Extension::None,
-        align: false,
+        ..op
     }
 }
 
@@ -67,10 +65,18 @@ pub fn w(location: Location) -> Operand {
 pub fn align(location: Location) -> Operand {
     assert!(location.uses_memory());
     Operand {
-        location,
-        mutability: Mutability::Read,
-        extension: Extension::None,
         align: true,
+        ..Operand::from(location)
+    }
+}
+
+/// An abbreviated constructor for an operand that is used by the instruction
+/// but not visible in its disassembly.
+pub fn implicit(location: Location) -> Operand {
+    assert!(matches!(location.kind(), OperandKind::FixedReg(_)));
+    Operand {
+        implicit: true,
+        ..Operand::from(location)
     }
 }
 
@@ -84,10 +90,8 @@ pub fn align(location: Location) -> Operand {
 pub fn sxq(location: Location) -> Operand {
     assert!(location.bits() <= 64);
     Operand {
-        location,
-        mutability: Mutability::Read,
         extension: Extension::SignExtendQuad,
-        align: false,
+        ..Operand::from(location)
     }
 }
 
@@ -101,10 +105,8 @@ pub fn sxq(location: Location) -> Operand {
 pub fn sxl(location: Location) -> Operand {
     assert!(location.bits() <= 32);
     Operand {
-        location,
-        mutability: Mutability::Read,
         extension: Extension::SignExtendLong,
-        align: false,
+        ..Operand::from(location)
     }
 }
 
@@ -118,10 +120,8 @@ pub fn sxl(location: Location) -> Operand {
 pub fn sxw(location: Location) -> Operand {
     assert!(location.bits() <= 16);
     Operand {
-        location,
-        mutability: Mutability::Read,
         extension: Extension::SignExtendWord,
-        align: false,
+        ..Operand::from(location)
     }
 }
 
@@ -204,6 +204,9 @@ pub struct Operand {
     /// address used in the operand must align to the size of the operand (e.g.,
     /// `m128` must be 16-byte aligned).
     pub align: bool,
+    /// Some register operands are implicit: that is, they do not appear in the
+    /// disassembled output even though they are used in the instruction.
+    pub implicit: bool,
 }
 
 impl core::fmt::Display for Operand {
@@ -213,6 +216,7 @@ impl core::fmt::Display for Operand {
             mutability,
             extension,
             align,
+            implicit,
         } = self;
         write!(f, "{location}")?;
         let mut flags = vec![];
@@ -224,6 +228,9 @@ impl core::fmt::Display for Operand {
         }
         if *align != false {
             flags.push("align".to_owned());
+        }
+        if *implicit {
+            flags.push("implicit".to_owned());
         }
         if !flags.is_empty() {
             write!(f, "[{}]", flags.join(","))?;
@@ -237,11 +244,13 @@ impl From<Location> for Operand {
         let mutability = Mutability::default();
         let extension = Extension::default();
         let align = false;
+        let implicit = false;
         Self {
             location,
             mutability,
             extension,
             align,
+            implicit,
         }
     }
 }
@@ -270,6 +279,9 @@ pub enum Location {
     ax,
     eax,
     rax,
+    dx,
+    edx,
+    rdx,
     cl,
 
     // Immediate values.
@@ -307,9 +319,9 @@ impl Location {
         use Location::*;
         match self {
             al | cl | imm8 | r8 | rm8 | m8 => 8,
-            ax | imm16 | r16 | rm16 | m16 => 16,
-            eax | imm32 | r32 | rm32 | m32 | xmm_m32 => 32,
-            rax | r64 | rm64 | m64 | xmm_m64 => 64,
+            ax | dx | imm16 | r16 | rm16 | m16 => 16,
+            eax | edx | imm32 | r32 | rm32 | m32 | xmm_m32 => 32,
+            rax | rdx | r64 | rm64 | m64 | xmm_m64 => 64,
             xmm | xmm_m128 => 128,
         }
     }
@@ -325,7 +337,8 @@ impl Location {
     pub fn uses_memory(&self) -> bool {
         use Location::*;
         match self {
-            al | cl | ax | eax | rax | imm8 | imm16 | imm32 | r8 | r16 | r32 | r64 | xmm => false,
+            al | ax | eax | rax | cl | dx | edx | rdx | imm8 | imm16 | imm32 | r8 | r16 | r32
+            | r64 | xmm => false,
             rm8 | rm16 | rm32 | rm64 | xmm_m32 | xmm_m64 | xmm_m128 | m8 | m16 | m32 | m64 => true,
         }
     }
@@ -337,8 +350,8 @@ impl Location {
         use Location::*;
         match self {
             imm8 | imm16 | imm32 => false,
-            al | ax | eax | rax | cl | r8 | r16 | r32 | r64 | rm8 | rm16 | rm32 | rm64 | xmm
-            | xmm_m32 | xmm_m64 | xmm_m128 | m8 | m16 | m32 | m64 => true,
+            al | ax | eax | rax | cl | dx | edx | rdx | r8 | r16 | r32 | r64 | rm8 | rm16
+            | rm32 | rm64 | xmm | xmm_m32 | xmm_m64 | xmm_m128 | m8 | m16 | m32 | m64 => true,
         }
     }
 
@@ -347,7 +360,7 @@ impl Location {
     pub fn kind(&self) -> OperandKind {
         use Location::*;
         match self {
-            al | ax | eax | rax | cl => OperandKind::FixedReg(*self),
+            al | ax | eax | rax | cl | dx | edx | rdx => OperandKind::FixedReg(*self),
             imm8 | imm16 | imm32 => OperandKind::Imm(*self),
             r8 | r16 | r32 | r64 | xmm => OperandKind::Reg(*self),
             rm8 | rm16 | rm32 | rm64 | xmm_m32 | xmm_m64 | xmm_m128 => OperandKind::RegMem(*self),
@@ -364,9 +377,8 @@ impl Location {
         use Location::*;
         match self {
             imm8 | imm16 | imm32 | m8 | m16 | m32 | m64 => None,
-            al | ax | eax | rax | cl | r8 | r16 | r32 | r64 | rm8 | rm16 | rm32 | rm64 => {
-                Some(RegClass::Gpr)
-            }
+            al | ax | eax | rax | cl | dx | edx | rdx | r8 | r16 | r32 | r64 | rm8 | rm16
+            | rm32 | rm64 => Some(RegClass::Gpr),
             xmm | xmm_m32 | xmm_m64 | xmm_m128 => Some(RegClass::Xmm),
         }
     }
@@ -385,6 +397,9 @@ impl core::fmt::Display for Location {
             eax => write!(f, "eax"),
             rax => write!(f, "rax"),
             cl => write!(f, "cl"),
+            dx => write!(f, "dx"),
+            edx => write!(f, "edx"),
+            rdx => write!(f, "rdx"),
 
             r8 => write!(f, "r8"),
             r16 => write!(f, "r16"),

cranelift/assembler-x64/meta/src/generate/format.rs
@@ -14,17 +14,33 @@ impl dsl::Format {
     /// once Cranelift has switched to using this assembler predominantly
     /// (TODO).
     #[must_use]
-    pub fn generate_att_style_operands(&self) -> String {
+    pub(crate) fn generate_att_style_operands(&self) -> String {
         let ordered_ops: Vec<_> = self
             .operands
             .iter()
+            .filter(|o| !o.implicit)
             .rev()
             .map(|o| format!("{{{}}}", o.location))
             .collect();
         ordered_ops.join(", ")
     }
 
-    pub fn generate_rex_encoding(&self, f: &mut Formatter, rex: &dsl::Rex) {
+    #[must_use]
+    pub(crate) fn generate_implicit_operands(&self) -> String {
+        let ops: Vec<_> = self
+            .operands
+            .iter()
+            .filter(|o| o.implicit)
+            .map(|o| format!("{{{}}}", o.location))
+            .collect();
+        if ops.is_empty() {
+            String::new()
+        } else {
+            format!(" ;; implicit: {}", ops.join(", "))
+        }
+    }
+
+    pub(crate) fn generate_rex_encoding(&self, f: &mut Formatter, rex: &dsl::Rex) {
         self.generate_prefixes(f, rex);
         self.generate_rex_prefix(f, rex);
         self.generate_opcodes(f, rex);
@@ -91,12 +107,17 @@ impl dsl::Format {
                 fmtln!(f, "let dst = self.{dst}.enc();");
                 fmtln!(f, "let rex = RexPrefix::two_op(digit, dst, {bits});");
             }
+            [FixedReg(_), RegMem(mem)] | [FixedReg(_), FixedReg(_), RegMem(mem)] => {
+                let digit = rex.digit.unwrap();
+                fmtln!(f, "let digit = 0x{digit:x};");
+                fmtln!(f, "let rex = self.{mem}.as_rex_prefix(digit, {bits});");
+            }
             [Mem(dst), Imm(_)] | [RegMem(dst), Imm(_)] | [RegMem(dst)] => {
                 let digit = rex.digit.unwrap();
                 fmtln!(f, "let digit = 0x{digit:x};");
                 fmtln!(f, "let rex = self.{dst}.as_rex_prefix(digit, {bits});");
             }
-            [Reg(dst), RegMem(src)] => {
+            [Reg(dst), RegMem(src)] | [Reg(dst), RegMem(src), Imm(_)] => {
                 fmtln!(f, "let dst = self.{dst}.enc();");
                 fmtln!(f, "let rex = self.{src}.as_rex_prefix(dst, {bits});");
             }
@@ -135,12 +156,17 @@ impl dsl::Format {
                 fmtln!(f, "let digit = 0x{digit:x};");
                 fmtln!(f, "self.{reg}.encode_modrm(buf, digit);");
             }
-            [Mem(mem), Imm(_)] | [RegMem(mem), Imm(_)] | [RegMem(mem)] => {
+            [Mem(mem), Imm(_)]
+            | [RegMem(mem), Imm(_)]
+            | [RegMem(mem)]
+            | [FixedReg(_), RegMem(mem)]
+            | [FixedReg(_), FixedReg(_), RegMem(mem)] => {
                 let digit = rex.digit.unwrap();
                 fmtln!(f, "let digit = 0x{digit:x};");
                 fmtln!(f, "self.{mem}.encode_rex_suffixes(buf, off, digit, 0);");
             }
             [Reg(reg), RegMem(mem)]
+            | [Reg(reg), RegMem(mem), Imm(_)]
             | [Mem(mem), Reg(reg)]
             | [RegMem(mem), Reg(reg)]
             | [RegMem(mem), Reg(reg), Imm(_)]

commit b226c23094a2ce7276f55ca53c0f209d8b32668d

x64: Fix panic compiling 16-bit multiply-with-immediate
This commit fixes a minor regression from #10782 found via fuzzing. The
regression is 16-bit immediates were forced to fit from an `i32` value
into a `u16` for 16-bit multiplication. This meant though that negative
numbers failed this conversion which meant that ISLE would panic due to
the value not being matched. This fixes the logic to first fit the i32
into an i16 and then cast that to a u16 where the first phase should hit
all the constants that are possible in Cranelift.

cranelift/codegen/src/isa/x64/inst.isle
@@ -2884,9 +2884,9 @@
 (rule 2 (x64_imul_imm $I16 src1 (i8_try_from_i32 src2))  (x64_imulw_rmi_sxb src1 src2))
 (rule 2 (x64_imul_imm $I32 src1 (i8_try_from_i32 src2))  (x64_imull_rmi_sxb src1 src2))
 (rule 2 (x64_imul_imm $I64 src1 (i8_try_from_i32 src2))  (x64_imulq_rmi_sxb src1 src2))
-(rule 1 (x64_imul_imm $I16 src1 (u16_try_from_i32 src2)) (x64_imulw_rmi src1 src2))
-(rule 1 (x64_imul_imm $I32 src1 (i32_as_u32 src2))       (x64_imull_rmi src1 src2))
-(rule 1 (x64_imul_imm $I64 src1 src2)                    (x64_imulq_rmi_sxl src1 src2))
+(rule 1 (x64_imul_imm $I16 src1 (i16_try_from_i32 src2)) (x64_imulw_rmi src1 (i16_as_u16 src2)))
+(rule 1 (x64_imul_imm $I32 src1 src2) (x64_imull_rmi src1 (i32_as_u32 src2)))
+(rule 1 (x64_imul_imm $I64 src1 src2) (x64_imulq_rmi_sxl src1 src2))
 
 ;; Helper for creating `mul` instructions or `imul` instructions (depending
 ;; on `signed`) for 8-bit operands.

cranelift/codegen/src/isle_prelude.rs
@@ -59,8 +59,8 @@ macro_rules! isle_common_prelude_methods {
         }
 
         #[inline]
-        fn i32_as_u32(&mut self, x: i32) -> Option<u32> {
-            Some(x as u32)
+        fn i32_as_u32(&mut self, x: i32) -> u32 {
+            x as u32
         }
 
         #[inline]
@@ -956,6 +956,14 @@ macro_rules! isle_common_prelude_methods {
 
         fn i8_try_from_u64(&mut self, val: u64) -> Option<i8> {
             i8::try_from(val).ok()
+        }
+
+        fn i16_as_u16(&mut self, val: i16) -> u16 {
+            val as u16
+        }
+
+        fn i16_try_from_i32(&mut self, val: i32) -> Option<i16> {
+            i16::try_from(val).ok()
         }
 
         fn i16_try_from_u64(&mut self, val: u64) -> Option<i16> {

cranelift/codegen/src/prelude.isle
@@ -142,8 +142,14 @@
 (decl pure partial i8_try_from_u64 (u64) i8)
 (extern constructor i8_try_from_u64 i8_try_from_u64)
 
+(decl pure i16_as_u16 (i16) u16)
+(extern constructor i16_as_u16 i16_as_u16)
+
 (decl pure partial i16_try_from_u64 (u64) i16)
 (extern constructor i16_try_from_u64 i16_try_from_u64)
+
+(decl pure partial i16_try_from_i32 (i16) i32)
+(extern extractor i16_try_from_i32 i16_try_from_i32)
 
 (decl pure partial i32_try_from_u64 (u64) i32)
 (extern constructor i32_try_from_u64 i32_try_from_u64)
@@ -152,8 +158,8 @@
 (extern constructor u32_as_u64 u32_as_u64)
 (convert u32 u64 u32_as_u64)
 
-(decl i32_as_u32 (u32) i32)
-(extern extractor i32_as_u32 i32_as_u32)
+(decl i32_as_u32 (i32) u32)
+(extern constructor i32_as_u32 i32_as_u32)
 
 (decl pure i32_as_i64 (i32) i64)
 (extern constructor i32_as_i64 i32_as_i64)

tests/disas/x64-mul16-negative.wat
@@ -1,0 +1,19 @@
+;;! target = 'x86_64'
+;;! test = 'compile'
+
+(module
+  (func (export "mul16") (param i32) (result i32)
+    local.get 0
+    i32.const -7937
+    i32.mul
+    i32.extend16_s
+  )
+)
+;; wasm[0]::function[0]:
+;;       pushq   %rbp
+;;       movq    %rsp, %rbp
+;;       imulw   $0xe0ff, %dx, %dx
+;;       movswl  %dx, %eax
+;;       movq    %rbp, %rsp
+;;       popq    %rbp
+;;       retq

tests/misc_testsuite/mul16-negative.wast
@@ -1,0 +1,10 @@
+(module
+  (func (export "mul16") (param i32) (result i32)
+    local.get 0
+    i32.const -7937
+    i32.mul
+    i32.extend16_s
+  )
+)
+
+(assert_return (invoke "mul16" (i32.const 100)) (i32.const -7268))