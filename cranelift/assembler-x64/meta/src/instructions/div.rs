6aa8f8b037746e1f745bcfef93459a25e895d39b
Rahul Chaphalkar <rahul.s.chaphalkar@intel.com>
2025-05-28 15:17:01 -0700

    align mem operand

--- cranelift/assembler-x64/meta/src/instructions/div.rs
+++ cranelift/assembler-x64/meta/src/instructions/div.rs
@@ -1,5 +1,5 @@
-use crate::dsl::{Feature::*, Inst, Location::*, VexLength::*};
-use crate::dsl::{fmt, implicit, inst, r, rex, vex, rw};
+use crate::dsl::{Feature::*, Inst, Location::*, VexLength::*, align};
+use crate::dsl::{fmt, implicit, inst, r, rex, rw, vex};
 
 #[rustfmt::skip] // Keeps instructions on a single line.
 pub fn list() -> Vec<Inst> {
@@ -13,8 +13,8 @@ pub fn list() -> Vec<Inst> {
         inst("idivl", fmt("M", [rw(implicit(eax)), rw(implicit(edx)), r(rm32)]), rex([0xF7]).digit(7), _64b | compat).has_trap(),
         inst("idivq", fmt("M", [rw(implicit(rax)), rw(implicit(rdx)), r(rm64)]), rex([0xF7]).digit(7).w(), _64b).has_trap(),
         // Vector instructions.
-        inst("divps", fmt("A", [rw(xmm1), r(xmm_m128)]), rex([0xF, 0x5E]).r(), _64b | compat | sse),
-        inst("divpd", fmt("A", [rw(xmm1), r(xmm_m128)]), rex([0x66, 0x0F, 0x5E]).r(), _64b | compat | sse2),
+        inst("divps", fmt("A", [rw(xmm1), align(xmm_m128)]), rex([0xF, 0x5E]).r(), _64b | compat | sse),
+        inst("divpd", fmt("A", [rw(xmm1), align(xmm_m128)]), rex([0x66, 0x0F, 0x5E]).r(), _64b | compat | sse2),
         inst("divss", fmt("A", [rw(xmm1), r(xmm_m32)]), rex([0xF3, 0xF, 0x5E]).r(), _64b | compat | sse),
         inst("divsd", fmt("A", [rw(xmm1), r(xmm_m64)]), rex([0xF2, 0xF, 0x5E]).r(), _64b | compat | sse2),
         inst("vdivps", fmt("B", [rw(xmm1), r(xmm2), r(xmm_m128)]), vex(L128)._0f().op(0x5E).r(), _64b | compat | avx),
