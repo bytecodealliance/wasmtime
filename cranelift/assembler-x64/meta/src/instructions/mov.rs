aeb2fbc44df6f993334207e1a35f2b5dc7169637
Andrew Brown <andrew.brown@intel.com>
2025-05-30 16:12:09 -0700

    review: model partial written-to locations with `w`

--- cranelift/assembler-x64/meta/src/instructions/mov.rs
+++ cranelift/assembler-x64/meta/src/instructions/mov.rs
@@ -9,22 +9,22 @@ pub fn list() -> Vec<Inst> {
         // registers if a REX prefix is used: AH, BH, CH, DH. Only moves that
         // overwrite all 64 bits are considered "write-only"; smaller-width
         // moves indicate that upper bits are preserved by being "read-write."
-        inst("movb", fmt("MR", [rw(rm8), r(r8)]), rex(0x88).r(), _64b | compat),
-        inst("movw", fmt("MR", [rw(rm16), r(r16)]), rex([0x66, 0x89]).r(), _64b | compat),
-        inst("movl", fmt("MR", [rw(rm32), r(r32)]), rex(0x89).r(), _64b | compat),
+        inst("movb", fmt("MR", [w(rm8), r(r8)]), rex(0x88).r(), _64b | compat),
+        inst("movw", fmt("MR", [w(rm16), r(r16)]), rex([0x66, 0x89]).r(), _64b | compat),
+        inst("movl", fmt("MR", [w(rm32), r(r32)]), rex(0x89).r(), _64b | compat),
         inst("movq", fmt("MR", [w(rm64), r(r64)]), rex(0x89).w().r(), _64b),
-        inst("movb", fmt("RM", [rw(r8), r(rm8)]), rex(0x8A).r(), _64b | compat),
-        inst("movw", fmt("RM", [rw(r16), r(rm16)]), rex([0x66, 0x8B]).r(), _64b | compat),
-        inst("movl", fmt("RM", [rw(r32), r(rm32)]), rex(0x8B).r(), _64b | compat),
+        inst("movb", fmt("RM", [w(r8), r(rm8)]), rex(0x8A).r(), _64b | compat),
+        inst("movw", fmt("RM", [w(r16), r(rm16)]), rex([0x66, 0x8B]).r(), _64b | compat),
+        inst("movl", fmt("RM", [w(r32), r(rm32)]), rex(0x8B).r(), _64b | compat),
         inst("movq", fmt("RM", [w(r64), r(rm64)]), rex(0x8B).w().r(), _64b),
-        inst("movb", fmt("OI", [rw(r8), r(imm8)]), rex(0xB0).rb().ib(), _64b | compat),
-        inst("movw", fmt("OI", [rw(r16), r(imm16)]), rex([0x66, 0xB8]).rw().iw(), _64b | compat),
-        inst("movl", fmt("OI", [rw(r32), r(imm32)]), rex(0xB8).rd().id(), _64b | compat),
+        inst("movb", fmt("OI", [w(r8), r(imm8)]), rex(0xB0).rb().ib(), _64b | compat),
+        inst("movw", fmt("OI", [w(r16), r(imm16)]), rex([0x66, 0xB8]).rw().iw(), _64b | compat),
+        inst("movl", fmt("OI", [w(r32), r(imm32)]), rex(0xB8).rd().id(), _64b | compat),
         // Capstone disassembles this (and only this) slightly differently.
         inst("movabsq", fmt("OI", [w(r64), r(imm64)]), rex(0xB8).w().ro().io(), _64b),
-        inst("movb", fmt("MI", [rw(rm8), r(imm8)]), rex(0xC6).digit(0).ib(), _64b | compat),
-        inst("movw", fmt("MI", [rw(rm16), r(imm16)]), rex([0x66, 0xC7]).digit(0).iw(), _64b | compat),
-        inst("movl", fmt("MI", [rw(rm32), r(imm32)]), rex(0xC7).digit(0).id(), _64b | compat),
+        inst("movb", fmt("MI", [w(rm8), r(imm8)]), rex(0xC6).digit(0).ib(), _64b | compat),
+        inst("movw", fmt("MI", [w(rm16), r(imm16)]), rex([0x66, 0xC7]).digit(0).iw(), _64b | compat),
+        inst("movl", fmt("MI", [w(rm32), r(imm32)]), rex(0xC7).digit(0).id(), _64b | compat),
         inst("movq", fmt("MI_SXL", [w(rm64), sxq(imm32)]), rex(0xC7).w().digit(0).id(), _64b),
 
         // Move integers between GPR and XMM locations. From the reference
