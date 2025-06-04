c71d3aabfd7edbd21b76514849e0f31c208cf0be
Nick Fitzgerald <fitzgen@gmail.com>
2025-06-04 20:53:37 +0200

    Deps: Update `wasm-tools` crates to 0.233.0 (#10915)
    * Update `wasm-tools` deps to 0.233.0
    
    * Update `cargo vet` metadata
    
    Just a prune and wildcard renewals
    
    * cargo fmt
    
    * Fix type export identifiers
    
    * Fix more type identifiers in exports in WAT

--- crates/environ/src/component/translate.rs
+++ crates/environ/src/component/translate.rs
@@ -1219,7 +1219,9 @@ impl<'a, 'data> Translator<'a, 'data> {
                     let idx = FuncIndex::from_u32(*idx);
                     ret.callback = Some(idx);
                 }
-                wasmparser::CanonicalOption::CoreType(_) => todo!(),
+                wasmparser::CanonicalOption::CoreType(_) | wasmparser::CanonicalOption::Gc => {
+                    todo!("component model and GC support")
+                }
             }
         }
         return ret;
