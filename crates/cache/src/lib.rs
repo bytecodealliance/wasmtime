4c8edb95327b6cea6bb61c7c53941a8a0b6e63f1
Alex Crichton <alex@alexcrichton.com>
2025-06-06 23:13:36 +0200

    More clearly flag internal crates as such (#10963)
    * More clearly flag internal crates as such
    
    This commit is an attempt to more clearly flag internal crates in this
    project as internal and not intended for external use. Specifically:
    
    * Many crates are renamed from `wasmtime-foo` to
      `wasmtime-internal-foo`.
    * All of these crates now have `INTERNAL: ...` in their crates.io
      description.
    * All of these crates now have a warning at the top of their
      documentation discouraging use.
    
    This change is a result of rustsec/advisory-db#1999 where the goal is to
    be crystal clear from a project perspective that usage of these crates
    are highly discouraged and not supported. We'll still probably get such
    advisories but we won't be considering them CVEs from the project itself
    due to the internal nature of these crates and the discouraging
    warnings.
    
    Some concrete changes used here are:
    
    * Inter-crate dependencies still use `wasmtime_foo` for naming and do
      so with Cargo's package-renaming features.
    * Crate renames are specified at the workspace level so the rename is
      only in one locations and all other inherit it.
    * Contribution documentation now has some brief guidelines about crate
      organization.
    
    * Update vet config
    
    * Update checks for wasmtime-fiber
    
    prtest:full
    
    * Update publish script
    
    * Another fiber rename
    
    * Fix some doc tests

--- crates/cache/src/lib.rs
+++ crates/cache/src/lib.rs
@@ -1,3 +1,10 @@
+//! > **⚠️ Warning ⚠️**: this crate is an internal-only crate for the Wasmtime
+//! > project and is not intended for general use. APIs are not strictly
+//! > reviewed for safety and usage outside of Wasmtime may have bugs. If
+//! > you're interested in using this feel free to file an issue on the
+//! > Wasmtime repository to start a discussion about doing so, but otherwise
+//! > be aware that your usage of this crate is not supported.
+
 use anyhow::Result;
 use base64::Engine;
 use log::{debug, trace, warn};
