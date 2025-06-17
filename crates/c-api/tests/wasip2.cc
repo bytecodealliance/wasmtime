f7b6a6e7aa242fc3e2d772830e0bac74008d8ea1
MangoPeachGrape <191630121+MangoPeachGrape@users.noreply.github.com>
2025-06-18 00:09:23 +0300

    Add a simple test

---
+++ crates/c-api/tests/wasip2.cc
@@ -0,0 +1,43 @@
+#include "component/utils.h"
+
+#include <gtest/gtest.h>
+#include <wasmtime.h>
+
+TEST(wasip2, smoke) {
+  static constexpr auto component_text = std::string_view{
+      R"END(
+(component)
+      )END",
+  };
+  const auto engine = wasm_engine_new();
+  EXPECT_NE(engine, nullptr);
+
+  const auto store = wasmtime_store_new(engine, nullptr, nullptr);
+  const auto context = wasmtime_store_context(store);
+
+  const auto cfg = wasmtime_wasip2_config_new();
+  wasmtime_wasip2_config_inherit_stdout(cfg);
+  wasmtime_context_set_wasip2(context, cfg);
+
+  wasmtime_component_t *component = nullptr;
+
+  auto err = wasmtime_component_new(
+      engine, reinterpret_cast<const uint8_t *>(component_text.data()),
+      component_text.size(), &component);
+
+  CHECK_ERR(err);
+
+  const auto linker = wasmtime_component_linker_new(engine);
+
+  wasmtime_component_linker_add_wasip2(linker);
+
+  wasmtime_component_instance_t instance = {};
+  err = wasmtime_component_linker_instantiate(linker, context, component,
+                                              &instance);
+  CHECK_ERR(err);
+
+  wasmtime_component_linker_delete(linker);
+
+  wasmtime_store_delete(store);
+  wasm_engine_delete(engine);
+}
