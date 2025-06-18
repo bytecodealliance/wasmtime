7cec026c8b68c6c389425839f35317cca4b462ba
Andrew Brown <andrew.brown@intel.com>
2025-06-18 16:59:05 -0700

    asm: add `VexPrefix::two_op`
    This is simply a convenience method when `vvvv` is unspecified.

--- cranelift/assembler-x64/meta/src/generate/format.rs
+++ cranelift/assembler-x64/meta/src/generate/format.rs
@@ -274,9 +274,8 @@ impl dsl::Format {
                     assert!(!vex.is4);
                     let reg = reg_or_vvvv;
                     fmtln!(f, "let reg = self.{reg}.enc();");
-                    fmtln!(f, "let vvvv = {};", "0b0");
                     fmtln!(f, "let rm = self.{rm}.encode_bx_regs();");
-                    fmtln!(f, "let vex = VexPrefix::three_op(reg, vvvv, rm, {bits});");
+                    fmtln!(f, "let vex = VexPrefix::two_op(reg, rm, {bits});");
                     ModRmStyle::RegMem {
                         reg: ModRmReg::Reg(*reg),
                         rm: *rm,
@@ -286,9 +285,8 @@ impl dsl::Format {
             [Reg(reg), Reg(rm)] => {
                 assert!(!vex.is4);
                 fmtln!(f, "let reg = self.{reg}.enc();");
-                fmtln!(f, "let vvvv = 0;");
-                fmtln!(f, "let rm = (Some(self.{rm}.enc()), None);");
-                fmtln!(f, "let vex = VexPrefix::three_op(reg, vvvv, rm, {bits});");
+                fmtln!(f, "let rm = self.{rm}.encode_bx_regs();");
+                fmtln!(f, "let vex = VexPrefix::two_op(reg, rm, {bits});");
                 ModRmStyle::Reg {
                     reg: ModRmReg::Reg(*reg),
                     rm: *rm,
