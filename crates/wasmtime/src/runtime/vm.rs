c81df20388865b6d0016d0f5d241098521d4620f
Alex Crichton <alex@alexcrichton.com>
2025-06-24 16:53:08 +0200

    Fix globals/tables holding their registered types (#11103)
    This commit fixes an issue where host-created tables and globals with
    concrete reference types previously did not keep their associated type
    registrations alive for the duration of the table or global itself. This
    could lead to runtime panics when reflecting on their type and
    additionally lead to some type confusion about the global/table itself.
    As described in #11102 this is not a security issue, just a bug that
    needs fixing.
    
    Closes #11102

--- crates/wasmtime/src/runtime/vm.rs
+++ crates/wasmtime/src/runtime/vm.rs
@@ -37,6 +37,7 @@ use crate::StoreContextMut;
 use crate::prelude::*;
 use crate::store::StoreInner;
 use crate::store::StoreOpaque;
+use crate::type_registry::RegisteredType;
 use alloc::sync::Arc;
 use core::fmt;
 use core::ops::Deref;
@@ -336,23 +337,23 @@ pub enum ModuleRuntimeInfo {
 #[derive(Clone)]
 pub struct BareModuleInfo {
     module: Arc<wasmtime_environ::Module>,
-    one_signature: Option<VMSharedTypeIndex>,
     offsets: VMOffsets<HostPtr>,
+    _registered_type: Option<RegisteredType>,
 }
 
 impl ModuleRuntimeInfo {
     pub(crate) fn bare(module: Arc<wasmtime_environ::Module>) -> Self {
-        ModuleRuntimeInfo::bare_maybe_imported_func(module, None)
+        ModuleRuntimeInfo::bare_with_registered_type(module, None)
     }
 
-    pub(crate) fn bare_maybe_imported_func(
+    pub(crate) fn bare_with_registered_type(
         module: Arc<wasmtime_environ::Module>,
-        one_signature: Option<VMSharedTypeIndex>,
+        registered_type: Option<RegisteredType>,
     ) -> Self {
         ModuleRuntimeInfo::Bare(Box::new(BareModuleInfo {
             offsets: VMOffsets::new(HostPtr, &module),
             module,
-            one_signature,
+            _registered_type: registered_type,
         }))
     }
 
@@ -454,10 +455,7 @@ impl ModuleRuntimeInfo {
                 .as_module_map()
                 .values()
                 .as_slice(),
-            ModuleRuntimeInfo::Bare(b) => match &b.one_signature {
-                Some(s) => core::slice::from_ref(s),
-                None => &[],
-            },
+            ModuleRuntimeInfo::Bare(_) => &[],
         }
     }
 
