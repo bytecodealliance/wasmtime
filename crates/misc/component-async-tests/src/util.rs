a9635772a46eaf252baa6caf386d1f9ae6d4d671
Joel Dice <joel.dice@fermyon.com>
2025-07-11 17:16:54 -0600

    simplify `component_async_tests::util::sleep`
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- crates/misc/component-async-tests/src/util.rs
+++ crates/misc/component-async-tests/src/util.rs
@@ -1,3 +1,6 @@
+use futures::channel::oneshot;
+use std::thread;
+
 pub async fn sleep(duration: std::time::Duration) {
     if cfg!(miri) {
         // TODO: We should be able to use `tokio::time::sleep` here, but as of
@@ -7,46 +10,13 @@ pub async fn sleep(duration: std::time::Duration) {
         // fix that we can stop using our own, special version of `sleep` and
         // switch back to the Tokio version.
 
-        use std::{
-            future,
-            sync::{
-                Arc, Mutex,
-                atomic::{AtomicU32, Ordering::SeqCst},
-            },
-            task::Poll,
-            thread,
-        };
-
-        let state = Arc::new(AtomicU32::new(0));
-        let waker = Arc::new(Mutex::new(None));
-        let mut join_handle = None;
-        future::poll_fn(move |cx| match state.load(SeqCst) {
-            0 => {
-                state.store(1, SeqCst);
-                let state = state.clone();
-                *waker.lock().unwrap() = Some(cx.waker().clone());
-                let waker = waker.clone();
-                join_handle = Some(thread::spawn(move || {
-                    thread::sleep(duration);
-                    state.store(2, SeqCst);
-                    let waker = waker.lock().unwrap().clone().unwrap();
-                    waker.wake();
-                }));
-                Poll::Pending
-            }
-            1 => {
-                *waker.lock().unwrap() = Some(cx.waker().clone());
-                Poll::Pending
-            }
-            2 => {
-                if let Some(handle) = join_handle.take() {
-                    _ = handle.join();
-                }
-                Poll::Ready(())
-            }
-            _ => unreachable!(),
-        })
-        .await;
+        let (tx, rx) = oneshot::channel();
+        let handle = thread::spawn(move || {
+            thread::sleep(duration);
+            _ = tx.send(());
+        });
+        _ = rx.await;
+        _ = handle.join();
     } else {
         tokio::time::sleep(duration).await;
     }
