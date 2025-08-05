75040a1e17b2226d640f72dea4a2c9ec37224545
Alex Crichton <alex@alexcrichton.com>
2025-08-05 13:06:07 -0700

    Update UDP denial test

--- crates/test-programs/src/bin/cli_no_udp.rs
+++ crates/test-programs/src/bin/cli_no_udp.rs
@@ -8,9 +8,8 @@ fn main() {
     let net = Network::default();
     let family = IpAddressFamily::Ipv4;
     let remote1 = IpSocketAddress::new(IpAddress::new_loopback(family), 4321);
-    let sock = UdpSocket::new(family).unwrap();
-
-    let bind = sock.blocking_bind(&net, remote1);
-    eprintln!("Result of binding: {bind:?}");
-    assert!(matches!(bind, Err(ErrorCode::AccessDenied)));
+    assert!(matches!(
+        UdpSocket::new(family),
+        Err(ErrorCode::AccessDenied)
+    ));
 }
