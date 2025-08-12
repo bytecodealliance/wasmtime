4db0e6242b6c5c793a396e924fcb99979e926ffb
Alex Crichton <alex@alexcrichton.com>
2025-08-12 15:55:46 -0700

    Fix warning

--- crates/test-programs/src/bin/cli_p3_hello_stdout.rs
+++ crates/test-programs/src/bin/cli_p3_hello_stdout.rs
@@ -6,7 +6,7 @@ export!(Component);
 
 impl exports::wasi::cli::run::Guest for Component {
     async fn run() -> Result<(), ()> {
-        let (mut tx, mut rx) = wit_stream::new();
+        let (mut tx, rx) = wit_stream::new();
         wasi::cli::stdout::set_stdout(rx);
         tx.write(b"hello, world\n".to_vec()).await;
         Ok(())
