fef78f68efe27717303b2fe9bec298ea4d02c2fd
Alex Crichton <alex@alexcrichton.com>
2025-08-15 15:00:13 -0700

    Update WASIp3 WITs to a tagged release
    Now that there's an official tag it's best to use that, and this'll help
    keep everything in sync as well.

--- crates/test-programs/src/p3/mod.rs
+++ crates/test-programs/src/p3/mod.rs
@@ -6,10 +6,10 @@ wit_bindgen::generate!({
         package wasmtime:test;
 
         world testp3 {
-            include wasi:cli/imports@0.3.0;
-            include wasi:http/imports@0.3.0-draft;
+            include wasi:cli/imports@0.3.0-rc-2025-08-15;
+            include wasi:http/imports@0.3.0-rc-2025-08-15;
 
-            export wasi:cli/run@0.3.0;
+            export wasi:cli/run@0.3.0-rc-2025-08-15;
         }
     ",
     path: "../wasi-http/src/p3/wit",
@@ -17,9 +17,9 @@ wit_bindgen::generate!({
     default_bindings_module: "test_programs::p3",
     pub_export_macro: true,
     async: [
-        "wasi:cli/run@0.3.0#run",
+        "wasi:cli/run@0.3.0-rc-2025-08-15#run",
     ],
-    generate_all
+    generate_all,
 });
 
 pub mod proxy {
@@ -28,7 +28,7 @@ pub mod proxy {
             package wasmtime:test;
 
             world proxyp3 {
-                include wasi:http/proxy@0.3.0-draft;
+                include wasi:http/proxy@0.3.0-rc-2025-08-15;
             }
         ",
         path: "../wasi-http/src/p3/wit",
@@ -36,14 +36,14 @@ pub mod proxy {
         default_bindings_module: "test_programs::p3::proxy",
         pub_export_macro: true,
         with: {
-            "wasi:http/handler@0.3.0-draft": generate,
-            "wasi:http/types@0.3.0-draft": crate::p3::wasi::http::types,
-            "wasi:random/random@0.3.0": crate::p3::wasi::random::random,
-            "wasi:cli/stdout@0.3.0": crate::p3::wasi::cli::stdout,
-            "wasi:cli/stderr@0.3.0": crate::p3::wasi::cli::stderr,
-            "wasi:cli/stdin@0.3.0": crate::p3::wasi::cli::stdin,
-            "wasi:clocks/monotonic-clock@0.3.0": crate::p3::wasi::clocks::monotonic_clock,
-            "wasi:clocks/wall-clock@0.3.0": crate::p3::wasi::clocks::wall_clock,
+            "wasi:http/handler@0.3.0-rc-2025-08-15": generate,
+            "wasi:http/types@0.3.0-rc-2025-08-15": crate::p3::wasi::http::types,
+            "wasi:random/random@0.3.0-rc-2025-08-15": crate::p3::wasi::random::random,
+            "wasi:cli/stdout@0.3.0-rc-2025-08-15": crate::p3::wasi::cli::stdout,
+            "wasi:cli/stderr@0.3.0-rc-2025-08-15": crate::p3::wasi::cli::stderr,
+            "wasi:cli/stdin@0.3.0-rc-2025-08-15": crate::p3::wasi::cli::stdin,
+            "wasi:clocks/monotonic-clock@0.3.0-rc-2025-08-15": crate::p3::wasi::clocks::monotonic_clock,
+            "wasi:clocks/wall-clock@0.3.0-rc-2025-08-15": crate::p3::wasi::clocks::wall_clock,
         },
     });
 }
--- crates/wasi/src/p3/random/host.rs
+++ crates/wasi/src/p3/random/host.rs
@@ -31,7 +31,7 @@ impl insecure::Host for WasiRandomCtx {
 }
 
 impl insecure_seed::Host for WasiRandomCtx {
-    fn insecure_seed(&mut self) -> wasmtime::Result<(u64, u64)> {
+    fn get_insecure_seed(&mut self) -> wasmtime::Result<(u64, u64)> {
         let seed: u128 = self.insecure_random_seed;
         Ok((seed as u64, (seed >> 64) as u64))
     }
