908946ccde9939e83ffdac8584c054d80b806e68
Alex Crichton <alex@alexcrichton.com>
2025-09-02 15:10:45 -0700

    Update wasm-tools crates
    Keeping up to date

--- crates/component-macro/src/bindgen.rs
+++ crates/component-macro/src/bindgen.rs
@@ -170,7 +170,8 @@ impl Parse for Config {
         let (resolve, pkgs, files) = parse_source(&paths, &inline)
             .map_err(|err| Error::new(call_site, format!("{err:?}")))?;
 
-        let world = select_world(&resolve, &pkgs, world.as_deref())
+        let world = resolve
+            .select_world(&pkgs, world.as_deref())
             .map_err(|e| Error::new(call_site, format!("{e:?}")))?;
         Ok(Config {
             opts,
@@ -226,43 +227,6 @@ fn parse_source(
     }
 
     Ok((resolve, pkgs, files))
-}
-
-fn select_world(
-    resolve: &Resolve,
-    pkgs: &[PackageId],
-    world: Option<&str>,
-) -> anyhow::Result<WorldId> {
-    if pkgs.len() == 1 {
-        resolve.select_world(pkgs[0], world)
-    } else {
-        assert!(!pkgs.is_empty());
-        match world {
-            Some(name) => {
-                if !name.contains(":") {
-                    anyhow::bail!(
-                        "with multiple packages a fully qualified \
-                         world name must be specified"
-                    )
-                }
-
-                // This will ignore the package argument due to the fully
-                // qualified name being used.
-                resolve.select_world(pkgs[0], world)
-            }
-            None => {
-                let worlds = pkgs
-                    .iter()
-                    .filter_map(|p| resolve.select_world(*p, None).ok())
-                    .collect::<Vec<_>>();
-                match &worlds[..] {
-                    [] => anyhow::bail!("no packages have a world"),
-                    [world] => Ok(*world),
-                    _ => anyhow::bail!("multiple packages have a world, must specify which to use"),
-                }
-            }
-        }
-    }
 }
 
 mod kw {
