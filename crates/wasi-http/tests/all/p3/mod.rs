19eff9e4998074dbc15ae2f4b484c2b488b53a5c
Roman Volosatovs <rvolosatovs@riseup.net>
2025-09-08 14:21:13 +0200

    test(p3-http): stream responses back
    This is something we've been doing in wasip3, but I forgot to port this
    over
    
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/wasi-http/tests/all/p3/mod.rs
+++ crates/wasi-http/tests/all/p3/mod.rs
@@ -155,21 +155,18 @@ async fn p3_http_outbound_request_timeout() -> anyhow::Result<()> {
     run_cli(P3_HTTP_OUTBOUND_REQUEST_TIMEOUT_COMPONENT, &server).await
 }
 
-#[ignore = "unimplemented"] // TODO: implement
 #[test_log::test(tokio::test(flavor = "multi_thread"))]
 async fn p3_http_outbound_request_post() -> anyhow::Result<()> {
     let server = Server::http1(1)?;
     run_cli(P3_HTTP_OUTBOUND_REQUEST_POST_COMPONENT, &server).await
 }
 
-#[ignore = "unimplemented"] // TODO: implement
 #[test_log::test(tokio::test(flavor = "multi_thread"))]
 async fn p3_http_outbound_request_large_post() -> anyhow::Result<()> {
     let server = Server::http1(1)?;
     run_cli(P3_HTTP_OUTBOUND_REQUEST_LARGE_POST_COMPONENT, &server).await
 }
 
-#[ignore = "unimplemented"] // TODO: implement
 #[test_log::test(tokio::test(flavor = "multi_thread"))]
 async fn p3_http_outbound_request_put() -> anyhow::Result<()> {
     let server = Server::http1(1)?;
