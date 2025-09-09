d1ac513d00b0cd3e2a869544c2bc067b5f6298b3
Masahiro Kozuka <masa.koz@kozuka.jp>
2025-09-09 00:42:59 +0000

    fix memory leak

--- crates/wasi-nn/src/wit.rs
+++ crates/wasi-nn/src/wit.rs
@@ -249,11 +249,11 @@ impl generated::inference::HostGraphExecutionContext for WasiNnView<'_> {
         tracing::debug!("compute with {} inputs", inputs.len());
 
         let mut named_tensors = Vec::new();
-        for (name, tensor_resopurce) in inputs.iter() {
-            let tensor = self.table.get(&tensor_resopurce)?;
+        for (name, tensor_resopurce) in inputs.into_iter() {
+            let tensor = self.table.delete(tensor_resopurce)?;
             named_tensors.push(crate::backend::NamedTensor {
-                name: name.clone(),
-                tensor: tensor.clone(),
+                name: name,
+                tensor: tensor,
             });
         }
 
