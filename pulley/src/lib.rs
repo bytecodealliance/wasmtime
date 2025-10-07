be3cc21b221b3495e0541086a7e825c225a90f95
Alex Crichton <alex@alexcrichton.com>
2025-10-07 18:22:37 -0500

    Rename `doc_auto_cfg` to `doc_cfg` (#11809)

--- pulley/src/lib.rs
+++ pulley/src/lib.rs
@@ -1,6 +1,6 @@
 //! The pulley bytecode for fast interpreters.
 
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(pulley_tail_calls, feature(explicit_tail_calls))]
 #![cfg_attr(pulley_tail_calls, allow(incomplete_features, unstable_features))]
 #![deny(missing_docs)]
