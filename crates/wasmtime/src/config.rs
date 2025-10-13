690d88c2fb24f058b28352998dd283540002bdf1
Chris Fallin <chris@cfallin.org>
2025-10-12 17:48:37 -0700

    Review feedback: update doc comment on `Config::guest_debug`.

--- crates/wasmtime/src/config.rs
+++ crates/wasmtime/src/config.rs
@@ -468,6 +468,10 @@ impl Config {
     /// Breakpoints, watchpoints, and stepping are not yet supported,
     /// but will be added in a future version of Wasmtime.
     ///
+    /// This enables use of the [`crate::StackView`] API which is
+    /// provided by [`crate::Caller::stack_values`] from within a
+    /// hostcall context.
+    ///
     /// ***Note*** Enabling this option is not compatible with the
     /// Winch compiler.
     #[cfg(feature = "debug")]
