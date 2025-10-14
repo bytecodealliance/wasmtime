41a695f27cb1d420c6028f1d093777bb27766d9d
Alex Crichton <alex@alexcrichton.com>
2025-10-14 14:27:33 -0500

    Remove more module linking support from Wizer (#11855)
    This commit simplifies some stages of Wizer by removing more support for
    the now-ancient "module linking" proposal which was the precursor to the
    component model. Much of this will come back, but in a very different
    form, for component model support as component-init is merged into this
    repository. For now though to prepare for this refactoring this commit
    removes all this historical support to keep core module wizening focused
    on just core modules.

--- crates/wizer/src/info.rs
+++ crates/wizer/src/info.rs
@@ -1,65 +1,5 @@
 use std::convert::TryFrom;
 use std::ops::Range;
-use types_interner::{EntityType, TypeId, TypesInterner};
-use wasm_encoder::SectionId;
-
-pub mod types_interner;
-
-/// A collection of info about modules within a module linking bundle.
-pub struct ModuleContext<'a> {
-    arena: Vec<ModuleInfo<'a>>,
-    types: TypesInterner,
-}
-
-impl<'a> ModuleContext<'a> {
-    /// Construct a new `ModuleContext`, pre-populated with an empty root
-    /// module.
-    pub(crate) fn new() -> Self {
-        Self {
-            arena: vec![ModuleInfo::Defined(DefinedModuleInfo::default())],
-            types: TypesInterner::default(),
-        }
-    }
-
-    /// Get the root module.
-    pub(crate) fn root(&self) -> Module {
-        Module { id: 0 }
-    }
-
-    /// Get the interned types set for this module context.
-    pub(crate) fn types(&self) -> &TypesInterner {
-        &self.types
-    }
-
-    /// Get a shared reference to the `DefinedModuleInfo` for this module,
-    /// following through aliases.
-    fn defined(&self, module: Module) -> &DefinedModuleInfo<'a> {
-        match &self.arena[module.id] {
-            ModuleInfo::Defined(d) => return d,
-        }
-    }
-
-    /// Get an exclusive reference to the `DefinedModuleInfo` for this module.
-    ///
-    /// Does not resolve through aliases, because you shouldn't ever mutate
-    /// aliased modules.
-    fn defined_mut(&mut self, module: Module) -> &mut DefinedModuleInfo<'a> {
-        match &mut self.arena[module.id] {
-            ModuleInfo::Defined(d) => d,
-        }
-    }
-
-    pub(crate) fn has_wasi_initialize(&self, module: Module) -> bool {
-        self.defined(module)
-            .exports
-            .iter()
-            .any(|e| e.name == "_initialize")
-    }
-}
-
-enum ModuleInfo<'a> {
-    Defined(DefinedModuleInfo<'a>),
-}
 
 /// Info that we keep track of on a per module-within-a-module-linking-bundle
 /// basis.
@@ -67,15 +7,9 @@ enum ModuleInfo<'a> {
 /// These are created during during our `parse` pass and then used throughout
 /// our later passes.
 #[derive(Default)]
-struct DefinedModuleInfo<'a> {
+pub struct ModuleContext<'a> {
     /// The raw sections from the original Wasm input.
     raw_sections: Vec<wasm_encoder::RawSection<'a>>,
-
-    /// Types available in this module.
-    ///
-    /// We keep track of these for determining how many things we need to
-    /// re-export for new instantiations and for inner module's aliases.
-    types: Vec<TypeId>,
 
     /// Imports made by this module.
     imports: Vec<wasmparser::Import<'a>>,
@@ -100,8 +34,8 @@ struct DefinedModuleInfo<'a> {
     exports: Vec<wasmparser::Export<'a>>,
 
     /// Maps from function index to the function's type index for all functions
-    /// defined, imported, and aliased in this module.
-    functions: Vec<TypeId>,
+    /// defined and imported in this module.
+    functions: Vec<u32>,
 
     /// Maps from table index to the table's type for all tables defined,
     /// imported, and aliased in this module.
@@ -118,110 +52,70 @@ struct DefinedModuleInfo<'a> {
     defined_memories_index: Option<u32>,
 }
 
-/// A module inside a module linking bundle.
-///
-/// This is a small, copy-able type that essentially just indexes into a
-/// `ModuleContext`.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub(crate) struct Module {
-    /// This module's id, aka its pre-order traversal index, aka its index in
-    /// `Modules::arena`.
-    id: usize,
-}
-
-impl Module {
-    /// Translate the given `wasmparser` entity type into its interned
-    /// representation using this module's types space.
-    pub fn entity_type(self, cx: &ModuleContext<'_>, ty: wasmparser::TypeRef) -> EntityType {
-        cx.types().entity_type(ty, &cx.defined(self).types)
-    }
-
+impl<'a> ModuleContext<'a> {
     /// Add a new raw section to this module info during parsing.
-    pub fn add_raw_section<'a>(
-        self,
-        cx: &mut ModuleContext<'a>,
-        id: SectionId,
-        range: Range<usize>,
-        full_wasm: &'a [u8],
-    ) {
-        cx.defined_mut(self)
-            .raw_sections
-            .push(wasm_encoder::RawSection {
-                id: id as u8,
-                data: &full_wasm[range.start..range.end],
-            })
-    }
-
-    /// Push a new type into this module's types space.
-    pub fn push_type<'a>(self, cx: &mut ModuleContext<'a>, ty: wasmparser::CompositeType) {
-        let types_space = match &cx.arena[self.id] {
-            ModuleInfo::Defined(d) => &d.types,
-        };
-        let ty = cx.types.insert_wasmparser(ty, types_space);
-        cx.defined_mut(self).types.push(ty);
+    pub(crate) fn add_raw_section(&mut self, id: u8, range: Range<usize>, full_wasm: &'a [u8]) {
+        self.raw_sections.push(wasm_encoder::RawSection {
+            id,
+            data: &full_wasm[range.start..range.end],
+        })
     }
 
     /// Push a new imported memory into this module's memory index space.
-    pub fn push_imported_memory(self, cx: &mut ModuleContext, memory_type: wasmparser::MemoryType) {
-        let info = cx.defined_mut(self);
-        assert!(info.defined_memories_index.is_none());
-        info.memories.push(memory_type);
+    pub(crate) fn push_imported_memory(&mut self, memory_type: wasmparser::MemoryType) {
+        assert!(self.defined_memories_index.is_none());
+        self.memories.push(memory_type);
     }
 
     /// Push a new defined memory into this module's memory index space.
-    pub fn push_defined_memory(self, cx: &mut ModuleContext, memory_type: wasmparser::MemoryType) {
-        let info = cx.defined_mut(self);
-        if info.defined_memories_index.is_none() {
-            info.defined_memories_index = Some(u32::try_from(info.memories.len()).unwrap());
+    pub(crate) fn push_defined_memory(&mut self, memory_type: wasmparser::MemoryType) {
+        if self.defined_memories_index.is_none() {
+            self.defined_memories_index = Some(u32::try_from(self.memories.len()).unwrap());
         }
-        info.memories.push(memory_type);
+        self.memories.push(memory_type);
     }
 
     /// Push a new imported global into this module's global index space.
-    pub fn push_imported_global(self, cx: &mut ModuleContext, global_type: wasmparser::GlobalType) {
-        let info = cx.defined_mut(self);
-        assert!(info.defined_globals_index.is_none());
-        info.globals.push(global_type);
+    pub(crate) fn push_imported_global(&mut self, global_type: wasmparser::GlobalType) {
+        assert!(self.defined_globals_index.is_none());
+        self.globals.push(global_type);
     }
 
     /// Push a new defined global into this module's global index space.
-    pub fn push_defined_global(self, cx: &mut ModuleContext, global_type: wasmparser::GlobalType) {
-        let info = cx.defined_mut(self);
-        if info.defined_globals_index.is_none() {
-            info.defined_globals_index = Some(u32::try_from(info.globals.len()).unwrap());
+    pub(crate) fn push_defined_global(&mut self, global_type: wasmparser::GlobalType) {
+        if self.defined_globals_index.is_none() {
+            self.defined_globals_index = Some(u32::try_from(self.globals.len()).unwrap());
         }
-        info.globals.push(global_type);
+        self.globals.push(global_type);
     }
 
     /// Push a new function into this module's function index space.
-    pub fn push_function(self, cx: &mut ModuleContext, func_type: TypeId) {
-        assert!(cx.types.get(func_type).is_func());
-        cx.defined_mut(self).functions.push(func_type);
+    pub(crate) fn push_function(&mut self, func_type: u32) {
+        self.functions.push(func_type);
     }
 
     /// Push a new table into this module's table index space.
-    pub fn push_table(self, cx: &mut ModuleContext, table_type: wasmparser::TableType) {
-        cx.defined_mut(self).tables.push(table_type);
+    pub(crate) fn push_table(&mut self, table_type: wasmparser::TableType) {
+        self.tables.push(table_type);
     }
 
     /// Push a new import into this module.
-    pub fn push_import<'a>(self, cx: &mut ModuleContext<'a>, import: wasmparser::Import<'a>) {
-        cx.defined_mut(self).imports.push(import);
+    pub(crate) fn push_import(&mut self, import: wasmparser::Import<'a>) {
+        self.imports.push(import);
 
         // Add the import to the appropriate index space for our current module.
         match import.ty {
             wasmparser::TypeRef::Memory(ty) => {
-                self.push_imported_memory(cx, ty);
+                self.push_imported_memory(ty);
             }
             wasmparser::TypeRef::Global(ty) => {
-                self.push_imported_global(cx, ty);
+                self.push_imported_global(ty);
             }
             wasmparser::TypeRef::Func(ty_idx) => {
-                let ty = self.type_id_at(cx, ty_idx);
-                self.push_function(cx, ty);
+                self.push_function(ty_idx);
             }
             wasmparser::TypeRef::Table(ty) => {
-                self.push_table(cx, ty);
+                self.push_table(ty);
             }
             wasmparser::TypeRef::Tag(_) => {
                 unreachable!("exceptions are unsupported; checked in validation")
@@ -230,81 +124,65 @@ impl Module {
     }
 
     /// Push an export into this module.
-    pub fn push_export<'a>(self, cx: &mut ModuleContext<'a>, export: wasmparser::Export<'a>) {
-        cx.defined_mut(self).exports.push(export);
-    }
-
-    /// Is this the root of the module linking bundle?
-    pub fn is_root(self) -> bool {
-        self.id == 0
+    pub(crate) fn push_export(&mut self, export: wasmparser::Export<'a>) {
+        self.exports.push(export);
     }
 
     /// The number of defined memories in this module.
-    pub fn defined_memories_len(self, cx: &ModuleContext) -> usize {
-        let info = cx.defined(self);
-        info.defined_memories_index.map_or(0, |n| {
+    pub(crate) fn defined_memories_len(&self) -> usize {
+        self.defined_memories_index.map_or(0, |n| {
             let n = usize::try_from(n).unwrap();
-            assert!(info.memories.len() > n);
-            info.memories.len() - n
+            assert!(self.memories.len() > n);
+            self.memories.len() - n
         })
     }
 
     /// Iterate over the defined memories in this module.
-    pub fn defined_memories<'b>(
-        self,
-        cx: &'b ModuleContext<'_>,
-    ) -> impl Iterator<Item = (u32, wasmparser::MemoryType)> + 'b {
-        let info = cx.defined(self);
-        info.memories
+    pub(crate) fn defined_memories(
+        &self,
+    ) -> impl Iterator<Item = (u32, wasmparser::MemoryType)> + '_ {
+        self.memories
             .iter()
             .copied()
             .enumerate()
             .skip(
-                info.defined_memories_index
-                    .map_or(info.memories.len(), |i| usize::try_from(i).unwrap()),
+                self.defined_memories_index
+                    .map_or(self.memories.len(), |i| usize::try_from(i).unwrap()),
             )
             .map(|(i, m)| (u32::try_from(i).unwrap(), m))
     }
 
     /// Iterate over the defined globals in this module.
-    pub fn defined_globals<'b>(
-        self,
-        cx: &'b ModuleContext<'_>,
-    ) -> impl Iterator<Item = (u32, wasmparser::GlobalType)> + 'b {
-        let info = cx.defined(self);
-        info.globals
+    pub(crate) fn defined_globals(
+        &self,
+    ) -> impl Iterator<Item = (u32, wasmparser::GlobalType)> + '_ {
+        self.globals
             .iter()
             .copied()
             .enumerate()
             .skip(
-                info.defined_globals_index
-                    .map_or(info.globals.len(), |i| usize::try_from(i).unwrap()),
+                self.defined_globals_index
+                    .map_or(self.globals.len(), |i| usize::try_from(i).unwrap()),
             )
             .map(|(i, g)| (u32::try_from(i).unwrap(), g))
     }
 
     /// Get a slice of this module's original raw sections.
-    pub fn raw_sections<'a, 'b>(
-        self,
-        cx: &'b ModuleContext<'a>,
-    ) -> &'b [wasm_encoder::RawSection<'a>] {
-        &cx.defined(self).raw_sections
+    pub(crate) fn raw_sections(&self) -> &[wasm_encoder::RawSection<'a>] {
+        &self.raw_sections
+    }
+
+    /// Get a slice of this module's imports.
+    pub(crate) fn imports(&self) -> &[wasmparser::Import<'a>] {
+        &self.imports
     }
 
     /// Get a slice of this module's exports.
-    pub fn exports<'a, 'b>(self, cx: &'b ModuleContext<'a>) -> &'b [wasmparser::Export<'a>] {
-        &cx.defined(self).exports
+    pub(crate) fn exports(&self) -> &[wasmparser::Export<'a>] {
+        &self.exports
     }
 
-    /// Get the full types index space for this module.
-    pub fn types<'a, 'b>(self, cx: &'b ModuleContext<'a>) -> &'b [TypeId] {
-        &cx.defined(self).types
-    }
-
-    /// Get the type at the given index.
-    ///
-    /// Panics if the types index space does not contain the given index.
-    pub fn type_id_at(self, cx: &ModuleContext<'_>, type_index: u32) -> TypeId {
-        cx.defined(self).types[usize::try_from(type_index).unwrap()]
+    pub(crate) fn has_wasi_initialize(&self) -> bool {
+        self.exports.iter().any(|e| e.name == "_initialize")
     }
 }
