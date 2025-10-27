639b36556f6c46c0e222af4c97de55f1ce87d9ec
Dan Gohman <dev@sunfishcode.online>
2025-10-27 10:35:28 -0700

    Revert more changes to vendored files.

--- crates/wasi-tls/src/bindings.rs
+++ crates/wasi-tls/src/bindings.rs
@@ -7,9 +7,9 @@ mod generated {
         world: "wasi:tls/imports",
         with: {
             "wasi:io": wasmtime_wasi::p2::bindings::io,
-            "wasi:tls/types.client-connection": crate::HostClientConnection,
-            "wasi:tls/types.client-handshake": crate::HostClientHandshake,
-            "wasi:tls/types.future-client-streams": crate::HostFutureClientStreams,
+            "wasi:tls/types/client-connection": crate::HostClientConnection,
+            "wasi:tls/types/client-handshake": crate::HostClientHandshake,
+            "wasi:tls/types/future-client-streams": crate::HostFutureClientStreams,
         },
         imports: { default: trappable },
         require_store_data_send: true,
