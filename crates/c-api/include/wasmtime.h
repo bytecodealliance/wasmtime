afe9bcd15bd7e8a85e90d5b86e14080904fe8513
wasmtime-publish <59749941+wasmtime-publish@users.noreply.github.com>
2025-11-05 02:03:14 +0100

    Bump Wasmtime to 40.0.0 (#11983)
    Co-authored-by: Wasmtime Publish <wasmtime-publish@users.noreply.github.com>

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -212,11 +212,11 @@
 /**
  * \brief Wasmtime version string.
  */
-#define WASMTIME_VERSION "39.0.0"
+#define WASMTIME_VERSION "40.0.0"
 /**
  * \brief Wasmtime major version number.
  */
-#define WASMTIME_VERSION_MAJOR 39
+#define WASMTIME_VERSION_MAJOR 40
 /**
  * \brief Wasmtime minor version number.
  */
