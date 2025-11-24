0a55f8048cbdfcfccb8fec0efbbed9473022df8a
Alex Crichton <alex@alexcrichton.com>
2025-11-24 11:09:20 -0600

    "Downgrade" threads support to tier 2, disable fuzzing (#12036)
    * "Downgrade" threads support to tier 2, disable fuzzing
    
    This commit is borne out of a fuzz bug that was opened recently. The
    fuzz bug specifically has to do with fallout from #12022, specifically
    `SharedMemory` being used to allocated instead of `Memory`. In this
    situation the resource limiter is no longer consulted meaning that
    shared memories bypass this and aren't caught by OOM checks. This is
    currently by design because `SharedMemory` instances don't know which
    resource limiter to hook into per-store.
    
    More generally though the implementation of wasm threads, while workable
    in Wasmtime, has a number of known relatively large deficiencies. These
    were not resolved prior to ungating the wasm proposal (that's on me) but
    nevertheless the quality of implementation is not quite up to "tier 1
    par" with the rest of what Wasmtime offers. Given this the threads
    proposal is now downgraded to tier 2. To help minimize the impact of
    this the wasm proposal is left enabled-by-default, but creation of a
    `SharedMemory` in the Rust API requires opting-in via a new
    `Config::shared_memory` method.
    
    This commit shuffles around some documentation of wasm proposals to
    split it into tier 1/2/3 instead of on/off-by-default and then adds a
    column for whether the proposal is on-by-default.
    
    * clangformat
    
    * Fix tests
    
    * Add tests for failed creation
    
    Fix an issue where defined shared memories weren't gated
    
    * Sync disabled threads stub
    
    * Fix another test
    
    prtest:full
    
    * Fix fuzzing tests
    
    * Fix dwarf tests

--- crates/fuzzing/src/oracles.rs
+++ crates/fuzzing/src/oracles.rs
@@ -709,6 +709,7 @@ pub fn wast_test(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<()> {
     crate::init_fuzzing();
 
     let mut fuzz_config: generators::Config = u.arbitrary()?;
+    fuzz_config.module_config.shared_memory = true;
     let test: generators::WastTest = u.arbitrary()?;
 
     let test = &test.test;
@@ -1310,7 +1311,6 @@ mod tests {
             | WasmFeatures::SIMD
             | WasmFeatures::MULTI_MEMORY
             | WasmFeatures::RELAXED_SIMD
-            | WasmFeatures::THREADS
             | WasmFeatures::TAIL_CALL
             | WasmFeatures::WIDE_ARITHMETIC
             | WasmFeatures::MEMORY64
