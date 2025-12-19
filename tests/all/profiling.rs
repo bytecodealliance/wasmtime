bytecodealliance/wasmtime

commit 1806c26562ec2eabbf3eaefbe038b75e90f61b59

Lookup functions in the text section by `FuncKey` at runtime (#11630)
This commit refactors our metadata, treating compiled functions homogeneously
and removing the need to add new tables to places like `CompiledModuleInfo`
whenever we add a new kind of function. This also simplifies the process of
constructing the metadata for a final, linked compilation artifact. Finally, it
paves the way to doing gc-sections during our linking process (which would give
us smaller code sizes by removing functions that have been inlined into every
caller, for example) as we now allow holes in certain types of function index
spaces that were previously always densely populated.

We have two kinds of index spaces:

1. Mostly-dense index spaces, which take O(max_index) space and provide O(1)
lookups.

2. Sparse index spaces, which take O(num_members) space and provide
O(log n) lookups.

Most of our function index spaces are currently dense, but we can tweak that in
the future if necessary.

Furthermore, code size of `.cwasm` binaries has shrunk very slightly with this
refactoring. Consider `spidermonkey.wasm`'s compiled `.cwasm`:

* Size before: 218756 `.wasmtime.info` section bytes, 20052632 total bytes
* Size after: 213761 `.wasmtime.info` section bytes, 20047640 total bytes

That is a 2.28% reduction on the size of the `.wasmtime.info` section, or a
0.025% reduction total.

However, we previously did a single metadata lookup to get the location of both
a Wasm function itself and its array-to-Wasm trampoline at the same time, and in
the new version of the code two lookups are performed. This is slightly slower,
as shown in our call-indirect micro-benchmark that combines lazy table
initialization (which delays looking up the function element's location until
runtime) with indirect-calling each table element exactly once (which defeats
the amortization of that lookup). So this micro-benchmark is both synthetic and

crates/wasmtime/src/runtime/instantiate.rs
@@ -9,19 +9,20 @@ use crate::{code_memory::CodeMemory, profiling_agent::ProfilingAgent};
 use alloc::sync::Arc;
 use core::str;
 use wasmtime_environ::{
-    CompiledFunctionInfo, CompiledModuleInfo, DefinedFuncIndex, FilePos, FuncIndex, FunctionLoc,
-    FunctionName, Metadata, Module, ModuleInternedTypeIndex, PrimaryMap,
+    CompiledFunctionsTable, CompiledModuleInfo, DefinedFuncIndex, EntityRef, FilePos, FuncIndex,
+    FuncKey, FunctionLoc, FunctionName, Metadata, Module, ModuleInternedTypeIndex,
+    StaticModuleIndex,
 };
 
 /// A compiled wasm module, ready to be instantiated.
 pub struct CompiledModule {
-    module: Arc<Module>,
-    funcs: PrimaryMap<DefinedFuncIndex, CompiledFunctionInfo>,
-    wasm_to_array_trampolines: Vec<(ModuleInternedTypeIndex, FunctionLoc)>,
-    meta: Metadata,
-    code_memory: Arc<CodeMemory>,
     /// A unique ID used to register this module with the engine.
     unique_id: CompiledModuleId,
+    code_memory: Arc<CodeMemory>,
+    module: Arc<Module>,
+    meta: Metadata,
+    index: Arc<CompiledFunctionsTable>,
+    /// Sorted list, by function index, of names we have for this module.
     func_names: Vec<FunctionName>,
 }
 
@@ -45,15 +46,15 @@ impl CompiledModule {
     pub fn from_artifacts(
         code_memory: Arc<CodeMemory>,
         info: CompiledModuleInfo,
+        index: Arc<CompiledFunctionsTable>,
         profiler: &dyn ProfilingAgent,
     ) -> Result<Self> {
         let mut ret = Self {
-            module: Arc::new(info.module),
-            funcs: info.funcs,
-            wasm_to_array_trampolines: info.wasm_to_array_trampolines,
-            code_memory,
-            meta: info.meta,
             unique_id: CompiledModuleId::new(),
+            code_memory,
+            module: Arc::new(info.module),
+            meta: info.meta,
+            index,
             func_names: info.func_names,
         };
         ret.register_profiling(profiler)?;
@@ -65,7 +66,7 @@ impl CompiledModule {
         // TODO-Bug?: "code_memory" is not exclusive for this module in the case of components,
         // so we may be registering the same code range multiple times here.
         profiler.register_module(&self.code_memory.mmap()[..], &|addr| {
-            let (idx, _) = self.func_by_text_offset(addr)?;
+            let idx = self.func_by_text_offset(addr)?;
             let idx = self.module.func_index(idx);
             let name = self.func_name(idx)?;
             let mut demangled = String::new();
@@ -105,6 +106,10 @@ impl CompiledModule {
         &self.module
     }
 
+    fn module_index(&self) -> StaticModuleIndex {
+        self.module.module_index
+    }
+
     /// Looks up the `name` section name for the function index `idx`, if one
     /// was specified in the original wasm module.
     pub fn func_name(&self, idx: FuncIndex) -> Option<&str> {
@@ -126,15 +131,15 @@ impl CompiledModule {
     pub fn finished_functions(
         &self,
     ) -> impl ExactSizeIterator<Item = (DefinedFuncIndex, &[u8])> + '_ {
-        self.funcs
-            .iter()
-            .map(move |(i, _)| (i, self.finished_function(i)))
+        self.module
+            .defined_func_indices()
+            .map(|i| (i, self.finished_function(i)))
     }
 
     /// Returns the body of the function that `index` points to.
     #[inline]
-    pub fn finished_function(&self, index: DefinedFuncIndex) -> &[u8] {
-        let loc = self.funcs[index].wasm_func_loc;
+    pub fn finished_function(&self, def_func_index: DefinedFuncIndex) -> &[u8] {
+        let loc = self.func_loc(def_func_index);
         &self.text()[loc.start as usize..][..loc.length as usize]
     }
 
@@ -145,8 +150,10 @@ impl CompiledModule {
     ///
     /// These trampolines are used for array callers (e.g. `Func::new`)
     /// calling Wasm callees.
-    pub fn array_to_wasm_trampoline(&self, index: DefinedFuncIndex) -> Option<&[u8]> {
-        let loc = self.funcs[index].array_to_wasm_trampoline?;
+    pub fn array_to_wasm_trampoline(&self, def_func_index: DefinedFuncIndex) -> Option<&[u8]> {
+        assert!(def_func_index.index() < self.module.num_defined_funcs());
+        let key = FuncKey::ArrayToWasmTrampoline(self.module_index(), def_func_index);
+        let loc = self.index.func_loc(key)?;
         Some(&self.text()[loc.start as usize..][..loc.length as usize])
     }
 
@@ -155,67 +162,45 @@ impl CompiledModule {
     /// These trampolines are used for filling in
     /// `VMFuncRef::wasm_call` for `Func::wrap`-style host funcrefs
     /// that don't have access to a compiler when created.
-    pub fn wasm_to_array_trampoline(&self, signature: ModuleInternedTypeIndex) -> &[u8] {
-        let idx = match self
-            .wasm_to_array_trampolines
-            .binary_search_by_key(&signature, |entry| entry.0)
-        {
-            Ok(idx) => idx,
-            Err(_) => panic!("missing trampoline for {signature:?}"),
-        };
-
-        let (_, loc) = self.wasm_to_array_trampolines[idx];
-        &self.text()[loc.start as usize..][..loc.length as usize]
+    pub fn wasm_to_array_trampoline(&self, signature: ModuleInternedTypeIndex) -> Option<&[u8]> {
+        let key = FuncKey::WasmToArrayTrampoline(signature);
+        let loc = self.index.func_loc(key)?;
+        Some(&self.text()[loc.start as usize..][..loc.length as usize])
     }
 
     /// Lookups a defined function by a program counter value.
     ///
     /// Returns the defined function index and the relative address of
     /// `text_offset` within the function itself.
-    pub fn func_by_text_offset(&self, text_offset: usize) -> Option<(DefinedFuncIndex, u32)> {
+    pub fn func_by_text_offset(&self, text_offset: usize) -> Option<DefinedFuncIndex> {
         let text_offset = u32::try_from(text_offset).unwrap();
-
-        let index = match self.funcs.binary_search_values_by_key(&text_offset, |e| {
-            debug_assert!(e.wasm_func_loc.length > 0);
-            // Return the inclusive "end" of the function
-            e.wasm_func_loc.start + e.wasm_func_loc.length - 1
-        }) {
-            Ok(k) => {
-                // Exact match, pc is at the end of this function
-                k
+        let key = self.index.func_by_text_offset(text_offset)?;
+        match key {
+            FuncKey::DefinedWasmFunction(module, def_func_index) => {
+                debug_assert_eq!(module, self.module_index());
+                Some(def_func_index)
             }
-            Err(k) => {
-                // Not an exact match, k is where `pc` would be "inserted"
-                // Since we key based on the end, function `k` might contain `pc`,
-                // so we'll validate on the range check below
-                k
-            }
-        };
-
-        let CompiledFunctionInfo { wasm_func_loc, .. } = self.funcs.get(index)?;
-        let start = wasm_func_loc.start;
-        let end = wasm_func_loc.start + wasm_func_loc.length;
-
-        if text_offset < start || end < text_offset {
-            return None;
+            _ => None,
         }
-
-        Some((index, text_offset - wasm_func_loc.start))
     }
 
     /// Gets the function location information for a given function index.
-    pub fn func_loc(&self, index: DefinedFuncIndex) -> &FunctionLoc {
-        &self
-            .funcs
-            .get(index)
+    pub fn func_loc(&self, def_func_index: DefinedFuncIndex) -> &FunctionLoc {
+        assert!(def_func_index.index() < self.module.num_defined_funcs());
+        let key = FuncKey::DefinedWasmFunction(self.module_index(), def_func_index);
+        self.index
+            .func_loc(key)
             .expect("defined function should be present")
-            .wasm_func_loc
     }
 
     /// Returns the original binary offset in the file that `index` was defined
     /// at.
-    pub fn func_start_srcloc(&self, index: DefinedFuncIndex) -> FilePos {
-        self.funcs[index].start_srcloc
+    pub fn func_start_srcloc(&self, def_func_index: DefinedFuncIndex) -> FilePos {
+        assert!(def_func_index.index() < self.module.num_defined_funcs());
+        let key = FuncKey::DefinedWasmFunction(self.module_index(), def_func_index);
+        self.index
+            .src_loc(key)
+            .expect("defined function should be present")
     }
 
     /// Creates a new symbolication context which can be used to further

cranelift/entity/src/primary.rs
@@ -67,6 +67,11 @@ where
         self.elems.get(k.index())
     }
 
+    /// Get the slice of values associated with the given range of keys, if any.
+    pub fn get_range(&self, range: core::ops::Range<K>) -> Option<&[V]> {
+        self.elems.get(range.start.index()..range.end.index())
+    }
+
     /// Get the element at `k` if it exists, mutable version.
     pub fn get_mut(&mut self, k: K) -> Option<&mut V> {
         self.elems.get_mut(k.index())
@@ -95,6 +100,11 @@ where
     /// Iterate over all the values in this map, mutable edition.
     pub fn values_mut(&mut self) -> slice::IterMut<'_, V> {
         self.elems.iter_mut()
+    }
+
+    /// Get this map's underlying values as a slice.
+    pub fn as_values_slice(&self) -> &[V] {
+        &self.elems
     }
 
     /// Iterate over all the keys and values in this map.
@@ -291,6 +301,18 @@ where
             elems: Vec::from_iter(iter),
             unused: PhantomData,
         }
+    }
+}
+
+impl<K, V> Extend<V> for PrimaryMap<K, V>
+where
+    K: EntityRef,
+{
+    fn extend<T>(&mut self, iter: T)
+    where
+        T: IntoIterator<Item = V>,
+    {
+        self.elems.extend(iter);
     }
 }
 

crates/environ/src/address_map.rs
@@ -30,10 +30,22 @@ impl FilePos {
         FilePos(pos)
     }
 
+    /// Get the null file position.
+    pub fn none() -> FilePos {
+        FilePos(u32::MAX)
+    }
+
+    /// Is this the null file position?
+    #[inline]
+    pub fn is_none(&self) -> bool {
+        *self == FilePos::none()
+    }
+
     /// Returns the offset that this offset was created with.
     ///
-    /// Note that the `Default` implementation will return `None` here, whereas
-    /// positions created with `FilePos::new` will return `Some`.
+    /// Note that positions created with `FilePos::none` and the `Default`
+    /// implementation will return `None` here, whereas positions created with
+    /// `FilePos::new` will return `Some`.
     pub fn file_offset(self) -> Option<u32> {
         if self.0 == u32::MAX {
             None
@@ -45,7 +57,7 @@ impl FilePos {
 
 impl Default for FilePos {
     fn default() -> FilePos {
-        FilePos(u32::MAX)
+        FilePos::none()
     }
 }
 

crates/environ/src/compile/mod.rs
@@ -3,10 +3,9 @@
 
 use crate::prelude::*;
 use crate::{
-    DefinedFuncIndex, FlagValue, FunctionLoc, ObjectKind, PrimaryMap, StaticModuleIndex, TripleExt,
-    WasmError, WasmFuncType,
+    DefinedFuncIndex, FlagValue, FuncKey, FunctionLoc, ObjectKind, PrimaryMap, StaticModuleIndex,
+    TripleExt, Tunables, WasmError, WasmFuncType, obj,
 };
-use crate::{Tunables, obj};
 use anyhow::Result;
 use object::write::{Object, SymbolId};
 use object::{Architecture, BinaryFormat, FileFlags};
@@ -17,7 +16,6 @@ use std::path;
 use std::sync::Arc;
 
 mod address_map;
-mod key;
 mod module_artifacts;
 mod module_environ;
 mod module_types;
@@ -25,7 +23,6 @@ mod stack_maps;
 mod trap_encoding;
 
 pub use self::address_map::*;
-pub use self::key::*;
 pub use self::module_artifacts::*;
 pub use self::module_environ::*;
 pub use self::module_types::*;

crates/environ/src/compile/module_artifacts.rs
@@ -3,9 +3,8 @@
 
 use crate::prelude::*;
 use crate::{
-    CompiledFunctionInfo, CompiledModuleInfo, DebugInfoData, DefinedFuncIndex, FunctionLoc,
-    FunctionName, MemoryInitialization, Metadata, ModuleInternedTypeIndex, ModuleTranslation,
-    PrimaryMap, Tunables, obj,
+    CompiledModuleInfo, DebugInfoData, FunctionName, MemoryInitialization, Metadata,
+    ModuleTranslation, Tunables, obj,
 };
 use anyhow::{Result, bail};
 use object::SectionKind;
@@ -113,12 +112,7 @@ impl<'a> ObjectBuilder<'a> {
     /// Returns the `CompiledModuleInfo` corresponding to this core Wasm module
     /// as a result of this append operation. This is then serialized into the
     /// final artifact by the caller.
-    pub fn append(
-        &mut self,
-        translation: ModuleTranslation<'_>,
-        funcs: PrimaryMap<DefinedFuncIndex, CompiledFunctionInfo>,
-        wasm_to_array_trampolines: Vec<(ModuleInternedTypeIndex, FunctionLoc)>,
-    ) -> Result<CompiledModuleInfo> {
+    pub fn append(&mut self, translation: ModuleTranslation<'_>) -> Result<CompiledModuleInfo> {
         let ModuleTranslation {
             mut module,
             debuginfo,
@@ -219,8 +213,6 @@ impl<'a> ObjectBuilder<'a> {
 
         Ok(CompiledModuleInfo {
             module,
-            funcs,
-            wasm_to_array_trampolines,
             func_names,
             meta: Metadata {
                 has_unparsed_debuginfo,

crates/environ/src/component/artifacts.rs
@@ -2,8 +2,8 @@
 //! which are serialized with `bincode` into output ELF files.
 
 use crate::{
-    CompiledModuleInfo, FunctionLoc, PrimaryMap, StaticModuleIndex,
-    component::{Component, ComponentTypes, TrampolineIndex, TypeComponentIndex},
+    CompiledFunctionsTable, CompiledModuleInfo, PrimaryMap, StaticModuleIndex,
+    component::{Component, ComponentTypes, TypeComponentIndex},
 };
 use serde_derive::{Deserialize, Serialize};
 
@@ -14,6 +14,8 @@ pub struct ComponentArtifacts {
     pub ty: TypeComponentIndex,
     /// Information all kept available at runtime as-is.
     pub info: CompiledComponentInfo,
+    /// The index of every compiled function's location in the text section.
+    pub table: CompiledFunctionsTable,
     /// Type information for this component and all contained modules.
     pub types: ComponentTypes,
     /// Serialized metadata about all included core wasm modules.
@@ -25,16 +27,4 @@ pub struct ComponentArtifacts {
 pub struct CompiledComponentInfo {
     /// Type information calculated during translation about this component.
     pub component: Component,
-
-    /// Where lowered function wasm-call trampolines that end up in a
-    /// `VMFuncRef` are located within the `text` section of `code_memory`.
-    pub wasm_call_trampolines: PrimaryMap<TrampolineIndex, FunctionLoc>,
-
-    /// Where lowered function array-call trampolines that end up in a
-    /// `VMFuncRef` are located within the `text` section of `code_memory`.
-    pub array_call_trampolines: PrimaryMap<TrampolineIndex, FunctionLoc>,
-
-    /// The location of the wasm-to-array trampoline for the `resource.drop`
-    /// intrinsic.
-    pub resource_drop_wasm_to_array_trampoline: Option<FunctionLoc>,
 }

crates/environ/src/key.rs
@@ -230,22 +230,8 @@ impl FuncKey {
         kind << Self::KIND_OFFSET
     }
 
-    /// Get this key's kind.
-    pub fn kind(self) -> FuncKeyKind {
-        self.namespace().kind()
-    }
-
-    /// Get this key's namespace.
-    pub fn namespace(self) -> FuncKeyNamespace {
-        self.into_parts().0
-    }
-
-    /// Get this key's index.
-    pub fn index(self) -> FuncKeyIndex {
-        self.into_parts().1
-    }
-
     /// Split this key into its namespace and index halves.
+    #[inline]
     pub fn into_parts(self) -> (FuncKeyNamespace, FuncKeyIndex) {
         let (namespace, index) = match self {
             FuncKey::DefinedWasmFunction(module, def_func) => {
@@ -294,16 +280,19 @@ impl FuncKey {
         (FuncKeyNamespace(namespace), FuncKeyIndex(index))
     }
 
-    /// Create a key from its namespace and index parts.
-    ///
-    /// Should only be called with namespaces and indices that are ultimately
-    /// derived from the same key. For example, if you attempt to pair an index
-    /// and namespace that come from different keys, that may panic. If it
-    /// happens not to panic, you'll end up with a valid key that names an
-    /// arbitrary function in the given namespace, but that function probably
-    /// does not actually exist in the compilation artifact.
-    pub fn from_parts(namespace: FuncKeyNamespace, index: FuncKeyIndex) -> Self {
-        Self::from_raw_parts(namespace.into_raw(), index.into_raw())
+    /// Get this key's kind.
+    pub fn kind(self) -> FuncKeyKind {
+        self.namespace().kind()
+    }
+
+    /// Get this key's namespace.
+    pub fn namespace(self) -> FuncKeyNamespace {
+        self.into_parts().0
+    }
+
+    /// Get this key's index.
+    pub fn index(self) -> FuncKeyIndex {
+        self.into_parts().1
     }
 
     /// Get the raw, underlying `(namespace, index)` representation of this
@@ -318,6 +307,18 @@ impl FuncKey {
     pub fn into_raw_parts(self) -> (u32, u32) {
         let (ns, index) = self.into_parts();
         (ns.into_raw(), index.into_raw())
+    }
+
+    /// Create a key from its namespace and index parts.
+    ///
+    /// Should only be called with namespaces and indices that are ultimately
+    /// derived from the same key. For example, if you attempt to pair an index
+    /// and namespace that come from different keys, that may panic. If it
+    /// happens not to panic, you'll end up with a valid key that names an
+    /// arbitrary function in the given namespace, but that function probably
+    /// does not actually exist in the compilation artifact.
+    pub fn from_parts(namespace: FuncKeyNamespace, index: FuncKeyIndex) -> Self {
+        Self::from_raw_parts(namespace.into_raw(), index.into_raw())
     }
 
     /// Create a key from its raw, underlying representation.

crates/environ/src/lib.rs
@@ -26,6 +26,7 @@ mod error;
 mod ext;
 mod gc;
 mod hostcall;
+mod key;
 mod module;
 mod module_artifacts;
 mod module_types;
@@ -46,6 +47,7 @@ pub use crate::demangling::*;
 pub use crate::error::*;
 pub use crate::gc::*;
 pub use crate::hostcall::*;
+pub use crate::key::*;
 pub use crate::module::*;
 pub use crate::module_artifacts::*;
 pub use crate::module_types::*;

commit 743d5b85afab05c085423fb07d850437fa23f724

Fix a panic enabling profiling
This fixes a regression from #11630 where a panic happened when loading
a component with profiling enabled.

Closes #12158

crates/wasmtime/src/runtime/instantiate.rs
@@ -176,8 +176,14 @@ impl CompiledModule {
         let key = self.index.func_by_text_offset(text_offset)?;
         match key {
             FuncKey::DefinedWasmFunction(module, def_func_index) => {
-                debug_assert_eq!(module, self.module_index());
-                Some(def_func_index)
+                // If this function is for `self` then pass it through,
+                // otherwise it's for some other module in this image so
+                // there's no `DefinedFuncIndex` for this offset.
+                if module == self.module_index() {
+                    Some(def_func_index)
+                } else {
+                    None
+                }
             }
             _ => None,
         }

tests/all/main.rs
@@ -43,6 +43,7 @@ mod native_debug;
 mod noextern;
 mod piped_tests;
 mod pooling_allocator;
+mod profiling;
 mod pulley;
 mod relocs;
 mod stack_creator;

tests/all/profiling.rs
@@ -1,0 +1,23 @@
+use wasmtime::component::Component;
+use wasmtime::{Config, Engine, Module, Result};
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn perfmap() -> Result<()> {
+    let mut config = Config::new();
+    config.profiler(wasmtime::ProfilingStrategy::PerfMap);
+    let engine = Engine::new(&config)?;
+
+    Module::new(&engine, "(module (func))")?;
+    Component::new(&engine, "(component)")?;
+    Component::new(&engine, "(component (core module (func)))")?;
+    Component::new(
+        &engine,
+        "(component
+            (core module (func))
+            (core module (func))
+        )",
+    )?;
+
+    Ok(())
+}