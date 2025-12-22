e490fb7ac616aa756bfa032aa61cc6b10e398f8d
Nick Fitzgerald <fitzgen@gmail.com>
2025-12-22 10:39:04 -0800

    A few follow ups for the `wasmtime_internal_error` crate

--- crates/error/src/boxed.rs
+++ crates/error/src/boxed.rs
@@ -1,6 +1,7 @@
 use super::{OutOfMemory, Result};
 use alloc::boxed::Box;
 use core::alloc::Layout;
+use core::mem::MaybeUninit;
 use core::ptr::NonNull;
 
 /// Try to allocate a block of memory that fits the given layout, or return an
@@ -24,27 +25,20 @@ pub(crate) unsafe fn try_alloc(layout: Layout) -> Result<NonNull<u8>, OutOfMemor
 
 /// Create a `Box<T>`, or return an `OutOfMemory` error.
 #[inline]
-pub(crate) fn try_box<T>(value: T) -> Result<Box<T>, OutOfMemory> {
-    let layout = alloc::alloc::Layout::new::<T>();
+pub(crate) fn try_new_uninit_box<T>() -> Result<Box<MaybeUninit<T>>, OutOfMemory> {
+    let layout = alloc::alloc::Layout::new::<MaybeUninit<T>>();
 
     if layout.size() == 0 {
         // Safety: `Box` explicitly allows construction from dangling pointers
         // (which are guaranteed non-null and aligned) for zero-sized types.
-        return Ok(unsafe { Box::from_raw(core::ptr::dangling::<T>().cast_mut()) });
+        return Ok(unsafe { Box::from_raw(core::ptr::dangling::<MaybeUninit<T>>().cast_mut()) });
     }
 
     // Safety: layout size is non-zero.
     let ptr = unsafe { try_alloc(layout)? };
 
-    let ptr = ptr.cast::<T>();
+    let ptr = ptr.cast::<MaybeUninit<T>>();
 
-    // Safety: The allocation succeeded, and it has `T`'s layout, so the pointer
-    // is valid for writing a `T`.
-    unsafe {
-        ptr.write(value);
-    }
-
-    // Safety: The pointer's memory block was allocated by the global allocator,
-    // is valid for `T`, and is initialized.
+    // Safety: The pointer's memory block was allocated by the global allocator.
     Ok(unsafe { Box::from_raw(ptr.as_ptr()) })
 }
