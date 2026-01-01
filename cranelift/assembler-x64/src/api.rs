ca60524b13788d65f86e718b66824848d930d9ff
SSD <96286755+the-ssd@users.noreply.github.com>
2026-01-01 22:43:54 +0100

    Update cranelift/assembler-x64/src/api.rs
    Co-authored-by: bjorn3 <17426603+bjorn3@users.noreply.github.com>

--- cranelift/assembler-x64/src/api.rs
+++ cranelift/assembler-x64/src/api.rs
@@ -7,6 +7,7 @@ use alloc::string::String;
 use alloc::vec::Vec;
 use core::fmt;
 use core::num::NonZeroU8;
+
 /// Describe how an instruction is emitted into a code buffer.
 pub trait CodeSink {
     /// Add 1 byte to the code section.
