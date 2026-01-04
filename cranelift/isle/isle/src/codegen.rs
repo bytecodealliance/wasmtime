d86ae74fad0ea194edd9c963a5a7bd4441d5179a
Bongjun <bongjun.jang@kaist.ac.kr>
2026-01-04 04:50:34 +0000

    remove unnecessary feature guard

--- cranelift/isle/isle/src/codegen.rs
+++ cranelift/isle/isle/src/codegen.rs
@@ -685,7 +685,7 @@ impl<L: Length, C> Length for ContextIterWrapper<L, C> {{
                                 let pp = pos.pretty_print_line(&self.files);
                                 writeln!(
                                     ctx.out,
-                                    "{}#[cfg(feature = \"trace-log\")] log::debug!(\"ISLE {{}} {{}}\", \"{}\", \"{}\");",
+                                    "{}log::debug!(\"ISLE {{}} {{}}\", \"{}\", \"{}\");",
                                     &ctx.indent, ctx.term_name, pp
                                 )?;
                             }
