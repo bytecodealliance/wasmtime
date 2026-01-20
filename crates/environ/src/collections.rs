ce21fed81279ba0dee3d30f3f4abe1c4cabf02a4
Nick Fitzgerald <fitzgen@gmail.com>
2026-01-20 12:16:14 -0800

    Remove `out_of_line_slow_path!` macro

--- crates/environ/src/collections.rs
+++ crates/environ/src/collections.rs
@@ -3,44 +3,6 @@
 use crate::error::OutOfMemory;
 use core::{alloc::Layout, ptr::NonNull};
 
-/// Internal macro to mark a block as a slow path, pulling it out into its own
-/// cold function that is never inlined.
-///
-/// This should be applied to the whole consequent/alternative block for a
-/// conditional, never to a single expression within a larger block.
-///
-/// # Example
-///
-/// ```ignore
-/// fn hot_function(x: u32) -> Result<()> {
-///     if very_rare_condition(x) {
-///         return out_of_line_slow_path! {
-///             // Handle the rare case...
-///             //
-///             // This pulls the handling of the rare condition out into
-///             // its own, separate function, which keeps the generated code
-///             // tight, handling only the common cases inline.
-///             Ok(())
-///         };
-///     }
-///
-///     // Handle the common case inline...
-///     Ok(())
-/// }
-/// ```
-macro_rules! out_of_line_slow_path {
-    ( $e:expr ) => {{
-        #[cold]
-        #[inline(never)]
-        #[track_caller]
-        fn out_of_line_slow_path<T>(f: impl FnOnce() -> T) -> T {
-            f()
-        }
-
-        out_of_line_slow_path(|| $e)
-    }};
-}
-
 mod arc;
 mod boxed;
 pub use arc::OomArc;
@@ -61,8 +23,6 @@ unsafe fn try_alloc(layout: Layout) -> Result<NonNull<u8>, OutOfMemory> {
     if let Some(ptr) = NonNull::new(ptr) {
         Ok(ptr)
     } else {
-        out_of_line_slow_path! {
-            Err(OutOfMemory::new(layout.size()))
-        }
+        Err(OutOfMemory::new(layout.size()))
     }
 }
