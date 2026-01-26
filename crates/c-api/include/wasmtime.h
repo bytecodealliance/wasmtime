f025b5a656cc1c0919ccc16e468b4f066337af02
Wasmtime Publish <wasmtime-publish@users.noreply.github.com>
2026-01-26 19:12:24 +0000

    Release Wasmtime 36.0.5
    [automatically-tag-and-release-this-commit]

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -213,7 +213,7 @@
 /**
  * \brief Wasmtime version string.
  */
-#define WASMTIME_VERSION "36.0.4"
+#define WASMTIME_VERSION "36.0.5"
 /**
  * \brief Wasmtime major version number.
  */
@@ -225,6 +225,6 @@
 /**
  * \brief Wasmtime patch version number.
  */
-#define WASMTIME_VERSION_PATCH 4
+#define WASMTIME_VERSION_PATCH 5
 
 #endif // WASMTIME_API_H
