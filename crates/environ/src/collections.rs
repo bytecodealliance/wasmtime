990a5ce0ee522794b2d204197a67d038e4a80e56
Nick Fitzgerald <fitzgen@gmail.com>
2026-01-27 12:12:56 -0800

    Add a fallible-allocations-only version of `SecondaryMap`
    Part of https://github.com/bytecodealliance/wasmtime/issues/12069

--- crates/environ/src/collections.rs
+++ crates/environ/src/collections.rs
@@ -2,7 +2,9 @@
 
 mod entity_set;
 mod primary_map;
+mod secondary_map;
 
 pub use entity_set::EntitySet;
 pub use primary_map::PrimaryMap;
+pub use secondary_map::SecondaryMap;
 pub use wasmtime_core::alloc::{TryNew, Vec, try_new};
