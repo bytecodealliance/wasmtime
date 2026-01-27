487d1fcc0b03da4c4aea4e69f0c103ed9d69c375
Bongjun Jang <bongjun.jang@kaist.ac.kr>
2026-01-28 01:39:46 +0900

    Optimize ISLE compilation (#12303)
    * optimize isle codegen for rustc
    
    * opt-in for optimizing isle codegen
    
    * have a controller threshold

--- cranelift/codegen/build.rs
+++ cranelift/codegen/build.rs
@@ -216,6 +216,14 @@ fn run_compilation(compilation: &IsleCompilation) -> Result<(), Errors> {
         // the generated code to help debug rule matching.
         options.emit_logging = std::env::var("CARGO_FEATURE_TRACE_LOG").is_ok();
 
+        // Enable optional match-arm splitting in iterator terms for faster compile times.
+        options.split_match_arms = std::env::var("CARGO_FEATURE_ISLE_SPLIT_MATCH").is_ok();
+        if let Ok(value) = std::env::var("ISLE_SPLIT_MATCH_THRESHOLD") {
+            options.match_arm_split_threshold = Some(value.parse().unwrap_or_else(|err| {
+                panic!("invalid ISLE_SPLIT_MATCH_THRESHOLD value '{value}': {err}");
+            }));
+        }
+
         if let Ok(out_dir) = std::env::var("OUT_DIR") {
             options.prefixes.push(isle::codegen::Prefix {
                 prefix: out_dir,
