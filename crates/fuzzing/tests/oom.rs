f0011184ee537581bd907870798d3b9163fb0909
Nick Fitzgerald <fitzgen@gmail.com>
2026-02-05 15:48:55 -0800

    Add a fallible-allocation-only version of `HashMap` (#12535)
    * Add a fallible-allocation-only version of `HashMap`
    
    * fix link in doc comment

--- crates/fuzzing/tests/oom.rs
+++ crates/fuzzing/tests/oom.rs
@@ -176,6 +176,50 @@ fn hash_set_insert() -> Result<()> {
 }
 
 #[test]
+fn hash_map_with_capacity() -> Result<()> {
+    OomTest::new().test(|| {
+        let _s = HashMap::<usize, usize>::with_capacity(100)?;
+        Ok(())
+    })
+}
+
+#[test]
+fn hash_map_reserve() -> Result<()> {
+    OomTest::new().test(|| {
+        let mut map = HashMap::<usize, usize>::new();
+        map.reserve(100)?;
+        Ok(())
+    })
+}
+
+#[test]
+fn hash_map_insert() -> Result<()> {
+    OomTest::new().test(|| {
+        let mut map = HashMap::<usize, usize>::new();
+        for i in 0..1024 {
+            map.insert(i, i * 2)?;
+        }
+        for i in 0..1024 {
+            map.insert(i, i * 2)?;
+        }
+        Ok(())
+    })
+}
+
+#[test]
+fn hash_map_try_clone() -> Result<()> {
+    OomTest::new().test(|| {
+        let mut map = HashMap::new();
+        for i in 0..10 {
+            map.insert(i, i * 2)?;
+        }
+        let map2 = map.try_clone()?;
+        assert_eq!(map, map2);
+        Ok(())
+    })
+}
+
+#[test]
 fn vec_with_capacity() -> Result<()> {
     OomTest::new().test(|| {
         let _v = wasmtime_environ::collections::Vec::<usize>::with_capacity(100)?;
