2d3ab3011e6767bf04ef2152ab0ac282e0e1953a
Michael McLoughlin <mcloughlin@cmu.edu>
2026-02-11 21:50:38 -0500

    "is permitted to be recursive"

--- cranelift/isle/isle/src/ast.rs
+++ cranelift/isle/isle/src/ast.rs
@@ -80,7 +80,7 @@ pub struct Decl {
     pub multi: bool,
     /// Whether this term's constructor can fail to match.
     pub partial: bool,
-    /// Whether this term is recursive.
+    /// Whether this term is permitted to be recursive.
     pub rec: bool,
     pub pos: Pos,
 }
