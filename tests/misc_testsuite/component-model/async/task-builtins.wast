bytecodealliance/wasmtime

commit 3764e7573e36e9e641e3033e066004c9f79eac9a

Refactor borrow state tracking for async tasks  (#12550)
* Refactor borrow state tracking for async tasks

This commit is a somewhat deep refactoring of how the state of
`borrow<T>` is managed for both the host and the guest with respect to
async tasks. This additionally refactors how some async task management
is done for host-called functions.

The fundamental problem being tackled here is #12510. In that issue it
was discovered that the way `CallContext`, the borrow tracking mechanism
in Wasmtime, is managed is incompatible with async tasks. Specifically
the previous assumption of the scope being mutated for a borrow is
somewhere on the call stack is no longer true. It's possible for an
async task to be suspended, for example, and then a sibling task drops a
borrow which should update the scope of the suspended task. There were a
number of other small issues I noticed here and there which this PR
additionally has tests for, all of which failed before this change and
pass afterwards.

The manner in which borrow state is manipulated is a pretty old part of
the component model implementation dating back to the original
implementation of resources. I decided to forgo any possible quick fix
and have attempted to more deeply refactor and integrate async tasks
into all of this infrastructure. A list of the changes made here are:

* The `CallContexts` structure, a stack of `CallContext`, was removed.
  Tasks now directly store a `CallContext` which is the source of truth
  for borrow tracking for that call, and it does not move from this
  location. The store `CallContexts` is now deleted in favor of updating
  the `Option<ConcurrentState>` in the store to be an `enum` of either
  concurrent state or a stack. In this manner the old stack-based
  structure is still used sometimes, but it's impossible to reach when
  concurrency is enabled.

* Entry to the host from guests now reliably pushes a `HostTask` into

crates/environ/src/fact/trampoline.rs
@@ -797,14 +797,13 @@ impl<'a, 'b> Compiler<'a, 'b> {
             self.instruction(Call(enter_sync_call.as_u32()));
 
             old_task_may_block
+        } else if self.emit_resource_call {
+            let enter_sync_call = self.module.import_enter_sync_call();
+            self.instruction(Call(enter_sync_call.as_u32()));
+            None
         } else {
             None
         };
-
-        if self.emit_resource_call {
-            let enter = self.module.import_resource_enter_call();
-            self.instruction(Call(enter.as_u32()));
-        }
 
         // Perform the translation of arguments. Note that `FLAG_MAY_LEAVE` is
         // cleared around this invocation for the callee as per the
@@ -866,18 +865,15 @@ impl<'a, 'b> Compiler<'a, 'b> {
             self.free_temp_local(tmp);
         }
 
-        if self.emit_resource_call {
-            let exit = self.module.import_resource_exit_call();
-            self.instruction(Call(exit.as_u32()));
-        }
-
-        if self.module.tunables.concurrency_support {
+        if self.emit_resource_call || self.module.tunables.concurrency_support {
             // Pop the task we pushed earlier off of the current task stack.
             //
             // FIXME: Apply the optimizations described in #12311.
             let exit_sync_call = self.module.import_exit_sync_call();
             self.instruction(Call(exit_sync_call.as_u32()));
+        }
 
+        if self.module.tunables.concurrency_support {
             // Restore old `may_block_field`
             if let Some(old_task_may_block) = old_task_may_block {
                 let task_may_block = self.module.import_task_may_block();

crates/wasmtime/src/runtime/component/func.rs
@@ -603,10 +603,8 @@ impl Func {
             bail!(crate::Trap::CannotEnterComponent);
         }
 
-        if store.0.concurrency_support() {
-            let async_type = self.abi_async(store.0);
-            store.0.enter_sync_call(None, async_type, instance)?;
-        }
+        let async_type = self.abi_async(store.0);
+        store.0.enter_guest_sync_call(None, async_type, instance)?;
 
         #[repr(C)]
         union Union<Params: Copy, Return: Copy> {
@@ -632,7 +630,6 @@ impl Func {
         assert!(mem::align_of_val(map_maybe_uninit!(space.ret)) == val_align);
 
         self.with_lower_context(store.as_context_mut(), |cx, ty| {
-            cx.enter_call();
             lower(cx, ty, map_maybe_uninit!(space.params))
         })?;
 
@@ -716,11 +713,8 @@ impl Func {
             store
                 .0
                 .component_resource_tables(Some(self.instance))
-                .exit_call()?;
-
-            if store.0.concurrency_support() {
-                store.0.exit_sync_call(false)?;
-            }
+                .validate_scope_exit()?;
+            store.0.exit_guest_sync_call(false)?;
         }
         Ok(())
     }

commit 85f629fc43ec04ab684cc8f68cf00f424a1da5ec

Fix the current guest task when calling `realloc`
This commit fixes some fallout from #12550 where after that PR the
current task registered when a guest's `realloc` was called was
incorrect. This additionally fixes issues where guest-to-guest
trampolines also had the wrong task registered when `realloc` was being
called. Finally this adjusts a few locations that "no borrows should be
active" traps happen to ensure that it happens around the time of when a
task returns rather than after the lowering of its values happens.

crates/environ/src/fact/trampoline.rs
@@ -840,6 +840,25 @@ impl<'a, 'b> Compiler<'a, 'b> {
         }
         result_locals.reverse();
 
+        // Handle a few things related to the concurrent task infrastructure
+        // after the callee has finished, such as:
+        //
+        // * Validate that the callee dropped all its borrows
+        // * Transition the current running task back to the caller.
+        //
+        // This is not necessary if there are no resources in this call, nor if
+        // concurrency support is disabled (no tasks). Note that this must
+        // happen before lowering below because semantically that's where the
+        // "you forgot to drop borrows" trap shows up and additionally the
+        // lowering below may call realloc which is in the context of the
+        // caller's task, not the callee.
+        //
+        // FIXME: Apply the optimizations described in #12311.
+        if self.emit_resource_call || self.module.tunables.concurrency_support {
+            let exit_sync_call = self.module.import_exit_sync_call();
+            self.instruction(Call(exit_sync_call.as_u32()));
+        }
+
         // Like above during the translation of results the caller cannot be
         // left (as we might invoke things like `realloc`). Again the precise
         // order of everything doesn't matter since intermediate states cannot
@@ -863,14 +882,6 @@ impl<'a, 'b> Compiler<'a, 'b> {
 
         for tmp in temps {
             self.free_temp_local(tmp);
-        }
-
-        if self.emit_resource_call || self.module.tunables.concurrency_support {
-            // Pop the task we pushed earlier off of the current task stack.
-            //
-            // FIXME: Apply the optimizations described in #12311.
-            let exit_sync_call = self.module.import_exit_sync_call();
-            self.instruction(Call(exit_sync_call.as_u32()));
         }
 
         if self.module.tunables.concurrency_support {

crates/wasmtime/src/runtime/component/concurrent.rs
@@ -1547,34 +1547,50 @@ impl StoreOpaque {
     /// relatively expensive table manipulations. This would ideally be
     /// optimized to avoid the full allocation of a `HostTask` in at least some
     /// situations.
-    pub fn enter_host_call(&mut self) -> Result<()> {
+    pub(crate) fn host_task_create(&mut self) -> Result<Option<TableId<HostTask>>> {
         if !self.concurrency_support() {
             self.enter_call_not_concurrent();
-            return Ok(());
+            return Ok(None);
         }
         let state = self.concurrent_state_mut();
         let caller = state.current_guest_thread()?;
         let task = state.push(HostTask::new(caller, HostTaskState::CalleeStarted))?;
         log::trace!("new host task {task:?}");
         self.set_thread(task)?;
+        Ok(Some(task))
+    }
+
+    /// Invoked before lowering the results of a host task to the guest.
+    ///
+    /// This is used to update the current thread annotations within the store
+    /// to ensure that it reflects the guest task, not the host task, since
+    /// lowering may execute guest code.
+    pub fn host_task_reenter_caller(&mut self) -> Result<()> {
+        if !self.concurrency_support() {
+            return Ok(());
+        }
+        let task = self.concurrent_state_mut().current_host_thread()?;
+        let caller = self.concurrent_state_mut().get_mut(task)?.caller;
+        self.set_thread(caller)?;
         Ok(())
     }
 
-    /// Dual of `enter_host_call` and signifies that the host has finished and
+    /// Dual of `host_task_create` and signifies that the host has finished and
     /// will be cleaned up.
     ///
     /// Note that this isn't invoked when the host is invoked asynchronously and
     /// the host isn't complete yet. In that situation the host task persists
-    /// and will be cleaned up separately.
-    pub fn exit_host_call(&mut self) -> Result<()> {
-        if !self.concurrency_support() {
-            self.exit_call_not_concurrent();
-            return Ok(());
+    /// and will be cleaned up separately in `subtask_drop`
+    pub(crate) fn host_task_delete(&mut self, task: Option<TableId<HostTask>>) -> Result<()> {
+        match task {
+            Some(task) => {
+                log::trace!("delete host task {task:?}");
+                self.concurrent_state_mut().delete(task)?;
+            }
+            None => {
+                self.exit_call_not_concurrent();
+            }
         }
-        let task = self.concurrent_state_mut().current_host_thread()?;
-        log::trace!("delete host task {task:?}");
-        let task = self.concurrent_state_mut().delete(task)?;
-        self.set_thread(task.caller)?;
         Ok(())
     }
 
@@ -4183,7 +4199,7 @@ type HostTaskFuture = Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>
 /// Represents the state of a pending host task.
 ///
 /// This is used to represent tasks when the guest calls into the host.
-struct HostTask {
+pub(crate) struct HostTask {
     common: WaitableCommon,
 
     /// Guest thread which called the host.

crates/wasmtime/src/runtime/component/func.rs
@@ -649,6 +649,13 @@ impl Func {
             )?;
         }
 
+        // Validate that the task, after returning, has no more active borrows
+        // as they're required to have been dropped by this point.
+        store
+            .0
+            .component_resource_tables(Some(self.instance))
+            .validate_scope_exit()?;
+
         // SAFETY: We're relying on the correctness of the structure of
         // `LowerReturn` and the type-checking performed to acquire the
         // `TypedFunc` to make this safe. It should be the case that
@@ -706,11 +713,6 @@ impl Func {
 
         unsafe {
             call_post_return(&mut store, post_return, arg, flags)?;
-
-            store
-                .0
-                .component_resource_tables(Some(self.instance))
-                .validate_scope_exit()?;
             store.0.exit_guest_sync_call()?;
         }
         Ok(())

crates/wasmtime/src/runtime/component/func/host.rs
@@ -378,9 +378,9 @@ where
         }
 
         // Enter the host by pushing a `HostTask` into the concurrent state.
-        store.0.enter_host_call()?;
+        let host_task = store.0.host_task_create()?;
 
-        let task_exited = if async_ {
+        let host_task_complete = if async_ {
             #[cfg(feature = "component-model-async")]
             {
                 self.call_async_lower(store.as_context_mut(), instance, ty, options, storage)?
@@ -395,13 +395,13 @@ where
             true
         };
 
-        // If the host task exited, then it's popped and deallocated.
+        // If the host task completed, then it's deallocated.
         //
         // Note that if the host task did not exit then the `call_async_lower`
         // function transitively would have updated the current guest thread to
         // the caller of this host function.
-        if task_exited {
-            store.0.exit_host_call()?;
+        if host_task_complete {
+            store.0.host_task_delete(host_task)?;
         }
 
         Ok(())
@@ -571,6 +571,16 @@ where
         ret: Option<R>,
         dst: Destination<'_>,
     ) -> Result<()> {
+        // Before lowering below semantically ensure that the caller has dropped
+        // all of its borrows and such.
+        lower.validate_scope_exit()?;
+
+        // At this point we're transitioning back to the caller task which means
+        // that the current task needs to be updated. This will restore the
+        // currently running thread as the caller's thread, for example if
+        // lowering below calls `realloc` it'll use the right context.
+        lower.store.0.host_task_reenter_caller()?;
+
         if let Some(ret) = ret {
             let caller_instance = lower.options().instance;
             let mut flags = lower.instance_mut().instance_flags(caller_instance);
@@ -582,7 +592,6 @@ where
                 flags.set_may_leave(true);
             }
         }
-        lower.validate_scope_exit()?;
         Ok(())
     }
 }

crates/wast/src/spectest.rs
@@ -218,5 +218,6 @@ pub fn link_component_spectest<T>(linker: &mut component::Linker<T>) -> Result<(
             Box::pin(async move { std::future::pending::<Result<()>>().await })
         },
     )?;
+    i.func_wrap("return-hi", |_cx, (): ()| Ok(("hi".to_string(),)))?;
     Ok(())
 }

tests/all/component_model/resources.rs
@@ -814,7 +814,7 @@ fn thread_through_borrow() -> Result<()> {
 }
 
 #[test]
-fn cannot_use_borrow_for_own() -> Result<()> {
+fn borrows_must_be_dropped_before_lifting() -> Result<()> {
     let engine = super::engine();
     let c = Component::new(
         &engine,
@@ -824,6 +824,9 @@ fn cannot_use_borrow_for_own() -> Result<()> {
 
                 (core module $m
                     (func (export "f") (param i32) (result i32)
+                        ;; this is a `borrow $t`, and we're returning it as an
+                        ;; `own $t`. This should have an error about remaining
+                        ;; borrows rather than a mismatch of own/borrow.
                         local.get 0
                     )
                 )
@@ -848,7 +851,63 @@ fn cannot_use_borrow_for_own() -> Result<()> {
 
     let resource = Resource::new_own(100);
     let err = f.call(&mut store, (&resource,)).unwrap_err();
-    assert_eq!(err.to_string(), "cannot lift own resource from a borrow");
+    assert_eq!(
+        err.to_string(),
+        "borrow handles still remain at the end of the call"
+    );
+    Ok(())
+}
+
+#[test]
+fn cannot_use_borrow_for_own() -> Result<()> {
+    let engine = super::engine();
+    let c = Component::new(
+        &engine,
+        r#"
+            (component
+                (import "t" (type $t (sub resource)))
+                (import "f" (func $f (param "x" (own $t))))
+
+                (core module $m
+                    (import "" "f" (func $f (param i32)))
+                    (func (export "f") (param i32)
+                        local.get 0
+                        call $f
+                    )
+                )
+                (core func $f (canon lower (func $f)))
+                (core instance $i (instantiate $m
+                    (with "" (instance
+                        (export "f" (func $f))
+                    ))
+                ))
+
+                (func (export "f") (param "x" (borrow $t))
+                    (canon lift (core func $i "f")))
+            )
+        "#,
+    )?;
+
+    struct MyType;
+
+    let mut store = Store::new(&engine, ());
+    let mut linker = Linker::new(&engine);
+    linker
+        .root()
+        .resource("t", ResourceType::host::<MyType>(), |_, _| Ok(()))?;
+    linker
+        .root()
+        .func_wrap("f", |_, _: (Resource<MyType>,)| Ok(()))?;
+    let i = linker.instantiate(&mut store, &c)?;
+
+    let f = i.get_typed_func::<(&Resource<MyType>,), ()>(&mut store, "f")?;
+
+    let resource = Resource::new_own(100);
+    let err = f.call(&mut store, (&resource,)).unwrap_err();
+    assert!(
+        format!("{err:?}").contains("cannot lift own resource from a borrow"),
+        "bad error: {err:?}",
+    );
     Ok(())
 }
 

tests/misc_testsuite/component-model/async/task-builtins.wast
@@ -1,4 +1,6 @@
 ;;! component_model_async = true
+;;! multi_memory = true
+;;! reference_types = true
 
 ;; backpressure.inc
 (component
@@ -116,6 +118,342 @@
   ))))
   (func (export "run")
     (canon lift (core func $dm "run") (post-return (func $dm "post-return"))))
+)
+
+(assert_return (invoke "run"))
+
+;; Test calling intrinsics during `realloc` and their values should be set for
+;; when main function is called
+(component
+  (core func $backpressure.inc (canon backpressure.inc))
+  (core func $backpressure.dec (canon backpressure.dec))
+  (core func $context.get (canon context.get i32 0))
+  (core func $context.set (canon context.set i32 0))
+
+  (core module $libc
+    (import "" "backpressure.inc" (func $backpressure.inc))
+    (import "" "backpressure.dec" (func $backpressure.dec))
+    (import "" "context.get" (func $context.get (result i32)))
+    (import "" "context.set" (func $context.set (param i32)))
+
+    (memory (export "memory") 1)
+    (func (export "realloc") (param i32 i32 i32 i32) (result i32)
+      (if (i32.ne (local.get 0) (i32.const 0)) (then (unreachable)))
+      (if (i32.ne (local.get 1) (i32.const 0)) (then (unreachable)))
+      (if (i32.ne (local.get 2) (i32.const 1)) (then (unreachable)))
+      (if (i32.ne (local.get 3) (i32.const 2)) (then (unreachable)))
+
+      call $context.get
+      i32.const 0
+      i32.ne
+      if unreachable end
+
+      i32.const 100
+      call $context.set
+
+      call $backpressure.inc
+      call $backpressure.dec
+
+      i32.const 200
+    )
+  )
+
+  (core instance $libc (instantiate $libc (with "" (instance
+    (export "backpressure.inc" (func $backpressure.inc))
+    (export "backpressure.dec" (func $backpressure.dec))
+    (export "context.get" (func $context.get))
+    (export "context.set" (func $context.set))
+  ))))
+
+  (core module $M
+    (import "" "backpressure.inc" (func $backpressure.inc))
+    (import "" "backpressure.dec" (func $backpressure.dec))
+    (import "" "context.get" (func $context.get (result i32)))
+    (import "" "context.set" (func $context.set (param i32)))
+
+    (func (export "run") (param i32 i32)
+      ;; ptr/len assertion
+      (if (i32.ne (local.get 0) (i32.const 200)) (then (unreachable)))
+      (if (i32.ne (local.get 1) (i32.const 2)) (then (unreachable)))
+
+      call $backpressure.inc
+      call $backpressure.dec
+
+      ;; context.get should be what was set in `realloc`
+      (if (i32.ne (call $context.get) (i32.const 100)) (then (unreachable)))
+    )
+  )
+  (core instance $m (instantiate $M (with "" (instance
+    (export "backpressure.inc" (func $backpressure.inc))
+    (export "backpressure.dec" (func $backpressure.dec))
+    (export "context.get" (func $context.get))
+    (export "context.set" (func $context.set))
+  ))))
+  (func (export "run") (param "x" string)
+    (canon lift (core func $m "run")
+      (realloc (func $libc "realloc"))
+      (memory $libc "memory")
+    )
+  )
+)
+
+(assert_return (invoke "run" (str.const "hi")))
+
+;; Test when realloc is called to communicate return values that various
+;; intrinsics work and have their expected values.
+(component
+  (component $A
+    (core module $libc (memory (export "memory") 1))
+    (core instance $libc (instantiate $libc))
+
+    (core func $task.return (canon task.return (result string) (memory $libc "memory")))
+
+    (core module $m
+      (import "" "task.return" (func $task.return (param i32 i32)))
+      (import "" "memory" (memory 1))
+      (func (export "run-sync") (result i32)
+        i32.const 40
+        i32.const 100
+        i32.store offset=0
+
+        i32.const 40
+        i32.const 2
+        i32.store offset=4
+
+        i32.const 40
+      )
+      (func (export "run-async") (result i32)
+        i32.const 100
+        i32.const 2
+        call $task.return
+        i32.const 0 ;; CALLBACK_CODE_EXIT
+      )
+
+      (func (export "run-async-cb") (param i32 i32 i32) (result i32) unreachable)
+      (data (i32.const 100) "hi")
+    )
+    (core instance $m (instantiate $m
+      (with "" (instance
+        (export "task.return" (func $task.return))
+        (export "memory" (memory $libc "memory"))
+      ))
+    ))
+    (func (export "run-sync") (result string)
+      (canon lift (core func $m "run-sync") (memory $libc "memory"))
+    )
+    (func (export "run-async") async (result string)
+      (canon lift (core func $m "run-async") (memory $libc "memory") async
+          (callback (func $m "run-async-cb")))
+    )
+  )
+
+  (component $B
+    (import "a" (instance $a
+      (export "run-sync" (func (result string)))
+      (export "run-async" (func async (result string)))
+    ))
+
+    (core func $backpressure.inc (canon backpressure.inc))
+    (core func $backpressure.dec (canon backpressure.dec))
+    (core func $context.get (canon context.get i32 0))
+    (core func $context.set (canon context.set i32 0))
+
+    (core module $libc
+      (import "" "backpressure.inc" (func $backpressure.inc))
+      (import "" "backpressure.dec" (func $backpressure.dec))
+      (import "" "context.get" (func $context.get (result i32)))
+      (import "" "context.set" (func $context.set (param i32)))
+
+      (memory (export "memory") 1)
+      (func (export "realloc") (param i32 i32 i32 i32) (result i32)
+        (if (i32.ne (local.get 0) (i32.const 0)) (then (unreachable)))
+        (if (i32.ne (local.get 1) (i32.const 0)) (then (unreachable)))
+        (if (i32.ne (local.get 2) (i32.const 1)) (then (unreachable)))
+        (if (i32.ne (local.get 3) (i32.const 2)) (then (unreachable)))
+
+        (if (i32.ne (call $context.get) (i32.const 400)) (then (unreachable)))
+        (call $context.set (i32.const 500))
+
+        call $backpressure.inc
+        call $backpressure.dec
+
+        i32.const 200
+      )
+    )
+
+    (core instance $libc (instantiate $libc (with "" (instance
+      (export "backpressure.inc" (func $backpressure.inc))
+      (export "backpressure.dec" (func $backpressure.dec))
+      (export "context.get" (func $context.get))
+      (export "context.set" (func $context.set))
+    ))))
+
+    (core func $sync-to-sync
+      (canon lower (func $a "run-sync")
+        (memory $libc "memory")
+        (realloc (func $libc "realloc"))
+      )
+    )
+    (core func $async-to-sync
+      (canon lower (func $a "run-sync")
+        async
+        (memory $libc "memory")
+        (realloc (func $libc "realloc"))
+      )
+    )
+    (core func $sync-to-async
+      (canon lower (func $a "run-async")
+        (memory $libc "memory")
+        (realloc (func $libc "realloc"))
+      )
+    )
+    (core func $async-to-async
+      (canon lower (func $a "run-async")
+        async
+        (memory $libc "memory")
+        (realloc (func $libc "realloc"))
+      )
+    )
+
+    (core module $M
+      (import "" "context.set" (func $context.set (param i32)))
+      (import "" "context.get" (func $context.get (result i32)))
+      (import "" "sync-to-sync" (func $sync-to-sync (param i32)))
+      (import "" "sync-to-async" (func $sync-to-async (param i32)))
+      (import "" "async-to-sync" (func $async-to-sync (param i32) (result i32)))
+      (import "" "async-to-async" (func $async-to-async (param i32) (result i32)))
+
+      ;; set this tasks's context before calling $run, in calling $run the
+      ;; runtime will then call `realloc` above for the string return value
+      ;; which should see our 400 value. That will then set 500 which we should
+      ;; then see after the return.
+
+      (func (export "sync-to-sync")
+        (call $context.set (i32.const 400))
+        (call $sync-to-sync (i32.const 20))
+        (if (i32.ne (call $context.get) (i32.const 500)) (then (unreachable)))
+      )
+
+      (func (export "sync-to-async")
+        (call $context.set (i32.const 400))
+        (call $sync-to-async (i32.const 20))
+        (if (i32.ne (call $context.get) (i32.const 500)) (then (unreachable)))
+      )
+
+      (func (export "async-to-sync")
+        (call $context.set (i32.const 400))
+        (if
+          (i32.ne
+            (call $async-to-sync (i32.const 20))
+            (i32.const 2) ;; RETURNED
+          )
+          (then (unreachable))
+        )
+        (if (i32.ne (call $context.get) (i32.const 500)) (then (unreachable)))
+      )
+
+      (func (export "async-to-async")
+        (call $context.set (i32.const 400))
+        (if
+          (i32.ne
+            (call $async-to-async (i32.const 20))
+            (i32.const 2) ;; RETURNED
+          )
+          (then (unreachable))
+        )
+        (if (i32.ne (call $context.get) (i32.const 500)) (then (unreachable)))
+      )
+    )
+    (core instance $m (instantiate $M (with "" (instance
+      (export "context.set" (func $context.set))
+      (export "context.get" (func $context.get))
+      (export "sync-to-sync" (func $sync-to-sync))
+      (export "sync-to-async" (func $sync-to-async))
+      (export "async-to-sync" (func $async-to-sync))
+      (export "async-to-async" (func $async-to-async))
+    ))))
+    (func (export "sync-to-sync") async (canon lift (core func $m "sync-to-sync")))
+    (func (export "sync-to-async") async (canon lift (core func $m "sync-to-async")))
+    (func (export "async-to-sync") async (canon lift (core func $m "async-to-sync")))
+    (func (export "async-to-async") async (canon lift (core func $m "async-to-async")))
+  )
+
+  (instance $a (instantiate $A))
+  (instance $b (instantiate $B (with "a" (instance $a))))
+  (export "sync-to-sync" (func $b "sync-to-sync"))
+)
+
+(assert_return (invoke "sync-to-sync"))
+
+;; Same as above, but when calling the host.
+(component
+  (import "host" (instance $host
+    (export "return-hi" (func (result string)))
+  ))
+
+  (core func $backpressure.inc (canon backpressure.inc))
+  (core func $backpressure.dec (canon backpressure.dec))
+  (core func $context.get (canon context.get i32 0))
+  (core func $context.set (canon context.set i32 0))
+
+  (core module $libc
+    (import "" "backpressure.inc" (func $backpressure.inc))
+    (import "" "backpressure.dec" (func $backpressure.dec))
+    (import "" "context.get" (func $context.get (result i32)))
+    (import "" "context.set" (func $context.set (param i32)))
+
+    (memory (export "memory") 1)
+    (func (export "realloc") (param i32 i32 i32 i32) (result i32)
+      (if (i32.ne (local.get 0) (i32.const 0)) (then (unreachable)))
+      (if (i32.ne (local.get 1) (i32.const 0)) (then (unreachable)))
+      (if (i32.ne (local.get 2) (i32.const 1)) (then (unreachable)))
+      (if (i32.ne (local.get 3) (i32.const 2)) (then (unreachable)))
+
+      (if (i32.ne (call $context.get) (i32.const 400)) (then (unreachable)))
+      (call $context.set (i32.const 500))
+
+      call $backpressure.inc
+      call $backpressure.dec
+
+      i32.const 200
+    )
+  )
+
+  (core instance $libc (instantiate $libc (with "" (instance
+    (export "backpressure.inc" (func $backpressure.inc))
+    (export "backpressure.dec" (func $backpressure.dec))
+    (export "context.get" (func $context.get))
+    (export "context.set" (func $context.set))
+  ))))
+
+  (core func $run
+    (canon lower (func $host "return-hi")
+      (memory $libc "memory")
+      (realloc (func $libc "realloc"))
+    )
+  )
+
+  (core module $M
+    (import "" "context.set" (func $context.set (param i32)))
+    (import "" "context.get" (func $context.get (result i32)))
+    (import "" "run" (func $run (param i32)))
+
+    (func (export "run")
+      ;; set this tasks's context before calling $run, in calling $run the
+      ;; runtime will then call `realloc` above for the string return value
+      ;; which should see our 400 value. That will then set 500 which we
+      ;; should then see after the return.
+      (call $context.set (i32.const 400))
+      (call $run (i32.const 20))
+      (if (i32.ne (call $context.get) (i32.const 500)) (then (unreachable)))
+    )
+  )
+  (core instance $m (instantiate $M (with "" (instance
+    (export "context.set" (func $context.set))
+    (export "context.get" (func $context.get))
+    (export "run" (func $run))
+  ))))
+  (func (export "run") (canon lift (core func $m "run")))
 )
 
 (assert_return (invoke "run"))