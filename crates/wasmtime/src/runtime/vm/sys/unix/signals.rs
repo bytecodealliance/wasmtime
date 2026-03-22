3b3f1e7510de2375d1df23dc59f33b810d219cfb
yuri@FreeBSD <yurivict@users.noreply.github.com>
2026-03-22 11:10:00 -0700

    Fix build on FreeBSD/aarch64 (#12820)

--- crates/wasmtime/src/runtime/vm/sys/unix/signals.rs
+++ crates/wasmtime/src/runtime/vm/sys/unix/signals.rs
@@ -383,6 +383,13 @@ unsafe fn store_handler_in_ucontext(cx: *mut libc::c_void, handler: &Handler) {
             cx.uc_mcontext.mc_rsp = handler.sp as _;
             cx.uc_mcontext.mc_rax = 0;
             cx.uc_mcontext.mc_rdx = 0;
+        } else if #[cfg(all(target_os = "freebsd", target_arch = "aarch64"))] {
+            let cx = unsafe { cx.cast::<libc::mcontext_t>().as_mut().unwrap() };
+            cx.mc_gpregs.gp_elr = handler.pc as _;
+            cx.mc_gpregs.gp_sp = handler.sp as _;
+            cx.mc_gpregs.gp_x[29] = handler.fp as _;
+            cx.mc_gpregs.gp_x[0] = 0;
+            cx.mc_gpregs.gp_x[1] = 0;
         } else if #[cfg(all(target_os = "openbsd", target_arch = "x86_64"))] {
             let cx = unsafe { cx.cast::<libc::ucontext_t>().as_mut().unwrap() };
             cx.sc_rip = handler.pc as _;
