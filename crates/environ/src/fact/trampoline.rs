9ff448fa168b2f447af6705979b2f58260c9b92d
Nick Fitzgerald <fitzgen@gmail.com>
2026-03-25 08:00:52 -0700

    Fix bounds checks in FACT's `string_to_compact` method
    We need to bounds check the source byte length, not the number of code units.

--- crates/environ/src/fact/trampoline.rs
+++ crates/environ/src/fact/trampoline.rs
@@ -1133,24 +1133,8 @@ impl Compiler<'_, '_> {
         dst_enc: FE,
     ) -> WasmString<'a> {
         assert!(dst_enc.width() >= src_enc.width());
-        self.validate_string_length(src, dst_enc);
 
-        // Calculate the source byte length given the size of each code
-        // unit. Note that this shouldn't overflow given
-        // `validate_string_length` above.
-        let mut src_byte_len_tmp = None;
-        let src_byte_len = if src_enc.width() == 1 {
-            src.len.idx
-        } else {
-            assert_eq!(src_enc.width(), 2);
-            self.instruction(LocalGet(src.len.idx));
-            self.ptr_uconst(src.opts, 1);
-            self.ptr_shl(src.opts);
-            let tmp = self.local_set_new_tmp(src.opts.ptr());
-            let ret = tmp.idx;
-            src_byte_len_tmp = Some(tmp);
-            ret
-        };
+        let (src_byte_len_tmp, src_byte_len) = self.source_string_byte_len(src, src_enc, src.opts);
 
         // Convert the source code units length to the destination byte
         // length type.
@@ -1208,6 +1192,39 @@ impl Compiler<'_, '_> {
 
         dst
     }
+
+    /// Calculate the source byte length given the size of each code
+    /// unit.
+    ///
+    /// Returns an optional temporary local if it was needed, which the caller
+    /// needs to deallocate with `free_temp_local`. Additionally returns the
+    /// index of the local which contains the byte length of the string, which
+    /// may point to the temporary local passed in.
+    fn source_string_byte_len(
+        &mut self,
+        src: &WasmString<'_>,
+        src_enc: FE,
+        src_mem_opts: &Options,
+    ) -> (Option<TempLocal>, u32) {
+        self.validate_string_length(src, src_enc);
+
+        if src_enc.width() == 1 {
+            (None, src.len.idx)
+        } else {
+            assert_eq!(src_enc.width(), 2);
+
+            // Note that this shouldn't overflow given `validate_string_length`
+            // above.
+            self.instruction(LocalGet(src.len.idx));
+            self.ptr_uconst(src_mem_opts, 1);
+            self.ptr_shl(src_mem_opts);
+            let tmp = self.local_set_new_tmp(src.opts.ptr());
+
+            let idx = tmp.idx;
+            (Some(tmp), idx)
+        }
+    }
+
     // Corresponding function for `store_string_to_utf8` in the spec.
     //
     // This translation works by possibly performing a number of
@@ -1552,7 +1569,7 @@ impl Compiler<'_, '_> {
         src_enc: FE,
         dst_opts: &'a Options,
     ) -> WasmString<'a> {
-        self.validate_string_length(src, src_enc);
+        let (src_byte_len_tmp, src_byte_len) = self.source_string_byte_len(src, src_enc, src.opts);
         self.convert_src_len_to_dst(src.len.idx, src.opts.ptr(), dst_opts.ptr());
         let dst_len = self.local_tee_new_tmp(dst_opts.ptr());
         let dst_byte_len = self.local_set_new_tmp(dst_opts.ptr());
@@ -1565,7 +1582,7 @@ impl Compiler<'_, '_> {
             }
         };
 
-        self.validate_string_inbounds(src, src.len.idx);
+        self.validate_string_inbounds(src, src_byte_len);
         self.validate_string_inbounds(&dst, dst_byte_len.idx);
 
         // Perform the initial latin1 transcode. This returns the number of
@@ -1684,6 +1701,9 @@ impl Compiler<'_, '_> {
 
         self.free_temp_local(src_len_tmp);
         self.free_temp_local(dst_byte_len);
+        if let Some(tmp) = src_byte_len_tmp {
+            self.free_temp_local(tmp);
+        }
 
         dst
     }
