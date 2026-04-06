552303c1a4d9844b76b542d9f217d27a2f534f28
Alex Crichton <alex@alexcrichton.com>
2026-04-06 10:17:08 -0500

    Fix intra-component stream/futures copies with multi-memory (#12966)
    This commit fixes a mistake from #12872 where an intra-component copy of
    a stream/future payload incorrectly use a `copy_within` when different
    memories were in use. Previously instance identity was used to test
    whether `copy_within` can be used but the correct check is to use memory
    identity. This handles the case where a single component instance has
    multiple memories in use, for example.

--- crates/wasmtime/src/runtime/component/concurrent/futures_and_streams.rs
+++ crates/wasmtime/src/runtime/component/concurrent/futures_and_streams.rs
@@ -3295,7 +3295,9 @@ impl Instance {
 
                     assert_eq!(read_length_in_bytes, write_length_in_bytes);
 
-                    if write_caller_instance == read_caller_instance {
+                    if self.options_memory(store_opaque, read_options).as_ptr()
+                        == self.options_memory(store_opaque, write_options).as_ptr()
+                    {
                         let memory = self.options_memory_mut(store_opaque, read_options);
                         memory.copy_within(
                             write_address..write_address + write_length_in_bytes,
@@ -3313,9 +3315,9 @@ impl Instance {
                         // above to be valid pointers as they're derived from
                         // slices that have the desired length with the desired
                         // read/write permission. The `unsafe` bit here is that
-                        // the memories are disjoint (present in different
-                        // instances) and there's no easy way to borrow both
-                        // simultaneously from the store. Different instances
+                        // the memories are disjoint (different base pointers)
+                        // and there's no easy way to borrow both
+                        // simultaneously from the store. Different memories
                         // are guaranteed to be disjoint, however, so the
                         // `unsafe` here should be ok.
                         unsafe {
