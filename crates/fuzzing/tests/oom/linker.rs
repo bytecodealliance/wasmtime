706b003ff50c19e6121f542a12872d2d59d8d3e9
Nick Fitzgerald <fitzgen@gmail.com>
2026-04-07 15:25:44 -0700

    Add OOM test for Linker::instantiate_async

--- crates/fuzzing/tests/oom/linker.rs
+++ crates/fuzzing/tests/oom/linker.rs
@@ -70,6 +70,53 @@ fn linker_instantiate_pre() -> Result<()> {
     })
 }
 
+#[tokio::test]
+async fn linker_instantiate_async() -> Result<()> {
+    let module_bytes = {
+        let mut config = Config::new();
+        config.concurrency_support(false);
+        let engine = Engine::new(&config)?;
+        let module = Module::new(
+            &engine,
+            r#"
+                (module
+                    (import "module" "func" (func (param i32) (result i32)))
+
+                    (memory (export "memory") 1)
+                    (data (i32.const 0) "a")
+
+                    (table (export "table") 1 funcref)
+                    (elem (i32.const 0) func 1)
+
+                    (func (export "func") (param i32) (result i32)
+                        (call 0 (local.get 0))
+                    )
+                )
+            "#,
+        )?;
+        module.serialize()?
+    };
+
+    let mut config = Config::new();
+    config.enable_compiler(false);
+    config.concurrency_support(false);
+
+    let engine = Engine::new(&config)?;
+
+    let mut linker = Linker::<()>::new(&engine);
+    linker.func_wrap("module", "func", |x: i32| x * 2)?;
+
+    let module = unsafe { Module::deserialize(&engine, &module_bytes)? };
+
+    OomTest::new()
+        .test_async(|| async {
+            let mut store = Store::try_new(&engine, ())?;
+            let _instance = linker.instantiate_async(&mut store, &module).await?;
+            Ok(())
+        })
+        .await
+}
+
 #[test]
 fn linker_define() -> Result<()> {
     let mut config = Config::new();
