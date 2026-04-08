4c1f98ed3f6cb4a5f5ce61317925750607c90564
Nick Fitzgerald <fitzgen@gmail.com>
2026-04-07 21:46:30 -0700

    Add OOM test for component Linker::instantiate

--- crates/fuzzing/tests/oom/component_linker.rs
+++ crates/fuzzing/tests/oom/component_linker.rs
@@ -41,3 +41,39 @@ async fn component_linker_instantiate_async() -> Result<()> {
         })
         .await
 }
+
+#[test]
+fn component_linker_instantiate() -> Result<()> {
+    let component_bytes = {
+        let mut config = Config::new();
+        config.concurrency_support(false);
+        let engine = Engine::new(&config)?;
+        Component::new(
+            &engine,
+            r#"
+                (component
+                    (core module $m
+                        (func (export "id") (param i32) (result i32) (local.get 0))
+                    )
+                    (core instance $i (instantiate $m))
+                    (func (export "id") (param "x" s32) (result s32)
+                        (canon lift (core func $i "id"))
+                    )
+                )
+            "#,
+        )?
+        .serialize()?
+    };
+    let mut config = Config::new();
+    config.enable_compiler(false);
+    config.concurrency_support(false);
+    let engine = Engine::new(&config)?;
+    let component = unsafe { Component::deserialize(&engine, &component_bytes)? };
+    let linker = Linker::<()>::new(&engine);
+
+    OomTest::new().test(|| {
+        let mut store = Store::try_new(&engine, ())?;
+        let _instance = linker.instantiate(&mut store, &component)?;
+        Ok(())
+    })
+}
