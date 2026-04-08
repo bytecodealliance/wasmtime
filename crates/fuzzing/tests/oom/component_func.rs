27e52f2e81b5b7877c17f8b40f792a57674eb38b
Nick Fitzgerald <fitzgen@gmail.com>
2026-04-07 21:44:07 -0700

    Add OOM test for component InstancePre::instantiate

--- crates/fuzzing/tests/oom/component_func.rs
+++ crates/fuzzing/tests/oom/component_func.rs
@@ -128,3 +128,40 @@ async fn component_typed_func_call_async() -> Result<()> {
         })
         .await
 }
+
+#[test]
+fn component_instance_pre_instantiate() -> Result<()> {
+    let component_bytes = {
+        let mut config = Config::new();
+        config.concurrency_support(false);
+        let engine = Engine::new(&config)?;
+        Component::new(
+            &engine,
+            r#"
+                (component
+                    (core module $m
+                        (func (export "id") (param i32) (result i32) (local.get 0))
+                    )
+                    (core instance $i (instantiate $m))
+                    (func (export "id") (param "x" s32) (result s32)
+                        (canon lift (core func $i "id"))
+                    )
+                )
+            "#,
+        )?
+        .serialize()?
+    };
+    let mut config = Config::new();
+    config.enable_compiler(false);
+    config.concurrency_support(false);
+    let engine = Engine::new(&config)?;
+    let component = unsafe { Component::deserialize(&engine, &component_bytes)? };
+    let linker = Linker::<()>::new(&engine);
+    let instance_pre = linker.instantiate_pre(&component)?;
+
+    OomTest::new().test(|| {
+        let mut store = Store::try_new(&engine, ())?;
+        let _instance = instance_pre.instantiate(&mut store)?;
+        Ok(())
+    })
+}
