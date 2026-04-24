11df8e5ace46dd8e6e4363af154f066c41104689
Jimmy Brisson <jbrisson@linux.ibm.com>
2026-04-24 10:13:53 -0500

    s390x: Rename vxrs_ext3_enabled to has_vxrs_ext3
    This matches the backend flag, making it more clear where this comes from

--- cranelift/codegen/src/isa/s390x/lower/isle.rs
+++ cranelift/codegen/src/isa/s390x/lower/isle.rs
@@ -228,7 +228,7 @@ impl generated_code::Context for IsleContext<'_, '_, MInst, S390xBackend> {
     }
 
     #[inline]
-    fn vxrs_ext3_enabled(&mut self) -> bool {
+    fn has_vxrs_ext3(&mut self) -> bool {
         self.backend.isa_flags.has_vxrs_ext3()
     }
 
