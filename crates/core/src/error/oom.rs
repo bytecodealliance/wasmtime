05fda18c334dffd6144f41fd6d70e5ec0c22a6e8
Nick Fitzgerald <fitzgen@gmail.com>
2026-04-28 13:34:03 -0700

    Expand the docs the `OutOfMemory` error
    And use this as a place to document which portions of Wasmtime attempt to handle
    OOM, how to enable `cfg(arc_try_new)` to handle more OOMs with a nightly rust
    toolchain, and clarify that OOM handling is not tier 1.

--- crates/core/src/error/oom.rs
+++ crates/core/src/error/oom.rs
@@ -1,15 +1,94 @@
 use crate::error::{Error, OomOrDynError};
 use core::{fmt, mem, ptr::NonNull};
 
-/// Out-of-memory error.
+/// An out-of-memory (OOM) error.
 ///
 /// This error is the sentinel for allocation failure due to memory exhaustion.
 ///
-/// Constructing an [`Error`] from an `OutOfMemory` does not
-/// allocate.
+/// Constructing an [`Error`] from an `OutOfMemory` does not allocate.
 ///
 /// Allocation failure inside any `Error` method that must allocate
 /// (e.g. [`Error::context`]) will propagate an `OutOfMemory` error.
+///
+/// # Out-of-Memory Handling in Wasmtime
+///
+/// Wasmtime performs out-of-memory (OOM) error handling on a **best-effort
+/// basis**. OOM handling does not have [tier 1
+/// support](https://docs.wasmtime.dev/stability-tiers.html) at this time and,
+/// therefore, while failure to handle OOM at some allocation site may be
+/// considered a bug, and might be a potential denial-of-service vector, it
+/// would not be considered a security vulnerability.[^limits]
+///
+/// [^limits]: Note that unconstrained guest-controlled resource usage is still
+/// be considered a vulnerability. Wasmtime has tier 1 support for limiting
+/// guest resources, but not for handling OOMs within those limits.
+///
+/// ## Where Wasmtime Attempts to Handle OOM
+///
+/// It is important to note that **not all portions of Wasmtime attempt to
+/// handle out-of-memory errors**. Notably, Wasmtime only ever attempts to
+/// handle OOM in the core *runtime* and never in the *compiler*. No attempt is
+/// made to handle allocation failure in the middle of compiling new `Module`s
+/// or `Component`s from Wasm to machine code (or Pulley bytecode). However,
+/// Wasmtime will attempt to handle OOM when running *pre-compiled* Wasm code
+/// (loaded via `Module::deserialize` or `Component::deserialize`).
+///
+/// Wasmtime's interfaces allow *you* to handle OOM in your own embedding's WASI
+/// implementations and host APIs, but Wasmtime's provided WASI implementations
+/// (e.g. `wasmtime_wasi_http`) will generally not attempt to handle OOM (as
+/// they often depend on third-party crates that do not attempt to handle OOM).
+///
+/// The API documentation for individual functions and methods that handle OOM
+/// should generally document this fact by listing `OutOfMemory` as one of the
+/// potential errors returned.
+///
+/// | **Where**                              | **Handles OOM?** |
+/// |----------------------------------------|------------------|
+/// | **Compiler**                           | **No**           |
+/// | &emsp;`wasmtime::Module::new`          | No               |
+/// | &emsp;`wasmtime::Component::new`       | No               |
+/// | &emsp;`wasmtime::CodeBuilder`          | No               |
+/// | &emsp;Etc...                           | No               |
+/// | **Runtime**                            | **Yes**          |
+/// | &emsp;`wasmtime::Store`                | Yes              |
+/// | &emsp;`wasmtime::Linker`               | Yes              |
+/// | &emsp;`wasmtime::Module::deserialize`  | Yes              |
+/// | &emsp;`wasmtime::Instance`             | Yes              |
+/// | &emsp;`wasmtime::Func::call`           | Yes              |
+/// | &emsp;Etc...                           | Yes              |
+/// | **WASI Implementations and Host APIs** | **Depends**      |
+/// | &emsp;`wasmtime_wasi`                  | No               |
+/// | &emsp;`wasmtime_wasi_http`             | No               |
+/// | &emsp;`wasmtime_wasi_*`                | No               |
+/// | &emsp;Your embedding's APIs            | If *you* implement OOM handling |
+///
+/// If you encounter an unhandled OOM inside Wasmtime, and it is within a
+/// portion of code where it should be handled, then please [file an
+/// issue](https://github.com/bytecodealliance/wasmtime/issues/new/choose).
+///
+/// ## Handling More OOMs with Rust Nightly APIs
+///
+/// Rust's standard library provides fallible allocation APIs, or the necessary
+/// building blocks for making our own fallible allocation APIs, for some of its
+/// types and collections. For example, it provides `Vec::try_reserve` which can
+/// be used to build a fallible version of `Vec::push` and fallible `Box`
+/// allocation can be built upon raw allocations from the global allocator and
+/// `Box::from_raw`.
+///
+/// However, the standard library does not provide these things for all the
+/// types and collections that Wasmtime uses. Some of these APIs are completely
+/// missing (such as a fallible version of
+/// `std::collections::hash_map::VacantEntry::insert`) and some APIs exist but
+/// are feature-gated on unstable, nightly-only Rust features. The most relevant
+/// API from this latter category is
+/// [`Arc::try_new`](https://doc.rust-lang.org/nightly/std/sync/struct.Arc.html#method.try_new),
+/// as Wasmtime's runtime uses a number of `Arc`s under the covers.
+///
+/// If handling OOMs is important for your Wasmtime embedding, then you should
+/// compile Wasmtime from source using a Nightly Rust toolchain and with the
+/// `RUSTFLAGS="--cfg arc_try_new"` environment variable set. This unlocks
+/// Wasmtime's internal usage of `Arc::try_new`, making more OOM handling at
+/// more allocation sites possible.
 #[derive(Clone, Copy)]
 // NB: `OutOfMemory`'s representation must be the same as `OomOrDynError`
 // (and therefore also `Error`).
