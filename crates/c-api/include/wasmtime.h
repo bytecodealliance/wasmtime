7431d9503a91f8f365f31b9751e9bd5eec89eb7a
Wasmtime Publish <wasmtime-publish@users.noreply.github.com>
2026-04-30 14:29:19 +0000

    Release Wasmtime 44.0.1
    [automatically-tag-and-release-this-commit]

--- crates/c-api/include/wasmtime.h
+++ crates/c-api/include/wasmtime.h
@@ -215,7 +215,7 @@
 /**
  * \brief Wasmtime version string.
  */
-#define WASMTIME_VERSION "44.0.0"
+#define WASMTIME_VERSION "44.0.1"
 /**
  * \brief Wasmtime major version number.
  */
@@ -227,6 +227,6 @@
 /**
  * \brief Wasmtime patch version number.
  */
-#define WASMTIME_VERSION_PATCH 0
+#define WASMTIME_VERSION_PATCH 1
 
 #endif // WASMTIME_API_H
