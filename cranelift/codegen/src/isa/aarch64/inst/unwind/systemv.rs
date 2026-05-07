ddf49cfdfb7d5ea1b081d217c57b243fa2d1a70b
Alex Crichton <alex@alexcrichton.com>
2026-05-06 22:12:58 -0700

    Fix test expectation

--- cranelift/codegen/src/isa/aarch64/inst/unwind/systemv.rs
+++ cranelift/codegen/src/isa/aarch64/inst/unwind/systemv.rs
@@ -148,7 +148,7 @@ mod tests {
 
         assert_eq!(
             format!("{fde:?}"),
-            "FrameDescriptionEntry { address: Constant(4321), length: 16, lsda: None, instructions: [] }"
+            "FrameDescriptionEntry { address: Constant(4321), length: 12, lsda: None, instructions: [] }"
         );
     }
 
