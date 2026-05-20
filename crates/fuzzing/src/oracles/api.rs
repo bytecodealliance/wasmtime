30d2acc3146a7db126d5dd9b35b1b3c1e047936e
Nick Fitzgerald <fitzgen@gmail.com>
2026-05-20 14:17:12 -0700

    Add support for creating and dropping multiple stores

--- crates/fuzzing/src/oracles/api.rs
+++ crates/fuzzing/src/oracles/api.rs
@@ -5,22 +5,37 @@ use wasmtime::*;
 
 /// Invoke the given API calls.
 pub fn make_api_calls(api: ApiCalls) {
-    let mut store: Option<Store<StoreLimits>> = None;
+    // The generator always starts with StoreNew; use its config to build the
+    // shared engine that all stores in this sequence will use.
+    let engine = match api.calls.first() {
+        Some(ApiCall::StoreNew { config, .. }) => Engine::new(&config.to_wasmtime()).unwrap(),
+        _ => return,
+    };
+
+    let mut stores: HashMap<usize, Store<StoreLimits>> = Default::default();
     let mut modules: HashMap<usize, Module> = Default::default();
-    let mut instances: HashMap<usize, Instance> = Default::default();
+    let mut instances: HashMap<usize, (Instance, usize)> = Default::default();
 
     for call in api.calls {
         match call {
-            ApiCall::StoreNew(config) => {
-                log::trace!("creating store");
-                assert!(store.is_none());
-                store = Some(config.to_store());
+            ApiCall::StoreNew { id, config } => {
+                log::trace!("creating store {id}");
+                let mut store = Store::new(&engine, StoreLimits::new());
+                config.configure_store(&mut store);
+                let old = stores.insert(id, store);
+                assert!(old.is_none());
+            }
+
+            ApiCall::StoreDrop { id } => {
+                log::trace!("dropping store {id}");
+                instances.retain(|_, (_, store_id)| *store_id != id);
+                stores.remove(&id);
             }
 
             ApiCall::ModuleNew { id, wasm } => {
                 log::debug!("creating module: {id}");
                 log_wasm(&wasm);
-                let module = match Module::new(store.as_ref().unwrap().engine(), &wasm) {
+                let module = match Module::new(&engine, &wasm) {
                     Ok(m) => m,
                     Err(_) => continue,
                 };
@@ -33,16 +48,18 @@ pub fn make_api_calls(api: ApiCalls) {
                 drop(modules.remove(&id));
             }
 
-            ApiCall::InstanceNew { id, module } => {
-                log::trace!("instantiating module {module} as {id}");
+            ApiCall::InstanceNew { id, module, store } => {
+                log::trace!("instantiating module {module} as {id} in store {store}");
                 let module = match modules.get(&module) {
                     Some(m) => m,
                     None => continue,
                 };
-
-                let store = store.as_mut().unwrap();
-                if let Some(instance) = instantiate_with_dummy(store, module) {
-                    instances.insert(id, instance);
+                let st = match stores.get_mut(&store) {
+                    Some(s) => s,
+                    None => continue,
+                };
+                if let Some(instance) = instantiate_with_dummy(st, module) {
+                    instances.insert(id, (instance, store));
                 }
             }
 
@@ -53,8 +70,8 @@ pub fn make_api_calls(api: ApiCalls) {
 
             ApiCall::CallExportedFunc { instance, nth } => {
                 log::trace!("calling instance export {instance} / {nth}");
-                let instance = match instances.get(&instance) {
-                    Some(i) => i,
+                let (inst, store_id) = match instances.get(&instance) {
+                    Some(&x) => x,
                     None => {
                         // Note that we aren't guaranteed to instantiate valid
                         // modules, see comments in `InstanceNew` for details on
@@ -65,9 +82,12 @@ pub fn make_api_calls(api: ApiCalls) {
                         continue;
                     }
                 };
-                let store = store.as_mut().unwrap();
+                let store = match stores.get_mut(&store_id) {
+                    Some(s) => s,
+                    None => continue,
+                };
 
-                let funcs = instance
+                let funcs = inst
                     .exports(&mut *store)
                     .filter_map(|e| match e.into_extern() {
                         Extern::Func(f) => Some(f),
@@ -81,7 +101,7 @@ pub fn make_api_calls(api: ApiCalls) {
 
                 let nth = nth % funcs.len();
                 let f = &funcs[nth];
-                let ty = f.ty(&store);
+                let ty = f.ty(&*store);
                 if let Some(params) = ty
                     .params()
                     .map(|p| p.default_value())
