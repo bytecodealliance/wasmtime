6f38081544a57cab3ba7f9e12c6fa71a6a5e9129
Pat Hickey <p.hickey@f5.com>
2026-05-21 09:42:23 -0700

    wasi-common: test stubs

--- crates/wasi-common/tests/all/async_.rs
+++ crates/wasi-common/tests/all/async_.rs
@@ -291,3 +291,9 @@ async fn p1_path_open_lots() {
 async fn p1_sleep_quickly_but_lots() {
     run(P1_SLEEP_QUICKLY_BUT_LOTS, true).await.unwrap()
 }
+#[test]
+fn p1_file_truncation_readonly() {
+    println!(
+        "blank placeholder test to satisfy assert_test_exists. This test exercises wasmtime-wasi functionality is not relevant to wasi-common"
+    );
+}
