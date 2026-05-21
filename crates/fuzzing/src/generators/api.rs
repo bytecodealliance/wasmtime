d57951685173f1a67bc4d4df0a9d05820a06c582
Nick Fitzgerald <fitzgen@gmail.com>
2026-05-21 11:34:14 -0700

    Add support for creating and dropping multiple stores (#13426)

--- crates/fuzzing/src/generators/api.rs
+++ crates/fuzzing/src/generators/api.rs
@@ -16,10 +16,12 @@
 
 use crate::generators::Config;
 use arbitrary::{Arbitrary, Unstructured};
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 #[derive(Arbitrary, Debug)]
 struct Swarm {
+    store_new: bool,
+    store_drop: bool,
     module_new: bool,
     module_drop: bool,
     instance_new: bool,
@@ -31,19 +33,48 @@ struct Swarm {
 #[derive(Arbitrary, Debug)]
 #[expect(missing_docs, reason = "self-describing fields")]
 pub enum ApiCall {
-    StoreNew(Config),
-    ModuleNew { id: usize, wasm: Vec<u8> },
-    ModuleDrop { id: usize },
-    InstanceNew { id: usize, module: usize },
-    InstanceDrop { id: usize },
-    CallExportedFunc { instance: usize, nth: usize },
+    StoreNew {
+        id: usize,
+        config: Config,
+    },
+    StoreDrop {
+        id: usize,
+    },
+    ModuleNew {
+        id: usize,
+        wasm: Vec<u8>,
+    },
+    ModuleDrop {
+        id: usize,
+    },
+    InstanceNew {
+        id: usize,
+        module: usize,
+        store: usize,
+    },
+    InstanceDrop {
+        id: usize,
+    },
+    CallExportedFunc {
+        instance: usize,
+        nth: usize,
+    },
 }
 use ApiCall::*;
 
 struct Scope {
     id_counter: usize,
+
+    /// Stores that are currently live.
+    stores: BTreeSet<usize>,
+
+    /// Modules that are currently live.
     modules: BTreeSet<usize>,
-    instances: BTreeSet<usize>,
+
+    /// Instances that are currently live. Maps from `instance_id` to the
+    /// instance's associated `store_id`.
+    instances: BTreeMap<usize, usize>,
+
     config: Config,
 }
 
@@ -70,26 +101,50 @@ impl<'a> Arbitrary<'a> for ApiCalls {
         let mut calls = vec![];
 
         let config = Config::arbitrary(input)?;
-        calls.push(StoreNew(config.clone()));
-
         let mut scope = Scope {
             id_counter: 0,
+            stores: BTreeSet::default(),
             modules: BTreeSet::default(),
-            instances: BTreeSet::default(),
-            config,
+            instances: BTreeMap::default(),
+            config: config.clone(),
         };
+
+        let store_id = scope.next_id();
+        scope.stores.insert(store_id);
+        calls.push(StoreNew {
+            id: store_id,
+            config,
+        });
 
         // Total limit on number of API calls we'll generate. This exists to
         // avoid libFuzzer timeouts.
         let max_calls = 100;
 
-        for _ in 0..input.arbitrary_len::<ApiCall>()? {
-            if calls.len() > max_calls {
-                break;
+        let mut choices: Vec<fn(&mut Unstructured<'a>, &mut Scope) -> arbitrary::Result<ApiCall>> =
+            vec![];
+
+        for _ in 0..std::cmp::min(max_calls, input.arbitrary_len::<ApiCall>()?) {
+            choices.clear();
+
+            if swarm.store_new {
+                choices.push(|_input, scope| {
+                    let id = scope.next_id();
+                    scope.stores.insert(id);
+                    Ok(StoreNew {
+                        id,
+                        config: scope.config.clone(),
+                    })
+                });
             }
-
-            let mut choices: Vec<fn(_, &mut Scope) -> arbitrary::Result<ApiCall>> = vec![];
-
+            if swarm.store_drop && scope.stores.len() > 1 {
+                choices.push(|input, scope| {
+                    let stores: Vec<_> = scope.stores.iter().collect();
+                    let id = **input.choose(&stores)?;
+                    scope.stores.remove(&id);
+                    scope.instances.retain(|_, store_id| *store_id != id);
+                    Ok(StoreDrop { id })
+                });
+            }
             if swarm.module_new {
                 choices.push(|input, scope| {
                     let id = scope.next_id();
@@ -109,18 +164,20 @@ impl<'a> Arbitrary<'a> for ApiCalls {
                     Ok(ModuleDrop { id })
                 });
             }
-            if swarm.instance_new && !scope.modules.is_empty() {
+            if swarm.instance_new && !scope.modules.is_empty() && !scope.stores.is_empty() {
                 choices.push(|input, scope| {
                     let modules: Vec<_> = scope.modules.iter().collect();
                     let module = **input.choose(&modules)?;
+                    let stores: Vec<_> = scope.stores.iter().collect();
+                    let store = **input.choose(&stores)?;
                     let id = scope.next_id();
-                    scope.instances.insert(id);
-                    Ok(InstanceNew { id, module })
+                    scope.instances.insert(id, store);
+                    Ok(InstanceNew { id, module, store })
                 });
             }
             if swarm.instance_drop && !scope.instances.is_empty() {
                 choices.push(|input, scope| {
-                    let instances: Vec<_> = scope.instances.iter().collect();
+                    let instances: Vec<_> = scope.instances.keys().collect();
                     let id = **input.choose(&instances)?;
                     scope.instances.remove(&id);
                     Ok(InstanceDrop { id })
@@ -128,7 +185,7 @@ impl<'a> Arbitrary<'a> for ApiCalls {
             }
             if swarm.call_exported_func && !scope.instances.is_empty() {
                 choices.push(|input, scope| {
-                    let instances: Vec<_> = scope.instances.iter().collect();
+                    let instances: Vec<_> = scope.instances.keys().collect();
                     let instance = **input.choose(&instances)?;
                     let nth = usize::arbitrary(input)?;
                     Ok(CallExportedFunc { instance, nth })
