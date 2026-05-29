0dfa0bee64a08885ab7bfa0be6e858a1456e234d
Alex Crichton <alex@alexcrichton.com>
2026-05-29 13:54:58 -0700

    Fix compile

--- crates/test-programs/src/bin/p2_clocks_zero_wait.rs
+++ crates/test-programs/src/bin/p2_clocks_zero_wait.rs
@@ -1,6 +1,6 @@
 fn main() {
     let pollable = wasip2::clocks::monotonic_clock::subscribe_duration(0);
-    for i in 0..20 {
+    for _ in 0..20 {
         if pollable.ready() {
             return;
         }
