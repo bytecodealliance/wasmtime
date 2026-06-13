e5f0a1d9a52bbafadd2e4aa14f702014b3d34f47
Christof Petig <christof.petig@arcor.de>
2026-06-14 00:53:03 +0200

    correct code for test

--- tests/all/component_model/fixed_length_list.rs
+++ tests/all/component_model/fixed_length_list.rs
@@ -1,8 +1,7 @@
 #![cfg(not(miri))]
 
-use wasmtime::Result;
-use wasmtime::Store;
 use wasmtime::component::{Component, Linker};
+use wasmtime::{Config, Engine, Result, Store};
 
 #[test]
 fn fixed_length_list() -> Result<()> {
@@ -11,165 +10,166 @@ fn fixed_length_list() -> Result<()> {
     // as this test was compiled from C(++) the assembly is less human readable
     let component = r##"
 (component
-  (type $ty-test:fixed-size-lists/to-test (;0;)
-    (instance
-      (type (;0;) (list u32 2))
-      (type (;1;) (list 0 2))
-      (type (;2;) (list s32 2))
-      (type (;3;) (list 2 2))
-      (type (;4;) (tuple 1 3))
-      (type (;5;) (func (param "a" 1) (param "b" 3) (result 4)))
-      (export (;0;) "nested-roundtrip" (func (type 5)))
-    )
-  )
-  (import "test:fixed-size-lists/to-test" (instance $test:fixed-size-lists/to-test (;0;) (type $ty-test:fixed-size-lists/to-test)))
   (core module $main (;0;)
-    (type (;0;) (func (param i32 i32 i32 i32 i32 i32 i32 i32 i32)))
-    (type (;1;) (func (result i32)))
-    (import "test:fixed-size-lists/to-test" "nested-roundtrip" (func (;0;) (type 0)))
+    (type (;0;) (func (param i32 i32 i32 i32 i32 i32 i32 i32) (result i32)))
     (memory (;0;) 2)
-    (global (;0;) (mut i32) i32.const 67232)
+    (global (;0;) (mut i32) i32.const 67248)
     (export "memory" (memory 0))
-    (export "run" (func 1))
-    (func (;1;) (type 1) (result i32)
-      (local i32 i32 i32 i32 i32 i32 i32 i32 i32)
+    (export "test:fixed-size-lists/to-test#nested-roundtrip" (func 0))
+    (func (;0;) (type 0) (param i32 i32 i32 i32 i32 i32 i32 i32) (result i32)
+      (local i32 i32 i64 i64)
       global.get 0
-      i32.const -64
-      i32.add
-      local.tee 0
+      i32.const 96
+      i32.sub
+      local.tee 8
       global.set 0
-      local.get 0
+      local.get 8
+      i32.const 56
+      i32.add
+      local.tee 9
+      local.get 2
+      i32.store
+      local.get 8
+      i32.const 40
+      i32.add
+      local.tee 2
+      local.get 6
+      i32.store
+      local.get 8
+      local.get 3
+      i32.store offset=60
+      local.get 8
       i32.const 16
       i32.add
-      local.tee 1
+      local.tee 3
       i32.const 8
       i32.add
-      i32.const 1032
-      i64.load align=4
+      local.tee 6
+      local.get 9
+      i64.load
       i64.store
-      local.get 0
+      local.get 8
+      local.get 7
+      i32.store offset=44
+      local.get 8
       i32.const 8
       i32.add
-      i32.const 1048
-      i64.load align=4
-      i64.store
-      local.get 0
-      i32.const 1024
-      i64.load align=4
-      i64.store offset=16
-      local.get 0
-      i32.const 1040
-      i64.load align=4
-      i64.store
-      global.get 0
-      i32.const 32
-      i32.sub
-      local.tee 2
-      global.set 0
-      local.get 1
-      i32.load
-      local.get 1
-      i32.load offset=4
-      local.get 1
-      i32.load offset=8
-      local.get 1
-      i32.load offset=12
-      local.get 0
-      i32.load
-      local.get 0
-      i32.load offset=4
-      local.get 0
-      i32.load offset=8
-      local.get 0
-      i32.load offset=12
-      local.get 2
-      call 0
-      local.get 0
-      i32.const 32
-      i32.add
-      local.tee 1
-      local.get 2
-      i64.load offset=24
-      i64.store offset=24 align=4
-      local.get 1
-      local.get 2
-      i64.load offset=16
-      i64.store offset=16 align=4
-      local.get 1
-      local.get 2
-      i64.load offset=8
-      i64.store offset=8 align=4
-      local.get 1
+      local.tee 7
       local.get 2
       i64.load
-      i64.store align=4
-      local.get 2
-      i32.const 32
-      i32.add
-      global.set 0
+      i64.store
+      local.get 8
       local.get 0
-      i32.load offset=48
-      local.set 2
-      local.get 0
-      i32.load offset=32
-      local.get 0
-      i32.load offset=36
-      local.set 3
-      local.get 0
-      i32.load offset=52
-      local.set 4
-      local.get 0
-      i32.load offset=40
-      local.set 5
-      local.get 0
-      i32.load offset=56
-      local.set 6
-      local.get 0
-      i32.load offset=44
-      local.set 7
-      local.get 0
-      i32.load offset=60
-      local.set 8
-      local.get 0
+      i64.extend_i32_u
+      local.get 1
+      i64.extend_i32_u
+      i64.const 32
+      i64.shl
+      i64.or
+      local.tee 10
+      i64.store offset=48
+      local.get 8
+      local.get 4
+      i64.extend_i32_u
+      local.get 5
+      i64.extend_i32_u
+      i64.const 32
+      i64.shl
+      i64.or
+      local.tee 11
+      i64.store offset=32
+      local.get 8
+      local.get 10
+      i64.store offset=16
+      local.get 8
+      local.get 11
+      i64.store
+      local.get 8
       i32.const -64
       i32.sub
-      global.set 0
-      i32.const 1
-      i32.ne
-      local.get 2
-      i32.const -1
-      i32.ne
-      i32.add
+      local.tee 0
       local.get 3
-      i32.const 2
-      i32.ne
+      i64.load align=4
+      i64.store align=4
+      local.get 0
+      local.get 8
+      i64.load align=4
+      i64.store offset=16 align=4
+      local.get 0
+      i32.const 8
       i32.add
-      local.get 4
-      i32.const -2
-      i32.ne
-      i32.add
-      local.get 5
-      i32.const 3
-      i32.ne
-      i32.add
+      local.tee 1
       local.get 6
-      i32.const -3
-      i32.ne
+      i64.load align=4
+      i64.store align=4
+      local.get 0
+      i32.const 24
       i32.add
       local.get 7
-      i32.const 4
-      i32.ne
-      i32.add
+      i64.load align=4
+      i64.store align=4
+      i32.const 1064
+      local.get 1
+      i64.load align=4
+      i64.store
+      i32.const 1056
       local.get 8
-      i32.const -4
-      i32.ne
+      i64.load offset=64 align=4
+      i64.store
+      i32.const 1072
+      local.get 8
+      i64.load offset=80 align=4
+      i64.store
+      i32.const 1080
+      local.get 8
+      i32.const 88
       i32.add
+      i64.load align=4
+      i64.store
+      local.get 8
+      i32.const 96
+      i32.add
+      global.set 0
+      i32.const 1056
     )
-    (data (;0;) (i32.const 1024) "\01\00\00\00\02\00\00\00\03\00\00\00\04\00\00\00\ff\ff\ff\ff\fe\ff\ff\ff\fd\ff\ff\ff\fc\ff\ff\ff")
-    (data (;1;) (i32.const 1056) "\ff\ff\ff\ff\00\00\02")
+    (data (;0;) (i32.const 1024) "\ff\ff\ff\ff\00\00\02")
     (@producers
       (processed-by "wit-component" "0.243.0")
     )
+  )
+  (core instance $main (;0;) (instantiate $main))
+  (alias core export $main "memory" (core memory $memory (;0;)))
+  (type (;0;) (list u32 2))
+  (type (;1;) (list 0 2))
+  (type (;2;) (list s32 2))
+  (type (;3;) (list 2 2))
+  (type (;4;) (tuple 1 3))
+  (type (;5;) (func (param "a" 1) (param "b" 3) (result 4)))
+  (alias core export $main "test:fixed-size-lists/to-test#nested-roundtrip" (core func $test:fixed-size-lists/to-test#nested-roundtrip (;0;)))
+  (func $nested-roundtrip (;0;) (type 5) (canon lift (core func $test:fixed-size-lists/to-test#nested-roundtrip) (memory $memory)))
+  (component $test:fixed-size-lists/to-test-shim-component (;0;)
+    (type (;0;) (list u32 2))
+    (type (;1;) (list 0 2))
+    (type (;2;) (list s32 2))
+    (type (;3;) (list 2 2))
+    (type (;4;) (tuple 1 3))
+    (type (;5;) (func (param "a" 1) (param "b" 3) (result 4)))
+    (import "import-func-nested-roundtrip" (func (;0;) (type 5)))
+    (type (;6;) (list u32 2))
+    (type (;7;) (list 6 2))
+    (type (;8;) (list s32 2))
+    (type (;9;) (list 8 2))
+    (type (;10;) (tuple 7 9))
+    (type (;11;) (func (param "a" 7) (param "b" 9) (result 10)))
+    (export (;1;) "nested-roundtrip" (func 0) (func (type 11)))
+  )
+  (instance $test:fixed-size-lists/to-test-shim-instance (;0;) (instantiate $test:fixed-size-lists/to-test-shim-component
+      (with "import-func-nested-roundtrip" (func $nested-roundtrip))
+    )
+  )
+  (export $test:fixed-size-lists/to-test (;1;) "test:fixed-size-lists/to-test" (instance $test:fixed-size-lists/to-test-shim-instance))
+  (@producers
+    (processed-by "wit-component" "0.243.0")
   )
 )
 "##;
@@ -189,17 +189,19 @@ package test:fixed-size-lists {
         imports: {default: trappable},
     });
 
-    let engine = super::engine();
+    let mut config = Config::new();
+    config.wasm_component_model_fixed_length_lists(true);
+    let engine = Engine::new(&config)?;
     let component = Component::new(&engine, component)?;
     let mut store = Store::new(&engine, 0);
-    let mut linker = Linker::new(&engine);
-    let to_test = ToTest::instantiate(&mut store, &component, &linker)?;
+    let linker = Linker::new(&engine);
+    let to_test = Root::instantiate(&mut store, &component, &linker)?;
     let inputa: [[u32; 2]; 2] = [[1, 2], [3, 4]];
     let inputb: [[i32; 2]; 2] = [[-1, -2], [-3, -4]];
     let result = to_test
-        .call_nested_roundtrip(&mut store, &inputa, &inputb)
-        .expect("no trap")
-        .expect("no errror returned");
+        .interface0
+        .call_nested_roundtrip(&mut store, inputa, inputb)
+        .expect("no trap");
     assert_eq!(result, ([[1, 2], [3, 4]], [[-1, -2], [-3, -4]]));
     Ok(())
 }
