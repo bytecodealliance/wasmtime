2d58c48da85e6213e9794bfbc02cff3ada8177c1
Alex Crichton <alex@alexcrichton.com>
2026-06-16 12:28:23 -0700

    Update component model spec test suite
    Minor update to help ungate a test

--- crates/test-util/src/wast.rs
+++ crates/test-util/src/wast.rs
@@ -472,10 +472,6 @@ impl WastTest {
         }
 
         let unsupported = [
-            // These tests in the `component-model` submodule have not yet been
-            // updated to account for the recent threading-related intrinsic
-            // changes.
-            "test/async/trap-if-block-and-sync.wast",
             // Wasmtime doesn't expose the component-model `cm64` feature toggle
             // yet, so this parser-only test can't be enabled here.
             "test/wasm-tools/memory64.wast",
