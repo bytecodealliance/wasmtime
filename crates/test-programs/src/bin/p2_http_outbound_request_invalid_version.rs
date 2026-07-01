61b7ee4952a10c8e1aa30cb48f705091595ffa69
Alex Crichton <alex@alexcrichton.com>
2026-07-01 09:17:57 -0700

    Paper over spurious failure in HTTP tests
    This is attempting to fix the spurious failure [seen here][log]. The
    test in question is making an HTTP 1.1 request to a server which is
    expecting HTTP 2. Most of the time an `HttpProtocolError` pops out but
    it seems that depending on scheduling it's possible to also time out in
    this situation. This updates the test to shorten the timeout and
    additionally allow a timeout to happen.
    
    [log]: https://github.com/bytecodealliance/wasmtime/actions/runs/28526860584/job/84565604311

--- crates/test-programs/src/bin/p2_http_outbound_request_invalid_version.rs
+++ crates/test-programs/src/bin/p2_http_outbound_request_invalid_version.rs
@@ -10,14 +10,22 @@ fn main() {
         None,
         Some(&[]),
         None,
-        None,
+        Some(1_000_000_000),
         None,
     );
 
-    assert!(matches!(
-        res.unwrap_err()
-            .downcast::<ErrorCode>()
-            .expect("expected a wasi-http ErrorCode"),
-        ErrorCode::HttpProtocolError,
-    ));
+    // The error seen during this test is mostly an `HttpProtocolError`, but
+    // depending on scheduling it's possible to get stuck in hyper right now
+    // where the server is indefinitely waiting on the client and the client
+    // times out. Accept both kinds of errors here, and note the explicit 1s
+    // timeout above to avoid this taking too long. in the timeout case.
+    let err = res.unwrap_err();
+    assert!(
+        matches!(
+            err.downcast_ref::<ErrorCode>()
+                .expect("expected a wasi-http ErrorCode"),
+            ErrorCode::HttpProtocolError | ErrorCode::ConnectionReadTimeout,
+        ),
+        "unexpected error: {err:?}"
+    );
 }
