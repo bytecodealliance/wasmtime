a3d79a0fbee48f1c82926949266faa4e1c9c81c3
Alex Crichton <alex@alexcrichton.com>
2026-08-27 14:32:46 -0700

    Remove the maybe-owned dependency

--- crates/wasi/src/filesystem/primitives/maybe_owned_file.rs
+++ crates/wasi/src/filesystem/primitives/maybe_owned_file.rs
@@ -1,5 +1,4 @@
 use crate::filesystem::primitives::{OpenOptions, open_unchecked};
-use maybe_owned::MaybeOwned;
 use std::ops::Deref;
 use std::path::Component;
 use std::{fmt, fs, io, mem};
@@ -18,57 +17,36 @@ use std::{fmt, fs, io, mem};
 ///
 /// [`Deref`]: std::ops::Deref
 /// [`Cow`]: std::borrow::Cow
-pub(super) struct MaybeOwnedFile<'borrow> {
-    inner: MaybeOwned<'borrow, fs::File>,
+pub(super) enum MaybeOwnedFile<'borrow> {
+    Borrowed(&'borrow fs::File),
+    Owned(fs::File),
 }
 
 impl<'borrow> MaybeOwnedFile<'borrow> {
     /// Constructs a new `MaybeOwnedFile` which is not owned.
     pub(super) fn borrowed(file: &'borrow fs::File) -> Self {
-        Self {
-            inner: MaybeOwned::Borrowed(file),
-        }
+        Self::Borrowed(file)
     }
 
     /// Constructs a new `MaybeOwnedFile` which is owned.
     pub(super) fn owned(file: fs::File) -> Self {
-        Self {
-            inner: MaybeOwned::Owned(file),
-        }
-    }
-
-    /// Like `borrowed` but does not do path checks.
-    #[allow(dead_code)]
-    pub(super) const fn borrowed_noassert(file: &'borrow fs::File) -> Self {
-        Self {
-            inner: MaybeOwned::Borrowed(file),
-        }
-    }
-
-    /// Like `owned` but does not do path checks.
-    #[allow(dead_code)]
-    pub(super) const fn owned_noassert(file: fs::File) -> Self {
-        Self {
-            inner: MaybeOwned::Owned(file),
-        }
+        Self::Owned(file)
     }
 
     /// Set this `MaybeOwnedFile` to a new owned file which is from a subtree
     /// of the current file. Return a `MaybeOwnedFile` representing the
     /// previous state.
     pub(super) fn descend_to(&mut self, to: MaybeOwnedFile<'borrow>) -> Self {
-        Self {
-            inner: mem::replace(&mut self.inner, to.inner),
-        }
+        mem::replace(self, to)
     }
 
     /// Produce an owned `File`. This uses `open` on "." if needed to convert a
     /// borrowed `File` to an owned one.
     #[cfg_attr(windows, allow(dead_code))]
     pub(super) fn into_file(self, options: &OpenOptions) -> io::Result<fs::File> {
-        match self.inner {
-            MaybeOwned::Owned(file) => Ok(file),
-            MaybeOwned::Borrowed(file) => {
+        match self {
+            Self::Owned(file) => Ok(file),
+            Self::Borrowed(file) => {
                 // The only situation in which we'd be asked to produce an owned
                 // `File` is when there's a need to open "." within a directory
                 // to obtain a new handle.
@@ -83,7 +61,10 @@ impl<'borrow> Deref for MaybeOwnedFile<'borrow> {
 
     #[inline]
     fn deref(&self) -> &Self::Target {
-        self.inner.as_ref()
+        match self {
+            Self::Borrowed(file) => file,
+            Self::Owned(file) => file,
+        }
     }
 }
 
