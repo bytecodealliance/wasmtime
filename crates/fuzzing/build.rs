2493a2d3c28bfb38119f3ec651246e81863ff1d2
Alex Crichton <alex@alexcrichton.com>
2026-08-28 08:29:02 -0700

    Skip stack-switching wast tests when fuzzing with asan
    See more details in #14222

--- crates/fuzzing/build.rs
+++ crates/fuzzing/build.rs
@@ -41,4 +41,21 @@ fn main() {
 
     code.push_str("];\n");
     std::fs::write(out_dir.join("wasttests.rs"), code).unwrap();
+
+    // NB: duplicating a workaround in the wasmtime-fiber build script.
+    custom_cfg("asan", cfg_is("sanitize", "address"));
+}
+
+fn cfg_is(key: &str, val: &str) -> bool {
+    std::env::var(&format!("CARGO_CFG_{}", key.to_uppercase()))
+        .ok()
+        .as_deref()
+        == Some(val)
+}
+
+fn custom_cfg(key: &str, enabled: bool) {
+    println!("cargo:rustc-check-cfg=cfg({key})");
+    if enabled {
+        println!("cargo:rustc-cfg={key}");
+    }
 }
