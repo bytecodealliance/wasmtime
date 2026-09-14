2aaa76427227faa431835f55c3507134c6b684d8
Alex Crichton <alex@alexcrichton.com>
2026-09-14 07:51:35 -0700

    Disable using `memset` on tables
    While the preexisting predicate worked for `funcref` tables it didn't
    work for tables of GC reference types which require barriers under some
    collectors. After trying a few different shapes of this I've opted to
    just disable the optimization entirely since I don't think we have much
    relying on it and it seems best to start correct and optimize from
    there.
    
    I've additionally opted for a unit test rather than the `*.wast` test
    from #14325 which, while it works, takes a moment to churn through so
    much memory locally. To keep the test suite running relatively fast a
    smaller `Drop`-based test was chosen.
    
    Closes #14325

--- crates/cranelift/src/func_environ.rs
+++ crates/cranelift/src/func_environ.rs
@@ -6532,7 +6532,7 @@ impl CheckedEntity {
             CheckedEntity::Elem(_) => false,
             // Tables that are lazily initialized can't be memset because the
             // initialized bit needs to be set when storing values.
-            CheckedEntity::Table { .. } => !env.tunables.table_lazy_init,
+            CheckedEntity::Table { .. } => false,
         }
     }
 }
