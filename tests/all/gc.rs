3c310e801e8baa42529c6a3cf95bd11a3777254c
Alex Crichton <alex@alexcrichton.com>
2026-09-14 15:07:58 +0000

    Disable using `memset` on tables (#14328)
    While the preexisting predicate worked for `funcref` tables it didn't
    work for tables of GC reference types which require barriers under some
    collectors. After trying a few different shapes of this I've opted to
    just disable the optimization entirely since I don't think we have much
    relying on it and it seems best to start correct and optimize from
    there.
    
    I've additionally opted for a unit test rather than the `*.wast` test
    from #14325 which, while it works, takes a moment to churn through so
    much memory locally. To keep the test suite running relatively fast a
    smaller `Drop`-based test was chosen.
    
    Closes #14325

--- tests/all/gc.rs
+++ tests/all/gc.rs
@@ -4207,3 +4207,45 @@ fn gc_during_epoch(wat: &str) -> Result<()> {
     }
     Ok(())
 }
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn table_fill_null_barriers_ok() -> Result<()> {
+    for collector in [Collector::Copying, Collector::DeferredReferenceCounting] {
+        eprintln!("collector: {collector:?}");
+        let engine = Engine::new(Config::new().collector(collector).table_lazy_init(false))?;
+        let module = Module::new(
+            &engine,
+            r#"
+                (module
+                    (table $t (export "t") 1 (ref null extern))
+                    (func (export "fill") (param i32) (param i32)
+                        (table.fill $t (local.get 0) (ref.null extern) (local.get 1)))
+                )
+            "#,
+        )?;
+
+        let mut store = Store::new(&engine, ());
+        let instance = Instance::new(&mut store, &module, &[])?;
+        let fill = instance.get_typed_func::<(u32, u32), ()>(&mut store, "fill")?;
+        let table = instance.get_table(&mut store, "t").unwrap();
+
+        let flag = Arc::new(AtomicBool::new(false));
+
+        {
+            let mut scope = RootScope::new(&mut store);
+            let r = ExternRef::new(&mut scope, SetFlagOnDrop(flag.clone()))?;
+            table.set(&mut scope, 0, r.into())?;
+        }
+
+        assert!(!flag.load(SeqCst));
+        store.gc(None)?;
+        assert!(!flag.load(SeqCst));
+
+        fill.call(&mut store, (0, 1))?;
+        store.gc(None)?;
+        assert!(flag.load(SeqCst));
+    }
+
+    Ok(())
+}
