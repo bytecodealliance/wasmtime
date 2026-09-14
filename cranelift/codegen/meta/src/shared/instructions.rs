cddd1016767329982faefd771ab63a9db55d4318
Erik Rose <erik.rose@fastly.com>
2026-09-14 11:19:23 -0400

    Declare that `dead_load_with_context` can store.
    Omitting this was a bug. It could have made optimizations assume that nothing `dead_load_with_context` transferred control to would store.

--- cranelift/codegen/meta/src/shared/instructions.rs
+++ cranelift/codegen/meta/src/shared/instructions.rs
@@ -413,6 +413,9 @@ fn define_control_flow(
         // https://github.com/bytecodealliance/wasmtime/pull/9078#issuecomment-2273869774.
         .call()
         .can_load()
+        // It may transfer control to something that may store. Declaring this
+        // makes us a memory fence.
+        .can_store()
         // Don't optimize me out just because I don't def anything. TODO: Can we use side_effects_idempotent()?
         .other_side_effects(),
         // If `load` is not can_trap(), this isn't either.
