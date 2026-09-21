e1e29054d2657b7186e58e0d89d45368564b1069
Alex Crichton <alex@alexcrichton.com>
2026-09-21 09:02:43 -0700

    Charge fuel for more host allocations with `Val`
    In addition to the previous charges made for types like lists and maps
    this commit additionally changes for allocations done in records,
    structs, enums, etc. The goal of this commit is to ensure that
    allocations made on the host on behalf of guests are generally as
    aligned-as-possible with the fuel tracking.

--- tests/all/cli_tests.rs
+++ tests/all/cli_tests.rs
@@ -4035,6 +4035,28 @@ fn compile_time_builtins_compile_subcommand() -> Result<()> {
         "--allow-precompiled",
         cwasm.to_str().unwrap(),
     ])?;
+    Ok(())
+}
 
+#[test]
+fn hostcall_fuel() -> Result<()> {
+    for func in ["f1()", "f2()", "f3()", "f4()", "f5()", "f6()", "f7()"] {
+        run_wasmtime(&[
+            "--invoke",
+            func,
+            "tests/all/cli_tests/hostcall_fuel.wat",
+            func,
+        ])?;
+        assert!(
+            run_wasmtime(&[
+                "-Shostcall-fuel=1000",
+                "--invoke",
+                func,
+                "tests/all/cli_tests/hostcall_fuel.wat",
+                func,
+            ])
+            .is_err()
+        );
+    }
     Ok(())
 }
