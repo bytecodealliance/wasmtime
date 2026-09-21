b18b17a2fe8db5471e498954f05564abeaaae97b
Chris Fallin <chris@cfallin.org>
2026-09-21 07:23:32 -0700

    Wasmtime Cranelift backend: properly save/restore fuel around `call_ref`.
    Previously, fuel instrumentation erroneously used the cached fuel value
    in the caller, without writing it out to VM state before a funcref call
    or reloading it afterward. This would result in accumulated fuel usage
    during the callee's execution being lost, leading to a potential fuel
    amplification mechanism and thus a DoS vector.

--- crates/cranelift/src/func_environ.rs
+++ crates/cranelift/src/func_environ.rs
@@ -605,6 +605,7 @@ impl<'module_environment> FuncEnvironment<'module_environment> {
             | Operator::Return
             | Operator::CallIndirect { .. }
             | Operator::Call { .. }
+            | Operator::CallRef { .. }
             | Operator::ReturnCall { .. }
             | Operator::ReturnCallRef { .. }
             | Operator::ReturnCallIndirect { .. }
@@ -670,7 +671,7 @@ impl<'module_environment> FuncEnvironment<'module_environment> {
         // After a function call we need to reload our fuel value since the
         // function may have changed it.
         match op {
-            Operator::Call { .. } | Operator::CallIndirect { .. } => {
+            Operator::Call { .. } | Operator::CallIndirect { .. } | Operator::CallRef { .. } => {
                 self.fuel_load_into_var(builder);
             }
             _ => {}
