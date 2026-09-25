4d3c80006dffb8c61efa0441da448d95aa14a84c
Nick Fitzgerald <fitzgen@gmail.com>
2026-09-24 17:43:26 -0700

    fix ci

--- crates/environ/src/union_find.rs
+++ crates/environ/src/union_find.rs
@@ -12,7 +12,7 @@
 //!   `union`. All others are implicit singletons that this structure never
 //!   allocates for or visits.
 
-#![expect(unused, reason = "used by following commits")]
+#![cfg_attr(not(test), expect(unused, reason = "used by following commits"))]
 
 use crate::prelude::*;
 use core::fmt;
